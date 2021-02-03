ea541bd955d58e1a9f90481b6fa2647a286e54f3
Carlos Thurber <carlos.thurber@admios-sa.com>
2021-02-03 18:30:39 -0500

    Adds e2e coverage (#23)

--- cypress/integration/accessions.js
+++ cypress/integration/accessions.js
@@ -54,8 +54,8 @@ describe("Accessions", () => {
       cy.get('#family').should('have.value', "Cornaceae");
       cy.get('#numberOfTrees').should('have.value', "3");
       cy.get('#founderId').should('have.value', "234908098");
-      cy.get('#endangered > .MuiButtonBase-root > .MuiIconButton-label > .jss108').should('have.checked', "true");
-      cy.get('#rare > .MuiButtonBase-root > .MuiIconButton-label > .jss108').should('have.checked', "true");
+      cy.get('#endangered > .MuiButtonBase-root > .MuiIconButton-label > .PrivateSwitchBase-input-52').should('have.checked', "true");
+      cy.get('#rare > .MuiButtonBase-root > .MuiIconButton-label > .PrivateSwitchBase-input-52').should('have.checked', "true");
 
       cy.get('#fieldNotes').should('have.value', "Some notes");
   
