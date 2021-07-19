dd494251e9b7fca59ce10ec7cc23d61a18debe1c
Carlos Thurber <carlos.thurber@admios-sa.com>
2021-07-19 12:05:40 -0500

    fixes tests

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -13,13 +13,18 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+require('dotenv').config();
+
 /**
  * @type {Cypress.PluginConfig}
  */
- module.exports = (on, config) => {
+module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  require('@cypress/code-coverage/task')(on, config)
+  require('@cypress/code-coverage/task')(on, config);
+
+  config.env.user = '1';
+  config.env.pass = 'test1234';
 
   return config
 }
