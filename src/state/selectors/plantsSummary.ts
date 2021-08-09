c87a5d47971134fbd91429e5d7f5bc3ee7dbf743
Carlos Thurber <carlos.thurber@admios-sa.com>
2021-08-09 16:25:36 -0500

    Display the total of species and plants in the dashboard

---
+++ src/state/selectors/plantsSummary.ts
@@ -0,0 +1,30 @@
+import { atom, selectorFamily } from 'recoil';
+import { getPlantSummary } from '../../api/plants';
+import { PlantSummary } from '../../api/types/plant';
+import { plantsPlantedLayerSelector } from './layers';
+import sessionSelector from './session';
+
+export const plantsSummaryAtom = atom({
+  key: 'plantsSummaryAtom',
+  default: 0,
+});
+
+export default selectorFamily<PlantSummary[] | undefined, number>({
+  key: 'plantsSummarySelector',
+  get: (daysOffset: number) => async ({ get }) => {
+    get(plantsSummaryAtom);
+    const session = get(sessionSelector);
+    const plantLayer = get(plantsPlantedLayerSelector);
+    if (session && plantLayer?.id) {
+      const date = new Date();
+      date.setDate(date.getDate() - daysOffset);
+
+      const plantsSummary = await getPlantSummary(session, plantLayer.id, date.toISOString());
+
+      return plantsSummary;
+    }
+  },
+  set: (daysOffset: number) => ({ set }) => {
+    set(plantsSummaryAtom, (v) => v + 1);
+  },
+});
