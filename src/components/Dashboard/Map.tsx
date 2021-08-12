bb9d658039868a0762d35c9c6cc60b98fd94896b
Carlos Thurber <carlos.thurber@admios-sa.com>
2021-08-12 07:51:14 -0500

    Improves loading of ProjectsDropdown and PlantsTable (#32)
    * Improves loading of ProjectsDropdown and PlantsTable
    
    * some refactor
    
    * more refactor
    
    * displays nothing while loading the NewSpecieModal
    
    * fixes linter
    
    * Revert "fixes linter"
    
    This reverts commit df8d74a83185ffb615e9a752d5e8bab56f8a9c92.
    
    * Revert "displays nothing while loading the NewSpecieModal"
    
    This reverts commit 4b07d81436537dd18fa6b58fcede5c9f250c82fc.

--- src/components/Dashboard/Map.tsx
+++ src/components/Dashboard/Map.tsx
@@ -196,13 +196,11 @@ function Map({ onFullscreen, isFullscreen }: Props): JSX.Element {
 
   return (
     <>
-      <React.Suspense fallback={strings.LOADING}>
-        <NewSpecieModal
-          open={editPlantModalOpen}
-          onClose={onCloseEditPlantModal}
-          value={selectedPlantForTable}
-        />
-      </React.Suspense>
+      <NewSpecieModal
+        open={editPlantModalOpen}
+        onClose={onCloseEditPlantModal}
+        value={selectedPlantForTable}
+      />
       <ReactMapGL
         latitude={center.latitude}
         longitude={center.longitude}
