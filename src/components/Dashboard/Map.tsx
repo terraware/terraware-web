1419e44fc06d734f7bf435bb19737b2d7510cdb4
constanzauanini <constanza.uanini@admios-sa.com>
2021-08-13 11:53:20 -0300

    Resize dashboard map (#37)
    * Improves Dashboard layout in different sizes
    
    fixes linter
    
    * Improves Dashboard layout in different sizes
    
    * Fix map resize
    
    * Make map responsive
    
    Co-authored-by: Carlos Thurber <carlos.thurber@admios-sa.com>

--- src/components/Dashboard/Map.tsx
+++ src/components/Dashboard/Map.tsx
@@ -64,7 +64,7 @@ const navControlStyle = {
   bottom: 10,
 };
 
-const DEFAULT_VIEWPORT = { width: '100%', height: '600px', zoom: 8 };
+const DEFAULT_VIEWPORT = { zoom: 8, width: 'fit', height: '100%' };
 
 interface Coordinate {
   latitude: number;
@@ -95,25 +95,25 @@ function Map({ onFullscreen, isFullscreen }: Props): JSX.Element {
   const selectedPlantForTable =
     selectedPlant && selectedFeature
       ? {
-        date: selectedPlant.created_time,
-        species: speciesForChart[selectedPlant.species_id!]
-          ? speciesForChart[selectedPlant.species_id!].speciesName.name
-          : undefined,
-        geolocation:
-          selectedFeature.geom &&
-            Array.isArray(selectedFeature?.geom.coordinates)
-            ? `${selectedFeature.geom.coordinates[1].toFixed(
-              6
-            )}, ${selectedFeature.geom.coordinates[0].toFixed(6)}`
+          date: selectedPlant.created_time,
+          species: speciesForChart[selectedPlant.species_id!]
+            ? speciesForChart[selectedPlant.species_id!].speciesName.name
             : undefined,
-        notes: selectedFeature.notes,
-        featureId: selectedFeature.id,
-        speciesId: selectedPlant.species_id,
-      }
+          geolocation:
+            selectedFeature.geom &&
+            Array.isArray(selectedFeature?.geom.coordinates)
+              ? `${selectedFeature.geom.coordinates[1].toFixed(
+                  6
+                )}, ${selectedFeature.geom.coordinates[0].toFixed(6)}`
+              : undefined,
+          notes: selectedFeature.notes,
+          featureId: selectedFeature.id,
+          speciesId: selectedPlant.species_id,
+        }
       : undefined;
 
   React.useEffect(() => {
-    setViewport({ ...DEFAULT_VIEWPORT });
+    setViewport({ ...DEFAULT_VIEWPORT, height: '60vh' });
   }, [isFullscreen]);
 
   const onCloseEditPlantModal = (snackbarMessage?: string) => {
@@ -189,6 +189,10 @@ function Map({ onFullscreen, isFullscreen }: Props): JSX.Element {
     return { latitude: 0, longitude: 0 };
   };
 
+  const onViewportChange = (newViewport: any) => {
+    setViewport({ ...newViewport, width: 'fit' });
+  };
+
   const center = getCenter();
   const selectedCoordinates = selectedFeature
     ? getCoordinates(selectedFeature)
@@ -205,7 +209,7 @@ function Map({ onFullscreen, isFullscreen }: Props): JSX.Element {
         latitude={center.latitude}
         longitude={center.longitude}
         {...viewport}
-        onViewportChange={setViewport}
+        onViewportChange={onViewportChange}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         mapStyle='mapbox://styles/mapbox/satellite-v9'
       >
@@ -246,29 +250,55 @@ function Map({ onFullscreen, isFullscreen }: Props): JSX.Element {
           })}
         {selectedFeature && selectedPlant && (
           <Popup
-            onClose={() => { setSelectedFeature(undefined); }}
+            onClose={() => {
+              setSelectedFeature(undefined);
+            }}
             latitude={selectedCoordinates ? selectedCoordinates.latitude : 0}
             longitude={selectedCoordinates ? selectedCoordinates.longitude : 0}
             captureClick={false}
             closeOnClick={false}
           >
             <div>
-              <Typography component='p' variant='subtitle2' id='feature-species-name'>
-                {selectedPlant.species_id ? speciesForChart[selectedPlant.species_id].speciesName.name : strings.OTHER}
+              <Typography
+                component='p'
+                variant='subtitle2'
+                id='feature-species-name'
+              >
+                {selectedPlant.species_id
+                  ? speciesForChart[selectedPlant.species_id].speciesName.name
+                  : strings.OTHER}
               </Typography>
-              <Typography component='p' variant='body2' className={classes.spacing} >
-                {strings.AS_OF} {cellDateFormatter(selectedFeature.entered_time)}
+              <Typography
+                component='p'
+                variant='body2'
+                className={classes.spacing}
+              >
+                {strings.AS_OF}{' '}
+                {cellDateFormatter(selectedFeature.entered_time)}
               </Typography>
-              <Typography component='p' variant='body2' className={classes.spacing} id='feature-coordinates'>
-                {selectedCoordinates ? selectedCoordinates.latitude.toFixed(6) : 0}
+              <Typography
+                component='p'
+                variant='body2'
+                className={classes.spacing}
+                id='feature-coordinates'
+              >
+                {selectedCoordinates
+                  ? selectedCoordinates.latitude.toFixed(6)
+                  : 0}
                 ,
-                {selectedCoordinates ? selectedCoordinates.longitude.toFixed(6) : 0}
+                {selectedCoordinates
+                  ? selectedCoordinates.longitude.toFixed(6)
+                  : 0}
               </Typography>
               <PlantPhoto featureId={selectedFeature?.id} />
               <Chip
                 id='new-species'
                 size='medium'
-                label={selectedPlant.species_id ? strings.EDIT_SPECIES : strings.ADD_SPECIES}
+                label={
+                  selectedPlant.species_id
+                    ? strings.EDIT_SPECIES
+                    : strings.ADD_SPECIES
+                }
                 onClick={onNewSpecie}
                 className={classes.newSpecies}
                 icon={selectedPlant.species_id ? <CreateIcon /> : <AddIcon />}
