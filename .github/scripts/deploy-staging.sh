25db49d3097cd1e220158486eab6458c7aeeff3d
manu-castro <castromanuelorlando@gmail.com>
2021-09-23 10:09:46 -0300

    Set environment variables to deploy on different envs
    - Based on github ref name
    - Some sort of parameter indirection to clean up the process of finding the corresponding secrets

--- .github/scripts/deploy-staging.sh
+++ .github/scripts/deploy-staging.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 mkdir -p ~/.ssh
-echo "$SSH_KEY" > ~/.ssh/staging.key
-chmod 600 ~/.ssh/staging.key
+echo "$SSH_KEY" > ~/.ssh/key
+chmod 600 ~/.ssh/key
 cat >> ~/.ssh/config <<END
 Host bastion
   HostName $SSH_HOST
   User $SSH_USER
-  IdentityFile ~/.ssh/staging.key
+  IdentityFile ~/.ssh/key
   StrictHostKeyChecking no
 END
 
