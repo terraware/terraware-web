2d70430999654ebf0b5ee9a50b3c56c49280b4ce
Carlos Thurber B <carlos.thurber@admios-sa.com>
2021-09-28 13:12:04 -0500

    fixes layer api

--- src/api/plants/layers.ts
+++ src/api/plants/layers.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { LayerListResponse, LayerResponse } from '../types/layer';
 
-const BASE_URL = `${process.env.REACT_APP_TERRAWARE_API}/api/v1/layers`;
+const BASE_URL = `${process.env.REACT_APP_TERRAWARE_API}/api/v1/gis/layers`;
 
 export const getLayers = async (siteId: number): Promise<LayerResponse[]> => {
-  const endpoint = `${BASE_URL}?site_id=${siteId}`;
+  const endpoint = `${BASE_URL}/list/${siteId}`;
 
   const response: LayerListResponse = (await axios.get(endpoint)).data;
 
