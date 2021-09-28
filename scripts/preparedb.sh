b1bb8415c6b74b6852fe3696efb4dd80a4902d32
Carlos Thurber B <carlos.thurber@admios-sa.com>
2021-09-28 10:37:05 -0500

    Adds cookie to preparedb script

--- scripts/preparedb.sh
+++ scripts/preparedb.sh
@@ -21,63 +21,81 @@ case "$(uname -s)" in
      ;;
 esac
 
+COOKIE="SESSION=Mjc2NzE0YWQtYWIwYS00OGFhLThlZjgtZGI2NWVjMmU5NTBh"
+
 echo "---- E2E: adding species records ----"
-curl 'http://localhost:8008/api/v1/species' \
+curl 'http://localhost:8080/api/v1/species' \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   --data '{"name":"Banana" }'
 
-curl 'http://localhost:8008/api/v1/species' \
+curl 'http://localhost:8080/api/v1/species' \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: application/json' \
   --data '{"name":"Coconut"}'
 
-curl 'http://localhost:8008/api/v1/gis/layers/list/1`' \
+curl 'http://localhost:8080/api/v1/gis/layers' \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
-  -H 'Content-Type: application/json'
+  -H 'Content-Type: application/json' \
+  --data '{"siteId":10, "layerType": "Plants Planted", "tileSetName": "Some tile set name", "proposed": "false", "hidden": "false"}'
 
 echo "---- E2E: adding features/plants/photos records ----"
-featureId=$(curl 'http://localhost:8008/api/v1/gis/features' \
+featureId=$(curl 'http://localhost:8080/api/v1/gis/features' \
+  --cookie "'"$COOKIE"'" \
   -H 'Content-Type: application/json' \
-  --data '{"layerId":3,"notes": "Testing notes", "geom":{"type":"Point","coordinates":[-75.546518086577947,45.467134581917357]}, "enteredTime":"'"$LAST_WEEK"'"}' | jq -r '.id' )
-curl "http://localhost:8008/api/v1/gis/features/${featureId}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --data '{"layerId":1,"notes": "Testing notes", "geom":{"type":"Point","coordinates":[-75.546518086577947,45.467134581917357]}, "enteredTime":"'"$LAST_WEEK"'"}' | jq -r '.id' )
+curl "http://localhost:8080/api/v1/gis/features/${featureId}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@./scripts/banana.jpeg;type=image/jpeg'
-curl 'http://localhost:8008/api/v1/gis/plants' \
+curl 'http://localhost:8080/api/v1/gis/plants' \
+  --cookie "${COOKIE}"  \
   -H 'Content-Type: application/json' \
   --data '{"featureId": 1,"label": "Plant 1","speciesId": 1}'
 
-featureId_2=$(curl 'http://localhost:8008/api/v1/features' \
+featureId_2=$(curl 'http://localhost:8080/api/v1/features' \
+  --cookie "'"$COOKIE"'" \
   -H 'Content-Type: application/json' \
-  --data '{"layerId":3,"notes":"Testing other note","geom":{"type":"Point","coordinates": [-75.3372987731628, 45.383321536272049]}, "enteredTime":"'"$LAST_WEEK"'"}'| jq -r '.id')
-curl "http://localhost:8008/api/v1/gis/features/${featureId_2}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --data '{"layerId":1,"notes":"Testing other note","geom":{"type":"Point","coordinates": [-75.3372987731628, 45.383321536272049]}, "enteredTime":"'"$LAST_WEEK"'"}'| jq -r '.id')
+curl "http://localhost:8080/api/v1/gis/features/${featureId_2}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@./scripts/banana.jpeg;type=image/jpeg'
-curl 'http://localhost:8008/api/v1/gis/plants' \
+curl 'http://localhost:8080/api/v1/gis/plants' \
+  --cookie "${COOKIE}"  \
   -H 'Content-Type: application/json' \
   --data '{"featureId": 2,"label": "Plant 2","speciesId": 1}'
 
-featureId_3=$(curl 'http://localhost:8008/api/v1/features' \
+featureId_3=$(curl 'http://localhost:8080/api/v1/features' \
+  --cookie "'"$COOKIE"'" \
   -H 'Content-Type: application/json' \
-  --data '{"layerId":3,"geom":{"type":"Point","coordinates": [-75.898610599532319, 45.295014379864874]}, "enteredTime":"'"$LAST_WEEK"'"}'| jq -r '.id')
-curl "http://localhost:8008/api/v1/gis/features/${featureId_3}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --data '{"layerId":1,"geom":{"type":"Point","coordinates": [-75.898610599532319, 45.295014379864874]}, "enteredTime":"'"$LAST_WEEK"'"}'| jq -r '.id')
+curl "http://localhost:8080/api/v1/gis/features/${featureId_3}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@./scripts/banana.jpeg;type=image/jpeg'
-curl 'http://localhost:8008/api/v1/gis/plants' \
+curl 'http://localhost:8080/api/v1/gis/plants' \
+  --cookie "${COOKIE}"  \
   -H 'Content-Type: application/json' \
   --data '{"featureId": 3,"label": "Plant 3","speciesId": 1}'
 
-featureId_4=$(curl 'http://localhost:8008/api/v1/features' \
+featureId_4=$(curl 'http://localhost:8080/api/v1/features' \
+  --cookie "'"$COOKIE"'" \
   -H 'Content-Type: application/json' \
-  --data '{"layerId":3,"geom":{"type":"Point","coordinates": [-76.898610599532319, 45.595014379864874]}, "enteredTime":"'"$NOW"'"}'| jq -r '.id')
-curl "http://localhost:8008/api/v1/gis/features/${featureId_4}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --data '{"layerId":1,"geom":{"type":"Point","coordinates": [-76.898610599532319, 45.595014379864874]}, "enteredTime":"'"$NOW"'"}'| jq -r '.id')
+curl "http://localhost:8080/api/v1/gis/features/${featureId_4}/photos?capturedTime=2021-02-03T11%3A33%3A44Z" \
+  --cookie "${COOKIE}"  \
   -H 'accept: application/json' \
   -H 'Content-Type: multipart/form-data' \
   -F 'file=@./scripts/banana.jpeg;type=image/jpeg'
-curl 'http://localhost:8008/api/v1/gis/plants' \
+curl 'http://localhost:8080/api/v1/gis/plants' \
+  --cookie "${COOKIE}"  \
   -H 'Content-Type: application/json' \
   --data '{"featureId": 4,"label": "Plant 4","speciesId": 2}'
 
