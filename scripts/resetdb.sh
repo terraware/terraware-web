4f49ba34f33b12a4f39fada059eb3520fbcd743d
constanzauanini <constanza.uanini@admios-sa.com>
2021-09-28 12:10:45 -0300

    env sample

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -1,4 +1,4 @@
 docker-compose down
 yarn docker:start
 yarn wait-be
-docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sqlyarn server:prepare
+docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sql
