70007ed6a8c2274d26c9b8733d06c250d03c2d05
Steven Grimm <sgrimm@users.noreply.github.com>
2021-09-28 10:42:03 -0700

    SW-134 Update CI builds for new terraware-server (#55)
    * SW-134 Update CI builds for new terraware-server
    
    Terraware-server now takes a different set of config options than it used to,
    reflecting the fact that oauth2-proxy is no longer needed. This caused CI builds
    to fail because the CI workflow was trying to start a server with the old options.
    
    Update the configuration to reflect the way things work now. Specifically:
    
    * Change the environment variable names that get passed to the server. Update both
      the CI environment and the documentation in the README.
    
    * The new staging environment runs the `tree-location-app` container, not
      `seedbank-app`, so update the image name.
    
    * Forward some more request URLs to terraware-server, e.g., the Swagger UI.
      The list of forwarded URLs was getting long enough that it made more sense to
      turn it into a regular expression.
    
    * To make it easier to diagnose CI failures in the future, only wait 2 minutes for
      terraware-server to launch; previously there was no timeout other than GitHub's
      maximum runtime for actions. If it fails to launch, dump its logs to make it
      easier to diagnose the failure by just looking at the CI job's output.
    
    * Don't rely on deploy-staging.sh being executable
    
    * Increase Cypress timeouts

--- scripts/resetseedbankdb.sh
+++ scripts/resetseedbankdb.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
 docker-compose down
 yarn docker:start
-yarn wait-be
-docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sql
+if yarn wait-be; then
+    docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sql
+else
+    echo
+    echo "No response from back end"
+    echo
+
+    docker logs tree-location-web_terraware-server_1
+    exit 1
+fi
