9fe60b728659af85a52dba0c1d9aee899f966c4e
constanzauanini <constanza.uanini@admios-sa.com>
2021-09-29 17:22:10 -0300

    Update 16-species.js

--- cypress/integration/16-species.js
+++ cypress/integration/16-species.js
@@ -12,16 +12,16 @@ describe('Species', () => {
       cy.get('#name').type('Species 1');
       cy.get('#save-specie').click();
 
-      cy.get('#species-table #row5-name').should('contain', 'Species 1');
+      cy.get('#species-table #row7-name').should('contain', 'Species 1');
       cy.get('#snackbar').should('contain', 'New species added just now.');
     });
 
     it('should edit a species', () => {
-      cy.get('#species-table #row5').click();
+      cy.get('#species-table #row7').click();
       cy.get('#name').clear().type('Species 2');
       cy.get('#save-specie').click();
 
-      cy.get('#species-table #row5-name').should('contain', 'Species 2');
+      cy.get('#species-table #row7-name').should('contain', 'Species 2');
       cy.get('#snackbar').should('contain', 'Changes saved just now.');
     });
   });
