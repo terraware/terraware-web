b20ff4a8949d021d8c8c09540b0dac168188e89d
constanzauanini <constanza.uanini@admios-sa.com>
2021-09-29 14:31:34 -0300

    Update plantsFeatures.ts

--- src/state/selectors/plantsFeatures.ts
+++ src/state/selectors/plantsFeatures.ts
@@ -1,4 +1,4 @@
-import { atom, selector } from 'recoil';
+import { atom, selector, selectorFamily } from 'recoil';
 import { getFeatures } from '../../api/plants/features';
 import { Feature } from '../../api/types/feature';
 import { plantsLayerSelector } from './layers';
@@ -28,6 +28,42 @@ export const plantsFeaturesSelector = selector<Feature[] | undefined>({
     reset(plantsSummarySelector(0));
     reset(plantsSummarySelector(7));
   },
+});
+
+export type PlantsPlantedFeaturesPaginatedOptions = {
+  limit: number;
+  skip: number;
+};
+
+export const plantsPlantedFeaturesPaginatedSelector = selectorFamily<
+  Feature[] | undefined,
+  PlantsPlantedFeaturesPaginatedOptions
+>({
+  key: 'plantsPlantedFeaturesPaginatedSelector',
+  get:
+    (plantsPlantedFeaturesPaginatedOptions) =>
+    async ({ get }) => {
+      get(plantsFeatureAtom);
+      const plantsPlantedLayer = get(plantsLayerSelector);
+      if (plantsPlantedLayer?.id) {
+        return await getFeatures(
+          plantsPlantedLayer?.id,
+          plantsPlantedFeaturesPaginatedOptions.limit,
+          plantsPlantedFeaturesPaginatedOptions.skip
+        );
+      }
+    },
+  set:
+    () =>
+    ({ set, reset }) => {
+      set(plantsFeatureAtom, (v) => v + 1);
+      // We have to reset this selector to force the plantQuerySelectorFamily to be executed again
+      reset(plantsSelector);
+
+      // We have to reset these selectors to force the summarySelectoFamily to be executed again
+      reset(plantsSummarySelector(0));
+      reset(plantsSummarySelector(7));
+    },
 });
 
 export const plantsFeaturesWithGeolocationSelector = selector<
