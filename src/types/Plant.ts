a73db2e10ad439a701a1fd280c4fbc9d6299af34
Kalina Allen <kalina@terraformation.com>
2021-10-21 13:50:57 -1000

    Implemented module to fetch Plant Photos
    Implemented getPlantPhoto() which returns a URL for the first photo
    associated with a plant. If the need arises, this function could easily
    be generalized to fetch an arbitrary number of photos for any feature.
    
    Wrote unit tests for getPlantPhoto(). Did manual integration testing
    with the server.

--- src/types/Plant.ts
+++ src/types/Plant.ts
@@ -33,9 +33,15 @@ export type PlantSummaries = {
 
 export type PlantSummariesByLayerId = Map<number, PlantSummaries>;
 
+export type PlantPhoto = {
+  featureId: number;
+  imgSrc: string | null;
+};
+
 export enum PlantRequestError {
   LayerIdNotFound = 'SERVER_RETURNED_404_LAYER_ID_NOT_FOUND',
   FeatureIdNotFound = 'SERVER_RETURNED_404_FEATURE_ID_NOT_FOUND',
+  NoPhotosFound = 'NO_PHOTOS_ASSOCIATED_WITH_FEATURE_ID',
   // Server returned any other error (3xx, 4xx, 5xxx), or server did not respond, or there was an error
   // setting up the request. In other words, there was a developer error or server outage.
   RequestFailed = 'AN_UNRECOVERABLE_ERROR_OCCURRED',
