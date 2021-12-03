4786c2924349c2e4a4f473a1750c4661f7967e3b
Kalina Allen <kallen07@users.noreply.github.com>
2021-12-03 12:09:01 -1000

    Remove time Recoil selector & add error handling to time API call (#87)
    Call getDate() directly rather than through Recoil. Wrap API call
    in getDate() with a try/catch. Always return the local time from
    getDate() so that callers have a fallback if we were unable to
    fetch the server time.
    
    Move around a couple of files in src/api.

---
+++ src/api/clock.ts
@@ -0,0 +1,32 @@
+import { paths } from 'src/api/types/generated-schema';
+import axios from './index';
+
+const clockEndpoint = '/api/v1/seedbank/clock';
+type ClockGetResponse = paths[typeof clockEndpoint]['get']['responses'][200]['content']['application/json'];
+
+/*
+ * getDate() always returns a promise that resolves. If we were able to fetch the server's time, then serverTime will
+ * be non-null. Otherwise, callers can use localTime as a fallback.
+ * Both serverTime and localTime are in seconds since the epoch.
+ */
+export type GetDateResponse = {
+  serverTime: number | null;
+  localTime: number;
+};
+
+export async function getDate(): Promise<GetDateResponse> {
+  const response: GetDateResponse = {
+    serverTime: null,
+    localTime: new Date().getTime(),
+  };
+
+  try {
+    const endpoint = `${process.env.REACT_APP_TERRAWARE_API}${clockEndpoint}`;
+    const apiResponse: ClockGetResponse = (await axios.get(endpoint)).data;
+    response.serverTime = Date.parse(apiResponse.currentTime);
+  } catch {
+    console.error('getDate() unable to fetch the server time. Callers will be forced to use the fallback local time');
+  }
+
+  return response;
+}
