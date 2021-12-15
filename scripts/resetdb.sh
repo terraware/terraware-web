29378e98cdcbeed0b23abe542a77747dbd13d675
Steven Grimm <steven@terraformation.com>
2021-12-15 10:15:23 -0800

    TRACK-528 Reduce dependence on server database schema
    Replace the hand-written database setup script with a dump of the server database
    after running the previous version of the script. The dump includes metadata about
    which schema migrations have been run. When terraware-server starts up, if there
    are additional schema changes, the appropriate migrations will run.
    
    This should reduce the number of times the front-end test suite is inadvertently
    broken by updates to the back end.
    
    As part of this, `scripts/resetdb.sh` is reorganized a bit so it restores the
    database dump before starting terraware-server. That's needed because
    terraware-server only runs database migrations when it starts up.

--- scripts/resetdb.sh
+++ scripts/resetdb.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
-docker-compose down
-yes | docker volume prune
-yarn docker:start
-if yarn wait-be; then
-    docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sql
-else
+docker-compose down --volumes
+docker-compose up -d postgres
+
+# Need to wait for the PostgreSQL server to start up and for the database to be
+# initialized; it's not enough to just wait for the server to accept connections.
+attempts_remaining=30
+while [ $attempts_remaining -gt 0 ]; do
+    if docker exec -i tree-location-web_postgres_1 psql -d terraware -U postgres < dump/dump.sql; then
+        break
+    fi
+    attempts_remaining=`expr $attempts_remaining - 1`
+done
+
+if [ $attempts_remaining = 0 ]; then
     echo
-    echo "No response from back end"
+    echo "No response from PostgreSQL."
     echo
 
-    docker logs tree-location-web_terraware-server_1
+    docker logs tree-location-web_postgres_1
     exit 1
 fi
-yarn server:prepare
+
+yarn docker:start
+yarn wait-be
