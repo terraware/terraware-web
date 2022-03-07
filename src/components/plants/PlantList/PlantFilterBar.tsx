6bff66ee0c941c5e956cc71d13589da4f6e1c67a
constanzauanini <constanza.uanini@admios-sa.com>
2022-03-07 13:19:26 -0300

    TRACK-769 Fix spacing issue for plants filters

--- src/components/plants/PlantList/PlantFilterBar.tsx
+++ src/components/plants/PlantList/PlantFilterBar.tsx
@@ -49,73 +49,73 @@ export default function PlantFilterBar(props: PlantFilterBarProps): JSX.Element 
 
   return (
     <MuiPickersUtilsProvider utils={MomentUtils}>
-      <Grid item xs={1} />
-      <Grid item xs={2}>
-        <DatePicker
-          label={strings.FROM}
-          id='minEnteredTime'
-          aria-label='min_entered_time'
-          onChange={onChange}
-          value={unsavedFilters.minEnteredTime}
-        />
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <DatePicker
+            label={strings.FROM}
+            id='minEnteredTime'
+            aria-label='min_entered_time'
+            onChange={onChange}
+            value={unsavedFilters.minEnteredTime}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <DatePicker
+            id='maxEnteredTime'
+            label={strings.TO}
+            aria-label='max_entered_time'
+            onChange={onChange}
+            value={unsavedFilters?.maxEnteredTime}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Dropdown
+            id='speciesName'
+            label={strings.SPECIES}
+            onChange={onChange}
+            selected={unsavedFilters.speciesName ?? ''}
+            values={speciesNames.map((name) => ({ label: name, value: name }))}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            id='notes'
+            role='searchbox'
+            aria-label={strings.NOTES}
+            placeholder={strings.NOTES}
+            variant='outlined'
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position='start'>
+                  <SearchIcon />
+                </InputAdornment>
+              ),
+            }}
+            value={unsavedFilters.notes ?? ''}
+            size='small'
+            onChange={(event) => {
+              onChange('notes', event.target.value);
+            }}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <Button
+            id='apply-filters'
+            label={strings.APPLY_FILTERS}
+            onClick={() => onApplyFilters(unsavedFilters)}
+            type='passive'
+            priority='secondary'
+          />
+          <Button
+            id='clear-filters'
+            label={strings.CLEAR_FILTERS}
+            onClick={onClear}
+            type='passive'
+            priority='secondary'
+            className={classes.buttonSpacing}
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={2}>
-        <DatePicker
-          id='maxEnteredTime'
-          label={strings.TO}
-          aria-label='max_entered_time'
-          onChange={onChange}
-          value={unsavedFilters?.maxEnteredTime}
-        />
-      </Grid>
-      <Grid item xs={2}>
-        <Dropdown
-          id='speciesName'
-          label={strings.SPECIES}
-          onChange={onChange}
-          selected={unsavedFilters.speciesName ?? ''}
-          values={speciesNames.map((name) => ({ label: name, value: name }))}
-        />
-      </Grid>
-      <Grid item xs={2}>
-        <TextField
-          id='notes'
-          role='searchbox'
-          aria-label={strings.NOTES}
-          placeholder={strings.NOTES}
-          variant='outlined'
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position='start'>
-                <SearchIcon />
-              </InputAdornment>
-            ),
-          }}
-          value={unsavedFilters.notes ?? ''}
-          size='small'
-          onChange={(event) => {
-            onChange('notes', event.target.value);
-          }}
-        />
-      </Grid>
-      <Grid item xs={2}>
-        <Button
-          id='apply-filters'
-          label={strings.APPLY_FILTERS}
-          onClick={() => onApplyFilters(unsavedFilters)}
-          type='passive'
-          priority='secondary'
-        />
-        <Button
-          id='clear-filters'
-          label={strings.CLEAR_FILTERS}
-          onClick={onClear}
-          type='passive'
-          priority='secondary'
-          className={classes.buttonSpacing}
-        />
-      </Grid>
-      <Grid item xs={1} />
     </MuiPickersUtilsProvider>
   );
 }
