60d41a461e1f5da268910f74ec4e092e5f00fc9c
Kalina Allen <kalina@terraformation.com>
2022-03-25 16:00:15 -1000

    TRACK-837 Allow server to redirect to client with an error message
    And reorganize how errors are handled by the app in general by making
    irrecoverable error handling route to a separate component, instead
    of being done by the main <App/>.

---
+++ src/AppError.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import ErrorBox from 'src/components/common/ErrorBox/ErrorBox';
+import { APP_PATHS } from 'src/constants';
+import strings from 'src/strings';
+import { createStyles, makeStyles } from '@material-ui/core';
+import useQuery from './utils/useQuery';
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    main: {
+      marginTop: '120px',
+    },
+  })
+);
+
+export default function AppError() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.main}>
+      <Switch>
+        <Route exact path={APP_PATHS.ERROR_MOBILE_NOT_SUPPORTED}>
+          <ErrorBox title={strings.NO_MOBILE_SUPPORT_TITLE} text={strings.NO_MOBILE_SUPPORT_DESC} />
+        </Route>
+        <Route exact path={APP_PATHS.ERROR_FAILED_TO_FETCH_ORG_DATA}>
+          <ErrorBox title={strings.ORGANIZATION_DATA_NOT_AVAILABLE} text={strings.CONTACT_US_TO_RESOLVE_ISSUE} />
+        </Route>
+        <Route path={APP_PATHS.ERROR}>
+          <QueryParamsError />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function QueryParamsError() {
+  const query = useQuery();
+
+  return <ErrorBox text={query.get('message') ?? 'An unexpected error occurred.'} />;
+}
