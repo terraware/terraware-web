c66ce31a802c200e9cccbc10f5a76214c5e74d87
constanzauanini <constanza.uanini@admios-sa.com>
2022-03-31 15:11:27 -0300

    TRACK-768 Go to previous page if resizing back to the original size

--- src/AppError.tsx
+++ src/AppError.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import ErrorBox from 'src/components/common/ErrorBox/ErrorBox';
 import { APP_PATHS } from 'src/constants';
 import strings from 'src/strings';
 import { createStyles, makeStyles } from '@material-ui/core';
 import useQuery from './utils/useQuery';
+import { useHistory, useRouteMatch } from 'react-router';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -16,6 +17,28 @@ const useStyles = makeStyles(() =>
 
 export default function AppError() {
   const classes = useStyles();
+  const history = useHistory();
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [redirecting, setRedirecting] = React.useState(false);
+
+  const isErrorMobileRoute = useRouteMatch(APP_PATHS.ERROR_MOBILE_NOT_SUPPORTED + '/');
+
+  useEffect(() => {
+    /* Inside of a "useEffect" hook add an event listener that updates
+       the "width" state variable when the window size changes */
+    window.addEventListener('resize', () => setWidth(window.innerWidth));
+
+    /* passing an empty array as the dependencies of the effect will cause this
+       effect to only run when the component mounts, and not each time it updates.
+       We only want the listener to be added once */
+  }, []);
+
+  useEffect(() => {
+    if (!redirecting && width > 760 && isErrorMobileRoute) {
+      history.go(-2);
+      setRedirecting(true);
+    }
+  }, [width, history, isErrorMobileRoute, redirecting]);
 
   return (
     <div className={classes.main}>
