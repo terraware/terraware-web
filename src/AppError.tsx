96b64c1f85be6f3ac243d091b7ca78aebdb731ec
constanzauanini <constanza.uanini@admios-sa.com>
2022-04-04 10:39:54 -0300

    Clean up event listener

--- src/AppError.tsx
+++ src/AppError.tsx
@@ -24,13 +24,11 @@ export default function AppError() {
   const isErrorMobileRoute = useRouteMatch(APP_PATHS.ERROR_MOBILE_NOT_SUPPORTED + '/');
 
   useEffect(() => {
-    /* Inside of a "useEffect" hook add an event listener that updates
-       the "width" state variable when the window size changes */
-    window.addEventListener('resize', () => setWidth(window.innerWidth));
-
-    /* passing an empty array as the dependencies of the effect will cause this
-       effect to only run when the component mounts, and not each time it updates.
-       We only want the listener to be added once */
+    const resizeHandler = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', resizeHandler);
+    return () => {
+      window.removeEventListener('resize', resizeHandler);
+    };
   }, []);
 
   useEffect(() => {
