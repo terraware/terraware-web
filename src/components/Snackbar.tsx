1d66afe5b4a9abad338d60735a121eb5ebead30a
Karthik Balachandran <kb@MacBook-Pro.lan>
2022-04-07 16:21:15 -0700

    fix lint err fix vercel err (author email mismatch)

--- src/components/Snackbar.tsx
+++ src/components/Snackbar.tsx
@@ -165,7 +165,7 @@ export default function SnackbarMessage({ displayType }: Props): JSX.Element | n
       open={Boolean(snackbar.msg && snackbar.type)}
       onClose={handleClose}
       autoHideDuration={isPage ? null : 5000}
-      id={displayType == 'page' ? 'pagesnackbar' : 'snackbar'}
+      id={displayType === 'page' ? 'pagesnackbar' : 'snackbar'}
       className={classes[`mainSnackbar_${displayType}`]}
     >
       <div className={`${classes.mainContainer} ${classes[snackbar.type]} body${snackbar.priority}`}>
