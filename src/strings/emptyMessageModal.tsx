13499ebff2a080bfb9c64357e20c71d8abb43817
Kalina Allen <kalina@terraformation.com>
2022-04-13 10:13:16 -1000

    yarn format

--- src/strings/emptyMessageModal.tsx
+++ src/strings/emptyMessageModal.tsx
@@ -5,8 +5,7 @@ const strings = new LocalizedStrings({
     CHECK_BACK_LATER: 'Please check back later.',
     EMPTY_MESSAGE_CONTRIBUTOR:
       'Once your administrator has created a project and site, you’ll be able to access your data.',
-    SEEDS_EMPTY_MSG_BODY:
-      'Start by creating a project now. You’ll be able to add a site in the next step.',
+    SEEDS_EMPTY_MSG_BODY: 'Start by creating a project now. You’ll be able to add a site in the next step.',
     SEEDS_EMPTY_MSG_TITLE: 'Just a moment, let’s create a project and site first.',
     SPECIES_EMPTY_MSG_BODY:
       'Enter species that you collect or plant now to simplify your experience when you’re out in the field.',
