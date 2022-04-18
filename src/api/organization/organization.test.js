982295dd326a12e848e42babd1a8d55973b5d81c
constanzauanini <constanza.uanini@admios-sa.com>
2022-04-18 16:00:05 -0300

    Remove layers test

--- src/api/organization/organization.test.js
+++ src/api/organization/organization.test.js
@@ -1,8 +1,6 @@
 /* tslint:disable:no-console */
-import { exportedForTesting, getOrganizations } from './organization';
+import { getOrganizations } from './organization';
 import axios from 'axios';
-
-const getLayers = exportedForTesting.getLayers;
 
 jest.mock('axios');
 
@@ -41,29 +39,6 @@ const ORGANIZATIONS = [
 
 const SUCCESSFUL_GET_ORGANIZATIONS_RESPONSE = { data: { organizations: ORGANIZATIONS, status: 'ok' } };
 
-const SITES = [
-  {
-    id: 10,
-    projectId: 1,
-  },
-  {
-    id: 20,
-    projectId: 2,
-  },
-];
-
-const LAYERS = [
-  {
-    id: 100,
-    siteId: 20,
-  },
-];
-// Server response includes layer type, which is used by getPlantLayers() to select only Plants Planted layers.
-const LAYERS_FROM_SERVER = LAYERS.map((layer) => {
-  return { ...layer, layerType: 'Plants Planted' };
-});
-const SUCCESSFUL_GET_LAYERS_RESPONSE = { data: { layers: LAYERS_FROM_SERVER, status: 'ok' } };
-
 const FAILURE_RESPONSE = { request: { status: 500 } };
 
 test('getOrganizations() returns all data when no errors thrown', async () => {
@@ -87,23 +62,5 @@ test('getOrganizations() fails', async () => {
   await expect(getOrganizations()).resolves.toEqual({
     organizations: [],
     error: 'GenericError',
-  });
-});
-
-test('getLayers() returns a rejected promise if fetching layers from any site fails', async () => {
-  axios.get.mockImplementation((url) => {
-    if (url.includes('gis/layers/list/20')) {
-      return Promise.resolve(SUCCESSFUL_GET_LAYERS_RESPONSE);
-    } else if (url.includes('gis/layers/list/10')) {
-      return Promise.reject(FAILURE_RESPONSE);
-    }
-
-    console.error('Axios mock called with an unexpected url');
-    throw Error('Axios mock called with an unexpected url');
-  });
-
-  await expect(getLayers(SITES)).resolves.toEqual({
-    layers: [],
-    requestSucceeded: false,
   });
 });
