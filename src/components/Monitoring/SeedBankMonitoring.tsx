f57aac21615d5ee1926ba74ea6a9c4733494dd8a
constanzauanini <constanza.uanini@admios-sa.com>
2022-06-03 12:33:14 -0300

    Update SeedBankMonitoring.tsx

--- src/components/Monitoring/SeedBankMonitoring.tsx
+++ src/components/Monitoring/SeedBankMonitoring.tsx
@@ -9,6 +9,21 @@ import { Facility } from 'src/api/types/facilities';
 import EmptyStateContent from '../emptyStatePages/EmptyStateContent';
 import { EMPTY_STATE_CONTENT_STYLES } from '../emptyStatePages/EmptyStatePage';
 import SensorKitSetup from './SensorKitSetup';
+import Select from '../common/Select/Select';
+import { listFacilityDevices } from 'src/api/facility/facility';
+import { Chart } from 'chart.js';
+import { Device } from 'src/types/Device';
+
+declare global {
+  interface Window {
+    myChart: any;
+  }
+}
+
+type HumidityValues = {
+  timestamp: string;
+  value: string;
+};
 
 const useStyles = makeStyles((theme) =>
   createStyles({
@@ -32,6 +47,24 @@ const useStyles = makeStyles((theme) =>
       marginTop: `max(10vh, ${theme.spacing(8)}px)`,
       maxWidth: '800px',
     },
+    graphContainer: {
+      border: '1px solid #A9B7B8',
+      padding: '24px',
+    },
+    graphTitle: {
+      fontWeight: 600,
+      fontSize: '20px',
+      margin: '0 0 24px 0',
+    },
+    dropDownsContainer: {
+      display: 'flex',
+    },
+    chartContainer: {
+      marginTop: '40px',
+    },
+    chart: {
+      width: '800px',
+    },
   })
 );
 
@@ -45,6 +78,9 @@ export default function Monitoring(props: SeedBankMonitoringProps): JSX.Element 
   const { organization, seedBank } = props;
   const [onboarding, setOnboarding] = useState<boolean>(false);
   const isConfigured = seedBank.connectionState === 'Configured';
+  const [availableLocations, setAvailableLocations] = useState<Device[]>();
+  const [selectedLocation, setSelectedLocation] = useState<Device>();
+  const [selectedPeriod, setSelectedPeriod] = useState<string>();
 
   useEffect(() => {
     const isConnected = seedBank.connectionState === 'Connected';
@@ -53,6 +89,150 @@ export default function Monitoring(props: SeedBankMonitoringProps): JSX.Element 
 
   const onFinishOnboarding = () => {
     setOnboarding(false);
+  };
+
+  useEffect(() => {
+    const populateLocations = async () => {
+      if (seedBank) {
+        const locations = await listFacilityDevices(seedBank);
+        setAvailableLocations(locations.devices);
+      }
+    };
+    populateLocations();
+  }, [seedBank]);
+
+  const onChangeLocation = (newValue: string) => {
+    setSelectedLocation(availableLocations?.find((aL) => aL.name === newValue));
+  };
+
+  // const getStartTime = (period: string) => {
+  //   switch (period) {
+  //     case 'Last 12 hours':
+  //       return moment(Date.now()).subtract(12, 'h');
+  //     case 'Last 24 hours':
+  //       return moment(Date.now()).subtract(24, 'h');
+  //     case 'Last 7 days':
+  //       return moment(Date.now()).subtract(7, 'd');
+  //     case 'Last 30 days':
+  //       return moment(Date.now()).subtract(30, 'd');
+  //     default:
+  //       return moment();
+  //   }
+  // };
+
+  const onChangeSelectedPeriod = (newValue: string) => {
+    setSelectedPeriod(newValue);
+    // let startTime = getStartTime(newValue);
+    // const endTime = moment();
+
+    const response = {
+      values: [
+        {
+          deviceId: 123,
+          timeseriesName: 'Temperature',
+          values: [
+            { timestamp: '1', value: '567.89' },
+            { timestamp: '4', value: '570.91' },
+            { timestamp: '5', value: '535.13' },
+          ],
+        },
+        {
+          deviceId: 123,
+          timeseriesName: 'Humidity',
+          values: [
+            { timestamp: '2', value: '350.89' },
+            { timestamp: '3', value: '200.89' },
+            { timestamp: '4', value: '160.91' },
+            { timestamp: '6', value: '110.13' },
+          ],
+        },
+      ],
+    };
+
+    if (window.myChart instanceof Chart) {
+      window.myChart.destroy();
+    }
+    createChart(response.values[0].values, response.values[1].values);
+  };
+
+  const chartRef = React.useRef<HTMLCanvasElement>(null);
+
+  const createChart = (values1: HumidityValues[], values2: HumidityValues[]) => {
+    const ctx = chartRef?.current?.getContext('2d');
+    if (ctx) {
+      window.myChart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+          datasets: [
+            {
+              data: values1?.map((entry) => {
+                return { x: entry.timestamp, y: entry.value };
+              }),
+              label: 'Temperature',
+              showLine: true,
+              fill: false,
+              borderColor: '#FE0003',
+              backgroundColor: '#FF5A5B',
+            },
+            {
+              data: values2?.map((entry) => {
+                return { x: entry.timestamp, y: entry.value };
+              }),
+              label: 'Humidity',
+              showLine: true,
+              fill: false,
+              borderColor: '#0067C8',
+              backgroundColor: '#007DF2',
+            },
+            {
+              data: ['1', '2', '3', '4', '5', '6'].map((entry) => {
+                return { x: entry, y: '200' };
+              }),
+              label: 'Temperature Thresholds',
+              showLine: false,
+              borderColor: '#FF9797',
+              backgroundColor: '#FFC1C1',
+              fill: false,
+              pointRadius: 0,
+            },
+            {
+              data: ['1', '2', '3', '4', '5', '6'].map((entry) => {
+                return { x: entry, y: '250' };
+              }),
+              showLine: false,
+              borderColor: '#FF9797',
+              pointRadius: 0,
+              fill: {
+                target: 2,
+                above: '#FFBFD035', // Area will be red above the origin
+              },
+            },
+          ],
+        },
+        options: {
+          scales: {
+            // @ts-ignore
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+          plugins: {
+            legend: {
+              labels: {
+                filter(legendItem: { text: string | string[] }, data: any) {
+                  // only show 2nd dataset in legend
+                  return legendItem.text !== undefined;
+                },
+              },
+            },
+          },
+        },
+      });
+    }
   };
 
   return (
@@ -82,7 +262,49 @@ export default function Monitoring(props: SeedBankMonitoringProps): JSX.Element 
       {isConfigured && !onboarding && (
         <div className={classes.placeholder}>
           <span className={classes.text}>
-            Monitoring dashboard and charts for selected seedbank in {organization.name}
+            {seedBank?.connectionState === 'Configured' ? (
+              <div className={classes.graphContainer}>
+                <p className={classes.graphTitle}>{strings.TEMPERATURE_AND_HUMIDITY_SENSOR_DATA}</p>
+                <div className={classes.dropDownsContainer}>
+                  <Select
+                    options={availableLocations?.map((aL) => aL.name)}
+                    selectedValue={selectedLocation?.name}
+                    onChange={onChangeLocation}
+                    label={strings.LOCATION}
+                  />
+                  <Select
+                    options={['Last 12 hours', 'Last 24 hours', 'Last 7 days', 'Last 30 days']}
+                    onChange={onChangeSelectedPeriod}
+                    selectedValue={selectedPeriod}
+                    label={strings.TIME_PERIOD}
+                  />
+                </div>
+                <div className={classes.chartContainer}>
+                  <canvas id='myChart' ref={chartRef} className={classes.chart} />
+                </div>
+              </div>
+            ) : (
+              <>
+                {isAdmin(organization) ? (
+                  <div className={classes.notSetUpContent}>
+                    <EmptyStateContent
+                      title={strings.SET_UP_YOUR_SENSOR_KIT}
+                      subtitle={strings.SET_UP_YOUR_SENSOR_KIT_MSG}
+                      listItems={[{ icon: 'monitoring', title: strings.SENSOR_KIT_SET_UP }]}
+                      buttonText={strings.START_SET_UP}
+                      onClickButton={() => true}
+                      styles={EMPTY_STATE_CONTENT_STYLES}
+                    />
+                  </div>
+                ) : (
+                  <EmptyMessage
+                    className={classes.message}
+                    title={emptyMessageStrings.NO_SEEDBANKS_NON_ADMIN_TITLE}
+                    text={emptyMessageStrings.NO_SEEDBANKS_SET_UP_NON_ADMIN_MSG}
+                  />
+                )}
+              </>
+            )}
           </span>
         </div>
       )}
