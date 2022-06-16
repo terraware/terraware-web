6ef910d2b48d23d5eaeaf9baaf3cb2cd231eb268
constanzauanini <constanza.uanini@admios-sa.com>
2022-06-16 12:29:26 -0300

    Show not data yet when no pv baterry level available

--- src/components/Monitoring/dashboard/SeedBankDashboard.tsx
+++ src/components/Monitoring/dashboard/SeedBankDashboard.tsx
@@ -128,7 +128,7 @@ export default function SeedBankDashboard(props: SeedBankDashboardProps): JSX.El
             <p>{strings.PV_BATTERY_CHARGE}</p>
             <Icon name='chargingBattery' />
           </div>
-          <p className={classes.panelValue}>{batteryLevel}</p>
+          <p className={classes.panelValue}>{batteryLevel || strings.NO_DATA_YET}</p>
         </div>
       </Grid>
       <Grid item xs={6}>
