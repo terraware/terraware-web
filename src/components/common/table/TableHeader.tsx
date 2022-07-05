6ae26c21036836393619c5b93f00e786be40eafa
constanzauanini <constanza.uanini@admios-sa.com>
2022-07-05 17:45:27 -0300

    Fix draggable columns

--- src/components/common/table/TableHeader.tsx
+++ src/components/common/table/TableHeader.tsx
@@ -1,26 +1,11 @@
-import { DragIndicator } from '@mui/icons-material';
-import { Checkbox, TableCell, TableHead, TableRow, TableSortLabel, Theme } from '@mui/material';
+import { Checkbox, TableCell, TableRow } from '@mui/material';
 import React from 'react';
-import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc';
 import { Order } from './sort';
 import { TableColumnType } from './types';
-import { makeStyles } from '@mui/styles';
-
-const dragIconStyles = makeStyles((theme: Theme) => ({
-  root: {
-    marginLeft: -20,
-    color: theme.palette.common.white,
-    '&:hover': {
-      color: theme.palette.neutral[600],
-    },
-  },
-}));
-
-const useStyles = makeStyles((theme: Theme) => ({
-  headcell: {
-    background: theme.palette.common.white,
-  },
-}));
+import { SortableContext } from '@dnd-kit/sortable';
+import TableHeaderItem from './TableHeaderItem';
+import { DndContext, KeyboardSensor, MouseSensor, TouchSensor, useSensor, useSensors } from '@dnd-kit/core';
+import { sortableKeyboardCoordinates } from '@dnd-kit/sortable';
 
 interface Props {
   onRequestSort: (event: React.MouseEvent<unknown>, property: string) => void;
@@ -48,19 +33,34 @@ function columnsToHeadCells(columns: TableColumnType[]): HeadCell[] {
 }
 
 export default function EnhancedTableHead(props: Props): JSX.Element {
-  const classes = useStyles();
   const { order, orderBy, onRequestSort, numSelected, rowCount, onSelectAllClick } = props;
-  const createSortHandler = (property: string) => (event: React.MouseEvent<unknown>) => {
-    onRequestSort(event, property);
-  };
 
   const [headCells, setHeadCells] = React.useState<HeadCell[]>(columnsToHeadCells(props.columns));
   React.useEffect(() => {
     setHeadCells(columnsToHeadCells(props.columns));
   }, [props.columns]);
 
+  const sensors = useSensors(
+    useSensor(MouseSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  function handleDragEnd(event: { active: any; over: any }) {
+    const { active, over } = event;
+    console.log(event);
+    if (active && over && active.id !== over.id && props.onReorderEnd) {
+      const oldIndex = headCells.findIndex((item) => item.id === active.id);
+      const newIndex = headCells.findIndex((item) => item.id === over.id);
+
+      props.onReorderEnd({ oldIndex, newIndex });
+    }
+  }
+
   return (
-    <SortableHead lockAxis='x' axis='x' onSortEnd={props.onReorderEnd} useDragHandle>
+    <DndContext sensors={sensors} onDragEnd={handleDragEnd}>
       <TableRow id='table-header'>
         {numSelected !== undefined && rowCount !== undefined && rowCount > 0 && onSelectAllClick && (
           <TableCell padding='checkbox'>
@@ -72,45 +72,20 @@ export default function EnhancedTableHead(props: Props): JSX.Element {
             />
           </TableCell>
         )}
-        {headCells.map((headCell, i) => (
-          <SortableCell
-            disabled={!props.onReorderEnd}
-            index={i}
-            key={headCell.id}
-            value={
-              <TableCell
-                id={`table-header-${headCell.id}`}
-                key={headCell.id}
-                align='left'
-                padding={headCell.disablePadding ? 'none' : 'normal'}
-                sortDirection={orderBy === headCell.id ? order : false}
-                className={classes.headcell}
-              >
-                {headCell.label && (
-                  <TableSortLabel
-                    active={orderBy === headCell.id}
-                    direction={orderBy === headCell.id ? order : 'asc'}
-                    onClick={createSortHandler(headCell.id)}
-                  >
-                    {i > 0 && <DragHandle />}
-                    {headCell.label}
-                  </TableSortLabel>
-                )}
-              </TableCell>
-            }
-          />
-        ))}
+        <SortableContext items={headCells}>
+          {headCells.map((headCell, i) => {
+            return (
+              <TableHeaderItem
+                headCell={headCell}
+                order={order}
+                orderBy={orderBy}
+                onRequestSort={onRequestSort}
+                i={i}
+              />
+            );
+          })}
+        </SortableContext>
       </TableRow>
-    </SortableHead>
+    </DndContext>
   );
 }
-
-const DragHandle = SortableHandle(() => <DragIndicator fontSize='small' classes={dragIconStyles()} />);
-
-const SortableHead = SortableContainer(({ children }: { children: React.ReactNode }) => {
-  return <TableHead>{children}</TableHead>;
-});
-
-const SortableCell = SortableElement(({ value }: { value: React.ReactNode }) => {
-  return <>{value}</>;
-});
