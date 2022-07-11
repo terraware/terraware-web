c1ca1df45829823f76f0eb1f91ebfb4a3d2fdbcc
constanzauanini <constanza.uanini@admios-sa.com>
2022-07-11 15:10:51 -0300

    Remove sites and projects strings

--- src/strings/emptyMessageModal.tsx
+++ src/strings/emptyMessageModal.tsx
@@ -3,10 +3,6 @@ import LocalizedStrings from 'react-localization';
 const strings = new LocalizedStrings({
   en: {
     CHECK_BACK_LATER: 'Please check back later.',
-    EMPTY_MESSAGE_CONTRIBUTOR:
-      'Once your administrator has created a project and site, you’ll be able to access your data.',
-    SEEDS_EMPTY_MSG_BODY: 'Start by creating a project now. You’ll be able to add a site in the next step.',
-    SEEDS_EMPTY_MSG_TITLE: 'Just a moment, let’s create a project and site first.',
     SPECIES_EMPTY_MSG_BODY:
       'Enter species that you collect or plant now to simplify your experience when you’re out in the field.',
     NO_SEEDBANKS_ADMIN_TITLE: 'Just a moment, let’s add a seed bank first.',
