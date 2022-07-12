b738a81357acb6f697776ccc5cfc212661768f35
constanzauanini <constanza.uanini@admios-sa.com>
2022-07-12 12:43:15 -0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -130,7 +130,6 @@ export default function Home({ organizations, selectedOrganization, setSelectedO
               linkStyle={'underline'}
             />
           </Grid>
-          <></>
         </Grid>
       </Container>
     </main>
