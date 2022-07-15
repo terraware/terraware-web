aa9eb9c021e2c6135e431018c71072652a890286
constanzauanini <constanza.uanini@admios-sa.com>
2022-07-15 11:55:56 -0300

    Update DatePicker.tsx

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -29,7 +29,7 @@ export default function DatePicker(props: Props): JSX.Element {
   return (
     <DesktopDatePicker
       label={props.label}
-      inputFormat='yyyy-mm-dd'
+      inputFormat='yyyy-MM-dd'
       value={props.value ?? null}
       onChange={(newValue: string | null) => {
         props.onChange(props.id, newValue);
