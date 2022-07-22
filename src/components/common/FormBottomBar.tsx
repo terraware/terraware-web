16accaaafd78a5f47c9b25ff9cccf2263e03e9d3
Karthik B <karthik@terraformation.com>
2022-07-22 15:18:17 -0700

    SW-1242 MyAccount mobile design (#392)
    * SW-1242 MyAccount mobile design
    
    * fix zindex on form bottom bar
    
    * fix styling

--- src/components/common/FormBottomBar.tsx
+++ src/components/common/FormBottomBar.tsx
@@ -1,9 +1,10 @@
-import { AppBar } from '@mui/material';
+import { AppBar, Theme } from '@mui/material';
 import React from 'react';
 import Button from './button/Button';
 import { makeStyles } from '@mui/styles';
+import useDeviceInfo from 'src/utils/useDeviceInfo';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   bottomBar: {
     filter: 'drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.2))',
     background: '#ffffff',
@@ -12,7 +13,17 @@ const useStyles = makeStyles(() => ({
     justifyContent: 'space-between',
     display: 'flex',
     padding: '16px 24px',
-    width: 'calc(100% - 200px)',
+    '&.desktop': {
+      width: 'calc(100% - 200px)',
+    },
+    '&.mobile': {
+      flexDirection: 'column-reverse',
+      paddingBottom: theme.spacing(5),
+    },
+    zIndex: 1000,
+  },
+  buttonMobile: {
+    marginTop: theme.spacing(1),
   },
 }));
 
@@ -23,16 +34,24 @@ export interface Props {
 
 export default function FormBottomBar({ onCancel, onSave }: Props): JSX.Element {
   const classes = useStyles();
+  const { isMobile, isDesktop } = useDeviceInfo();
 
   return (
     <AppBar
       position='fixed'
       color='primary'
       style={{ top: 'auto', bottom: 0, right: 'auto' }}
-      className={classes.bottomBar}
+      className={`${classes.bottomBar} ${isDesktop ? 'desktop' : ''} ${isMobile ? 'mobile' : ''}`}
     >
-      <Button size='medium' label='Cancel' onClick={onCancel} priority='secondary' type='passive' />
-      <Button size='medium' label='Save' onClick={onSave} />
+      <Button
+        size='medium'
+        label='Cancel'
+        onClick={onCancel}
+        priority='secondary'
+        type='passive'
+        className={isMobile ? classes.buttonMobile : ''}
+      />
+      <Button size='medium' label='Save' onClick={onSave} className={isMobile ? classes.buttonMobile : ''} />
     </AppBar>
   );
 }
