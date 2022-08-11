2442d1baa4bd934d18535e5546b3d1f349b12876
Karthik Balachandran <karthik@terraformation.com>
2022-08-11 13:18:23 -0700

    fix strings

--- src/strings/contactUsPage.tsx
+++ src/strings/contactUsPage.tsx
@@ -2,8 +2,7 @@ import LocalizedStrings from 'react-localization';
 
 const strings = new LocalizedStrings({
   en: {
-    DESCRIPTION_REPORT_PROBLEM:
-      'If you’re having trouble using Terraware, please report any issues you’re having and we’ll make it right. Let us know this build version "${process.env.REACT_APP_TERRAWARE_FE_BUILD_VERSION}".',
+    DESCRIPTION_REPORT_PROBLEM: `If you’re having trouble using Terraware, please report any issues you’re having and we’ll make it right. Let us know this build version "${process.env.REACT_APP_TERRAWARE_FE_BUILD_VERSION}".`,
     DESCRIPTION_REQUEST_FEATURE:
       'We’re always working to improve your Terraware experience and welcome your feedback on our products.',
     DESCRIPTION_TEST_APP: 'Be the first to try our new, field-tested mobile app and let us know what you think.',
