b0800f570eaae6a6bafe8f73fae183bbbccd8318
constanzauanini <constanza.uanini@admios-sa.com>
2022-08-17 13:20:50 -0300

    Use url params to navigate to accessions list

--- src/components/seeds/summary/AccessionByStatus.tsx
+++ src/components/seeds/summary/AccessionByStatus.tsx
@@ -1,6 +1,5 @@
 import { Box, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
-import { FieldNodePayload, SeedSearchCriteria } from 'src/api/seeds/search';
 import { AccessionState } from 'src/api/types/accessions';
 import { APP_PATHS } from 'src/constants';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
@@ -9,23 +8,10 @@ interface Props {
   label: string;
   status: AccessionState;
   quantity: number | undefined;
-  setSeedSearchCriteria: (criteria: SeedSearchCriteria) => void;
 }
 
-export default function AccessionByStatus({ label, status, quantity, setSeedSearchCriteria }: Props): JSX.Element {
+export default function AccessionByStatus({ label, status, quantity }: Props): JSX.Element {
   const { isMobile } = useDeviceInfo();
-
-  const onClick = (state: AccessionState) => {
-    const filter: FieldNodePayload = {
-      field: 'state',
-      values: [state],
-      type: 'Exact',
-      operation: 'field',
-    };
-
-    setSeedSearchCriteria({ state: filter });
-  };
-
   return (
     <Box
       borderRadius='10px'
@@ -35,12 +21,7 @@ export default function AccessionByStatus({ label, status, quantity, setSeedSear
       width={isMobile ? '100%' : '20%'}
       padding='22px 10px 40px 10px'
     >
-      <Link
-        onClick={() => onClick(status)}
-        id={`update-${status}`}
-        to={APP_PATHS.ACCESSIONS}
-        style={{ textDecoration: 'none' }}
-      >
+      <Link id={`update-${status}`} to={`${APP_PATHS.ACCESSIONS}?stage=${status}`} style={{ textDecoration: 'none' }}>
         <Typography color='#0067C8' fontSize='36px'>
           {quantity}
         </Typography>
