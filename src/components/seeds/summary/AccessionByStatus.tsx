9c82116729837162c73c22b5f743c00aec1852f0
constanzauanini <constanza.uanini@admios-sa.com>
2022-08-24 11:49:16 -0300

    Make entire box clickable on Dashboard (#456)

--- src/components/seeds/summary/AccessionByStatus.tsx
+++ src/components/seeds/summary/AccessionByStatus.tsx
@@ -13,22 +13,26 @@ interface Props {
 export default function AccessionByStatus({ label, status, quantity }: Props): JSX.Element {
   const { isMobile } = useDeviceInfo();
   return (
-    <Box
-      borderRadius='10px'
-      border='1px solid #E5E5E5'
-      margin='12px'
-      textAlign='center'
-      width={isMobile ? '100%' : '20%'}
-      padding='22px 10px 40px 10px'
+    <Link
+      id={`update-${status}`}
+      to={`${APP_PATHS.ACCESSIONS}?stage=${status}`}
+      style={{
+        textDecoration: 'none',
+        padding: '22px 10px 40px 10px',
+        width: isMobile ? '100%' : '20%',
+        borderRadius: '10px',
+        border: '1px solid #E5E5E5',
+        margin: '12px',
+      }}
     >
-      <Link id={`update-${status}`} to={`${APP_PATHS.ACCESSIONS}?stage=${status}`} style={{ textDecoration: 'none' }}>
+      <Box textAlign='center'>
         <Typography color='#0067C8' fontSize='36px'>
           {quantity}
         </Typography>
-      </Link>
-      <Typography color='#000000' fontSize='16px'>
-        {label}
-      </Typography>
-    </Box>
+        <Typography color='#000000' fontSize='16px'>
+          {label}
+        </Typography>
+      </Box>
+    </Link>
   );
 }
