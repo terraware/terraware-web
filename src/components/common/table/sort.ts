ff91b64997b317b29440a81a45222eb5711de144
constanzauanini <constanza.uanini@admios-sa.com>
2022-08-24 14:13:28 -0300

    Import order from web-components

--- src/components/common/table/sort.ts
+++ src/components/common/table/sort.ts
@@ -1,4 +1,4 @@
 import { descendingComparator, getComparator, stableSort } from '@terraware/web-components';
+import { Order } from '@terraware/web-components/components/table/sort';
 export { descendingComparator, getComparator, stableSort };
-
-export type Order = 'asc' | 'desc';
+export type { Order };
