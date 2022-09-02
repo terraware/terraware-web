f2acd373ed0fdab8fa5b67292ba2aa79a6557ede
constanzauanini <constanza.uanini@admios-sa.com>
2022-09-02 12:10:14 -0300

    Improvement

--- src/ErrorContent.tsx
+++ src/ErrorContent.tsx
@@ -15,7 +15,7 @@ const useStyles = makeStyles(() => ({
     paddingTop: '120px',
     height: (props: StyleProps) => (props.inApp ? '100%' : 'calc(100% - 120px)'),
     background:
-      'url(/assets/error/wind.png) no-repeat 0% 100%/auto 300px, url(/assets/error/moon.png) no-repeat 900px 52%/auto 190px, url(/assets/error/ufo.png) no-repeat 0 100%/auto 600px, url(/assets/error/land.png) repeat-x 0 100%/auto 142px, url(/assets/error/mountains.png) no-repeat 0 100%/auto 392px, url(/assets/error/stars.png) no-repeat 0 100%/auto 100%, url(/assets/error/background.png) no-repeat 100% 0/100% 100%, linear-gradient(to bottom right, rgb(255, 255, 255) 0%, rgb(199, 226, 234) 100%) no-repeat 0 0/auto',
+      'url(/assets/error/wind.png) no-repeat 0% 100%/auto 300px, url(/assets/error/ufo.png) no-repeat 0 100%/auto 600px, url(/assets/error/land1.png) no-repeat bottom left/898px auto, url(/assets/error/land2.png) no-repeat bottom right/898px auto, url(/assets/error/mountains.png) no-repeat 0 100%/100% 400px, url(/assets/error/moon.png) no-repeat 60% 350px/auto 190px, url(/assets/error/stars.png) no-repeat 0 100%/auto 100%, url(/assets/error/background.png) no-repeat 100% 0/100% 100%, linear-gradient(to bottom right, rgb(255, 255, 255) 0%, rgb(199, 226, 234) 100%) no-repeat 0 0/auto',
   },
   buttonLeft: {
     marginRight: '16px',
