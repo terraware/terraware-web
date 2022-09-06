d3aca2f0555a155e2797910138ae79819a469642
constanzauanini <constanza.uanini@admios-sa.com>
2022-09-06 17:24:33 -0300

    Add view photos modal (#474)

--- src/components/accession2/DetailPanel.tsx
+++ src/components/accession2/DetailPanel.tsx
@@ -1,11 +1,13 @@
 import { Box, Grid, Typography, useTheme } from '@mui/material';
 import { Button } from '@terraware/web-components';
 import moment from 'moment';
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Accession2 } from 'src/api/accessions2/accession';
 import { APP_PATHS } from 'src/constants';
 import strings from 'src/strings';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
+import ViewPhotosModal from './ViewPhotosModal';
 
 type DetailPanelProps = {
   accession?: Accession2;
@@ -34,6 +36,9 @@ export default function DetailPanel(props: DetailPanelProps): JSX.Element {
 
   const age = seedCollectionDate ? today.diff(seedCollectionDate, 'months') : undefined;
 
+  const [photosModalOpened, setPhotosModalOpened] = useState(false);
+  const [selectedSlide, setSelectedSlide] = useState(0);
+
   const displayAge = () => {
     if (age === undefined) {
       return '';
@@ -51,115 +56,129 @@ export default function DetailPanel(props: DetailPanelProps): JSX.Element {
   };
 
   return accession ? (
-    <Grid container>
-      {isMobile ? (
-        <Grid item xs={12}>
-          <Button label={strings.EDIT} onClick={goToEdit} />
-        </Grid>
-      ) : null}
-      <Grid item xs={mainStructureSize}>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.QUANTITY}
+    <>
+      <ViewPhotosModal
+        accessionId={accession.id.toString()}
+        photosNames={accession.photoFilenames || []}
+        open={photosModalOpened}
+        onClose={() => setPhotosModalOpened(false)}
+        selectedSlide={selectedSlide}
+      />
+      <Grid container>
+        {isMobile ? (
+          <Grid item xs={12}>
+            <Button label={strings.EDIT} onClick={goToEdit} />
           </Grid>
-          <Grid item xs={gridRightSide}>
-            {accession.remainingQuantity?.quantity} {accession.remainingQuantity?.units}
-            {accession.remainingQuantity?.grams ? `(${accession.remainingQuantity?.grams} ${strings.GRAMS})` : ''}
+        ) : null}
+        <Grid item xs={mainStructureSize}>
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.QUANTITY}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {accession.remainingQuantity?.quantity} {accession.remainingQuantity?.units}
+              {accession.remainingQuantity?.grams ? `(${accession.remainingQuantity?.grams} ${strings.GRAMS})` : ''}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.VIABILITY}
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.VIABILITY}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {accession.latestViabilityPercent}
+            </Grid>
           </Grid>
-          <Grid item xs={gridRightSide}>
-            {accession.latestViabilityPercent}
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.AGE}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {displayAge()}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.AGE}
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.COLLECTION_DATE}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {accession.collectedDate}
+            </Grid>
           </Grid>
-          <Grid item xs={gridRightSide}>
-            {displayAge()}
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.COLLECTOR}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {accession.collectors && accession.collectors.length > 0 ? accession.collectors[0] : ''}
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.COLLECTION_DATE}
-          </Grid>
-          <Grid item xs={gridRightSide}>
-            {accession.collectedDate}
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.COLLECTOR}
-          </Grid>
-          <Grid item xs={gridRightSide}>
-            {accession.collectors && accession.collectors.length > 0 ? accession.collectors[0] : ''}
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.COLLECTING_LOCATION}
-          </Grid>
-          <Grid item xs={gridRightSide}>
-            {accession.collectionSiteName}
-            {accession.collectionSiteLandowner ? (
-              <Typography>
-                ({strings.OWNER}: {accession.collectionSiteLandowner})
-              </Typography>
-            ) : (
-              ''
-            )}
-            {accession.collectionSiteCity &&
-            accession.collectionSiteCountrySubdivision &&
-            accession.collectionSiteCountryCode
-              ? `${accession.collectionSiteCity}, ${accession.collectionSiteCountrySubdivision},
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.COLLECTING_LOCATION}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {accession.collectionSiteName}
+              {accession.collectionSiteLandowner ? (
+                <Typography>
+                  ({strings.OWNER}: {accession.collectionSiteLandowner})
+                </Typography>
+              ) : (
+                ''
+              )}
+              {accession.collectionSiteCity &&
+              accession.collectionSiteCountrySubdivision &&
+              accession.collectionSiteCountryCode
+                ? `${accession.collectionSiteCity}, ${accession.collectionSiteCountrySubdivision},
               ${accession.collectionSiteCountryCode}`
-              : ''}
-            {accession.collectionSiteCoordinates && accession.collectionSiteCoordinates.length > 0
-              ? accession.collectionSiteCoordinates?.map(
-                  (coordinate) => `${coordinate.longitude},  ${coordinate.latitude}`
-                )
-              : ''}
+                : ''}
+              {accession.collectionSiteCoordinates && accession.collectionSiteCoordinates.length > 0
+                ? accession.collectionSiteCoordinates?.map(
+                    (coordinate) => `${coordinate.longitude},  ${coordinate.latitude}`
+                  )
+                : ''}
+            </Grid>
+          </Grid>
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.PLANT_AND_SITE}
+            </Grid>
+            <Grid item xs={gridRightSide}>
+              {`${strings.COLLECTED_FROM} ${accession.plantsCollectedFromMax}-${accession.plantsCollectedFromMin} ${strings.WILD_PLANT}`}
+              {accession.founderId ? <Typography>{`${strings.PLANT_ID}: ${accession.founderId}`}</Typography> : ''}
+              {accession.notes}
+            </Grid>
+          </Grid>
+          <Grid item xs={12} sx={gridRowStyle}>
+            <Grid item xs={gridLeftSide} sx={categoryStyle}>
+              {strings.PHOTOS}
+            </Grid>
+            <Grid item xs={gridRightSide} display='flex'>
+              {accession.photoFilenames?.map((file, index) => {
+                return (
+                  <Box paddingRight={theme.spacing(2)} key={`photo-${index}`}>
+                    <img
+                      src={`/api/v1/seedbank/accessions/${accession.id}/photos/${file}?maxHeight=100`}
+                      alt=''
+                      onClick={() => {
+                        setSelectedSlide(index);
+                        setPhotosModalOpened(true);
+                      }}
+                    />
+                  </Box>
+                );
+              })}
+            </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.PLANT_AND_SITE}
-          </Grid>
-          <Grid item xs={gridRightSide}>
-            {`${strings.COLLECTED_FROM} ${accession.plantsCollectedFromMax}-${accession.plantsCollectedFromMin} ${strings.WILD_PLANT}`}
-            {accession.founderId ? <Typography>{`${strings.PLANT_ID}: ${accession.founderId}`}</Typography> : ''}
-            {accession.notes}
-          </Grid>
-        </Grid>
-        <Grid item xs={12} sx={gridRowStyle}>
-          <Grid item xs={gridLeftSide} sx={categoryStyle}>
-            {strings.PHOTOS}
-          </Grid>
-          <Grid item xs={gridRightSide} display='flex'>
-            {accession.photoFilenames?.map((file, index) => {
-              return (
-                <Box paddingRight={theme.spacing(2)} key={`photo-${index}`}>
-                  <a href={`/api/v1/seedbank/accessions/${accession.id}/photos/${file}`} target='blank'>
-                    <img src={`/api/v1/seedbank/accessions/${accession.id}/photos/${file}?maxHeight=100`} alt='' />
-                  </a>
-                </Box>
-              );
-            })}
-          </Grid>
-        </Grid>
-      </Grid>
 
-      <Grid />
-      {!isMobile ? (
-        <Grid item xs={3}>
-          <Button label={strings.EDIT} onClick={goToEdit} />
-        </Grid>
-      ) : null}
-    </Grid>
+        <Grid />
+        {!isMobile ? (
+          <Grid item xs={3}>
+            <Button label={strings.EDIT} onClick={goToEdit} />
+          </Grid>
+        ) : null}
+      </Grid>
+    </>
   ) : (
     <Box />
   );
---
+++ src/components/accession2/ViewPhotosModal.tsx
@@ -0,0 +1,111 @@
+import React, { useEffect, useRef, useState } from 'react';
+import strings from 'src/strings';
+import Button from '../common/button/Button';
+import DialogBox from '../common/DialogBox/DialogBox';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { Box } from '@mui/material';
+
+export interface ViewPhotosDialogProps {
+  open: boolean;
+  onClose: () => void;
+  photosNames: string[];
+  accessionId: string;
+  selectedSlide: number;
+}
+
+export default function ViewPhotosDialog(props: ViewPhotosDialogProps): JSX.Element {
+  const { onClose, open, photosNames, accessionId, selectedSlide } = props;
+  const [isPreviousDisabled, setIsPreviousDisabled] = useState(false);
+  const [isNextDisabled, setIsNextDisabled] = useState(false);
+
+  const myCarousel = useRef<Carousel>(null);
+  const responsive = {
+    mobile: {
+      breakpoint: { max: 4000, min: 0 },
+      items: 1,
+    },
+  };
+
+  const disableButtons = () => {
+    if (myCarousel.current) {
+      console.log('entraaaa');
+      console.log(myCarousel.current.state.currentSlide);
+      if (myCarousel.current.state.currentSlide + 1 >= photosNames.length) {
+        setIsNextDisabled(true);
+      } else {
+        setIsNextDisabled(false);
+      }
+      if (myCarousel.current.state.currentSlide - 1 < 0) {
+        setIsPreviousDisabled(true);
+      } else {
+        setIsPreviousDisabled(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (myCarousel.current) {
+      myCarousel.current.goToSlide(selectedSlide);
+    }
+  }, [selectedSlide, open]);
+
+  return (
+    <DialogBox
+      onClose={onClose}
+      open={open}
+      title={strings.PHOTOS}
+      size='large'
+      middleButtons={[
+        <Button
+          label={strings.PREVIOUS_ARROW}
+          priority='secondary'
+          onClick={() => {
+            if (myCarousel.current) {
+              const prevSlide = myCarousel.current.state.currentSlide - 1;
+              myCarousel.current.previous(prevSlide);
+            }
+          }}
+          key='button-1'
+          disabled={isPreviousDisabled}
+        />,
+        <Button
+          label={strings.NEXT_ARROW}
+          onClick={() => {
+            if (myCarousel.current) {
+              const nextSlide = myCarousel.current.state.currentSlide + 1;
+              myCarousel.current.next(nextSlide);
+            }
+          }}
+          key='button-2'
+          disabled={isNextDisabled}
+        />,
+      ]}
+    >
+      <Box
+        sx={{
+          '& .react-multi-carousel-list': {
+            paddingBottom: '20px',
+          },
+        }}
+      >
+        <Carousel
+          responsive={responsive}
+          ref={myCarousel}
+          showDots={true}
+          arrows={false}
+          ssr={true}
+          afterChange={disableButtons}
+        >
+          {photosNames.map((photoName, i) => (
+            <div key={`photo-${i}-container`}>
+              <a href={`/api/v1/seedbank/accessions/${accessionId}/photos/${photoName}`} target='blank'>
+                <img src={`/api/v1/seedbank/accessions/${accessionId}/photos/${photoName}?maxHeight=500`} alt='' />
+              </a>
+            </div>
+          ))}
+        </Carousel>
+      </Box>
+    </DialogBox>
+  );
+}
