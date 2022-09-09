6a1ddae408f3e7f964a603f6b8d916ac86f0128d
Karthik Balachandran <karthik@terraformation.com>
2022-09-09 11:10:41 -0700

    Show v1 states in test, v2 needs enabled feature

--- cypress/integration/10-database.js
+++ cypress/integration/10-database.js
@@ -544,7 +544,7 @@ describe('Database', () => {
       // checking state list
       cy.get('#filter-state').click();
       cy.get('#filter-list-state').should('be.visible');
-      cy.get('#filter-list-state').children().should('have.length', 12);
+      cy.get('#filter-list-state').children().should('have.length', 9);
       cy.get('#filter-list-state .Mui-disabled .MuiCheckbox-root').should('have.length', 5);
       cy.get('#filter-list-state').type('{esc}');
       cy.get('#filter-list-state').should('not.exist');
