c952632ef481cd31de04e8f210900527d61c5d09
constanzauanini <constanza.uanini@admios-sa.com>
2022-09-15 16:17:19 -0300

    Fix mobile style

--- src/components/accession2/ViabilityTestingPanel.tsx
+++ src/components/accession2/ViabilityTestingPanel.tsx
@@ -1,5 +1,6 @@
 import { Box, Typography } from '@mui/material';
 import { Button } from '@terraware/web-components';
+import { useDeviceInfo } from '@terraware/web-components/utils';
 import { useState } from 'react';
 import { Accession2 } from 'src/api/accessions2/accession';
 import strings from 'src/strings';
@@ -11,6 +12,7 @@ type ViabilityTestingPanelProps = {
 };
 export default function ViabilityTestingPanel(props: ViabilityTestingPanelProps): JSX.Element {
   const { accession, reload } = props;
+  const { isMobile } = useDeviceInfo();
 
   const [newTestOpened, setNewTestOpened] = useState(false);
 
@@ -25,7 +27,7 @@ export default function ViabilityTestingPanel(props: ViabilityTestingPanelProps)
       {accession?.viabilityTests ? (
         <Box>Viability Testing</Box>
       ) : (
-        <Box width={'420px'} textAlign='center' sx={{ margin: '0 auto', paddingTop: 4 }}>
+        <Box width={isMobile ? '100%' : '420px'} textAlign='center' sx={{ margin: '0 auto', paddingTop: 4 }}>
           <Typography>{strings.VIABILITY_TESTING_EMPTY_MESSAGE}</Typography>
           <Box sx={{ marginTop: 4 }}>
             <Button priority='secondary' label={strings.START_TESTING} onClick={() => setNewTestOpened(true)} />
