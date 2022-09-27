164cfd960f2f952171b228e92a1e7a31e2f331e8
constanzauanini <constanza.uanini@admios-sa.com>
2022-09-27 13:43:45 -0300

    Create import modal component and use it for import species and import accessions (#541)

---
+++ src/api/types/uploadFile.ts
@@ -0,0 +1,14 @@
+import { paths } from './generated-schema';
+const UPLOAD_STATUS = '/api/v1/species/uploads/{uploadId}';
+
+export type GetUploadStatusResponsePayload =
+  paths[typeof UPLOAD_STATUS]['get']['responses'][200]['content']['application/json'];
+
+export type UploadFileResponse = {
+  id: number;
+  requestSucceeded: boolean;
+};
+
+export type ResolveResponse = {
+  requestSucceeded: boolean;
+};
