c124c1555eda167e33f1c42427fddd433bf284a7
constanzauanini <constanza.uanini@admios-sa.com>
2022-09-28 16:29:08 -0300

    Align button on mobile

--- src/components/accession2/view/Accession2View.tsx
+++ src/components/accession2/view/Accession2View.tsx
@@ -402,7 +402,7 @@ export default function Accession2View(props: Accession2ViewProps): JSX.Element 
         </>
       )}
       <Box padding={3}>
-        <Box display='flex' justifyContent='space-between'>
+        <Box display='flex' justifyContent='space-between' alignItems='center'>
           <Typography>{accession?.accessionNumber}</Typography>
           <Box display='flex' alignItems='center'>
             <IconButton sx={{ marginLeft: 3, height: '24px' }} onClick={() => setOpenDeleteAccession(true)}>
