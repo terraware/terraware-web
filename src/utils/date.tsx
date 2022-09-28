16a7511afa45a01ffef2f057b26f91aebdee1766
Karthik B <karthik@terraformation.com>
2022-09-28 13:22:56 -0700

    SW-1801 Use same default date for withdraw modal as other modals like create accession (#548)
    - update util to use same code that other modals use
    
    - replace getDateDisplayValue with getTodaysDateFormatted

--- src/utils/date.tsx
+++ src/utils/date.tsx
@@ -1,7 +1,6 @@
 import { getDateDisplayValue } from '@terraware/web-components/utils';
-import moment from 'moment';
 export default getDateDisplayValue;
 
 export const getTodaysDateFormatted = () => {
-  return moment().format('YYYY-MM-DD');
+  return getDateDisplayValue(Date.now());
 };
