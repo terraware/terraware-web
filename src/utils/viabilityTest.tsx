6cd9477a961e950e3136ce02f4b233a468d417c7
constanzauanini <constanza.uanini@admios-sa.com>
2022-10-11 11:38:09 -0300

    SW-1973 Get correct substrate options on withdrawal modal (#607)

--- src/utils/viabilityTest.tsx
+++ src/utils/viabilityTest.tsx
@@ -1,4 +1,5 @@
 import strings from 'src/strings';
+import { LAB_SUBSTRATES, NURSERY_SUBSTRATES } from 'src/types/Accession';
 
 export const getFullTestType = (testType: 'Lab' | 'Nursery' | 'Cut') => {
   if (testType === 'Lab') {
@@ -7,5 +8,15 @@ export const getFullTestType = (testType: 'Lab' | 'Nursery' | 'Cut') => {
     return strings.NURSERY_GERMINATION;
   } else {
     return strings.CUT_TEST;
+  }
+};
+
+export const getSubstratesAccordingToType = (type?: string) => {
+  if (type === 'Lab') {
+    return LAB_SUBSTRATES;
+  } else if (type === 'Nursery') {
+    return NURSERY_SUBSTRATES;
+  } else {
+    return [];
   }
 };
