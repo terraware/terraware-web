f44e2a8754f44f460ec533ddc2cd842a767647d6
Charlie Keith <charlie@terraformation.com>
2022-10-31 14:15:20 -0600

    rm unused import

--- src/components/common/PageHeaderWrapper.tsx
+++ src/components/common/PageHeaderWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Box, useTheme } from '@mui/material';
 
 const TOP_BAR_HEIGHT = 64;
