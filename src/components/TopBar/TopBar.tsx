2e7270b58b52940f7af5582917354c3334b585e5
Josh Buchea <verctrl@joshbuchea.com>
2022-11-01 17:44:42 -1000

    format w/ prettier

--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -17,8 +17,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     justifyContent: 'space-between',
   },
-  right: {
-  },
+  right: {},
   mobile: {
     minHeight: '64px',
   },
