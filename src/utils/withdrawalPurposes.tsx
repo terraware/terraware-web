529cc65e3dde100cce3615865b548d24093ba2b7
Karthik B <karthik@terraformation.com>
2022-11-07 08:11:37 -0800

    SW-1416 Remove mapping from old withdrawal purpose to new (#708)
    - mapping usage was removed as part of v1 accessions code deletion
    - the mapping function was left around, removing that now

--- src/utils/withdrawalPurposes.tsx
+++ src/utils/withdrawalPurposes.tsx
@@ -1,14 +1,1 @@
 export const WITHDRAWAL_PURPOSES = ['Nursery', 'Out-planting', 'Viability Testing', 'Other'];
-
-export const getSelectedPurpose = (value: string | undefined) => {
-  if (value && WITHDRAWAL_PURPOSES.indexOf(value) > -1) {
-    return value;
-  } else {
-    switch (value) {
-      case 'Propagation':
-        return 'Out-planting';
-      default:
-        return 'Other';
-    }
-  }
-};
