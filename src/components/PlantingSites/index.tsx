1adb7f729c8931b70dda17a2f71ac2ba9d4576b5
constanzauanini <constanza.uanini@admios-sa.com>
2022-11-09 12:27:58 -0300

    SW-2103 Add Planting Sites empty state

--- src/components/PlantingSites/index.tsx
+++ src/components/PlantingSites/index.tsx
@@ -1,6 +1,10 @@
 import { Box } from '@mui/material';
-import strings from 'src/strings';
+import EmptyStatePage from '../emptyStatePages/EmptyStatePage';
 
 export default function PlantingSitesList(): JSX.Element {
-  return <Box>{strings.PLANTING_SITES}</Box>;
+  return (
+    <Box>
+      <EmptyStatePage pageName={'PlantingSites'} />
+    </Box>
+  );
 }
