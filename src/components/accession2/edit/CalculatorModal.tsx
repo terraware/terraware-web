4375b24374da6e085853fb74d75d0a5e19b6d34f
constanzauanini <constanza.uanini@admios-sa.com>
2022-11-11 11:12:50 -0300

    Compare values using grams

--- src/components/accession2/edit/CalculatorModal.tsx
+++ src/components/accession2/edit/CalculatorModal.tsx
@@ -50,7 +50,11 @@ export default function CalculatorModal(props: CalculatorModalProps): JSX.Elemen
     if (validateFields()) {
       const response = await updateAccession2(record, true);
       if (response.requestSucceeded && response.accession) {
-        if (response.accession.estimatedCount?.toString() === '0') {
+        if (
+          response.accession.subsetWeight?.grams &&
+          response.accession.latestObservedQuantity?.grams &&
+          response.accession.subsetWeight?.grams > response.accession.latestObservedQuantity?.grams
+        ) {
           setSubsetError(strings.SUBSET_ERROR);
           return;
         }
