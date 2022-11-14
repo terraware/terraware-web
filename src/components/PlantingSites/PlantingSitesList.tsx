70a6621b5877a1a16dd89e97fae5996286aaf9e2
constanzauanini <constanza.uanini@admios-sa.com>
2022-11-14 12:47:05 -0300

    SW-2110 Planting site search capability

--- src/components/PlantingSites/PlantingSitesList.tsx
+++ src/components/PlantingSites/PlantingSitesList.tsx
@@ -7,6 +7,7 @@ import { search, SearchResponseElement } from 'src/api/search';
 import { APP_PATHS } from 'src/constants';
 import strings from 'src/strings';
 import { ServerOrganization } from 'src/types/Organization';
+import useDebounce from 'src/utils/useDebounce';
 import PageHeaderWrapper from '../common/PageHeaderWrapper';
 import TfMain from '../common/TfMain';
 import EmptyStatePage from '../emptyStatePages/EmptyStatePage';
@@ -20,7 +21,9 @@ export default function PlantingSitesList(props: PlantingSitesListProps): JSX.El
   const { organization } = props;
   const contentRef = useRef(null);
   const [searchResults, setSearchResults] = useState<SearchResponseElement[] | null>();
+  const [plantingSites, setPlantingSites] = useState<SearchResponseElement[] | null>();
   const [temporalSearchValue, setTemporalSearchValue] = useState('');
+  const debouncedSearchTerm = useDebounce(temporalSearchValue, 250);
   const { isMobile } = useDeviceInfo();
   const history = useHistory();
 
@@ -36,15 +39,29 @@ export default function PlantingSitesList(props: PlantingSitesListProps): JSX.El
       fields: ['boundary', 'id', 'name', 'numPlantingZones', 'numPlots'],
       prefix: 'plantingSites',
       search: {
-        field: 'organization_id',
-        operation: 'field',
-        values: [organization.id],
+        operation: 'and',
+        children: [
+          {
+            field: 'organization_id',
+            operation: 'field',
+            values: [organization.id],
+          },
+          {
+            operation: 'field',
+            field: 'name',
+            type: 'Fuzzy',
+            values: [debouncedSearchTerm],
+          },
+        ],
       },
       count: 0,
     };
     const apiSearchResults = await search(params);
+    if (!debouncedSearchTerm) {
+      setPlantingSites(apiSearchResults);
+    }
     setSearchResults(apiSearchResults);
-  }, [organization]);
+  }, [organization, debouncedSearchTerm]);
 
   useEffect(() => {
     onSearch();
@@ -59,7 +76,7 @@ export default function PlantingSitesList(props: PlantingSitesListProps): JSX.El
               {strings.PLANTING_SITES}
             </Typography>
           </Grid>
-          {searchResults && searchResults.length > 0 ? (
+          {plantingSites && plantingSites.length > 0 ? (
             <Grid item xs={6} sx={{ textAlign: 'right' }}>
               {isMobile ? (
                 <Button
@@ -82,9 +99,9 @@ export default function PlantingSitesList(props: PlantingSitesListProps): JSX.El
         </Box>
       </PageHeaderWrapper>
       <Grid>
-        {searchResults ? (
+        {plantingSites ? (
           <Box>
-            {searchResults?.length === 0 ? (
+            {plantingSites?.length === 0 ? (
               <Container sx={{ paddingY: 4 }}>
                 <EmptyStatePage pageName={'PlantingSites'} />
               </Container>
