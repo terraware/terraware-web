8562efd402f3e464d3753f9e6b2b82a3bb629379
constanzauanini <constanza.uanini@admios-sa.com>
2022-11-28 12:31:32 -0300

    SW-2314 Add title and dropdown of planting sites to select current planting site (#818)

--- src/components/Plants/index.tsx
+++ src/components/Plants/index.tsx
@@ -1,17 +1,85 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { ServerOrganization } from 'src/types/Organization';
 import strings from 'src/strings';
 import TfMain from 'src/components/common/TfMain';
-import Title from 'src/components/common/Title';
+import { Box, Grid, Typography, useTheme } from '@mui/material';
+import { Select } from '@terraware/web-components';
+import { listPlantingSites } from 'src/api/tracking/tracking';
+import { PlantingSite } from 'src/api/types/tracking';
+import { useDeviceInfo } from '@terraware/web-components/utils';
+import { useHistory, useParams } from 'react-router-dom';
+import { APP_PATHS } from 'src/constants';
+import useSnackbar from 'src/utils/useSnackbar';
 
 type PlantsDashboardProps = {
   organization: ServerOrganization;
 };
 
 export default function PlantsDashboard(props: PlantsDashboardProps): JSX.Element {
+  const { organization } = props;
+  const [selectedPlantingSite, setSelectedPlantingSite] = useState<PlantingSite>();
+  const [plantingSites, setPlantingSites] = useState<PlantingSite[]>([]);
+  const theme = useTheme();
+  const { isMobile } = useDeviceInfo();
+  const { plantingSiteId } = useParams<{ plantingSiteId: string }>();
+  const history = useHistory();
+  const [snackbar] = useState(useSnackbar());
+
+  const setActivePlantingSite = useCallback(
+    (site: PlantingSite | undefined) => {
+      if (site) {
+        history.push(APP_PATHS.PLANTING_SITE_DASHBOARD.replace(':plantingSiteId', site.id.toString()));
+      }
+    },
+    [history]
+  );
+
+  useEffect(() => {
+    const populatePlantingSites = async () => {
+      const serverResponse = await listPlantingSites(organization.id);
+      if (serverResponse.requestSucceeded) {
+        setPlantingSites(serverResponse.sites ?? []);
+      } else {
+        snackbar.toastError(serverResponse.error);
+      }
+    };
+    populatePlantingSites();
+  }, [organization.id, snackbar]);
+
+  useEffect(() => {
+    if (plantingSites.length) {
+      const plantingSiteIdToUse = plantingSiteId;
+      const requestedPlantingSite = plantingSites.find((ps) => ps?.id === parseInt(plantingSiteIdToUse, 10));
+      const plantingSiteToUse = requestedPlantingSite || plantingSites[0];
+      if (plantingSiteToUse.id.toString() === plantingSiteId) {
+        setSelectedPlantingSite(plantingSiteToUse);
+      } else {
+        setActivePlantingSite(plantingSiteToUse);
+      }
+    }
+  }, [plantingSites, plantingSiteId, setActivePlantingSite]);
+
+  const onChangePlantingSite = (newValue: string) => {
+    setActivePlantingSite(plantingSites.find((ps) => ps.name === newValue));
+  };
+
   return (
     <TfMain>
-      <Title page={strings.DASHBOARD} parentPage={''} />
+      <Grid item xs={12} display={isMobile ? 'block' : 'flex'}>
+        <Typography sx={{ fontSize: '24px', fontWeight: 600, alignItems: 'center' }}>{strings.DASHBOARD}</Typography>
+        {!isMobile && (
+          <Box sx={{ margin: '0 1%', width: '1px', height: '32px', backgroundColor: theme.palette.TwClrBgTertiary }} />
+        )}
+        <Box display='flex' alignItems='center' paddingTop={isMobile ? 2 : 0}>
+          <Typography sx={{ paddingRight: 1, fontSize: '16px', fontWeight: 500 }}>{strings.PLANTING_SITE}</Typography>
+          <Select
+            options={plantingSites.map((ps) => ps?.name || '')}
+            onChange={onChangePlantingSite}
+            selectedValue={selectedPlantingSite?.name}
+            placeholder={strings.SELECT}
+          />
+        </Box>
+      </Grid>
     </TfMain>
   );
 }
