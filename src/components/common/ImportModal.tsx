d89c786db7d9cb74d1c3f0483903c6f10af4b409
alexTerraformation <104874529+alexTerraformation@users.noreply.github.com>
2022-11-28 11:24:12 -0800

    SW-2194 Remove "data" and the [import icon] in import dialog boxes (#827)

--- src/components/common/ImportModal.tsx
+++ src/components/common/ImportModal.tsx
@@ -292,7 +292,7 @@ export default function ImportSpeciesModal(props: ImportSpeciesModalProps): JSX.
             className={classes.spacing}
             key='mb-1'
           />,
-          <Button onClick={importDataHandler} icon='iconImport' label={strings.IMPORT_DATA} key='mb-2' />,
+          <Button onClick={importDataHandler} label={strings.IMPORT} key='mb-2' />,
         ];
       }
     }
