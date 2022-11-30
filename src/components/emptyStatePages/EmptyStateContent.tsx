e1910f98aabc516f33ae65ebdb71148b96c1fd18
Josh Buchea <verctrl@joshbuchea.com>
2022-11-30 10:38:43 -1000

    use "brand" color for links

--- src/components/emptyStatePages/EmptyStateContent.tsx
+++ src/components/emptyStatePages/EmptyStateContent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Theme, Grid } from '@mui/material';
+import { Container, Theme, Grid, useTheme } from '@mui/material';
 import Button from 'src/components/common/button/Button';
 import Icon from 'src/components/common/icon/Icon';
 import { IconName } from 'src/components/common/icon/icons';
@@ -120,6 +120,7 @@ export default function EmptyStateContent(props: EmptyStateContentProps): JSX.El
   const { title, subtitle, listItems, buttonText, buttonIcon, onClickButton, footnote, styles } = props;
   const { isMobile } = useDeviceInfo();
   const classes = useStyles({ ...styles, isMobile });
+  const theme = useTheme();
 
   const gridSize = () => {
     if (isMobile) {
@@ -143,7 +144,7 @@ export default function EmptyStateContent(props: EmptyStateContentProps): JSX.El
                   {item.description}
                 </p>
                 {item.linkText && item.onLinkClick && (
-                  <Link onClick={item.onLinkClick} className={classes.itemLink}>
+                  <Link onClick={item.onLinkClick} className={classes.itemLink} color={theme.palette.TwClrTxtBrand}>
                     {item.linkText}
                   </Link>
                 )}
