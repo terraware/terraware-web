fdbe8d25d1eabab8c70e3e106ec6b2c80336c22e
Josh Buchea <verctrl@joshbuchea.com>
2022-11-30 11:11:51 -1000

    update title component to inherit body color, render as h1 element

--- src/components/common/Title.tsx
+++ src/components/common/Title.tsx
@@ -6,11 +6,8 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     alignItems: 'center',
   },
-  title: {
-    color: 'gray',
-  },
+  title: {},
   selectedSection: {
-    color: 'black',
     fontWeight: 600,
     fontSize: 24,
   },
@@ -26,7 +23,9 @@ export default function Title({ page }: TitleProps): JSX.Element {
   return (
     <div className={classes.titleContainer}>
       <div className={classes.title}>
-        <Typography className={classes.selectedSection}>{page}</Typography>
+        <Typography className={classes.selectedSection} variant='h1'>
+          {page}
+        </Typography>
       </div>
     </div>
   );
