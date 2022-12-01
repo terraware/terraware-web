c97bfaeb1c738f8485d916fe322ffba0b4f9dfec
Karthik Balachandran <karthik@terraformation.com>
2022-12-01 09:59:13 -0800

    refactor

--- src/components/Map/GenericMap.tsx
+++ src/components/Map/GenericMap.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress } from '@mui/material';
 import { getMapboxToken } from 'src/api/tracking/tracking';
 import useSnackbar from 'src/utils/useSnackbar';
 import Map from './Map';
-import { MapOptions } from './MapModels';
+import { MapOptions, MapPopupRenderer } from './MapModels';
 
 const DUMMY_MAP_OPTIONS: MapOptions = {
   bbox: {
@@ -14,13 +14,16 @@ const DUMMY_MAP_OPTIONS: MapOptions = {
 };
 
 type GenericMapProps = {
+  contextRenderer?: MapPopupRenderer;
+  options?: MapOptions;
   style?: object;
 };
 
-export default function GenericMap({ style }: GenericMapProps): JSX.Element | null {
+export default function GenericMap({ contextRenderer, options, style }: GenericMapProps): JSX.Element | null {
   const [snackbar] = useState(useSnackbar());
   const [token, setToken] = useState<string>();
   const [mapId, setMapId] = useState<string>();
+  const [tokenPromise, setTokenPromise] = useState<Promise<void>>();
 
   // fetch token
   const fetchMapboxToken = useCallback(async () => {
@@ -33,12 +36,21 @@ export default function GenericMap({ style }: GenericMapProps): JSX.Element | nu
     }
   }, [snackbar]);
 
-  useEffect(() => {
-    if (token) {
+  const refreshToken = useCallback(() => {
+    const promise = fetchMapboxToken();
+    setTokenPromise(promise);
+  }, [fetchMapboxToken]);
+
+  const getToken = useCallback(() => {
+    if (tokenPromise) {
       return;
     }
-    fetchMapboxToken();
-  }, [mapId, token, fetchMapboxToken]);
+    refreshToken();
+  }, [tokenPromise, refreshToken]);
+
+  useEffect(() => {
+    getToken();
+  }, [getToken]);
 
   if (!token) {
     return (
@@ -50,7 +62,14 @@ export default function GenericMap({ style }: GenericMapProps): JSX.Element | nu
 
   return (
     <Box sx={{ display: 'flex', flexGrow: 1 }}>
-      <Map token={token} options={DUMMY_MAP_OPTIONS} onTokenExpired={fetchMapboxToken} mapId={mapId} style={style} />
+      <Map
+        token={token}
+        options={options || DUMMY_MAP_OPTIONS}
+        onTokenExpired={refreshToken}
+        mapId={mapId}
+        style={style}
+        popupRenderer={contextRenderer}
+      />
     </Box>
   );
 }
--- src/components/Map/PlantingSiteMap.tsx
+++ src/components/Map/PlantingSiteMap.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Box, CircularProgress, useTheme } from '@mui/material';
 import hexRgb from 'hex-rgb';
-import { getMapboxToken } from 'src/api/tracking/tracking';
 import { MultiPolygon, PlantingSite } from 'src/api/types/tracking';
 import useSnackbar from 'src/utils/useSnackbar';
-import Map from './Map';
+import GenericMap from './GenericMap';
 import { MapGeometry, MapOptions, MapPopupRenderer, MapSource } from './MapModels';
 import { getBoundingBox } from './MapUtils';
 import _ from 'lodash';
@@ -21,9 +20,7 @@ export default function PlantingSiteMap(props: PlantingSiteMapProps): JSX.Elemen
   const { plantingSite, style, contextRenderer } = props;
   const theme = useTheme();
   const [snackbar] = useState(useSnackbar());
-  const [token, setToken] = useState<string>();
   const [mapOptions, setMapOptions] = useState<MapOptions>();
-  const [mapId, setMapId] = useState<string>();
 
   const getRenderAttributes = useCallback(
     (objectType: 'site' | 'zone' | 'plot') => {
@@ -143,24 +140,6 @@ export default function PlantingSiteMap(props: PlantingSiteMapProps): JSX.Elemen
     [getPolygons, getRenderAttributes, theme.palette.TwClrBaseWhite]
   );
 
-  // fetch token
-  const fetchMapboxToken = useCallback(async () => {
-    const response = await getMapboxToken();
-    if (response.requestSucceeded) {
-      setToken(response.token);
-      setMapId(Date.now().toString());
-    } else {
-      snackbar.toastError(response.error);
-    }
-  }, [snackbar]);
-
-  useEffect(() => {
-    if (token) {
-      return;
-    }
-    fetchMapboxToken();
-  }, [plantingSite.id, fetchMapboxToken, token]);
-
   // fetch polygons and boundaries
   useEffect(() => {
     const fetchPlantingSite = () => {
@@ -187,7 +166,7 @@ export default function PlantingSiteMap(props: PlantingSiteMapProps): JSX.Elemen
     fetchPlantingSite();
   }, [plantingSite, snackbar, extractPlantingSite, extractPlantingZones, extractPlots, mapOptions]);
 
-  if (!token || !mapOptions) {
+  if (!mapOptions) {
     return (
       <Box sx={{ display: 'flex', flexGrow: 1, height: '100%', margin: 'auto' }}>
         <CircularProgress sx={{ margin: 'auto' }} />
@@ -197,14 +176,7 @@ export default function PlantingSiteMap(props: PlantingSiteMapProps): JSX.Elemen
 
   return (
     <Box sx={{ display: 'flex', flexGrow: 1 }}>
-      <Map
-        token={token}
-        options={mapOptions}
-        onTokenExpired={fetchMapboxToken}
-        mapId={mapId}
-        popupRenderer={contextRenderer}
-        style={style}
-      />
+      <GenericMap options={mapOptions} contextRenderer={contextRenderer} style={style} />
     </Box>
   );
 }
