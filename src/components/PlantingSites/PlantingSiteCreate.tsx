cef28ad957cdc88a0ba297e7ccaa6d4eb6b59b80
Karthik Balachandran <karthik@terraformation.com>
2022-12-04 19:11:36 -0800

    remove planting sites link in edit/create views

--- src/components/PlantingSites/PlantingSiteCreate.tsx
+++ src/components/PlantingSites/PlantingSiteCreate.tsx
@@ -1,7 +1,6 @@
 import { ServerOrganization } from 'src/types/Organization';
 import TfMain from 'src/components/common/TfMain';
-import { Typography, Box, Container, Grid, useTheme, Theme } from '@mui/material';
-import { Icon } from '@terraware/web-components';
+import { Typography, Box, Container, Grid, useTheme } from '@mui/material';
 import strings from 'src/strings';
 import FormBottomBar from '../common/FormBottomBar';
 import { useDeviceInfo } from '@terraware/web-components/utils';
@@ -14,12 +13,11 @@ import {
   updatePlantingSite,
 } from 'src/api/tracking/tracking';
 import { APP_PATHS } from 'src/constants';
-import { Link, useHistory, useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import useSnackbar from 'src/utils/useSnackbar';
 import TextField from '@terraware/web-components/components/Textfield/Textfield';
 import { useEffect, useState } from 'react';
 import PageSnackbar from '../PageSnackbar';
-import { makeStyles } from '@mui/styles';
 import { PlantingSite } from 'src/api/types/tracking';
 import BoundariesAndPlots from './BoundariesAndPlots';
 
@@ -27,20 +25,6 @@ type CreatePlantingSiteProps = {
   organization: ServerOrganization;
   reloadPlantingSites: () => void;
 };
-
-const useStyles = makeStyles((theme: Theme) => ({
-  backIcon: {
-    fill: theme.palette.TwClrIcnBrand,
-    marginRight: theme.spacing(1),
-  },
-  back: {
-    display: 'flex',
-    textDecoration: 'none',
-    color: theme.palette.TwClrTxtBrand,
-    fontSize: '14px',
-    alignItems: 'center',
-  },
-}));
 
 export default function CreatePlantingSite(props: CreatePlantingSiteProps): JSX.Element {
   const { isMobile } = useDeviceInfo();
@@ -50,7 +34,6 @@ export default function CreatePlantingSite(props: CreatePlantingSiteProps): JSX.
   const history = useHistory();
   const snackbar = useSnackbar();
   const [nameError, setNameError] = useState('');
-  const classes = useStyles();
   const [selectedPlantingSite, setSelectedPlantingSite] = useState<PlantingSite>();
   const [loaded, setLoaded] = useState(false);
 
@@ -140,12 +123,6 @@ export default function CreatePlantingSite(props: CreatePlantingSiteProps): JSX.
         {loaded && (
           <>
             <Grid container spacing={3} flexGrow={0} display='flex' flexDirection='column'>
-              <Grid item xs={12}>
-                <Link id='back' to={APP_PATHS.PLANTING_SITES} className={classes.back}>
-                  <Icon name='caretLeft' className={classes.backIcon} />
-                  {strings.PLANTING_SITES}
-                </Link>
-              </Grid>
               <Box paddingLeft={theme.spacing(3)} marginBottom={theme.spacing(4)} display='flex' flexDirection='column'>
                 <Typography
                   fontSize={selectedPlantingSite ? '20px' : '24px'}
