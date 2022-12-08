418daad7bd647a4acaaeab095448eb80a1a12ee2
Karthik B <karthik@terraformation.com>
2022-12-08 05:39:53 -0800

    Render tables in 16px, fix font color on nav (#896)
    - bump web-components version

--- src/components/People/TableCellRenderer.tsx
+++ src/components/People/TableCellRenderer.tsx
@@ -11,11 +11,7 @@ export default function Renderer(props: RendererProps<TableRowType>): JSX.Elemen
     const personLocation = {
       pathname: APP_PATHS.PEOPLE_VIEW.replace(':personId', row.id.toString()),
     };
-    return (
-      <Link fontSize='16px' to={personLocation.pathname}>
-        {iValue}
-      </Link>
-    );
+    return <Link to={personLocation.pathname}>{iValue}</Link>;
   };
 
   if (column.key === 'firstName' || column.key === 'lastName') {
