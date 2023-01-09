4845ca0fc927930f300afbefaadda1a15c9548be
Karthik Balachandran <karthik@terraformation.com>
2023-01-09 10:09:32 -0800

    Temporary time zones hook to unblock selector component and other page views

--- src/providers/hooks.ts
+++ src/providers/hooks.ts
@@ -2,3 +2,11 @@ import { useContext } from 'react';
 import { UserContext } from './contexts';
 
 export const useUser = () => useContext(UserContext);
+
+/**
+ * Default time zones until we have a provider.
+ */
+const DEFAULT_TIME_ZONES = (Intl as any).supportedValuesOf('timeZone').map((tz: any) => ({ id: tz, longName: tz }));
+const SUPPORTED_TIME_ZONES = [...DEFAULT_TIME_ZONES, { id: 'Etc/UTC', longName: 'Coordinated Universal Time' }];
+
+export const useTimeZones = () => SUPPORTED_TIME_ZONES;
