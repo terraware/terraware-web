3820539b3dd6a747d517caa982ad229cf50d981a
Karthik B <karthik@terraformation.com>
2023-01-09 11:47:30 -0800

    Update hooks.ts

--- src/providers/hooks.ts
+++ src/providers/hooks.ts
@@ -6,8 +6,8 @@ export const useUser = () => useContext(UserContext);
 /**
  * Default time zones until we have a provider.
  */
-//const DEFAULT_TIME_ZONES = (Intl as any).supportedValuesOf('timeZone').map((tz: any) => ({ id: tz, longName: tz }));
-//const SUPPORTED_TIME_ZONES = [...DEFAULT_TIME_ZONES, { id: 'Etc/UTC', longName: 'Coordinated Universal Time' }];
+// const DEFAULT_TIME_ZONES = (Intl as any).supportedValuesOf('timeZone').map((tz: any) => ({ id: tz, longName: tz }));
+// const SUPPORTED_TIME_ZONES = [...DEFAULT_TIME_ZONES, { id: 'Etc/UTC', longName: 'Coordinated Universal Time' }];
 
 export const useTimeZones = () => [];
 
