65dd82dc93955a07acd70a961c790cec0a199bfc
constanzauanini <constanza.uanini@admios-sa.com>
2023-01-12 14:59:06 -0300

    use const

--- src/utils/useTimeZoneUtils.ts
+++ src/utils/useTimeZoneUtils.ts
@@ -76,7 +76,7 @@ export const useLocationTimeZone = () => {
 
 export const changeTimezone = (date: Date, ianatz: string) => {
   // suppose the date is 12:00 UTC
-  var invdate = new Date(
+  const invdate = new Date(
     date.toLocaleString('en-US', {
       timeZone: ianatz,
     })
@@ -84,7 +84,7 @@ export const changeTimezone = (date: Date, ianatz: string) => {
 
   // then invdate will be 07:00 in Toronto
   // and the diff is 5 hours
-  var diff = date.getTime() - invdate.getTime();
+  const diff = date.getTime() - invdate.getTime();
 
   // so 12:00 in Toronto is 17:00 UTC
   return new Date(date.getTime() - diff); // needs to substract
