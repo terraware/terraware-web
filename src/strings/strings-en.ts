284c2fb736751d83b3e2f03b28752782581d3d26
Karthik Balachandran <karthik@terraformation.com>
2023-01-23 12:38:36 -0800

    Create component for region selection

--- src/strings/strings-en.ts
+++ src/strings/strings-en.ts
@@ -167,7 +167,6 @@ export const strings = {
   COUNT: 'Count',
   COUNTRY_REQUIRED: 'Country *',
   COUNTRY: 'Country',
-  COUNTRY_REQUIRED: 'Country *',
   CREATE_ACCESSION: 'Create Accession',
   CREATE_ENTRY: 'Create Entry',
   CREATE_NEW_ORGANIZATION: 'Create New Organization',
