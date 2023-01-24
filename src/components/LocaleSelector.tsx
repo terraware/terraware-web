terraware/terraware-web

commit fe1261459a81e25017b531177d53790a28b98934

SW-2776 Load and save user locale selection (#1092)
At initialization time, use the locale from the user's profile, if any.

When the user changes locales, save the new selection to their profile.

When the user edits their profile, save the currently-selected locale along with
the other profile data.

src/providers/LocalizationProvider.tsx
@@ -3,9 +3,10 @@ import { LocalizationContext } from './contexts';
 import { getTimeZones } from 'src/api/timezones/timezones';
 import { TimeZoneDescription } from 'src/types/TimeZones';
 import strings, { ILocalizedStringsMap } from 'src/strings';
-import { ProvidedLocalizationData } from '.';
+import { ProvidedLocalizationData, useUser } from '.';
 import { supportedLocales } from '../strings/locales';
 import axios from 'axios';
+import { updateUserProfile } from 'src/api/user/user';
 
 export type LocalizationProviderProps = {
   children?: React.ReactNode;
@@ -23,6 +24,13 @@ export default function LocalizationProvider({
   setLoadedStringsForLocale,
 }: LocalizationProviderProps): JSX.Element | null {
   const [timeZones, setTimeZones] = useState<TimeZoneDescription[]>([]);
+  const { user, reloadUser } = useUser();
+
+  useEffect(() => {
+    if (user?.locale) {
+      setLocale(user.locale);
+    }
+  }, [user?.locale, setLocale]);
 
   useEffect(() => {
     axios.defaults.headers = { ...axios.defaults.headers, 'Accept-Language': locale };
@@ -58,6 +66,17 @@ export default function LocalizationProvider({
 
     fetchStrings();
   }, [locale, setLoadedStringsForLocale]);
+
+  useEffect(() => {
+    const updateUserLocale = async () => {
+      if (user && user.locale !== locale) {
+        await updateUserProfile({ ...user, locale }, true);
+        reloadUser();
+      }
+    };
+
+    updateUserLocale();
+  }, [locale, user, reloadUser]);
 
   const context: ProvidedLocalizationData = {
     bootstrapped: !!loadedStringsForLocale,

src/api/types/generated-schema.ts
@@ -2046,6 +2046,11 @@ export interface components {
       firstName: string;
       lastName: string;
       /**
+       * @description IETF locale code containing user's preferred language.
+       * @example en
+       */
+      locale?: string;
+      /**
        * @description Time zone name in IANA tz database format
        * @example America/New_York
        */
@@ -2141,6 +2146,11 @@ export interface components {
       emailNotificationsEnabled: boolean;
       firstName?: string;
       lastName?: string;
+      /**
+       * @description IETF locale code containing user's preferred language.
+       * @example en
+       */
+      locale?: string;
       /**
        * @description Time zone name in IANA tz database format
        * @example America/New_York
@@ -2684,7 +2694,7 @@ export interface operations {
   listTimeZoneNames: {
     parameters: {
       query: {
-        /** Language code and optional country code suffix. */
+        /** Language code and optional country code suffix. If not specified, the preferred locale from the Accept-Language header is used if supported; otherwise US English is the default. */
         locale?: string;
       };
     };

src/api/user/user.ts
@@ -39,6 +39,7 @@ export async function getUser(): Promise<GetUserResponse> {
       lastName: serverResponse.user.lastName,
       emailNotificationsEnabled: serverResponse.user.emailNotificationsEnabled,
       timeZone: serverResponse.user.timeZone,
+      locale: serverResponse.user.locale,
     };
     setCurrentUser(response.user);
   } catch {
@@ -60,6 +61,7 @@ export async function updateUserProfile(user: User, skipAcknowledgeTimeZone?: bo
       firstName: user.firstName || '',
       lastName: user.lastName || '',
       timeZone: user.timeZone,
+      locale: user.locale,
     };
     if (user.emailNotificationsEnabled !== undefined) {
       serverRequest.emailNotificationsEnabled = user.emailNotificationsEnabled;

src/components/MyAccount/index.tsx
@@ -97,6 +97,7 @@ const MyAccountContent = ({
   const contentRef = useRef(null);
   const timeZonesEnabled = isEnabled('Timezones');
   const weightUnitsEnabled = isEnabled('Weight units');
+  const { locale } = useLocalization();
   const timeZones = useTimeZones();
   const tz = timeZones.find((timeZone) => timeZone.id === record.timeZone) || getUTC(timeZones);
   const [preferredWeightSystemSelected, setPreferredWeightSystemSelected] = useState(
@@ -197,7 +198,9 @@ const MyAccountContent = ({
   };
 
   const saveProfileChanges = async () => {
-    const updateUserResponse = await updateUserProfile(record);
+    // Save the currently-selected locale, even if it differs from the locale in the profile data we
+    // fetched from the server.
+    const updateUserResponse = await updateUserProfile({ ...record, locale });
     return updateUserResponse;
   };
 

src/types/User.ts
@@ -7,6 +7,7 @@ export type User = {
   email?: string;
   emailNotificationsEnabled?: boolean;
   timeZone?: string;
+  locale?: string;
 };
 
 export type OrganizationUser = {

commit eac21025a34d580db73156a2d5fd3068f8220f0a

Fix flapping when non-English locale is selected
Commit fe1261459 introduced a bug: if the user's profile had a non-English locale,
the UI would flap back and forth between English and the selected locale.

This was because LocalizationProvider was treating any update of the locale as a
trigger to save the new value to the user's profile. So when the user's profile
was loaded and the locale was set based on that, it triggered a write to the
user's profile, which then triggered another locale update, and so on.

Fix it by only updating the user's profile when they actually change the locale
selector.

src/components/LocaleSelector.tsx
@@ -2,8 +2,11 @@ import { DropdownItem } from '@terraware/web-components';
 import { supportedLocales } from '../strings/locales';
 import { LocalizationContext } from '../providers/contexts';
 import Dropdown from './common/Dropdown';
+import { updateUserProfile } from '../api/user/user';
+import { useUser } from '../providers';
 
 export default function LocaleSelector(): JSX.Element {
+  const { user, reloadUser } = useUser();
   const localeItems: DropdownItem[] = supportedLocales.map((supportedLocale) => ({
     value: supportedLocale.id,
     label: supportedLocale.name,
@@ -12,9 +15,22 @@ export default function LocaleSelector(): JSX.Element {
   return (
     <LocalizationContext.Consumer>
       {({ locale, setLocale }) => {
-        return (
-          <Dropdown id={'localeSelector'} label={''} onChange={setLocale} selected={locale} values={localeItems} />
-        );
+        const onChange = (newValue: string) => {
+          if (locale !== newValue) {
+            setLocale(newValue);
+          }
+
+          if (user && user.locale !== newValue) {
+            const updateUserLocale = async () => {
+              await updateUserProfile({ ...user, locale: newValue }, true);
+              reloadUser();
+            };
+
+            updateUserLocale();
+          }
+        };
+
+        return <Dropdown id={'localeSelector'} label={''} onChange={onChange} selected={locale} values={localeItems} />;
       }}
     </LocalizationContext.Consumer>
   );

src/providers/LocalizationProvider.tsx
@@ -6,7 +6,6 @@ import strings, { ILocalizedStringsMap } from 'src/strings';
 import { ProvidedLocalizationData, useUser } from '.';
 import { supportedLocales } from '../strings/locales';
 import axios from 'axios';
-import { updateUserProfile } from 'src/api/user/user';
 
 export type LocalizationProviderProps = {
   children?: React.ReactNode;
@@ -24,7 +23,7 @@ export default function LocalizationProvider({
   setLoadedStringsForLocale,
 }: LocalizationProviderProps): JSX.Element | null {
   const [timeZones, setTimeZones] = useState<TimeZoneDescription[]>([]);
-  const { user, reloadUser } = useUser();
+  const { user } = useUser();
 
   useEffect(() => {
     if (user?.locale) {
@@ -66,17 +65,6 @@ export default function LocalizationProvider({
 
     fetchStrings();
   }, [locale, setLoadedStringsForLocale]);
-
-  useEffect(() => {
-    const updateUserLocale = async () => {
-      if (user && user.locale !== locale) {
-        await updateUserProfile({ ...user, locale }, true);
-        reloadUser();
-      }
-    };
-
-    updateUserLocale();
-  }, [locale, user, reloadUser]);
 
   const context: ProvidedLocalizationData = {
     bootstrapped: !!loadedStringsForLocale,