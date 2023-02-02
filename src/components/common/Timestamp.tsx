c761ae4b5776ff28d7eea6f7812e1461db24c2f0
Steven Grimm <steven@terraformation.com>
2023-02-02 10:40:09 -0800

    Use default (browser) locale if locale isn't set when component is first rendered

--- src/components/common/Timestamp.tsx
+++ src/components/common/Timestamp.tsx
@@ -8,29 +8,28 @@ export interface TimestampProps {
   isoString: string;
 }
 
-/**
- * Renders a timestamp using the current locale and the user's time zone.
- */
+/** Returns a DateTimeFormat object for a locale and time zone. */
+function newDateTimeFormat(locale?: string, timeZone?: string) {
+  return new Intl.DateTimeFormat(locale, {
+    dateStyle: 'long',
+    timeStyle: 'medium',
+    timeZone,
+    hourCycle: locale === 'gx' ? 'h24' : undefined,
+  });
+}
+
+/** Renders a timestamp using the current locale and the user's time zone. */
 export default function Timestamp({ className, isoString }: TimestampProps): JSX.Element | null {
   const timeZone = useUserTimeZone()?.id;
   const { locale } = useLocalization();
-  const [dateTimeFormat, setDateTimeFormat] = useState<Intl.DateTimeFormat | null>(null);
+  const [dateTimeFormat, setDateTimeFormat] = useState<Intl.DateTimeFormat>(newDateTimeFormat(locale, timeZone));
 
   useEffect(() => {
-    if (locale || timeZone) {
-      setDateTimeFormat(
-        new Intl.DateTimeFormat(locale, {
-          dateStyle: 'long',
-          timeStyle: 'medium',
-          timeZone,
-          hourCycle: locale === 'gx' ? 'h24' : undefined,
-        })
-      );
-    }
+    setDateTimeFormat(newDateTimeFormat(locale, timeZone));
   }, [locale, timeZone, setDateTimeFormat]);
 
   const format = () => {
-    const formatted = dateTimeFormat?.format(new Date(isoString));
+    const formatted = dateTimeFormat.format(new Date(isoString));
     if (locale === 'gx') {
       return formatted?.replace(/[A-Za-z]/g, 'XY');
     } else {
