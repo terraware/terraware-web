40e44beacce663bc6defa5bb63e0f36a8c97cb2a
Steven Grimm <steven@terraformation.com>
2023-02-02 14:26:30 -0800

    Handle text without link delimiters

--- src/components/common/TextWithLink.tsx
+++ src/components/common/TextWithLink.tsx
@@ -10,14 +10,26 @@ export interface TextWithLinkProps {
  *
  * @param className
  * @param text The text to render. The link should be surrounded by square brackets. For
- * example, `'See [Jane] run'` would be rendered as `See <a href=...>Jane</a> run`.
+ * example, `'See [Jane] run'` would be rendered as `See <a href=...>Jane</a> run`. If there are
+ * no square brackets, [text] is rendered as a link. For example, `'See Jane run'` would be rendered
+ * as, `<a href=...>See Jane run</a>`.
  */
 export default function TextWithLink({ className, href, onClick, text }: TextWithLinkProps): JSX.Element {
   const linkStart = text.indexOf('[');
   const linkEnd = text.indexOf(']');
-  const prefix = text.substring(0, linkStart);
-  const linkText = text.substring(linkStart + 1, linkEnd);
-  const suffix = text.substring(linkEnd + 1);
+  let prefix: string;
+  let linkText: string;
+  let suffix: string;
+
+  if (linkStart >= 0 && linkEnd >= 0) {
+    prefix = text.substring(0, linkStart);
+    linkText = text.substring(linkStart + 1, linkEnd);
+    suffix = text.substring(linkEnd + 1);
+  } else {
+    prefix = '';
+    linkText = text;
+    suffix = '';
+  }
 
   return (
     <>
