541a45a5cf130e5173baf73835bdd5bb906e1183
Steven Grimm <sgrimm@users.noreply.github.com>
2023-02-07 09:43:40 -0800

    SW-1828 Ignore diacritics in species typeahead (#1150)
    Some species common names have accented characters, but we don't want to force
    users to type the correct accent marks in the species typeahead when adding
    accessions or seedling batches.

--- src/utils/text.tsx
+++ src/utils/text.tsx
@@ -1,2 +1,17 @@
 import { isWhitespaces } from '@terraware/web-components/utils';
+
+/**
+ * Removes accents and other diacritics from letters. This is typically used for things like
+ * typeaheads where we want a value of "ábc" to match user input of "abc".
+ */
+export function removeDiacritics(s: string): string {
+  // First, decompose characters into combining forms: "á" gets turned into a two-character sequence
+  // of "a" followed by a combining character that modifies the previous character to add an accent
+  // mark.
+  const normalized = s.normalize('NFD');
+
+  // Now remove all the combining characters, resulting in a string without diacritics.
+  return normalized.replace(/\p{Mn}/gu, '');
+}
+
 export default isWhitespaces;
