21e51c7206574c6ea0611063fc9f3e0ecf0c5ee3
Karthik B <karthik@terraformation.com>
2023-02-08 12:07:50 -0800

    SW-2917 make plants dashboard work (#1165)
    - use number parser
    - use number formatter
    - create chart only when all data is available.. to avoid constructing charts in parallel

--- src/components/Plants/TotalCount.tsx
+++ src/components/Plants/TotalCount.tsx
@@ -1,6 +1,9 @@
+import { useMemo } from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
 import strings from 'src/strings';
 import { cardTitleStyle } from './PlantingSiteDetails';
+import { useUser } from 'src/providers';
+import { useNumberFormatter } from 'src/utils/useNumber';
 
 type TotalCountProps = {
   totalCount?: number;
@@ -8,14 +11,19 @@ type TotalCountProps = {
 
 export default function TotalCount(props: TotalCountProps): JSX.Element {
   const theme = useTheme();
+  const { user } = useUser();
+  const numberFormatter = useNumberFormatter();
+
   const { totalCount } = props;
+
+  const numericFormatter = useMemo(() => numberFormatter(user?.locale), [numberFormatter, user?.locale]);
 
   return (
     <>
       <Typography sx={cardTitleStyle}>{strings.TOTAL_NUMBER_OF_PLANTS}</Typography>
       <Box sx={{ marginTop: theme.spacing(3) }}>
         <Typography fontSize='32px' fontWeight={600}>
-          {totalCount || 0}
+          {numericFormatter.format(totalCount || 0)}
         </Typography>
       </Box>
     </>
