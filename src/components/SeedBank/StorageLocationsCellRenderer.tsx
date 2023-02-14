413ef5076a6cd8b7954bd05e69014a83081c71c5
Karthik Balachandran <karthik@terraformation.com>
2023-02-14 06:48:36 -0800

    make number of accessions as link

--- src/components/SeedBank/StorageLocationsCellRenderer.tsx
+++ src/components/SeedBank/StorageLocationsCellRenderer.tsx
@@ -15,9 +15,9 @@ export default function StorageLocationsCellRenderer({
   numericFormatter,
 }: StorageLocationsCellRendererProps) {
   return (props: RendererProps<TableRowType>): JSX.Element => {
-    const { column, value } = props;
+    const { column, value, row } = props;
 
-    const createLinkToAccessions = (locationName: string) => {
+    const createLinkToAccessions = (locationName: string, numAccessions: string) => {
       const to = [
         `${APP_PATHS.ACCESSIONS}/?`,
         `storageLocationName=${locationName}`,
@@ -25,15 +25,16 @@ export default function StorageLocationsCellRenderer({
         ...ActiveStatuses().map((status) => `stage=${status}`),
       ].join('&');
 
-      return <Link to={to}>{locationName}</Link>;
+      return <Link to={to}>{numAccessions}</Link>;
     };
 
-    if (column.key === 'name') {
-      return <CellRenderer {...props} value={createLinkToAccessions(value as string)} />;
-    }
-
     if (column.key === 'activeAccessions') {
-      return <CellRenderer {...props} value={numericFormatter.format(value as number)} />;
+      return (
+        <CellRenderer
+          {...props}
+          value={createLinkToAccessions(row.name as string, numericFormatter.format(value as number))}
+        />
+      );
     }
 
     return <CellRenderer {...props} value={value} />;
