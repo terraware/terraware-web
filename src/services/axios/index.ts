f001028b52d256df36de83fba32370463c48fbb8
Karthik Balachandran <karthik@terraformation.com>
2023-02-15 17:12:04 -0800

    use error match

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -28,7 +28,7 @@ axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      const href = location.href.endsWith('/error/cannot-fecth-org-data') ? location.origin : location.href;
+      const href = location.href.match('/error') ? location.origin : location.href;
       const redirect = encodeURIComponent(href);
       location.href = `/api/v1/login?redirect=${redirect}`;
     }
