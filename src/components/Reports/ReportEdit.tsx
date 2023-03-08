9fb548aa64a2dac7076dcc3fe066b57a05db9605
constanzauanini <constanza.uanini@admios-sa.com>
2023-03-08 12:50:25 -0300

    feedback

--- src/components/Reports/ReportEdit.tsx
+++ src/components/Reports/ReportEdit.tsx
@@ -116,6 +116,8 @@ export default function ReportEdit({ organization }: ReportEditProps): JSX.Eleme
       setShowAnnual(true);
       if (!saveResult.requestSucceeded) {
         snackbar.toastError(strings.GENERIC_ERROR, strings.REPORT_COULD_NOT_SAVE);
+      } else {
+        await ReportService.uploadReportPhotos(report.id, photos);
       }
     }
   };
