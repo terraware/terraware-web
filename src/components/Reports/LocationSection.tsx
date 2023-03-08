b945fb3f72887ee24cdc36ddb92e437f18739bc1
constanzauanini <constanza.uanini@admios-sa.com>
2023-03-08 09:23:30 -0300

    SW-3090 Adding planting sites section to report (#1274)

--- src/components/Reports/LocationSection.tsx
+++ src/components/Reports/LocationSection.tsx
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
 import { Grid, Theme, Typography, useTheme } from '@mui/material';
-import { DatePicker, Textfield } from '@terraware/web-components';
+import { DatePicker, TableColumnType, Textfield } from '@terraware/web-components';
 import strings from 'src/strings';
 import OverviewItemCard from 'src/components/common/OverviewItemCard';
 import useDebounce from 'src/utils/useDebounce';
 import { makeStyles } from '@mui/styles';
-import { ReportNursery, ReportSeedBank } from 'src/types/Report';
+import { ReportNursery, ReportPlantingSite, ReportSeedBank } from 'src/types/Report';
+import PlantingSiteSpeciesCellRenderer from './PlantingSitesSpeciesCellRenderer';
+import Table from '../common/table';
+import { useOrganization } from 'src/providers';
+import { SpeciesService } from 'src/services';
+import { Species } from 'src/types/Species';
+
+type PlantingSiteSpecies = {
+  id: number;
+  name: string;
+  growthForm?: string;
+  mortalityRateInField?: number | undefined;
+  mortalityRateInNursery?: number | undefined;
+  totalPlanted?: number | undefined;
+};
 
 const DEBOUNCE_TIME_MS = 500;
 
 export type LocationSectionProps = {
   editable: boolean;
-  location: ReportSeedBank | ReportNursery;
+  location: ReportSeedBank | ReportNursery | ReportPlantingSite;
   onUpdateLocation: (field: string, value: any) => void;
   onUpdateWorkers: (workersField: string, value: any) => void;
-  locationType: 'seedBank' | 'nursery';
+  locationType: 'seedBank' | 'nursery' | 'plantingSite';
 };
 
 export default function LocationSection(props: LocationSectionProps): JSX.Element {
@@ -23,6 +37,11 @@ export default function LocationSection(props: LocationSectionProps): JSX.Elemen
   const { isMobile } = useDeviceInfo();
   const theme = useTheme();
   const classes = useStyles();
+  const { selectedOrganization } = useOrganization();
+
+  const isSeedBank = locationType === 'seedBank';
+  const isNursery = locationType === 'nursery';
+  const isPlantingSite = locationType === 'plantingSite';
 
   const [workersPaidEngaged, setWorkersPaidEngaged] = useState<number>(location.workers?.paidWorkers ?? 0);
   const [workersPaidFemale, setWorkersPaidFemale] = useState<number>(location.workers?.femalePaidWorkers ?? 0);
@@ -45,51 +64,139 @@ export default function LocationSection(props: LocationSectionProps): JSX.Elemen
   const smallItemGridWidth = () => (isMobile ? 12 : 4);
   const mediumItemGridWidth = () => (isMobile ? 12 : 8);
 
+  const getNotesLabel = () => {
+    if (isSeedBank) {
+      return strings.SEED_BANK_NOTES;
+    }
+    if (isNursery) {
+      return strings.ADDITIONAL_NURSERY_NOTES;
+    }
+    return strings.ADDITIONAL_PLANTING_SITES_NOTES;
+  };
+
+  const [allSpecies, setAllSpecies] = useState<Species[]>();
+  const [plantingSiteSpecies, setPlantingSiteSpecies] = useState<PlantingSiteSpecies[]>([]);
+
+  useEffect(() => {
+    const populateSpecies = async () => {
+      const response = await SpeciesService.getAllSpecies(selectedOrganization.id);
+      if (response.requestSucceeded) {
+        setAllSpecies(response.species);
+      }
+    };
+    if (isPlantingSite) {
+      populateSpecies();
+    }
+  }, [isPlantingSite, selectedOrganization.id, location]);
+
+  useEffect(() => {
+    if (allSpecies && isPlantingSite) {
+      const psSpecies: PlantingSiteSpecies[] = [];
+      (location as ReportPlantingSite).species.forEach((iSpecies) => {
+        const foundSpecies = allSpecies.find((serverSpecies) => serverSpecies.id === iSpecies.id);
+        if (foundSpecies) {
+          psSpecies.push({
+            id: iSpecies.id,
+            name: foundSpecies.scientificName,
+            growthForm: foundSpecies.growthForm,
+            mortalityRateInField: iSpecies.mortalityRateInField,
+            mortalityRateInNursery: iSpecies.mortalityRateInNursery,
+            totalPlanted: iSpecies.totalPlanted,
+          });
+        }
+      });
+      setPlantingSiteSpecies(psSpecies);
+    }
+  }, [allSpecies, isPlantingSite, location]);
+
+  const columns: TableColumnType[] = [
+    {
+      key: 'name',
+      name: strings.SPECIES,
+      type: 'string',
+    },
+    {
+      key: 'growthForm',
+      name: strings.GROWTH_FORM,
+      type: 'string',
+    },
+    {
+      key: 'totalPlanted',
+      name: strings.TOTAL_PLANTED,
+      type: 'string',
+    },
+    { key: 'mortalityRateInField', name: strings.MORTALITY_RATE_IN_FIELD, type: 'string' },
+    { key: 'mortalityRateInNursery', name: strings.MORTALITY_RATE_IN_NURSERY, type: 'string' },
+  ];
+
+  const onEditPlantingSiteReport = (species: PlantingSiteSpecies, fromColumn?: string, value?: any) => {
+    if (fromColumn && value) {
+      const speciesToEditIndex = (location as ReportPlantingSite).species.findIndex(
+        (iSpecies) => iSpecies.id === species.id
+      );
+      const speciesToEdit = (location as ReportPlantingSite).species[speciesToEditIndex];
+
+      const newSpecies = [...(location as ReportPlantingSite).species];
+      const speciesModified = {
+        ...speciesToEdit,
+        [fromColumn as 'mortalityRateInField' | 'mortalityRateInNursery' | 'totalPlanted']: value,
+      };
+
+      newSpecies[speciesToEditIndex] = speciesModified;
+
+      onUpdateLocation('species', newSpecies);
+    }
+  };
+
   return (
     <>
-      <Grid item xs={smallItemGridWidth()}>
-        <InfoField
-          id={`${location.id}-${locationType}-buildStartDate`}
-          label={
-            locationType === 'seedBank'
-              ? strings.REPORT_SEEDBANK_BUILD_START_DATE
-              : strings.REPORT_NURSERY_BUILD_START_DATE
-          }
-          editable={editable && location.buildStartedDateEditable}
-          value={location.buildStartedDate ?? ''}
-          onChange={(value) => onUpdateLocation('buildStartedDate', value)}
-          type='date'
-        />
-      </Grid>
-      <Grid item xs={smallItemGridWidth()}>
-        <InfoField
-          id={`${location.id}-${locationType}-buildCompletedDate`}
-          label={
-            locationType === 'seedBank'
-              ? strings.REPORT_SEEDBANK_BUILD_COMPLETION_DATE
-              : strings.REPORT_NURSERY_BUILD_COMPLETION_DATE
-          }
-          editable={editable && location.buildCompletedDateEditable}
-          value={location.buildCompletedDate ?? ''}
-          onChange={(value) => onUpdateLocation('buildCompletedDate', value)}
-          type='date'
-        />
-      </Grid>
-      <Grid item xs={smallItemGridWidth()}>
-        <InfoField
-          id={`${location.id}-${locationType}-operationStartDate`}
-          label={
-            locationType === 'seedBank'
-              ? strings.REPORT_SEEDBANK_OPERATION_START_DATE
-              : strings.REPORT_NURSERY_OPERATION_START_DATE
-          }
-          editable={editable && location.operationStartedDateEditable}
-          value={location.operationStartedDate ?? ''}
-          onChange={(value) => onUpdateLocation('operationStartedDate', value)}
-          type='date'
-        />
-      </Grid>
-      {locationType === 'seedBank' ? (
+      {(isSeedBank || isNursery) && (
+        <>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-${locationType}-buildStartDate`}
+              label={
+                locationType === 'seedBank'
+                  ? strings.REPORT_SEEDBANK_BUILD_START_DATE
+                  : strings.REPORT_NURSERY_BUILD_START_DATE
+              }
+              editable={editable && (location as ReportSeedBank | ReportNursery).buildStartedDateEditable}
+              value={(location as ReportSeedBank | ReportNursery).buildStartedDate ?? ''}
+              onChange={(value) => onUpdateLocation('buildStartedDate', value)}
+              type='date'
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-${locationType}-buildCompletedDate`}
+              label={
+                locationType === 'seedBank'
+                  ? strings.REPORT_SEEDBANK_BUILD_COMPLETION_DATE
+                  : strings.REPORT_NURSERY_BUILD_COMPLETION_DATE
+              }
+              editable={editable && (location as ReportSeedBank | ReportNursery).buildCompletedDateEditable}
+              value={(location as ReportSeedBank | ReportNursery).buildCompletedDate ?? ''}
+              onChange={(value) => onUpdateLocation('buildCompletedDate', value)}
+              type='date'
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-${locationType}-operationStartDate`}
+              label={
+                locationType === 'seedBank'
+                  ? strings.REPORT_SEEDBANK_OPERATION_START_DATE
+                  : strings.REPORT_NURSERY_OPERATION_START_DATE
+              }
+              editable={editable && (location as ReportSeedBank | ReportNursery).operationStartedDateEditable}
+              value={(location as ReportSeedBank | ReportNursery).operationStartedDate ?? ''}
+              onChange={(value) => onUpdateLocation('operationStartedDate', value)}
+              type='date'
+            />
+          </Grid>
+        </>
+      )}
+      {isSeedBank && (
         <Grid item xs={12}>
           <OverviewItemCard
             isEditable={false}
@@ -98,7 +205,8 @@ export default function LocationSection(props: LocationSectionProps): JSX.Elemen
             className={classes.infoCardStyle}
           />
         </Grid>
-      ) : (
+      )}
+      {isNursery && (
         <>
           <Grid item xs={smallItemGridWidth()}>
             <InfoField
@@ -126,6 +234,78 @@ export default function LocationSection(props: LocationSectionProps): JSX.Elemen
               className={classes.infoCardStyle}
             />
           </Grid>
+        </>
+      )}
+      {isPlantingSite && (
+        <>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-planting-site-area`}
+              label={strings.TOTAL_PLANTING_SITE_AREA_HA}
+              value={(location as ReportPlantingSite).totalPlantingSiteArea ?? ''}
+              editable={editable}
+              onChange={(value) => onUpdateLocation('totalPlantingSiteArea', value)}
+              type='text'
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-total-planted-area`}
+              label={strings.TOTAL_PLANTED_AREA_HA}
+              value={(location as ReportPlantingSite).totalPlantedArea ?? ''}
+              editable={editable}
+              onChange={(value) => onUpdateLocation('totalPlantedArea', value)}
+              type='text'
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()} />
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-total-trees-planted`}
+              label={strings.TOTAL_TREES_PLANTED}
+              value={(location as ReportPlantingSite).totalTreesPlanted ?? ''}
+              editable={editable}
+              onChange={(value) => onUpdateLocation('totalTreesPlanted', value)}
+              type='text'
+              helper={strings.TOTAL_TREES_PLANTED_HELPER_TEXT}
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-total-plants-planted`}
+              label={strings.TOTAL_PLANTS_PLANTED}
+              value={(location as ReportPlantingSite).totalPlantsPlanted ?? ''}
+              editable={editable}
+              onChange={(value) => onUpdateLocation('totalPlantsPlanted', value)}
+              type='text'
+              helper={strings.TOTAL_PLANTS_PLANTED_HELPER_TEXT}
+            />
+          </Grid>
+          <Grid item xs={smallItemGridWidth()}>
+            <InfoField
+              id={`${location.id}-mortality-rate`}
+              label={strings.MORTALITY_RATE}
+              value={(location as ReportPlantingSite).mortalityRate ?? ''}
+              editable={editable}
+              onChange={(value) => onUpdateLocation('mortalityRate', value)}
+              type='text'
+            />
+          </Grid>
+          {plantingSiteSpecies && (
+            <Grid item xs={12} marginBottom={3}>
+              <Table
+                id='species-table'
+                columns={columns}
+                rows={plantingSiteSpecies}
+                Renderer={PlantingSiteSpeciesCellRenderer({ editMode: editable })}
+                showPagination={false}
+                onSelect={onEditPlantingSiteReport}
+                controlledOnSelect={true}
+                orderBy={'species'}
+                isClickable={() => false}
+              />
+            </Grid>
+          )}
         </>
       )}
       <Grid item xs={smallItemGridWidth()}>
@@ -160,7 +340,7 @@ export default function LocationSection(props: LocationSectionProps): JSX.Elemen
       </Grid>
       <Grid item xs={mediumItemGridWidth()}>
         <Textfield
-          label={locationType === 'seedBank' ? strings.SEED_BANK_NOTES : strings.ADDITIONAL_NURSERY_NOTES}
+          label={getNotesLabel()}
           id={`${location.id}-notes`}
           type='textarea'
           disabled={!editable}
@@ -193,14 +373,23 @@ type InfoFieldProps = {
   value: string | number;
   onChange: (value: any) => void;
   type: 'text' | 'date';
+  helper?: string;
 };
 
 function InfoField(props: InfoFieldProps): JSX.Element {
-  const { id, label, editable, value, onChange, type } = props;
+  const { id, label, editable, value, onChange, type, helper } = props;
   const classes = useStyles();
   return editable ? (
     type === 'text' ? (
-      <Textfield label={label} id={id} type='number' value={value} readonly={!editable} onChange={onChange} />
+      <Textfield
+        label={label}
+        id={id}
+        type='number'
+        value={value}
+        readonly={!editable}
+        onChange={onChange}
+        helperText={helper}
+      />
     ) : type === 'date' ? (
       <DatePicker id={id} label={label} value={value as string} onChange={onChange} aria-label='date-picker' />
     ) : (
