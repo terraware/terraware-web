823dea8566c7bff96057393b251e404b5d278ef8
Charlie Keith <charlie@terraformation.com>
2023-03-10 16:45:46 -0700

    ignore uncaught exception

--- cypress/e2e/10-database.cy.js
+++ cypress/e2e/10-database.cy.js
@@ -1,5 +1,8 @@
 describe('Database', () => {
   it('visits accessions before running tests to clear network problems', () => {
+    cy.on('uncaught:exception', () => {
+      return false;
+    })
     cy.visit('/accessions');
     cy.wait(5000);
   });
