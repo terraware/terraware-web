523c6bef672b1347ce484224fb4f49ff5fccd68e
Steven Grimm <steven@terraformation.com>
2023-04-05 13:39:49 -0700

    Add Spanish to list of locales
    Allow users to select Spanish in the language selector.

--- src/strings/locales.ts
+++ src/strings/locales.ts
@@ -3,5 +3,6 @@ import { LocaleDetails } from '.';
 /** Supported locales in the order they should appear in the locale selector. */
 export const supportedLocales: LocaleDetails[] = [
   { id: 'en', name: 'English', loadModule: () => import('./strings-en') },
+  { id: 'es', name: 'Español', loadModule: () => import('./strings-es') },
   { id: 'gx', name: 'Gibberish', loadModule: () => import('./strings-gx') },
 ];
