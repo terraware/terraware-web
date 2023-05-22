e284206119cf315e88569e00df159560925cccc8
Charlie Keith <charlie@terraformation.com>
2023-05-22 13:02:36 -0600

    SW-3634 MapLegend Component

---
+++ src/stories/MapLegend.stories.tsx
@@ -0,0 +1,37 @@
+import MapLegend from 'src/components/Observations/MapLegend';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'MapLegend',
+  component: MapLegend,
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+} satisfies Meta<typeof MapLegend>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    title: 'Boundaries',
+    items: [
+      {
+        fillColor: '#009900',
+        borderColor: '#005500',
+        label: 'Forest',
+      },
+      {
+        fillColor: '#000099',
+        borderColor: '#000055',
+        label: 'Water',
+      },
+      {
+        fillColor: '#00ff00',
+        borderColor: '#000000',
+        label: 'Radioactive Waste',
+      },
+    ],
+  },
+};
