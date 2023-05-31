cc596ab545b7414a139c69bfc73736159266072f
Karthik B <karthik@terraformation.com>
2023-05-31 13:21:27 -0700

    SW-3667 list/map selector component with a grouped view (#1426)
    - selector to switch between list/map
    - a search/selector/list/map grouped view/pane (used in observations
    page and planting site details page)
    - added stories
    
    ### list/map view
    
    <img width="898" alt="ListMapView - Default ⋅ Storybook 2023-05-31
    10-08-46"
    src="https://github.com/terraware/terraware-web/assets/1865174/da3942e8-6b55-4f37-aeb2-ff6f6c41235e">
    
    <img width="181" alt="ListMapView - Default ⋅ Storybook 2023-05-31
    10-08-25"
    src="https://github.com/terraware/terraware-web/assets/1865174/2cbed99e-2042-4d77-8794-c089d38a7f5f">
    
    ### list/map selector
    
    <img width="312" alt="ListMapSelector - Default ⋅ Storybook 2023-05-31
    09-47-35"
    src="https://github.com/terraware/terraware-web/assets/1865174/649c7f92-bfed-4d82-a372-1d69fe204ab8">
    
    <img width="432" alt="ListMapSelector - Default ⋅ Storybook 2023-05-31
    09-47-06"
    src="https://github.com/terraware/terraware-web/assets/1865174/4edbdb34-ab15-42d4-a1fd-0a1777a1b489">

---
+++ src/components/ListMapView/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { Box, useTheme } from '@mui/material';
+import Card from 'src/components/common/Card';
+import ListMapSelector, { View } from 'src/components/common/ListMapSelector';
+
+/**
+ * Props include an optional search component for the top left.
+ * List and map components are optional, absence of either will
+ * disable corresponding selector.
+ */
+export type ListMapViewProps = {
+  search: React.ReactNode;
+  list: React.ReactNode;
+  map: React.ReactNode;
+  initialView: View;
+  onView?: (view: View) => void;
+};
+
+export default function ListMapView({ search, list, map, onView, initialView }: ListMapViewProps): JSX.Element {
+  const [view, setView] = useState<View>(initialView);
+  const theme = useTheme();
+
+  const updateView = (nextView: View) => {
+    setView(nextView);
+    if (onView) {
+      onView(nextView);
+    }
+  };
+
+  return (
+    <Card
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+      }}
+    >
+      <Box display='flex' flexDirection='row' justifyContent='space-between' alignItems='center'>
+        {search}
+        <ListMapSelector view={view} onView={updateView} />
+      </Box>
+      <Box display='flex' flexGrow={1} marginTop={theme.spacing(2)}>
+        <Box display={view === 'list' ? 'flex' : 'none'}>{list}</Box>
+        <Box display={view === 'map' ? 'flex' : 'none'}>{map}</Box>
+      </Box>
+    </Card>
+  );
+}
---
+++ src/components/common/ListMapSelector.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Box, Theme, Typography, useTheme } from '@mui/material';
+import hexRgb from 'hex-rgb';
+import strings from 'src/strings';
+import useDeviceInfo from 'src/utils/useDeviceInfo';
+import Icon from './icon/Icon';
+import { IconName } from './icon/icons';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  icon: {
+    '& path': {
+      fill: theme.palette.TwClrBaseGray500,
+    },
+  },
+  box: {
+    display: 'inline-flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+  },
+}));
+
+export type View = 'list' | 'map';
+
+export type ListMapSelectorProps = {
+  view: View;
+  onView: (view: View) => void;
+};
+
+export default function ListMapSelector({ view, onView }: ListMapSelectorProps): JSX.Element {
+  const classes = useStyles();
+  const { isMobile } = useDeviceInfo();
+  const theme = useTheme();
+
+  const renderSelector = (iconName: IconName, title: string, selectorView: View) => {
+    const isSelected = view === selectorView;
+    return (
+      <Box
+        className={classes.box}
+        width={isMobile ? '32px' : 'auto'}
+        minWidth={isMobile ? '32px' : '67px'}
+        borderRadius={theme.spacing(0.75)}
+        padding={theme.spacing(0.5)}
+        gap={theme.spacing(0.75)}
+        sx={{
+          background: isSelected ? theme.palette.TwClrBaseWhite : 'transparent',
+          boxShadow: isSelected
+            ? `0.0px 2.0px 4.0px 0px ${hexRgb(`${theme.palette.TwClrBaseGray800}`, { alpha: 0.2, format: 'css' })}`
+            : 'none',
+          cursor: 'pointer',
+        }}
+        onClick={() => onView(selectorView)}
+      >
+        <Icon name={iconName} size='medium' className={classes.icon} />
+        {!isMobile && (
+          <Typography
+            fontSize='16px'
+            fontWeight={400}
+            lineHeight='24px'
+            textAlign='center'
+            color={theme.palette.TwClrTxt}
+          >
+            {title}
+          </Typography>
+        )}
+      </Box>
+    );
+  };
+
+  return (
+    <Box
+      className={classes.box}
+      justifyContent='space-between'
+      minWidth={isMobile ? '76px' : '152px'}
+      borderRadius={theme.spacing(1)}
+      padding={theme.spacing(0.5)}
+      gap={theme.spacing(0.5)}
+      style={{ backgroundColor: theme.palette.TwClrBgSecondary }}
+    >
+      {renderSelector('iconList', strings.LIST, 'list')}
+      {renderSelector('iconTreasureMap', strings.MAP, 'map')}
+    </Box>
+  );
+}
