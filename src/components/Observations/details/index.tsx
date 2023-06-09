235c2350c2d49dfcf6058148cae3efe532cee3f8
Karthik Balachandran <karthik@terraformation.com>
2023-06-09 14:04:49 -0700

    - fix check

--- src/components/Observations/details/index.tsx
+++ src/components/Observations/details/index.tsx
@@ -58,7 +58,7 @@ export default function ObservationDetails({ search, onSearch }: ObservationDeta
     <DetailsPage title={title} plantingSiteId={plantingSiteId}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
-          <AggregatedPlantsStats {...(details || {})} />
+          <AggregatedPlantsStats {...(details ?? {})} />
         </Grid>
         <Grid item xs={12}>
           <Card style={isMobile ? { borderRadius: 0, marginLeft: -3, marginRight: -3 } : {}}>
