8b91490a06e7a97b1a7eefe87a7588fb9024932f
Karthik B <karthik@terraformation.com>
2023-06-13 13:53:13 -0700

    Display monitoring plot observation photos (#1458)
    - created a generic PhotosList component (to be reused in other places)
    - used PhotosList to render photos for monitoring plots
    - added photos in staging, switch to dev org and go here
    /observations/56/3/106/270028
    
    <img width="190" alt="Terraware App 2023-06-13 11-38-26"
    src="https://github.com/terraware/terraware-web/assets/1865174/d8d8f5e0-1e2e-4d68-b868-15a41586f3ac">
    
    <img width="751" alt="Terraware App 2023-06-13 11-33-30"
    src="https://github.com/terraware/terraware-web/assets/1865174/0963555b-35e0-4afa-8a97-5674e299cefa">
    
    <img width="557" alt="Terraware App 2023-06-13 11-49-04"
    src="https://github.com/terraware/terraware-web/assets/1865174/4dab1972-f0d9-4c4d-a073-5d0170a663fb">

---
+++ src/components/Observations/plot/MonitoringPlotPhotos.tsx
@@ -0,0 +1,30 @@
+import { useMemo } from 'react';
+import { ObservationMonitoringPlotPhoto } from 'src/types/Observations';
+import PhotosList from 'src/components/common/PhotosList';
+
+const PHOTO_URL = '/api/v1/tracking/observations/{observationId}/plots/{monitoringPlotId}/photos/{fileId}';
+
+export type MonitoringPlotPhotosProps = {
+  observationId: number;
+  monitoringPlotId: number;
+  photos?: ObservationMonitoringPlotPhoto[];
+};
+
+export default function MonitoringPlotPhotos({
+  observationId,
+  monitoringPlotId,
+  photos,
+}: MonitoringPlotPhotosProps): JSX.Element {
+  const photoUrls = useMemo(() => {
+    const rootUrl = PHOTO_URL.replace('{observationId}', observationId.toString()).replace(
+      '{monitoringPlotId}',
+      monitoringPlotId.toString()
+    );
+
+    return (photos ?? []).map((photo: ObservationMonitoringPlotPhoto) =>
+      rootUrl.replace('{fileId}', photo.fileId.toString())
+    );
+  }, [observationId, monitoringPlotId, photos]);
+
+  return <PhotosList photoUrls={photoUrls} />;
+}
