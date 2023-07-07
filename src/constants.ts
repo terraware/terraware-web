e403d214001558788a9ef2a130521dad3df8d5c0
Karthik Balachandran <karthik@terraformation.com>
2023-07-07 10:36:54 -0700

    SW-3869 Refactor routing for planting sites views to support drill downs
    - similar to observations routing, refactor planting sites routing
    - App.tsx now calls the root planting sites component for any route starting with `/planting-sites`
    - the root planting sites component handles the various routing for planting sites view, edit, create, drill downs
    - this also simplifies App.tsx context
    - added placeholder content for zone/subzone views

--- src/constants.ts
+++ src/constants.ts
@@ -56,6 +56,8 @@ export enum APP_PATHS {
   PLANTING_SITES_NEW = '/planting-sites/new',
   PLANTING_SITES_VIEW = '/planting-sites/:plantingSiteId',
   PLANTING_SITES_EDIT = '/planting-sites/:plantingSiteId/edit',
+  PLANTING_SITES_ZONE_VIEW = '/planting-sites/:plantingSiteId/zone/:zoneId',
+  PLANTING_SITES_SUBZONE_VIEW = '/planting-sites/:plantingSiteId/zone/:zoneId/subzone/:subzoneId',
   NURSERY_WITHDRAWALS = '/nursery/withdrawals',
   NURSERY_WITHDRAWALS_DETAILS = '/nursery/withdrawals/:withdrawalId',
   NURSERY_REASSIGNMENT = '/nursery/reassignment/:deliveryId',
