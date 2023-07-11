fe2d9332c56f3dfce92840e80b66d234e9a5e7a2
Karthik Balachandran <karthik@terraformation.com>
2023-07-11 13:20:26 -0700

    - add more comments

--- src/redux/features/observations/plantingSiteDetailsSelectors.ts
+++ src/redux/features/observations/plantingSiteDetailsSelectors.ts
@@ -31,17 +31,29 @@ export const selectPlantingSiteZones = createSelector(
      */
 
     const zones: Record<number, ObservationsAggregation> = {};
+    /**
+     * Iterate through all the planting zones in the observations results to book-keep subzones and monitoring plots for the zone
+     */
     observationsResults
       ?.flatMap((result) => result.plantingZones)
       .forEach((zone) => {
         const { plantingZoneId, completedTime, plantingSubzones } = zone;
         if (!zones[plantingZoneId]) {
+          // initialize book-keeping data for zone
           zones[plantingZoneId] = { subzones: {}, plots: {}, completedTime };
         }
+        /**
+         * Iterate through planting zones and maintain a running set of all monitoring plots for a planting sub zone (across observations results)
+         * Ex. if result1 has subzoneA->[monitoring-plot-1, monitoring-plot-2] and result2 has subzoneA->[monitoring-plot-2, monitoring-plot-3],
+         *     we want to generate a complete set of subzoneA->[monitoring-plot-1, monitoring-plot-2, monitoring-plot-3]
+         *     In addition, we maintain a map of plotId->monitoring-plot-data (geometry, plot type, completed time, etc.), this is used to eventually generate
+         *     a mapping of subzoneA->[monitoring plots data]
+         */
         plantingSubzones.forEach((subzone) => {
           const { plantingSubzoneId, monitoringPlots } = subzone;
           const monitoringPlotIds = monitoringPlots.map((plot) => plot.monitoringPlotId);
           const lastSubzones = zones[plantingZoneId].subzones[plantingSubzoneId];
+          // book keep subzoneId->[set of monitoring plot ids]
           if (!lastSubzones) {
             zones[plantingZoneId].subzones[plantingSubzoneId] = new Set(monitoringPlotIds);
           } else {
@@ -50,6 +62,7 @@ export const selectPlantingSiteZones = createSelector(
               ...monitoringPlotIds,
             ]);
           }
+          // populate map of monitoringPlotId->monitoringPlotData (keep the one with latest observed time)
           monitoringPlots.forEach((monitoringPlot) => {
             const { monitoringPlotId } = monitoringPlot;
             const lastUpdated = zones[plantingZoneId].plots[monitoringPlotId];
@@ -59,21 +72,31 @@ export const selectPlantingSiteZones = createSelector(
             zones[plantingZoneId].plots[monitoringPlotId] = monitoringPlot;
           });
         });
+        // keep the latest observed time for the zone
         const lastCompletedTime = zones[plantingZoneId].completedTime;
         if (lastCompletedTime !== completedTime && isAfter(completedTime, lastCompletedTime)) {
           zones[plantingZoneId].completedTime = completedTime;
         }
       });
 
+    /**
+     * Here we repopulate the planting site zones with additional aggregated information
+     * Additional data: 
+     *  zone-> completedTime, plantingCompleted (in addition to existing fields in PlantingZone)
+     *  subzone-> union of monitoring plots across observation results (in addition to existing fields in PlantingSubzone)
     return (
       plantingSite?.plantingZones?.map(
         (zone): ZoneObservationsAggregation => ({
           ...zone,
+          // get completed time from book-keeping
           completedTime: zones[zone.id]?.completedTime,
+          // roll up planting completed from sub zones
           plantingCompleted:
             zone.plantingSubzones.length > 0 && !zone.plantingSubzones.some((sz) => !sz.plantingCompleted),
+          // repopulate subzones with additional monitoring plots data
           plantingSubzones: zone.plantingSubzones.map((sz) => ({
             ...sz,
+            // generate monitoring plots from book-keeping data
             monitoringPlots: zone.plantingSubzones
               .filter((plantingSubzone) => !!zones[zone.id]?.subzones[plantingSubzone.id])
               .reduce((acc, plantingSubzone) => {
