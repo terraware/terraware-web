0055fa1c73800f3d4df15403f995920869cd10cb
Karthik B <karthik@terraformation.com>
2023-07-13 15:31:32 -0700

    SW-3920 Separate Tracking v2/v2 code paths for  Nursery Withdrawals (#1556)
    - added an initial empty placeholder component
    - added an intermediate component that routes to old vs new components

---
+++ src/components/NurseryWithdrawals/NurseryPlantingsAndWithdrawals.tsx
@@ -0,0 +1,15 @@
+/**
+ * Nursery plantings and withdrawals
+ */
+import NurseryWithdrawals from './NurseryWithdrawals';
+
+export default function NurseryPlantingsAndWithdrawals(): JSX.Element {
+  /**
+   * TODO: Add tabs, etc.
+   */
+  return (
+    <>
+      <NurseryWithdrawals />;
+    </>
+  );
+}
--- src/components/NurseryWithdrawals/index.tsx
+++ src/components/NurseryWithdrawals/index.tsx
@@ -1,5 +1,20 @@
+import isEnabled from 'src/features';
 import NurseryReassignment from './NurseryReassignment';
-import NurseryWithdrawals from './NurseryWithdrawals';
+import NurseryWithdrawalsBase from './NurseryWithdrawals';
+import NurseryPlantingsAndWithdrawals from './NurseryPlantingsAndWithdrawals';
 import NurseryWithdrawalsDetails from './NurseryWithdrawalsDetails';
+
+/**
+ * Primary route management for nursery withdrawals.
+ */
+export default function NurseryWithdrawals(): JSX.Element {
+  const trackingV2 = isEnabled('TrackingV2');
+
+  if (trackingV2) {
+    return <NurseryPlantingsAndWithdrawals />;
+  }
+
+  return <NurseryWithdrawalsBase />;
+}
 
 export { NurseryReassignment, NurseryWithdrawals, NurseryWithdrawalsDetails };
