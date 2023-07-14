fab9104c60fa79ebc6af044b9219d67c68da7c14
constanzauanini <constanza.uanini@admios-sa.com>
2023-07-14 18:00:07 -0300

    Feedback

--- src/redux/features/Plantings/plantingsSelectors.ts
+++ src/redux/features/Plantings/plantingsSelectors.ts
@@ -1,18 +1,20 @@
 import { RootState } from 'src/redux/rootReducer';
 import { selectPlantingSites } from '../tracking/trackingSelectors';
 import { createSelector } from '@reduxjs/toolkit';
+import { PlantingSearchData } from './plantingsSlice';
 
 export const selectPlantings = (state: RootState) => state.plantings?.plantings;
 
-export const selectTotalPlantsBySubzone = (state: RootState) => {
+export const getTotalPlantsBySubzone = (plantings: PlantingSearchData[]) => {
   const plantingsBySubzone: Record<number, { totalPlants: number }> = {};
-  state.plantings?.plantings?.forEach((planting) => {
-    if (plantingsBySubzone[Number(planting.plantingSubzone.id)]) {
-      plantingsBySubzone[Number(planting.plantingSubzone.id)] = {
-        totalPlants: plantingsBySubzone[Number(planting.plantingSubzone.id)].totalPlants + planting.totalPlants,
+  plantings?.forEach((planting) => {
+    const subzoneId = Number(planting.plantingSubzone.id);
+    if (plantingsBySubzone[subzoneId]) {
+      plantingsBySubzone[subzoneId] = {
+        totalPlants: plantingsBySubzone[subzoneId].totalPlants + planting.totalPlants,
       };
     } else {
-      plantingsBySubzone[Number(planting.plantingSubzone.id)] = { totalPlants: planting.totalPlants };
+      plantingsBySubzone[subzoneId] = { totalPlants: planting.totalPlants };
     }
   });
   return plantingsBySubzone;
@@ -27,21 +29,39 @@ export const selectPlantingsDateRange = (state: RootState, dateRange: string[]) 
   }) ?? [];
 
 export const selectPlantingProgressSubzones = createSelector(
-  [(state: RootState) => selectPlantingSites(state), (state: RootState) => selectTotalPlantsBySubzone(state)],
-  (plantingSites, plantingsBySubzone) => {
-    plantingSites?.flatMap((ps) => {
-      return ps.plantingZones?.flatMap((zone) => {
-        return zone.plantingSubzones.map((sz) => {
-          return {
-            subzoneName: sz.fullName,
-            plantingComplete: sz.plantingCompleted,
-            plantingSite: ps.name,
-            zone: zone.name,
-            targetPlantingDensity: zone.targetPlantingDensity,
-            totalSeedlingsSent: plantingsBySubzone[sz.id].totalPlants,
-          };
-        });
+  [(state: RootState) => selectPlantingSites(state), (state: RootState) => selectPlantings(state)],
+  (plantingSites, plantings) => {
+    if (plantings) {
+      const plantingsBySubzone = getTotalPlantsBySubzone(plantings);
+      return plantingSites?.map((ps) => {
+        return {
+          siteId: ps.id,
+          siteName: ps.name,
+          reported: ps.plantingZones?.flatMap((zone) => {
+            return zone.plantingSubzones.map((sz) => {
+              return {
+                subzoneName: sz.fullName,
+                plantingComplete: sz.plantingCompleted,
+                plantingSite: ps.name,
+                zone: zone.name,
+                targetPlantingDensity: zone.targetPlantingDensity,
+                totalSeedlingsSent: plantingsBySubzone[sz.id].totalPlants,
+              };
+            });
+          }),
+        };
       });
-    });
+    }
   }
 );
+
+// selector for single site
+export const getReportedPlantsForSite = (state: RootState, siteId: number) =>
+  selectPlantingProgressSubzones(state)?.find((report) => report.siteId === siteId);
+
+// selector for search and filtering
+const searchReportedPlants = (state: RootState, siteIds: string[], query: string) => {
+  selectPlantingProgressSubzones(state)?.filter((report) => {
+    return !!siteIds.find((siteId) => siteId === report.siteId.toString());
+  });
+};
