2912db97c61d08e24f358105f0a04e31baa597a3
Charlie Keith <charlie@terraformation.com>
2023-07-14 12:51:25 -0600

    SW-3839 Plants Dashboard Map Tooltips

--- src/components/PlantsV2/components/PlantingProgressPerZoneCard.tsx
+++ src/components/PlantsV2/components/PlantingProgressPerZoneCard.tsx
@@ -4,7 +4,7 @@ import strings from 'src/strings';
 import BarChart from 'src/components/common/Chart/BarChart';
 import React, { useEffect, useMemo, useState } from 'react';
 import { useAppSelector } from 'src/redux/store';
-import { selectPlantingSite } from 'src/redux/features/tracking/trackingSelectors';
+import { selectPlantingSite, selectZoneProgress } from 'src/redux/features/tracking/trackingSelectors';
 import { truncate } from 'src/utils/text';
 
 const MAX_ZONE_NAME_LENGTH = 20;
@@ -15,31 +15,16 @@ type PlantingProgressPerZoneCardProps = {
 
 export default function PlantingProgressPerZoneCard({ plantingSiteId }: PlantingProgressPerZoneCardProps): JSX.Element {
   const theme = useTheme();
-  const plantingSite = useAppSelector((state) => selectPlantingSite(state, plantingSiteId));
   const [labels, setLabels] = useState<string[]>();
   const [values, setValues] = useState<number[]>();
   const [tooltipTitles, setTooltipTitles] = useState<string[]>();
 
+  const zoneProgress = useAppSelector((state) => selectZoneProgress(state, plantingSiteId));
   useEffect(() => {
-    if (plantingSite) {
-      const zoneProgress: Record<string, number> = {};
-      plantingSite.plantingZones?.forEach((zone) => {
-        let completedPlantingArea = 0;
-        zone.plantingSubzones.forEach((sz) => {
-          completedPlantingArea += sz.plantingCompleted ? +sz.areaHa : 0;
-        });
-        const percentProgress = Math.round((100 * completedPlantingArea) / +zone.areaHa);
-        zoneProgress[zone.name] = percentProgress;
-      });
-      setLabels(Object.keys(zoneProgress).map((name) => truncate(name, MAX_ZONE_NAME_LENGTH)));
-      setValues(Object.values(zoneProgress));
-      setTooltipTitles(Object.keys(zoneProgress));
-    } else {
-      setLabels([]);
-      setValues([]);
-      setTooltipTitles([]);
-    }
-  }, [plantingSite]);
+    setLabels(Object.values(zoneProgress).map((val) => truncate(val.name, MAX_ZONE_NAME_LENGTH)));
+    setValues(Object.values(zoneProgress).map((val) => val.progress));
+    setTooltipTitles(Object.values(zoneProgress).map((val) => val.name));
+  }, [zoneProgress]);
 
   const chartData = useMemo(() => {
     if (!labels?.length || !values?.length) {
--- src/redux/features/tracking/sitePopulationSelector.ts
+++ src/redux/features/tracking/sitePopulationSelector.ts
@@ -1,5 +1,26 @@
 import { RootState } from 'src/redux/rootReducer';
+import { createCachedSelector } from 're-reselect';
 
 export const selectSitePopulation = (state: RootState) => state.sitePopulation?.zones;
 
 export const selectSitePopulationError = (state: RootState) => state.sitePopulation?.error;
+
+export const selectZonePopulationStats = createCachedSelector(
+  (state: RootState) => selectSitePopulation(state),
+  (zones) => {
+    const zoneStats: Record<number, { name: string; reportedPlants: number; reportedSpecies: number }> = {};
+    zones?.forEach((zone) => {
+      const speciesSet = new Set<string>();
+      let numPlants = 0;
+      zone.plantingSubzones?.forEach((subzone) =>
+        subzone.populations?.forEach((population) => {
+          const count = +population['totalPlants(raw)'];
+          numPlants += isNaN(count) ? 0 : count;
+          speciesSet.add(population.species_scientificName);
+        })
+      );
+      zoneStats[+zone.id] = { name: zone.name, reportedPlants: numPlants, reportedSpecies: speciesSet.size };
+    });
+    return zoneStats;
+  }
+)(() => 'zonePopulationStats');
