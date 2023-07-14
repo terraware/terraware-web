49f7a5c289313b1b2a69adcef0c68cb2c7e208ef
constanzauanini <constanza.uanini@admios-sa.com>
2023-07-14 11:02:35 -0300

    Add planting progress selector

--- src/redux/features/Plantings/plantingsSelectors.ts
+++ src/redux/features/Plantings/plantingsSelectors.ts
@@ -1,5 +1,6 @@
 import { RootState } from 'src/redux/rootReducer';
-import { requestPlantingSiteReportedPlants } from './plantingsThunks';
+import { selectPlantingSite } from '../tracking/trackingSelectors';
+import { createSelector } from '@reduxjs/toolkit';
 
 export const selectPlantings = (state: RootState) => state.plantings?.plantings;
 
@@ -13,3 +14,26 @@ export const selectPlantingsDateRange = (state: RootState, dateRange: string[]) 
 
 export const selectPlantingSiteReportedPlants = (state: RootState, plantingSiteId: number) =>
   state.plantingSiteReportedPlants;
+
+export const selectPlantingProgressSubzones = createSelector(
+  [
+    (state: RootState, plantingSiteId: number) => selectPlantingSite(state, plantingSiteId),
+    (state: RootState, plantingSiteId: number) => selectPlantingSiteReportedPlants(state, plantingSiteId),
+  ],
+  (plantingSite, plantingSiteReportedPlants) => {
+    plantingSite?.plantingZones?.flatMap((zone) => {
+      return zone.plantingSubzones.map((sz) => {
+        return {
+          subzoneName: sz.fullName,
+          plantingComplete: sz.plantingCompleted,
+          plantingSite: plantingSite.name,
+          zone: zone.name,
+          targetPlantingDensity: zone.targetPlantingDensity,
+          totalSeedlingsSent: plantingSiteReportedPlants.site?.plantingZones
+            .find((reportedZone) => reportedZone.id === zone.id)
+            ?.plantingSubzones.find((reportedSubzone) => reportedSubzone.id === sz.id)?.totalPlants,
+        };
+      });
+    });
+  }
+);
