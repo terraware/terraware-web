0f73764acd1e44d60849d9a99d8feb89f77ed0cf
Karthik B <karthik@terraformation.com>
2023-07-17 07:47:35 -0700

    SW-3893 Add tabs to tracking v2 withdrawals page (#1562)
    - refactor existing withdrawals page to extract out the table content
    into NurseryWithdrawalsTable
    - create tabs in v2 plantings and withdrawals
    - reuse NurseryWithdrawalsTable in withdrawal history tab
    - instantiate empty plantings component
    
    <img width="749" alt="Terraware App 2023-07-17 07-09-00"
    src="https://github.com/terraware/terraware-web/assets/1865174/76fc9b2f-3333-414e-b4cf-61acb53d88e4">
    
    <img width="743" alt="Terraware App 2023-07-17 07-08-33"
    src="https://github.com/terraware/terraware-web/assets/1865174/f5e0afb8-8bbb-40f6-bab2-46e8ddc90caf">

---
+++ src/components/NurseryWithdrawals/NurseryWithdrawalsTabContent.tsx
@@ -0,0 +1,23 @@
+import { Typography, useTheme } from '@mui/material';
+import strings from 'src/strings';
+import Card from 'src/components/common/Card';
+import NurseryWithdrawalsTable from './NurseryWithdrawalsTable';
+
+export default function NurseryWithdrawalsTabContent(): JSX.Element {
+  const theme = useTheme();
+
+  return (
+    <Card flushMobile>
+      <Typography
+        sx={{
+          fontSize: '20px',
+          fontWeight: 600,
+          color: theme.palette.TwClrTxt,
+        }}
+      >
+        {strings.WITHDRAWAL_HISTORY}
+      </Typography>
+      <NurseryWithdrawalsTable />
+    </Card>
+  );
+}
