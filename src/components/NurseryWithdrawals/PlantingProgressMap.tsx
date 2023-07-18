c01f84694df3eda73e8e5fee8ad4805f5c223d0a
Charlie Keith <114949086+charlie-tf@users.noreply.github.com>
2023-07-18 11:12:27 -0600

    SW-3903 Add Planting Site Selector for Map View (#1572)
    - create a common PlantingSiteSelector component
    - create a component that variably displays search or the selector based
      on view
    - keep track of the selected planting site id and pass to map

--- src/components/NurseryWithdrawals/PlantingProgressMap.tsx
+++ src/components/NurseryWithdrawals/PlantingProgressMap.tsx
@@ -1,3 +1,7 @@
-export default function PlantingProgressList(): JSX.Element {
-  return <p>'I am a map'</p>;
+type PlantingProgressMapProps = {
+  plantingSiteId: number;
+};
+
+export default function PlantingProgressMap({ plantingSiteId }: PlantingProgressMapProps): JSX.Element {
+  return <p>{`I am a map of planting site ${plantingSiteId}`}</p>;
 }
