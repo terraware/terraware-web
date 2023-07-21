6889217810718d97966b7e0127d0c0955ef08692
Charlie Keith <charlie@terraformation.com>
2023-07-21 14:26:21 -0600

    SW-3908 Warn When Undoing Planting Complete will Delete Stats
    - determine the zone for a subzone when clicking map
    - select whether that zone has statistics
    - if there are no statistics then allow update without a warning
    - if there are statistics:
      - open warning modal
      - set a callback that is executed if the deletion is confirmed
    - after update refresh observation results as well as planting site

--- src/components/NurseryWithdrawals/PlantingProgressMap.tsx
+++ src/components/NurseryWithdrawals/PlantingProgressMap.tsx
@@ -2,17 +2,23 @@ import { useAppDispatch, useAppSelector } from 'src/redux/store';
 import { selectPlantingSite } from 'src/redux/features/tracking/trackingSelectors';
 import { MapService } from 'src/services';
 import { MapData, MapSourceProperties } from 'src/types/Map';
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { PlantingSiteMap } from 'src/components/Map';
 import { Typography, useTheme } from '@mui/material';
 import PlantingProgressMapDialog from './PlantingProgressMapDialog';
 import { makeStyles } from '@mui/styles';
 import { requestPlantingSites, requestSitePopulation } from 'src/redux/features/tracking/trackingThunks';
 import { useOrganization } from 'src/providers';
-import { selectUpdatePlantingCompleted } from 'src/redux/features/plantings/plantingsSelectors';
+import {
+  selectUpdatePlantingCompleted,
+  selectZonesHaveStatistics,
+} from 'src/redux/features/plantings/plantingsSelectors';
 import strings from 'src/strings';
 import { requestUpdatePlantingCompleted } from 'src/redux/features/plantings/plantingsAsyncThunks';
 import useSnackbar from 'src/utils/useSnackbar';
+import { useDefaultTimeZone } from 'src/utils/useTimeZoneUtils';
+import StatsWarningDialog from 'src/components/NurseryWithdrawals/StatsWarningModal';
+import { requestObservationsResults } from 'src/redux/features/observations/observationsThunks';
 
 export const useStyles = makeStyles(() => ({
   popup: {
@@ -33,6 +39,7 @@ export default function PlantingProgressMap({ plantingSiteId }: PlantingProgress
   const classes = useStyles();
   const dispatch = useAppDispatch();
   const org = useOrganization();
+  const defaultTimeZone = useDefaultTimeZone();
   const plantingSite = useAppSelector((state) => selectPlantingSite(state, plantingSiteId));
   const [mapData, setMapData] = useState<MapData | undefined>();
   const [dispatching, setDispatching] = useState(false);
@@ -40,6 +47,12 @@ export default function PlantingProgressMap({ plantingSiteId }: PlantingProgress
   const updateStatus = useAppSelector((state) => selectUpdatePlantingCompleted(state, requestId));
   const [focusEntities, setFocusEntities] = useState<{ sourceId: string; id: number }[]>([]);
   const snackbar = useSnackbar();
+
+  const [zoneIdSelected, setZoneIdSelected] = useState<number>(-1);
+  const selectedZoneHasStats = useAppSelector((state) =>
+    selectZonesHaveStatistics(state, { [plantingSiteId]: new Set([zoneIdSelected]) }, defaultTimeZone.get().id)
+  );
+  const [statsWarningDialogOpen, setStatsWarningDialogOpen] = useState(false);
 
   useEffect(() => {
     if (!mapData?.site?.entities || plantingSite?.id !== mapData.site.entities[0]?.id) {
@@ -73,48 +86,74 @@ export default function PlantingProgressMap({ plantingSiteId }: PlantingProgress
       if (updateStatus.status === 'error') {
         snackbar.toastError(strings.GENERIC_ERROR);
       } else if (updateStatus.status === 'success') {
-        // refresh planting site data to get new completed state for subzone
+        // refresh planting site and observations data
         dispatch(requestPlantingSites(org.selectedOrganization.id));
+        dispatch(requestObservationsResults(org.selectedOrganization.id));
       }
     }
   }, [updateStatus, dispatch, snackbar, org.selectedOrganization.id]);
 
-  const updatePlantingComplete = useCallback(
-    (id: number, val: boolean) => {
-      // TODO: warn if undoing planting complete will erase statistics
-      const request = dispatch(
-        requestUpdatePlantingCompleted({
-          subzoneId: id,
-          planting: {
-            plantingCompleted: val,
-          },
-        })
-      );
-      setRequestId(request.requestId);
-      setFocusEntities([]);
-      setDispatching(true);
-    },
-    [dispatch]
-  );
+  const [confirmDeleteCb, setConfirmDeleteCb] = useState<() => void>(() => () => null);
+  const completeUpdate = (id: number, val: boolean) => () => {
+    const request = dispatch(
+      requestUpdatePlantingCompleted({
+        subzoneId: id,
+        planting: {
+          plantingCompleted: val,
+        },
+      })
+    );
+    setRequestId(request.requestId);
+    setFocusEntities([]);
+    setConfirmDeleteCb(() => () => null);
+    setDispatching(true);
+  };
+
+  const updatePlantingComplete = (id: number, val: boolean) => {
+    if (!selectedZoneHasStats) {
+      completeUpdate(id, val)();
+    } else {
+      setConfirmDeleteCb(() => completeUpdate(id, val));
+      setStatsWarningDialogOpen(true);
+    }
+  };
+
+  const onOpenMapDialog = (id: number) => {
+    const selectedZone = plantingSite?.plantingZones?.find((zone) =>
+      zone.plantingSubzones.map((sz) => sz.id).includes(id)
+    );
+    setZoneIdSelected(selectedZone?.id ?? -1);
+  };
 
   return mapData ? (
-    <PlantingSiteMap
-      mapData={mapData}
-      focusEntities={focusEntities}
-      contextRenderer={{
-        render: (properties: MapSourceProperties) => (
-          <PlantingProgressMapDialog
-            id={properties.id}
-            name={properties.fullName}
-            plantingComplete={subzonesComplete[properties.id]}
-            onUpdatePlantingComplete={updatePlantingComplete}
-            busy={dispatching}
-          />
-        ),
-        className: classes.popup,
-        anchor: 'bottom',
-      }}
-    />
+    <>
+      <StatsWarningDialog
+        open={statsWarningDialogOpen}
+        onClose={() => setStatsWarningDialogOpen(false)}
+        onSubmit={() => {
+          setStatsWarningDialogOpen(false);
+          confirmDeleteCb();
+        }}
+      />
+      <PlantingSiteMap
+        mapData={mapData}
+        focusEntities={focusEntities}
+        contextRenderer={{
+          render: (properties: MapSourceProperties) => (
+            <PlantingProgressMapDialog
+              id={properties.id}
+              name={properties.fullName}
+              plantingComplete={subzonesComplete[properties.id]}
+              onUpdatePlantingComplete={updatePlantingComplete}
+              onOpen={onOpenMapDialog}
+              busy={dispatching}
+            />
+          ),
+          className: classes.popup,
+          anchor: 'bottom',
+        }}
+      />
+    </>
   ) : (
     <Typography fontSize='14px' fontWeight={400} color={theme.palette.TwClrTxt}>
       No map data (placeholder).
