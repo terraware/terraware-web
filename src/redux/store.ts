6bf93ea851a6d1449856e9c39dcf7365e1f90bef
Karthik Balachandran <karthik@terraformation.com>
2023-07-25 09:25:24 -0700

    SW-3961 Fix SerializableStateInvariantMiddleware warning in app
    - the default middleware checks for immutability/serializability of redux state / responses
    - this happens in dev mode only!
    - can be slow for some of our API such as observations which can take time
    - middleware complains that it takes too much time
    - disabled these checks (reference https://github.com/reduxjs/redux-toolkit/issues/805)
    - not sure on implications of disabling these checks yet

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,7 +5,11 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 // configure the root store
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      immutableCheck: false,
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
