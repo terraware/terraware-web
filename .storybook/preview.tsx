b3680ebbe91f354b4d9bb1cca580887ac02efcff
constanzauanini <constanza.uanini@admios-sa.com>
2023-08-30 09:49:07 -0300

    Remove recoil from storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,7 @@
 import type { Preview } from '@storybook/react';
 import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import { MemoryRouter } from 'react-router';
-import { RecoilRoot } from 'recoil';
 import { Provider } from 'react-redux';
-import { store } from 'src/redux/store';
 
 const preview: Preview = {
   parameters: {
@@ -38,11 +36,9 @@ export const decorators = [
         >
           <StyledEngineProvider injectFirst>
             <MemoryRouter initialEntries={['/']}>
-              <RecoilRoot>
-                <Provider store={store}>
-                  <Story />
-                </Provider>
-              </RecoilRoot>
+              <Provider store={store}>
+                <Story />
+              </Provider>
             </MemoryRouter>
           </StyledEngineProvider>
         </LocalizationProvider>
