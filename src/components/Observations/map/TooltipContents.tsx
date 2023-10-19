a30258f1d4f271c7b2d52f41db15a3d3011cde84
Karthik B <karthik@terraformation.com>
2023-10-19 09:02:49 +0530

    SW-4316 Replace observation plots from map view, intermediate implementation (#1731)
    - similar to list view, added replace observation plot in observations
    map view
    - moved the modal into a separate directory for reuse
    - refactored the map tooltip to accept the entire plot (to access more
    properties)
    - introduced react Portal in this PR since we can't efficiently render a
    modal within a modal, rendering it in a Portal instead (end result is
    similar to rendering a modal from the main app, just not within a modal)
    - processing the response from BE etc will be done separately
    - only plots that haven't been monitored (no completed time) will be
    eligible for reassignment
    
    <img width="371" alt="Terraware App 2023-10-18 13-39-38"
    src="https://github.com/terraware/terraware-web/assets/1865174/49e7b9c9-a8e9-43ca-843d-63400e8294c7">
    
    <img width="358" alt="Terraware App 2023-10-18 13-39-55"
    src="https://github.com/terraware/terraware-web/assets/1865174/8720b6b8-bd84-4fe2-8e5b-88c089c25610">

--- src/components/Observations/map/TooltipContents.tsx
+++ src/components/Observations/map/TooltipContents.tsx
@@ -1,48 +1,92 @@
+import { useState } from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
+import { Portal } from '@mui/base';
+import { makeStyles } from '@mui/styles';
+import { Button } from '@terraware/web-components';
 import strings from 'src/strings';
-import { ObservationState } from 'src/types/Observations';
+import { ObservationState, ObservationMonitoringPlotResultsPayload } from 'src/types/Observations';
+import ReplaceObservationPlotModal from 'src/components/Observations/replacePlot/ReplaceObservationPlotModal';
+
+const useStyles = makeStyles(() => ({
+  button: {
+    marginLeft: 'auto',
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+}));
 
 type TooltipContentsProps = {
-  title: string;
+  monitoringPlot: ObservationMonitoringPlotResultsPayload;
+  observationId?: number;
   observationState?: ObservationState;
-  numPlants?: number;
-  numSpecies?: number;
-  plantingDensity?: number;
-  percentMortality?: number;
+  title: string;
 };
 
 export default function TooltipContents(props: TooltipContentsProps): JSX.Element {
-  const { title, observationState, numPlants, numSpecies, plantingDensity, percentMortality } = props;
+  const { monitoringPlot, observationId, observationState, title } = props;
   const theme = useTheme();
+  const classes = useStyles();
+  const [showReplacePlotModal, setShowReplacePlotModal] = useState<boolean>(false);
 
   const observationInProgress = observationState === 'InProgress';
   const observationOverdue = observationState === 'Overdue';
 
+  const numPlants = monitoringPlot?.totalPlants;
+  const numSpecies = monitoringPlot?.totalSpecies;
+  const plantingDensity = monitoringPlot?.plantingDensity;
+  const percentMortality = monitoringPlot?.mortalityRate;
+
   return (
-    <Box display='flex' flexDirection='column' padding={theme.spacing(1)}>
-      <Typography fontSize='16px' fontWeight={600} marginBottom={theme.spacing(2)}>
-        {title}
-      </Typography>
-      {observationInProgress ? (
-        <Typography>{strings.OBSERVATION_IN_PROGRESS}</Typography>
-      ) : observationOverdue ? (
-        <Typography>{strings.OBSERVATION_OVERDUE}</Typography>
-      ) : (
-        <>
-          <Typography fontSize='16px' fontWeight={400}>
-            {`${strings.PLANTS}: ${numPlants ?? strings.UNKNOWN}`}
-          </Typography>
-          <Typography fontSize='16px' fontWeight={400}>
-            {`${strings.SPECIES}: ${numSpecies ?? strings.UNKNOWN}`}
-          </Typography>
-          <Typography fontSize='16px' fontWeight={400}>
-            {`${strings.PLANTING_DENSITY}: ${plantingDensity ?? strings.UNKNOWN} ${strings.PLANTS_PER_HECTARE}`}
-          </Typography>
-          <Typography fontSize='16px' fontWeight={400}>
-            {`${strings.MORTALITY_RATE}: ${percentMortality ?? strings.UNKNOWN}%`}
-          </Typography>
-        </>
+    <>
+      {showReplacePlotModal && observationId && monitoringPlot && (
+        <Portal>
+          <ReplaceObservationPlotModal
+            onClose={() => setShowReplacePlotModal(false)}
+            observationId={observationId}
+            monitoringPlot={monitoringPlot}
+          />
+        </Portal>
       )}
-    </Box>
+      <Box>
+        <Box display='flex' flexDirection='column' padding={theme.spacing(2)}>
+          <Typography fontSize='16px' fontWeight={600} marginBottom={theme.spacing(2)}>
+            {title}
+          </Typography>
+          {observationInProgress ? (
+            <Typography>{strings.OBSERVATION_IN_PROGRESS}</Typography>
+          ) : observationOverdue ? (
+            <Typography>{strings.OBSERVATION_OVERDUE}</Typography>
+          ) : (
+            <>
+              <Typography fontSize='16px' fontWeight={400}>
+                {`${strings.PLANTS}: ${numPlants ?? strings.UNKNOWN}`}
+              </Typography>
+              <Typography fontSize='16px' fontWeight={400}>
+                {`${strings.SPECIES}: ${numSpecies ?? strings.UNKNOWN}`}
+              </Typography>
+              <Typography fontSize='16px' fontWeight={400}>
+                {`${strings.PLANTING_DENSITY}: ${plantingDensity ?? strings.UNKNOWN} ${strings.PLANTS_PER_HECTARE}`}
+              </Typography>
+              <Typography fontSize='16px' fontWeight={400}>
+                {`${strings.MORTALITY_RATE}: ${percentMortality ?? strings.UNKNOWN}%`}
+              </Typography>
+            </>
+          )}
+        </Box>
+        {!monitoringPlot?.completedTime && (
+          <Box display='flex' padding={theme.spacing(2)} sx={{ backgroundColor: theme.palette.TwClrBgSecondary }}>
+            <Button
+              className={classes.button}
+              id='reassignPlot'
+              label={`${strings.REQUEST_REASSIGNMENT}...`}
+              type='passive'
+              onClick={() => setShowReplacePlotModal(true)}
+              priority='secondary'
+            />
+          </Box>
+        )}
+      </Box>
+    </>
   );
 }
