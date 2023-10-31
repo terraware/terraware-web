19f4e0eadb0bc34ff1e508f25d994fbd41578150
Karthik B <karthik@terraformation.com>
2023-10-31 11:43:25 -0700

    SW-4330-followup Add map style dropdown and fullscreen button to editable map view (#1751)
    - extracted out the map view style component for reuse
    - added a react hook to share the set/get map view style properties, for
    reuse
    - added map view style and fullscreen controls to the editable map view
    - the color contrast can be off in outdoors view sometimes
    
    <img width="924" alt="Terraware App 2023-10-31 09-56-16"
    src="https://github.com/terraware/terraware-web/assets/1865174/efad2c86-5fcd-4d92-918d-049f45397dff">

---
+++ src/components/Map/MapViewStyleControl.tsx
@@ -0,0 +1,106 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import { Box, Theme, useTheme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { MapViewStyle } from 'src/types/Map';
+import { DropdownItem, PopoverMenu } from '@terraware/web-components';
+import { useMapPortalContainer } from './MapRenderUtils';
+import { useLocalization } from 'src/providers';
+import strings from 'src/strings';
+
+export const useMapViewStyle = (): [MapViewStyle, (style: MapViewStyle) => void] => {
+  const [mapViewStyle, setMapViewStyle] = useState<MapViewStyle>(
+    localStorage.getItem('mapViewStyle') === 'Outdoors' ? 'Outdoors' : 'Satellite'
+  );
+
+  const onChangeMapViewStyle = useCallback(
+    (viewStyle: MapViewStyle) => {
+      setMapViewStyle(viewStyle);
+      localStorage.setItem('mapViewStyle', viewStyle);
+    },
+    [setMapViewStyle]
+  );
+
+  return useMemo<[MapViewStyle, (style: MapViewStyle) => void]>(
+    () => [mapViewStyle, onChangeMapViewStyle],
+    [mapViewStyle, onChangeMapViewStyle]
+  );
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+  viewControl: {
+    '& .MuiButtonBase-root': {
+      padding: 0,
+    },
+    '& .MuiButtonBase-root.MuiMenuItem-root': {
+      fontSize: '12px',
+    },
+    '& svg': {
+      marginLeft: 0,
+    },
+  },
+  viewControlSelected: {
+    fontSize: '12px',
+    paddingLeft: theme.spacing(0.5),
+    color: theme.palette.TwClrTxt,
+  },
+}));
+
+export type MapViewStyleControlProps = {
+  mapViewStyle?: MapViewStyle;
+  onChangeMapViewStyle: (style: MapViewStyle) => void;
+};
+
+const MapViewStyleControl = ({ mapViewStyle, onChangeMapViewStyle }: MapViewStyleControlProps): JSX.Element | null => {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { activeLocale } = useLocalization();
+  const mapPortalContainer = useMapPortalContainer();
+
+  const setMapViewStyle = (item: DropdownItem) => {
+    const style: MapViewStyle = item.value === 'Outdoors' ? 'Outdoors' : 'Satellite';
+    onChangeMapViewStyle(style);
+  };
+
+  const viewOptions = useMemo<DropdownItem[]>(() => {
+    if (!activeLocale) {
+      return [];
+    }
+    return [
+      { label: strings.OUTDOORS, value: 'Outdoors' },
+      { label: strings.SATELLITE, value: 'Satellite' },
+    ];
+  }, [activeLocale]);
+
+  if (mapPortalContainer) {
+    return null;
+  }
+
+  return (
+    <Box
+      sx={{
+        position: 'absolute',
+        top: '10px',
+        left: '45px',
+        zIndex: 10,
+        height: 28,
+        backgroundColor: `${theme.palette.TwClrBaseWhite}`,
+        borderRadius: '4px',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+      className={classes.viewControl}
+    >
+      <PopoverMenu
+        anchor={
+          <span className={classes.viewControlSelected}>
+            {mapViewStyle === 'Outdoors' ? strings.OUTDOORS : strings.SATELLITE}
+          </span>
+        }
+        menuSections={[viewOptions]}
+        onClick={setMapViewStyle}
+      />
+    </Box>
+  );
+};
+
+export default MapViewStyleControl;
