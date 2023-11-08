a5d85b095f3790e3ec62a70bb44a38b4792701c3
Karthik Balachandran <karthik@terraformation.com>
2023-11-07 16:48:23 -0800

    - wip

--- src/components/common/SubLocations/SubLocationsCellRenderer.tsx
+++ src/components/common/SubLocations/SubLocationsCellRenderer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { APP_PATHS } from 'src/constants';
 import CellRenderer, { TableRowType } from 'src/components/common/table/TableCellRenderer';
 import { RendererProps } from 'src/components/common/table/types';
 import Link from 'src/components/common/Link';
@@ -7,15 +6,17 @@ import { ActiveStatuses } from 'src/types/Accession';
 import { NumericFormatter } from 'src/types/Number';
 
 export type SubLocationsCellRendererProps = {
-  seedBankId?: number;
+  facilityId?: number;
   numericFormatter: NumericFormatter;
   editMode: boolean;
+  renderLink?: (facilityId: number, subLocationId: number) => string;
 };
 
 export default function SubLocationsCellRenderer({
   seedBankId,
   numericFormatter,
   editMode,
+  renderLink,
 }: SubLocationsCellRendererProps) {
   return (props: RendererProps<TableRowType>): JSX.Element => {
     const { column, value, row, onRowClick } = props;
---
+++ src/components/common/SubLocations/index.tsx
@@ -0,0 +1,189 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+import strings from 'src/strings';
+import { SubLocationService } from 'src/services';
+import { DEFAULT_SUB_LOCATIONS, PartialSubLocation } from 'src/types/Facility';
+import { useLocalization } from 'src/providers';
+import Table from 'src/components/common/table';
+import { TableColumnType } from 'src/components/common/table/types';
+import { useNumberFormatter } from 'src/utils/useNumber';
+import SubLocationsCellRenderer from './SubLocationsCellRenderer';
+import { TopBarButton } from '@terraware/web-components/components/table';
+import { Button } from '@terraware/web-components';
+import useDeviceInfo from 'src/utils/useDeviceInfo';
+import AddEditSubLocationModal from '.AddEditSubLocationModal';
+import _ from 'lodash';
+
+export type FacilityType = 'seedbank' | 'nursery';
+
+export type SublocationsProps = {
+  facilityType: FacilityType;
+  facilityId?: number;
+  onEdit?: (subLocations: PartialSubLocation[]) => void;
+  renderLink?: (facilityId: number, subLocationId: number) => string;
+};
+
+const subLocationWith = (name: string, id: number, facilityType: FacilityType) => ({
+  name,
+  id: -id,
+  activeAccessions: facilityType === 'seedbank' ? 0 : undefined,
+  activeBatches: facilityType === 'nursery' ? 0 : undefined,
+});
+
+const baseColumn = (): TableColumnType => (
+  { key: 'name', name: strings.NAME, type: 'string' },
+);
+
+const seedBankColumn = (): TableColumnType => (
+  { key: 'activeAccessions', name: strings.ACTIVE_ACCESSIONS, type: 'number' },
+);
+
+const nurseryColumn = (): TableColumnType => (
+  { key: 'activeBatches', name: strings.BATCHES, type: 'number' },
+);
+
+export default function SubLocations({ faciliyType, facilityId, onEdit, renderLink }: SublocationsProps): JSX.Element | null {
+  const isSeedbank = facilityType == 'seedbank';
+  const { activeLocale } = useLocalization();
+  const { isMobile } = useDeviceInfo();
+  const numberFormatter = useNumberFormatter();
+  const [selectedRows, setSelectedRows] = useState<PartialSubLocation[]>([]);
+  const [selectedSubLocation, setSelectedSubLocation] = useState<PartialSubLocation | undefined>();
+  const [subLocations, setSubLocations] = useState<PartialSubLocation[]>([]);
+  const [openSubLocationModal, setOpenSubLocationModal] = useState<boolean>(false);
+  const numericFormatter = useMemo(() => numberFormatter(activeLocale), [numberFormatter, activeLocale]);
+
+  const columns = useCallback(() => {
+    if (!activeLocale) {
+      return [];
+    }
+    return [
+      ...baseColumn(),
+      isSeedbank ? ...seedBankColumn() : ...nurseryColumn(),
+  }, [activeLocale, isSeedbank]);
+
+  useEffect(() => {
+    const fetchSubLocations = async () => {
+      const response = await SubLocationService.getSubLocations(facilityId!);
+      if (response.requestSucceeded) {
+        if (activeLocale) {
+          const collator = new Intl.Collator(activeLocale);
+          setSubLocations(response.subLocations.sort((a, b) => collator.compare(a.name, b.name)));
+        }
+      }
+    };
+
+    if (facilityId) {
+      fetchSubLocations();
+    } else if (isSeedbank) {
+      setSubLocations(DEFAULT_SUB_LOCATIONS().map((name, index) => subLocationWith(name, index)));
+    }
+  }, [facilityId, activeLocale, isSeedbank]);
+
+  const getTopBarButtons = () => {
+    const topBarButtons: TopBarButton[] = [
+      {
+        buttonType: 'destructive',
+        buttonText: strings.DELETE,
+        // we don't want to delete locations that have active data
+        disabled: selectedRows.some((location) => location.activeAccessions || location.activeBatches),
+        onButtonClick: () => deleteSubLocations(selectedRows.filter((location) => !(location.activeAccessions || location.activeBatches))),
+      },
+    ];
+
+    return topBarButtons;
+  };
+
+  const onSubLocationSelected = (location: PartialSubLocation, fromColumn?: string) => {
+    setSelectedSubLocation(location);
+    setOpenSubLocationModal(true);
+  };
+
+  const onAddSubLocationClick = () => {
+    setSelectedSubLocation(undefined);
+    setOpenSubLocationModal(true);
+  };
+
+  const setNewSubLocations = (locations: PartialSubLocation[]) => {
+    setSubLocations(locations);
+    if (onEdit) {
+      onEdit(locations);
+    }
+  };
+
+  const addSubLocation = (name: string) => {
+    setNewSubLocations([...subLocations, subLocationWith(name, Date.now(), facilityType)]);
+  };
+
+  const editSubLocation = (location: PartialSubLocation) => {
+    setNewSubLocations([...subLocations.filter((l) => l.id !== location.id), location]);
+  };
+
+  const deleteSubLocations = (locations: PartialSubLocation[]) => {
+    setNewSubLocations([..._.differenceWith(subLocations, locations, (l1, l2) => l1.id === l2.id)]);
+  };
+
+  const editMode: boolean = !!onEdit;
+
+  return (
+    <>
+      <AddEditSubLocationModal
+        open={openSubLocationModal}
+        onClose={() => setOpenSubLocationModal(false)}
+        onAddSubLocation={(locationName: string) => addSubLocation(locationName)}
+        onEditSubLocation={(location: PartialSubLocation) => editSubLocation(location)}
+        selectedSubLocation={selectedSubLocation}
+        subLocations={subLocations}
+      />
+      <Grid
+        item
+        xs={12}
+        display='flex'
+        alignItems='center'
+        justifyContent='space-between'
+        marginTop={4}
+        marginBottom={2}
+      >
+        <Typography fontSize='16px' fontWeight={600}>
+          {strings.SUB_LOCATIONS}
+        </Typography>
+        {editMode && (
+          <Button
+            id='new-batch'
+            icon='plus'
+            label={isMobile ? '' : strings.ADD_SUB_LOCATION}
+            onClick={onAddSubLocationClick}
+            size='small'
+            type='passive'
+          />
+        )}
+      </Grid>
+      <Grid item xs={12}>
+        {(subLocations.length > 0 || editMode) && (
+          <Table
+            id='sub-locations-table'
+            columns={columns}
+            rows={subLocations}
+            orderBy='name'
+            Renderer={SubLocationsCellRenderer({ facilityId, numericFormatter, editMode, renderLink })}
+            selectedRows={selectedRows}
+            setSelectedRows={setSelectedRows}
+            showCheckbox={editMode}
+            isClickable={() => false}
+            showTopBar={editMode}
+            topBarButtons={getTopBarButtons()}
+            onSelect={onSubLocationSelected}
+            controlledOnSelect={true}
+          />
+        )}
+        {subLocations.length === 0 && !editMode && (
+          <Box paddingY={isMobile ? 3 : 6} textAlign='center'>
+            <Typography fontSize='14px' fontWeight={400}>
+              {strings.NO_SUB_LOCATIONS}
+            </Typography>
+          </Box>
+        )}
+      </Grid>
+    </>
+  );
+}
