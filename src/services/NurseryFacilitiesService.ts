0fcc7d3c7543a7f8f5d8749becee9304a260141b
Nick Graziano <nick.graziano@gmail.com>
2023-11-16 12:21:48 -0700

    Implement new NurseryFacilities API as a service, implement 'get nursery summary' API call

---
+++ src/services/NurseryFacilitiesService.ts
@@ -0,0 +1,31 @@
+import HttpService, { Response } from './HttpService';
+import { paths } from '../api/types/generated-schema';
+
+const ENDPOINT_NURSERY_FACILITY_SUMMARY = '/api/v1/nursery/facilities/{facilityId}/summary';
+
+type GetNurserySummaryResponsePayload =
+  paths[typeof ENDPOINT_NURSERY_FACILITY_SUMMARY]['get']['responses'][200]['content']['application/json'];
+
+export type NurserySummaryPayload = GetNurserySummaryResponsePayload['summary'];
+export type NurserySummarySpecies = NurserySummaryPayload['species'][0];
+
+const httpNurseryFacilitySummary = HttpService.root(ENDPOINT_NURSERY_FACILITY_SUMMARY);
+
+const getNurserySummary = (nurseryId: number): Promise<Response & NurserySummaryPayload> =>
+  httpNurseryFacilitySummary.get<GetNurserySummaryResponsePayload, NurserySummaryPayload | undefined>(
+    {
+      urlReplacements: {
+        '{facilityId}': `${nurseryId}`,
+      },
+    },
+    (response?: GetNurserySummaryResponsePayload) => response?.summary
+  );
+
+/**
+ * Exported functions
+ */
+const NurseryFacilityService = {
+  getNurserySummary,
+};
+
+export default NurseryFacilityService;
