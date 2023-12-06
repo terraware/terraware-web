fd9075d2e9f8db4a5b84c00a39837abcc969f936
Nick Graziano <nick.graziano@gmail.com>
2023-12-06 09:59:48 -0700

    Pull project data into project view and render name and description

--- src/components/ProjectView/index.tsx
+++ src/components/ProjectView/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { Grid, Typography } from '@mui/material';
 import { Crumb, Page } from 'src/components/BreadCrumbs';
 import Card from 'src/components/common/Card';
 import strings from 'src/strings';
 import { APP_PATHS } from 'src/constants';
+import theme from 'src/theme';
+import { useAppDispatch, useAppSelector } from 'src/redux/store';
+import { selectProject } from 'src/redux/features/projects/projectsSelectors';
+import { requestProject } from 'src/redux/features/projects/projectsThunks';
 
 const crumbs: Crumb[] = [
   {
@@ -13,15 +18,46 @@ const crumbs: Crumb[] = [
 ];
 
 export default function ProjectView(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const pathParams = useParams<{ projectId: string }>();
+  const projectId = Number(pathParams.projectId);
+
+  const project = useAppSelector(selectProject(projectId));
+
+  useEffect(() => {
+    if (!project) {
+      void dispatch(requestProject(projectId));
+    }
+  }, [projectId, project, dispatch]);
+
+  const makeFieldLabel = (label: string) => (
+    <Typography color={theme.palette.TwClrTxtSecondary} sx={{ marginBottom: theme.spacing(1) }}>
+      {label}
+    </Typography>
+  );
+
+  const makeFieldValue = (value: string | undefined) => (
+    <Typography
+      color={theme.palette.TwClrTxt}
+      fontSize={theme.typography.h6.fontSize}
+      sx={{ marginBottom: theme.spacing(2) }}
+    >
+      {value}
+    </Typography>
+  );
+
   return (
-    <Page crumbs={crumbs} title={'project name'}>
-      <Card flushMobile style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+    <Page crumbs={crumbs} title={project?.name || ''}>
+      <Card flushMobile style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }} borderRadius={6}>
         <Grid container>
-          <Grid item xs={5}>
-            <Typography>{strings.NAME}</Typography>
+          <Grid item xs={4}>
+            {makeFieldLabel(strings.NAME)}
+            {makeFieldValue(project?.name)}
           </Grid>
-          <Grid item xs={7}>
-            <Typography>{strings.DESCRIPTION}</Typography>
+          <Grid item xs={8}>
+            {makeFieldLabel(strings.DESCRIPTION)}
+            {makeFieldValue(project?.description)}
           </Grid>
         </Grid>
       </Card>
