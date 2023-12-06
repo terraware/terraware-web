3d1ba951f6be96c487bd646957346fb98ffc67f3
Nick Graziano <nick.graziano@gmail.com>
2023-12-06 09:55:03 -0700

    Add single project selector and action to put project into state

--- src/redux/features/projects/projectsSelectors.ts
+++ src/redux/features/projects/projectsSelectors.ts
@@ -1,3 +1,9 @@
 import { RootState } from 'src/redux/rootReducer';
+import { Project } from 'src/types/Project';
 
 export const selectProjects = (state: RootState) => state.projects.projects;
+
+export const selectProject =
+  (projectId: number) =>
+  (state: RootState): Project | undefined =>
+    state.projects?.projects?.find((project) => project.id === projectId);
