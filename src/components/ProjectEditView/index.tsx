8330ef2176f76c5def17cfd4443ae5872a8a8625
Nick Graziano <nick.graziano@gmail.com>
2023-12-07 12:27:40 -0700

    Use createAsyncThunk for project update request, create new reducer for future async thunk requests for projects

--- src/components/ProjectEditView/index.tsx
+++ src/components/ProjectEditView/index.tsx
@@ -1,26 +1,31 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useHistory } from 'react-router';
 import { Typography } from '@mui/material';
 import { useAppDispatch, useAppSelector } from 'src/redux/store';
-import { selectProject } from 'src/redux/features/projects/projectsSelectors';
-import { requestProject, requestProjectUpdate } from 'src/redux/features/projects/projectsThunks';
+import { selectProject, selectProjectRequest } from 'src/redux/features/projects/projectsSelectors';
+import { requestProject } from 'src/redux/features/projects/projectsThunks';
 import TfMain from 'src/components/common/TfMain';
 import strings from 'src/strings';
 import ProjectForm from 'src/components/ProjectNewView/flow/ProjectForm';
 import { UpdateProjectRequest } from 'src/types/Project';
 import useStateLocation, { getLocation } from 'src/utils/useStateLocation';
 import { APP_PATHS } from 'src/constants';
+import { requestProjectUpdate } from 'src/redux/features/projects/projectsAsyncThunks';
+import useSnackbar from 'src/utils/useSnackbar';
 
 export default function ProjectEditView(): JSX.Element {
   const dispatch = useAppDispatch();
 
+  const snackbar = useSnackbar();
   const history = useHistory();
   const location = useStateLocation();
   const pathParams = useParams<{ projectId: string }>();
   const projectId = Number(pathParams.projectId);
 
   const project = useAppSelector(selectProject(projectId));
+  const [requestId, setRequestId] = useState<string>('');
+  const projectUpdateRequest = useAppSelector((state) => selectProjectRequest(state, requestId));
 
   useEffect(() => {
     if (!project) {
@@ -30,7 +35,8 @@ export default function ProjectEditView(): JSX.Element {
 
   const saveProject = useCallback(
     (_project: UpdateProjectRequest) => {
-      dispatch(requestProjectUpdate({ projectId, project: _project }));
+      const dispatched = dispatch(requestProjectUpdate({ projectId, project: _project }));
+      setRequestId(dispatched.requestId);
     },
     [dispatch, projectId]
   );
@@ -42,6 +48,18 @@ export default function ProjectEditView(): JSX.Element {
     );
     history.push(editProjectLocation);
   }, [history, location, pathParams.projectId]);
+
+  useEffect(() => {
+    if (!projectUpdateRequest) {
+      return;
+    }
+
+    if (projectUpdateRequest.status === 'error') {
+      snackbar.toastError();
+    } else if (projectUpdateRequest.status === 'success') {
+      goToProject();
+    }
+  }, [projectUpdateRequest, snackbar, goToProject]);
 
   return (
     <TfMain>
