08ec17f29a3c4a6d8db2b9fc1072a905991b62d1
Nicholas Graziano <nick.graziano@gmail.com>
2023-12-08 16:32:44 -0700

    Get rid of isBatch typeguard which was breaking the save process in the modal. Extended the FormRecord type to include the UpdateBatchRequestPayloadWithId type which is used for updates (#1876)

--- src/types/Batch.ts
+++ src/types/Batch.ts
@@ -1,26 +1,7 @@
 import { components } from 'src/api/types/generated-schema';
 import strings from 'src/strings';
-import { isArrayOfT, isNumber, isObject, isOptionalNumber, isString } from './utils';
 
 export type Batch = components['schemas']['BatchResponsePayload']['batch'];
-export const isBatch = (input: unknown): input is Batch =>
-  isObject(input) &&
-  isOptionalNumber(input.accessionId) &&
-  isString(input.addedDate) &&
-  isString(input.batchNumber) &&
-  isNumber(input.facilityId) &&
-  isNumber(input.germinatingQuantity) &&
-  isOptionalNumber(input.germinationRate) &&
-  isNumber(input.id) &&
-  isOptionalNumber(input.facilityId) &&
-  isString(input.notes) &&
-  isNumber(input.notReadyQuantity) &&
-  isString(input.readyByDate) &&
-  isNumber(input.readyQuantity) &&
-  isNumber(input.speciesId) &&
-  isArrayOfT<number>(input.subLocationIds, isNumber) &&
-  isNumber(input.version);
-
 export type BatchHistoryItem = components['schemas']['BatchHistoryPayload'];
 export type CreateBatchRequestPayload = components['schemas']['CreateBatchRequestPayload'];
 export type NurseryWithdrawal = components['schemas']['GetNurseryWithdrawalResponsePayload']['withdrawal'];
