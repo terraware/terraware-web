ae4c0bf4f61218e667a852a176c9652356e6561c
Nick Graziano <nick.graziano@gmail.com>
2023-12-12 16:29:21 -0700

    WIP - Add sublocation edit functionality to batch details view

--- src/components/InventoryV2/BatchSummary.tsx
+++ src/components/InventoryV2/BatchSummary.tsx
@@ -1,44 +1,58 @@
+import React, { useCallback, useState } from 'react';
 import { Grid, useTheme } from '@mui/material';
 import strings from 'src/strings';
 import { Batch } from 'src/types/Batch';
-import OverviewItemCard from '../common/OverviewItemCard';
-import { SubLocationService } from 'src/services';
-import React, { useEffect, useState } from 'react';
+import OverviewItemCard from 'src/components/common/OverviewItemCard';
 import Link from 'src/components/common/Link';
 import { APP_PATHS } from 'src/constants';
+import { useSubLocations } from 'src/components/InventoryV2/form/useSubLocations';
+import SubLocationsDropdown from 'src/components/InventoryV2/form/SubLocationsDropdown';
 
 interface BatchSummaryProps {
   batch: Batch;
 }
 
 export default function BatchSummary(props: BatchSummaryProps): JSX.Element {
-  const { batch } = props;
   const theme = useTheme();
-  const [batchSubLocations, setBatchSubLocations] = useState<string[]>([]);
 
-  useEffect(() => {
-    const setLocations = async () => {
-      if (batch?.facilityId) {
-        const response = await SubLocationService.getSubLocations(Number(batch.facilityId));
-        if (response.requestSucceeded) {
-          const nurserySLs: string[] = [];
-          batch.subLocationIds.forEach((subLocId) => {
-            const found = response.subLocations.find((iSublocation) => iSublocation.id === subLocId);
-            if (found) {
-              nurserySLs.push(found.name);
-            }
-          });
-          setBatchSubLocations(nurserySLs);
-        }
-      }
-    };
-    setLocations();
-  }, [batch]);
+  const [batch, setBatch] = useState<Batch>(props.batch);
+  const { availableSubLocations, selectedSubLocations } = useSubLocations(batch.facilityId, batch);
+
+  const [showSubLocationEdit, setShowSubLocationEdit] = useState<boolean>(false);
+
+  const handleSubLocationEdit = useCallback(() => {
+    console.log('edit');
+    setShowSubLocationEdit(!showSubLocationEdit);
+  }, [showSubLocationEdit]);
+
+  const handleUpdateSubLocations = useCallback(
+    (setFn: (previousBatch: Batch) => Batch) => {
+      const nextBatch = setFn(batch);
+      setBatch(nextBatch);
+    },
+    [batch]
+  );
 
   return (
     <Grid container spacing={3} marginBottom={theme.spacing(4)}>
       <Grid item xs={2}>
-        <OverviewItemCard isEditable={false} title={strings.SUB_LOCATION} contents={batchSubLocations.join(',')} />
+        <OverviewItemCard
+          title={strings.SUB_LOCATION}
+          contents={
+            showSubLocationEdit ? (
+              <SubLocationsDropdown<Batch>
+                availableSubLocations={availableSubLocations}
+                record={batch}
+                setRecord={handleUpdateSubLocations}
+                minimal
+              />
+            ) : (
+              (selectedSubLocations || []).map((subLocation) => subLocation.name).join(',')
+            )
+          }
+          isEditable
+          handleEdit={handleSubLocationEdit}
+        />
       </Grid>
       <Grid item xs={2}>
         <OverviewItemCard isEditable={false} title={strings.GERMINATION_RATE} contents={batch.germinationRate || '%'} />
--- src/components/InventoryV2/form/SubLocationsDropdown.tsx
+++ src/components/InventoryV2/form/SubLocationsDropdown.tsx
@@ -2,22 +2,36 @@ import { MultiSelect } from '@terraware/web-components';
 import strings from '../../../strings';
 import { SubLocation } from '../../../types/Facility';
 import React from 'react';
+import { makeStyles } from '@mui/styles';
+import { Theme } from '@mui/material';
 
 type SubLocationsDropdownProps<T extends { subLocationIds?: number[] } | undefined> = {
   availableSubLocations: SubLocation[] | undefined;
   record: T;
   setRecord: (setFn: (previousValue: T) => T) => void;
+  minimal: boolean;
 };
+
+const useStyles = makeStyles((theme: Theme) => ({
+  multiSelectStyle: {
+    height: '100%',
+    width: '100%',
+  },
+}));
 
 function SubLocationsDropdown<T extends { subLocationIds?: number[] } | undefined>({
   availableSubLocations,
   record,
   setRecord,
+  minimal,
 }: SubLocationsDropdownProps<T>) {
+  const classes = useStyles();
+
   return (
     <MultiSelect<number, string>
+      className={classes.multiSelectStyle}
       fullWidth={true}
-      label={strings.SUB_LOCATIONS}
+      label={!minimal ? strings.SUB_LOCATIONS : undefined}
       onAdd={(subLocationId: number) => {
         setRecord((previousValue) => ({
           ...previousValue,
