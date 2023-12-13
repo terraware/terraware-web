bfb0ddbe3a719011e26467d0160cf7d23ff46229
Karthik B <karthik@terraformation.com>
2023-12-13 09:08:56 -0800

    SW-4570 react hook to manage data with undo/redo capability (#1899)
    - a simple hook similar to useState that allows undo/redo functionality
    - for now this is super simple and can be extended later as needed,
    current functionality supports what we need for user drawn detailed
    sites
    - added some tests

---
+++ src/hooks/useUndoRedoState.ts
@@ -0,0 +1,53 @@
+import { useCallback, useMemo, useState } from 'react';
+
+export type SetFn<T> = (data: T) => void;
+export type UndoFn = (() => void) | undefined;
+export type RedoFn = (() => void) | undefined;
+
+/**
+ * A useState like functionality that keeps track of changes
+ * to support undo/redo.
+ *
+ * Returns array of [data, setData, undoFunction, redoFunction]
+ * where,
+ *  data is the current state of data
+ *  setData allows updating data, thereby adding to the stack of changes that can be undone/redone
+ *  undo is a callback function to undo last change, if function is 'undefined', undo is no longer possible (beginning of stack)
+ *  redo is a callback function to redo last change, if function is 'undefined', redo is no longer possible (end of stack)
+ */
+export default function useUndoRedoState<T>(initialValue?: T): [T | undefined, SetFn<T>, UndoFn, RedoFn] {
+  const [stack, setStack] = useState<(T | undefined)[]>([initialValue]);
+  const [stackIndex, setStackIndex] = useState<number>(0);
+
+  const data = useMemo<T | undefined>(() => stack[stackIndex], [stackIndex]);
+
+  const setData = useCallback(
+    (value: T) => {
+      setStack((curr: (T | undefined)[]) => {
+        const truncatedStack = curr.splice(0, stackIndex + 1);
+        truncatedStack.push(value);
+        return truncatedStack;
+      });
+      setStackIndex((curr: number) => curr + 1);
+    },
+    [stackIndex, setStack]
+  );
+
+  const undo = useMemo(() => {
+    if (stackIndex > 0) {
+      return () => void setStackIndex((curr: number) => curr - 1);
+    } else {
+      return undefined;
+    }
+  }, [stackIndex, setStackIndex]);
+
+  const redo = useMemo(() => {
+    if (stackIndex < stack.length - 1) {
+      return () => void setStackIndex((curr: number) => curr + 1);
+    } else {
+      return undefined;
+    }
+  }, [stack.length, stackIndex, setStackIndex]);
+
+  return [data, setData, undo, redo];
+}
