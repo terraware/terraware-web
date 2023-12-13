ddfa48465d4f22f466292d158dabd4eef79e31ff
Karthik B <karthik@terraformation.com>
2023-12-13 15:33:42 -0800

    SW-4571 Update site selection buttons with active/hover/selected styles (#1904)
    - with new info from Chudo
    - also refactored the component into 2, a selector and a button
    
    ![screencast 2023-12-13
    15-14-14](https://github.com/terraware/terraware-web/assets/1865174/e4d11479-9424-40e0-9abc-1c6c12b7d54a)

--- src/components/PlantingSites/PlantingSiteSelectTypeModal2.tsx
+++ src/components/PlantingSites/PlantingSiteSelectTypeModal2.tsx
@@ -1,9 +1,10 @@
+import React, { useCallback, useState } from 'react';
 import strings from 'src/strings';
-import React, { useState } from 'react';
 import { Box, Theme, Typography, useTheme } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { Button, DialogBox } from '@terraware/web-components';
 import { SiteType } from 'src/types/PlantingSite';
+import { getRgbaFromHex } from 'src/utils/color';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -64,14 +65,14 @@ export default function PlantingSiteSelectTypeModal(props: PlantingSiteSelectTyp
           {strings.ADD_PLANTING_SITE_TITLE}
         </Typography>
         <Box display='flex' flexDirection={isMobile ? 'column' : 'row'} gap={3}>
-          <SiteTypeButton
+          <SiteTypeSelector
             imageName='select-planting-site-simple@2x.png'
             description={strings.ADD_PLANTING_SITE_SIMPLE_SITE}
             isSelected={detailed === false}
             onClick={() => setDetailed(false)}
             title={strings.SIMPLE_SITE}
           />
-          <SiteTypeButton
+          <SiteTypeSelector
             imageName='select-planting-site-detailed@2x.png'
             description={strings.ADD_PLANTING_SITE_DETAILED_SITE}
             isSelected={detailed === true}
@@ -84,46 +85,76 @@ export default function PlantingSiteSelectTypeModal(props: PlantingSiteSelectTyp
   );
 }
 
+/**
+ * The clickable button/image for planting site type selection.
+ * This can be extracted out as a reusable component as needed later.
+ */
 type SiteTypeButtonProps = {
   imageName: string;
-  description: string;
   isSelected: boolean;
   onClick: () => void;
+};
+
+const SiteTypeButton = ({ imageName, isSelected, onClick }: SiteTypeButtonProps): JSX.Element => {
+  const theme = useTheme();
+
+  const rgb = useCallback(
+    (opacity: number): string => getRgbaFromHex(theme.palette.TwClrBgSelected as string, opacity / 100),
+    [theme]
+  );
+
+  return (
+    <Box
+      width='192px'
+      height='192px'
+      padding='10px'
+      borderRadius={theme.spacing(3)}
+      border={`2px solid ${theme.palette.TwClrBrdrTertiary}`}
+      margin='0 auto'
+      display='flex'
+      justifyContent='center'
+      sx={{
+        cursor: 'pointer',
+        backgroundColor: isSelected ? rgb(100) : rgb(0),
+        '&:hover': { backgroundColor: rgb(10) },
+        '&:active': { backgroundColor: rgb(20) },
+      }}
+      onClick={onClick}
+    >
+      <Box
+        width='160px'
+        height='160px'
+        margin='auto'
+        borderRadius='8px'
+        sx={{
+          backgroundImage: `url(/assets/${imageName})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          '&:active': { border: `1px solid ${theme.palette.TwClrBgSelected}` },
+        }}
+      />
+    </Box>
+  );
+};
+
+/**
+ * Planting Site type selector that renders a selection button
+ * with title and description.
+ */
+type SiteTypeSelectorProps = SiteTypeButtonProps & {
+  description: string;
   title: string;
 };
 
-const SiteTypeButton = ({ imageName, description, isSelected, onClick, title }: SiteTypeButtonProps): JSX.Element => {
+const SiteTypeSelector = (props: SiteTypeSelectorProps): JSX.Element => {
   const theme = useTheme();
   const { isMobile } = useDeviceInfo();
+  const { description, title } = props;
+  const { ...buttonProps }: SiteTypeButtonProps = props;
 
   return (
     <Box minWidth={isMobile ? 'auto' : '350px'} display='flex' flexDirection='column'>
-      <Box
-        width='192px'
-        height='192px'
-        padding='10px'
-        borderRadius={theme.spacing(3)}
-        border={
-          isSelected ? `4px solid ${theme.palette.TwClrBrdrSelected}` : `2px solid ${theme.palette.TwClrBrdrTertiary}`
-        }
-        margin='0 auto'
-        display='flex'
-        justifyContent='center'
-        sx={{ cursor: 'pointer' }}
-        onClick={onClick}
-      >
-        <Box
-          width='160px'
-          height='160px'
-          margin='auto'
-          borderRadius='8px'
-          sx={{
-            backgroundImage: `url(/assets/${imageName})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-          }}
-        />
-      </Box>
+      <SiteTypeButton {...buttonProps} />
       <Typography fontSize='20px' lineHeight='28px' fontWeight={600} textAlign='center' margin={theme.spacing(2, 0, 3)}>
         {title}
       </Typography>
