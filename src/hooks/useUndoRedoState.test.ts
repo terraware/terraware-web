5d84587f9946ca4ea13c4a91a2d1dfc8ab9c495b
Karthik Balachandran <karthik@terraformation.com>
2023-12-12 18:05:21 -0800

    SW-4570 react hook to manage data with undo/redo capability
    - a simple hook similar to useState that allows undo/redo functionality
    - for now this is super simple and can be extended later as needed, current functionality supports what we need for user drawn detailed sites
    - added some tests

---
+++ src/hooks/useUndoRedoState.test.ts
@@ -0,0 +1,104 @@
+import { renderHook, act } from '@testing-library/react-hooks';
+import useUndoRedoState from './useUndoRedoState';
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+const dataWith = (result: any) => result.current[0];
+const setDataWith = (result: any) => result.current[1];
+const undoWith = (result: any) => result.current[2];
+const redoWith = (result: any) => result.current[3];
+
+describe('useUndoRedoState', () => {
+  test('should show un-initialized value with nothing to undo or redo, upon initialization', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>());
+
+    const [data, setData, undo, redo] = result.current;
+
+    expect(data).toBeUndefined();
+    expect(setData).not.toBeUndefined();
+    expect(undo).toBeUndefined();
+    expect(redo).toBeUndefined();
+  });
+
+  test('should show initialized value with nothing to undo or redo, upon initialization', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    const [data, setData, undo, redo] = result.current;
+
+    expect(data).toEqual({ name: 'bugs', age: 25 });
+    expect(setData).not.toBeUndefined();
+    expect(undo).toBeUndefined();
+    expect(redo).toBeUndefined();
+  });
+
+  test('should allow setting data', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    act(() => void setDataWith(result)({ name: 'bunny', age: 5 }));
+
+    expect(dataWith(result)).toEqual({ name: 'bunny', age: 5 });
+    expect(undoWith(result)).not.toBeUndefined();
+    expect(redoWith(result)).toBeUndefined();
+  });
+
+  test('should allow undoing data', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    act(() => void setDataWith(result)({ name: 'bunny', age: 5 }));
+    act(() => void undoWith(result)());
+
+    expect(dataWith(result)).toEqual({ name: 'bugs', age: 25 });
+    expect(undoWith(result)).toBeUndefined();
+  });
+
+  test('should allow redoing data', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    act(() => void setDataWith(result)({ name: 'bunny', age: 5 }));
+    expect(dataWith(result)).toEqual({ name: 'bunny', age: 5 });
+
+    act(() => void undoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'bugs', age: 25 });
+
+    act(() => void redoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'bunny', age: 5 });
+  });
+
+  test('should allow multiple undo/redo across history', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    act(() => void setDataWith(result)({ name: 'bunny', age: 5 }));
+    act(() => void setDataWith(result)({ name: 'roadrunner', age: 35 }));
+    act(() => void setDataWith(result)({ name: 'wiley', age: 55 }));
+
+    expect(dataWith(result)).toEqual({ name: 'wiley', age: 55 });
+
+    act(() => void undoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'roadrunner', age: 35 });
+
+    act(() => void undoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'bunny', age: 5 });
+
+    act(() => void redoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'roadrunner', age: 35 });
+
+    act(() => void redoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'wiley', age: 55 });
+  });
+
+  test('changing data should clear redo stack', () => {
+    const { result } = renderHook(() => useUndoRedoState<Person>({ name: 'bugs', age: 25 }));
+
+    act(() => void setDataWith(result)({ name: 'bunny', age: 5 }));
+    expect(dataWith(result)).toEqual({ name: 'bunny', age: 5 });
+
+    act(() => void undoWith(result)());
+    expect(dataWith(result)).toEqual({ name: 'bugs', age: 25 });
+
+    act(() => void setDataWith(result)({ name: 'roadrunner', age: 35 }));
+    expect(redoWith(result)).toBeUndefined();
+  });
+});
---
+++ src/hooks/useUndoRedoState.ts
@@ -0,0 +1,53 @@
+import { useCallback, useMemo, useState } from 'react';
+
+export type SetFn<T> = (data: T) => void;
+export type UndoFn = (() => void) | undefined;
+export type RedoFn = (() => void) | undefined;
+
+/**
+ * A useState like functionality that keeps track of changes
+ * to support undo/redo.
+ *
+ * Returns array of [data, setData, undoFunction, redoFunction]
+ * where,
+ *  data is the current state of data
+ *  setData allows updating data, thereby adding to the history of changes that can be undone/redone
+ *  undo is a callback function to undo last change, if function is 'undefined', undo is no longer possible (beginning of history)
+ *  redo is a callback function to redo last change, if function is 'undefined', redo is no longer possible (end of history)
+ */
+export default function useUndoRedoState<T>(initialValue?: T): [T, SetFn<T>, UndoFn, RedoFn] {
+  const [history, setHistory] = useState<T[]>(initialValue !== undefined ? [initialValue] : []);
+  const [historyIndex, setHistoryIndex] = useState<number>(0);
+
+  const data = useMemo<T>(() => history[historyIndex], [historyIndex]);
+
+  const setData = useCallback(
+    (value: T) => {
+      setHistory((curr: T[]) => {
+        const truncatedHistory = curr.splice(0, historyIndex + 1);
+        truncatedHistory.push(value);
+        return truncatedHistory;
+      });
+      setHistoryIndex((curr: number) => curr + 1);
+    },
+    [historyIndex, setHistory]
+  );
+
+  const undo = useMemo(() => {
+    if (historyIndex > 0) {
+      return () => void setHistoryIndex((curr: number) => curr - 1);
+    } else {
+      return undefined;
+    }
+  }, [historyIndex, setHistoryIndex]);
+
+  const redo = useMemo(() => {
+    if (historyIndex < history.length - 1) {
+      return () => void setHistoryIndex((curr: number) => curr + 1);
+    } else {
+      return undefined;
+    }
+  }, [history.length, historyIndex, setHistoryIndex]);
+
+  return [data, setData, undo, redo];
+}
