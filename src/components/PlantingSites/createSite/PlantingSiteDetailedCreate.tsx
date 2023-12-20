1c5c3895f8cc71cf21cfdff0e51d6d917922683d
Karthik Balachandran <karthik@terraformation.com>
2023-12-20 11:20:14 -0800

    SW-4539 Add instructions modal with placeholder content
    - implementation assumes a youtube video, this can be changed later depending on content type
    - moved files into a createSite directory as there will be a lot more content coming up
    - the video modal is currently specific to planting site create and not generic, we can extract things out once we have multiple use-cases
    - added a story

--- src/components/PlantingSites/createSite/PlantingSiteDetailedCreate.tsx
+++ src/components/PlantingSites/createSite/PlantingSiteDetailedCreate.tsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import TfMain from 'src/components/common/TfMain';
 import { Box, Container, Typography } from '@mui/material';
+import InstructionsModal from './InstructionsModal';
 
 type PlantingSiteDetailedCreateProps = {
   reloadPlantingSites: () => void;
 };
 
 export default function PlantingSiteDetailedCreate(props: PlantingSiteDetailedCreateProps): JSX.Element {
+  // this is a placeholder for the instructions modal trigger
+  const [showModal, setShowModal] = useState<boolean>(true);
+  const onClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <TfMain>
+      <InstructionsModal open={showModal} onClose={onClose} />
       <Container maxWidth={false} sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1, paddingRight: 0 }}>
         <Box display='flex' margin='auto auto'>
           <Typography fontSize='24px' fontWeight='bold'>
--- src/docLinks.ts
+++ src/docLinks.ts
@@ -1,22 +1,29 @@
 import { useLocalization } from 'src/providers';
 import { SupportedLocaleId } from 'src/strings/locales';
 
-export type DocType = 'privacy_policy' | 'report_a_problem' | 'request_a_feature' | 'contact_us';
+export type DocType =
+  | 'contact_us'
+  | 'planting_site_create_instructions_video'
+  | 'privacy_policy'
+  | 'report_a_problem'
+  | 'request_a_feature';
 
 type DocLink = Record<DocType, string>;
 
 const DOC_LINKS: Record<SupportedLocaleId, DocLink> = {
   en: {
+    contact_us: 'https://www.terraformation.com/contact-us/terraware-support-contact-us',
+    planting_site_create_instructions_video: 'https://www.youtube.com/embed/kfPlGeiFebw', // placeholder, SW-4538
     privacy_policy: 'https://www.terraformation.com/privacy-policy',
     report_a_problem: 'https://www.terraformation.com/contact-us/terraware-support-bug-report',
     request_a_feature: 'https://www.terraformation.com/contact-us/terraware-support-feature-request',
-    contact_us: 'https://www.terraformation.com/contact-us/terraware-support-contact-us',
   },
   es: {
+    contact_us: 'https://www.terraformation.com/contact-us/soporte-terraware-contactenos',
+    planting_site_create_instructions_video: 'https://www.youtube.com/embed/kfPlGeiFebw', // placeholder, SW-4538
     privacy_policy: 'https://www.terraformation.com/politica-de-privacidad',
     report_a_problem: 'https://www.terraformation.com/contact-us/soporte-terraware-informe-de-un-problema',
     request_a_feature: 'https://www.terraformation.com/contact-us/soporte-terraware-solicite-una-funcion',
-    contact_us: 'https://www.terraformation.com/contact-us/soporte-terraware-contactenos',
   },
 };
 
