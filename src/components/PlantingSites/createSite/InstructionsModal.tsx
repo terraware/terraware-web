34bd496a683a087e7b8e25e2ab55dbf916accc5c
Karthik B <karthik@terraformation.com>
2023-12-20 12:06:41 -0800

    SW-4539 Add instructions modal with placeholder content (#1940)
    - implementation assumes a youtube video, this can be changed later
    depending on content type
    - moved files into a createSite directory as there will be a lot more
    content coming up
    - the video modal is currently specific to planting site create and not
    generic, we can extract things out once we have multiple use-cases
    - added a story
    
    <img width="923" alt="Terraware App 2023-12-20 10-37-00"
    src="https://github.com/terraware/terraware-web/assets/1865174/2743b100-5ba9-4f7c-9713-c554f96a33b5">
    
    <img width="222" alt="Terraware App 2023-12-20 10-37-19"
    src="https://github.com/terraware/terraware-web/assets/1865174/5fa44f47-e957-49f3-b099-b0cdb9a35d3d">

---
+++ src/components/PlantingSites/createSite/InstructionsModal.tsx
@@ -0,0 +1,63 @@
+import strings from 'src/strings';
+import { Box, Theme, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { Button, DialogBox } from '@terraware/web-components';
+import { useDocLinks } from 'src/docLinks';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  buttonSpacing: {
+    marginRight: theme.spacing(2),
+  },
+  icon: {
+    width: '150px',
+    height: '150px',
+  },
+}));
+
+export type InstructionsModalProps = {
+  open: boolean;
+  onClose: (dontShowAgain?: boolean) => void;
+};
+
+export default function InstructionsModal(props: InstructionsModalProps): JSX.Element {
+  const { open, onClose } = props;
+  const classes = useStyles();
+  const docLinks = useDocLinks();
+
+  return (
+    <DialogBox
+      scrolled
+      onClose={() => onClose()}
+      open={open}
+      title={strings.PLANTING_SITE_CREATE_INSTRUCTIONS_TITLE}
+      size={'large'}
+      middleButtons={[
+        <Button
+          onClick={() => onClose(true)}
+          id='dont-show-again'
+          label={strings.DONT_SHOW_AGAIN}
+          priority='secondary'
+          type='passive'
+          className={classes.buttonSpacing}
+          key='button-1'
+        />,
+        <Button onClick={() => onClose()} id='next' label={strings.CLOSE} key='button-2' />,
+      ]}
+    >
+      <Box display='flex' flexDirection='column'>
+        <Typography textAlign='center' marginBottom={2}>
+          {strings.PLANTING_SITE_CREATE_INSTRUCTIONS_DESCRIPTION}
+        </Typography>
+        <iframe
+          width='100%'
+          height='320px'
+          src={docLinks.planting_site_create_instructions_video}
+          title={strings.PLANTING_SITE_CREATE_INSTRUCTIONS_TITLE}
+          frameBorder='0'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+          allowFullScreen
+        />
+      </Box>
+    </DialogBox>
+  );
+}
