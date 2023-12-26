4f2c8bef5c3432ae8bc9fcbc746f184caffbcec3
Nick Graziano <nick.graziano@gmail.com>
2023-12-26 11:54:25 -0700

    SW-4623 Add accessions search functionality to redux, store accessions in state based on org ID and species ID requested. Falls back to species ID '0' if we are requesting all accessions for an org.

---
+++ src/redux/features/accessions/accessionsThunks.ts
@@ -0,0 +1,66 @@
+import { Dispatch } from 'redux';
+import { RootState } from 'src/redux/rootReducer';
+import { SearchNodePayload } from 'src/types/Search';
+import strings from 'src/strings';
+import SeedBankService from 'src/services/SeedBankService';
+import { setAccessionsAction } from './accessionsSlice';
+
+const SEARCH_FIELDS_ACCESSIONS = ['id', 'accessionNumber', 'speciesName'];
+
+export type SearchResponseAccession = {
+  id: string;
+  accessionNumber: string;
+  speciesName: string;
+};
+
+export const requestAccessions = (organizationId: number, speciesId?: number) => {
+  return async (dispatch: Dispatch, _getState: () => RootState) => {
+    const stateSpeciesId = speciesId ?? 0;
+    const orgIdSpeciesId = `${organizationId}-${stateSpeciesId}`;
+
+    try {
+      const searchCriteria: { [key: string]: SearchNodePayload } = {};
+      searchCriteria.excludeUsedUp = {
+        operation: 'not',
+        child: {
+          operation: 'field',
+          field: 'state',
+          type: 'Exact',
+          values: [strings.USED_UP],
+        },
+      };
+
+      if (stateSpeciesId) {
+        searchCriteria.speciesIds = {
+          operation: 'field',
+          field: 'species_id',
+          type: 'Exact',
+          values: [stateSpeciesId.toString()],
+        };
+      }
+
+      const results: SearchResponseAccession[] | null = await SeedBankService.searchAccessions({
+        organizationId,
+        fields: SEARCH_FIELDS_ACCESSIONS,
+        searchCriteria,
+      });
+
+      if (!results?.length) {
+        dispatch(setAccessionsAction({ orgIdSpeciesId, data: { error: true } }));
+        return;
+      }
+
+      dispatch(setAccessionsAction({ orgIdSpeciesId, data: { accessions: results } }));
+    } catch (e: unknown) {
+      // should not happen, the response above captures any http request errors
+      // tslint:disable-next-line: no-console
+      const errorMessage = (e as Error).message ?? e;
+      dispatch(
+        setAccessionsAction({
+          orgIdSpeciesId,
+          data: { error: `Error dispatching request accessions - ${errorMessage}` },
+        })
+      );
+    }
+  };
+};
