cecc291d00eda46d263147da6880777d07f70d4e
Nick Graziano <nick.graziano@gmail.com>
2023-12-28 15:29:47 -0700

    Fix import

--- src/redux/features/accessions/accessionsSlice.ts
+++ src/redux/features/accessions/accessionsSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { SearchResponseAccession } from './accessionsThunks';
+import { SearchResponseAccession } from 'src/services/SeedBankService';
 
 type OrgIdSpeciesId = string;
 
