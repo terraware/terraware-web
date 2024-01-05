ac5843c790893513947566052710d28a269f4d85
Nick Graziano <nick.graziano@gmail.com>
2024-01-05 13:57:10 -0700

    SW-4520 Implement reports settings update API call

--- src/services/ReportSettingsService.ts
+++ src/services/ReportSettingsService.ts
@@ -1,24 +1,35 @@
 import { paths } from 'src/api/types/generated-schema';
-import HttpService, { Response } from './HttpService';
+import HttpService, { Response, Response2 } from './HttpService';
 
 const REPORTS_SETTINGS_ENDPOINT = '/api/v1/reports/settings';
 
-export type ReportsSettingsResponsePayload =
+export type GetReportsSettingsResponsePayload =
   paths[typeof REPORTS_SETTINGS_ENDPOINT]['get']['responses'][200]['content']['application/json'];
 
-export type ReportsSettingsResponse = Response & ReportsSettingsResponsePayload;
+export type PutReportsSettingsRequestPayload =
+  paths[typeof REPORTS_SETTINGS_ENDPOINT]['put']['requestBody']['content']['application/json'];
+export type PutReportsSettingsResponsePayload =
+  paths[typeof REPORTS_SETTINGS_ENDPOINT]['put']['responses'][200]['content']['application/json'];
+
+export type GetReportsSettingsResponse = Response & GetReportsSettingsResponsePayload;
+export type ReportsSettings = Omit<GetReportsSettingsResponsePayload, 'status'>;
 
 const httpReportsSettings = HttpService.root(REPORTS_SETTINGS_ENDPOINT);
 
-const getReportsSettings = (organizationId: number): Promise<ReportsSettingsResponse> => {
-  return httpReportsSettings.get<ReportsSettingsResponsePayload, ReportsSettingsResponsePayload | undefined>(
+const getReportsSettings = (organizationId: number): Promise<GetReportsSettingsResponse> =>
+  httpReportsSettings.get<GetReportsSettingsResponsePayload, GetReportsSettingsResponsePayload | undefined>(
     { params: { organizationId: organizationId.toString() } },
     (data) => data
   );
-};
+
+const updateSettings = (
+  payload: PutReportsSettingsRequestPayload
+): Promise<Response2<PutReportsSettingsResponsePayload>> =>
+  httpReportsSettings.put2<PutReportsSettingsResponsePayload>({ entity: payload });
 
 const ReportsSettingsService = {
   getReportsSettings,
+  updateSettings,
 };
 
 export default ReportsSettingsService;
