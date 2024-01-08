00fc85871f9ad1a96bf5794d2a28aa2d994c8d7f
Nick Graziano <nick.graziano@gmail.com>
2024-01-08 11:17:29 -0700

    relative paths

--- src/components/Reports/ReportLink.tsx
+++ src/components/Reports/ReportLink.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { APP_PATHS } from 'src/constants';
-import Link from 'src/common/Link';
+import Link from 'src/components/common/Link';
 import { ListReport } from 'src/types/Report';
 
 interface ReportLinkProps {
