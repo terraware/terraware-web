db8e41dacf6e11cbd2942c58c3f6cda28d336a49
Steven Grimm <steven@terraformation.com>
2024-01-08 10:57:20 -0800

    Try running with just Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -51,6 +52,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
+    */
 
     /* Test against mobile viewports. */
     // {
@@ -75,7 +77,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
+  //   command: 'craco start',
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
