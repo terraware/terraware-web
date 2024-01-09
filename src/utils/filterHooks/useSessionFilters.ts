01a6e9a763ac7bae1a70b68ecf2776aa7f03dcf1
Nick Graziano <nick.graziano@gmail.com>
2024-01-09 12:22:17 -0700

    commit rest of files

--- src/utils/filterHooks/useSessionFilters.ts
+++ src/utils/filterHooks/useSessionFilters.ts
@@ -2,104 +2,60 @@ import { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import useQuery from 'src/utils/useQuery';
 import useStateLocation, { getLocation } from 'src/utils/useStateLocation';
-import { isArray } from 'src/types/utils';
-import { useSessionFilters } from 'src/utils/filterHooks/useSessionFilters';
-import { denormalizeValue, filtersEqual, FiltersType, normalizeValue } from 'src/utils/filterHooks';
+import {
+  FiltersType,
+  getFiltersFromQuery,
+  getFiltersFromSession,
+  resetQuery,
+  writeFiltersToQuery,
+  writeFiltersToSession,
+} from 'src/utils/filterHooks';
 
-const isFilterKey = (key: string) => key.includes('_filter_');
-const scrubFilterKey = (key: string) => key.replace(/[a-z]+_filter_(.+)/i, '$1');
-const makeFilterKey = (viewIdentifier: string, key: string) => `${viewIdentifier}_filter_${key}`;
-
-export const useQueryFilters = (viewIdentifier: string) => {
+export const useSessionFilters = (viewIdentifier: string, writeToQuery = true) => {
   const location = useStateLocation();
   const history = useHistory();
   const query = useQuery();
-  const { sessionFilters, setSessionFilters } = useSessionFilters(viewIdentifier);
 
-  const [queryFilters, setQueryFilters] = useState<FiltersType>({});
+  const [localFilters, setLocalFilters] = useState<FiltersType>({});
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
 
-  const resetQuery = useCallback(() => {
-    query.forEach((value, key) => {
-      if (isFilterKey(key)) {
-        query.delete(key);
-      }
-    });
-  }, [query]);
-
-  // Sync filters to query
-  const _setQueryFilters = useCallback(
+  // Sync filters to query and session, this happens when filters are changed within the consumer
+  const _setLocalFilters = useCallback(
     (filters: FiltersType) => {
-      console.log('filters', filters);
-      setQueryFilters(filters);
-      resetQuery();
+      setLocalFilters(filters);
 
-      Object.keys(filters).forEach((key) => {
-        const value = filters[key];
-        if (!value || (isArray(value) && value.length === 0)) {
-          return;
-        }
+      writeFiltersToSession(viewIdentifier, filters);
 
-        const queryKey = makeFilterKey(viewIdentifier, key);
-        console.log('queryKey, normalizeValue(value)', queryKey, normalizeValue(value));
-        query.set(queryKey, normalizeValue(value));
-      });
-
+      resetQuery(query, viewIdentifier);
+      writeFiltersToQuery(query, viewIdentifier, filters);
       history.replace(getLocation(location.pathname, location, query.toString()));
     },
-    [history, location, query, resetQuery, viewIdentifier]
+    [history, location, query, viewIdentifier]
   );
 
-  // Sync from session
-  useEffect(() => {
-    console.log('queryFilters, sessionFilters', queryFilters, sessionFilters);
-    if (!filtersEqual(queryFilters, sessionFilters)) {
-      setQueryFilters(sessionFilters);
-    }
-  }, [queryFilters, sessionFilters]);
-
-  // Sync from query
+  // Query overrides session, pull filters from query and session, write merged filters back to session
   const init = useCallback(() => {
     if (isInitialized) {
       return;
     }
 
-    const currentQueryFilters: FiltersType = {};
+    const currentQueryFilters = getFiltersFromQuery(query, viewIdentifier);
+    const currentSessionFilters = getFiltersFromSession(viewIdentifier);
 
-    const queryKeys: string[] = [];
+    const mergedFilters: FiltersType = {
+      ...currentSessionFilters,
+      ...currentQueryFilters,
+    };
 
-    const keysIter = query.keys();
-    let keyPos = keysIter.next();
+    setLocalFilters(mergedFilters);
 
-    while (!keyPos.done) {
-      if (query.get(keyPos.value)) {
-        queryKeys.push(keyPos.value);
-      }
-      keyPos = keysIter.next();
-    }
+    writeFiltersToSession(viewIdentifier, mergedFilters);
 
-    console.log('queryKeys', queryKeys);
-    queryKeys
-      .sort((a, b) => a.localeCompare(b))
-      .forEach((key) => {
-        if (isFilterKey(key)) {
-          const value = query.get(key);
-          console.log('value', value);
-          if (!value) {
-            return;
-          }
-
-          const filterKey = scrubFilterKey(key);
-          currentQueryFilters[filterKey] = denormalizeValue(value);
-        }
-      });
-
-    console.log('currentQueryFilters', currentQueryFilters);
-    setQueryFilters(currentQueryFilters);
-    setSessionFilters(currentQueryFilters);
+    writeFiltersToQuery(query, viewIdentifier, mergedFilters);
+    history.replace(getLocation(location.pathname, location, query.toString()));
 
     setIsInitialized(true);
-  }, [query, setSessionFilters, isInitialized]);
+  }, [history, isInitialized, location, query, viewIdentifier]);
 
   useEffect(() => {
     if (isInitialized) {
@@ -110,5 +66,8 @@ export const useQueryFilters = (viewIdentifier: string) => {
     setIsInitialized(true);
   }, [init, isInitialized]);
 
-  return { queryFilters, setQueryFilters: _setQueryFilters };
+  return {
+    sessionFilters: localFilters,
+    setSessionFilters: _setLocalFilters,
+  };
 };
