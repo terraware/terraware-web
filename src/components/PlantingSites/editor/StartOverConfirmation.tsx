a6034145c68514db1e4adf88186877741a52c638
Karthik B <karthik@terraformation.com>
2024-01-12 13:30:52 -0800

    SW-4690 Planting site edit start over confirmation (#2030)
    - show confirmation modal and reset if user confirms
    
    <img width="789" alt="Terraware App 2024-01-10 15-11-17"
    src="https://github.com/terraware/terraware-web/assets/1865174/88740be3-bac2-43c4-8e2a-0a69bea11cfe">

---
+++ src/components/PlantingSites/editor/StartOverConfirmation.tsx
@@ -0,0 +1,35 @@
+import { Typography } from '@mui/material';
+import { Button, DialogBox } from '@terraware/web-components';
+import strings from 'src/strings';
+
+export type StartOverConfirmationProps = {
+  onClose: () => void;
+  onConfirm: () => void;
+};
+
+export default function StartOverConfirmation(props: StartOverConfirmationProps): JSX.Element {
+  const { onClose, onConfirm } = props;
+
+  return (
+    <DialogBox
+      onClose={onClose}
+      open={true}
+      title={strings.START_OVER}
+      size='medium'
+      middleButtons={[
+        <Button
+          id='cancelStartOver'
+          label={strings.NO}
+          type='passive'
+          onClick={onClose}
+          priority='secondary'
+          key='button-1'
+        />,
+        <Button id='confirmStartOver' onClick={onConfirm} type='destructive' label={strings.YES} key='button-2' />,
+      ]}
+      message={strings.START_OVER_WARNING}
+    >
+      <Typography>{strings.ARE_YOU_SURE}</Typography>
+    </DialogBox>
+  );
+}
