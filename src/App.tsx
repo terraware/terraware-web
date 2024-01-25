5d223a98b12ccc996a7e87a3da9f05fef8494f62
Nicholas Graziano <nick.graziano@gmail.com>
2024-01-25 16:19:28 -0700

    SW-4761 Create RedirectsRouter that contains all the '$path/' -> '$path' redirects (#2078)

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,7 @@ import InventoryRouter from 'src/scenes/InventoryRouter';
 import BatchBulkWithdrawView from 'src/scenes/BatchBulkWithdrawView';
 import NurseryRouter from 'src/scenes/NurseryRouter';
 import MyAccountRouter from 'src/scenes/MyAccountRouter';
+import RedirectsRouter from 'src/scenes/RedirectsRouter';
 
 interface StyleProps {
   isDesktop?: boolean;
@@ -376,37 +377,7 @@ function AppContent() {
               </Route>
             )}
 
-            {/* Redirects. Invalid paths will redirect to the closest valid path. */}
-            {/* In alphabetical order for easy reference with APP_PATHS, except for /home which must go last */}
-            <Route path={APP_PATHS.ACCESSIONS + '/'}>
-              <Redirect to={APP_PATHS.ACCESSIONS} />
-            </Route>
-            <Route path={APP_PATHS.CHECKIN + '/'}>
-              <Redirect to={APP_PATHS.CHECKIN} />
-            </Route>
-            <Route path={APP_PATHS.CONTACT_US + '/'}>
-              <Redirect to={APP_PATHS.CONTACT_US} />
-            </Route>
-            <Route exact path={APP_PATHS.ORGANIZATION + '/'}>
-              <Redirect to={APP_PATHS.ORGANIZATION} />
-            </Route>
-            <Route exact path={APP_PATHS.PEOPLE + '/'}>
-              <Redirect to={APP_PATHS.PEOPLE} />
-            </Route>
-            {featureFlagProjects && (
-              <Route exact path={APP_PATHS.PROJECTS + '/'}>
-                <Redirect to={APP_PATHS.PROJECTS} />
-              </Route>
-            )}
-            <Route path={APP_PATHS.SEEDS_DASHBOARD + '/'}>
-              <Redirect to={APP_PATHS.SEEDS_DASHBOARD} />
-            </Route>
-            <Route path={APP_PATHS.SPECIES + '/'}>
-              <Redirect to={APP_PATHS.SPECIES} />
-            </Route>
-            <Route path='*'>
-              <Redirect to={APP_PATHS.HOME} />
-            </Route>
+            <RedirectsRouter />
           </Switch>
         </ErrorBoundary>
       </div>
