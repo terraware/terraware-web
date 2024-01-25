6be753c42a255f01916072f452e86a546057ce4d
Karthik Balachandran <karthik@terraformation.com>
2024-01-25 05:45:29 -0800

    - add return

--- src/components/PlantingSites/editor/Subzones.tsx
+++ src/components/PlantingSites/editor/Subzones.tsx
@@ -226,6 +226,7 @@ export default function Subzones({ onValidate, site }: SubzonesProps): JSX.Eleme
       }
       if (selectedZone === undefined || selectedZone !== zone.properties.id) {
         setSelectedZone(zone.properties.id);
+        return undefined;
       } else {
         // select the subzone under the click
         return subzone;
