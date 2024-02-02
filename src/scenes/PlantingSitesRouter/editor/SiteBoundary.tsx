2b6d246c547355dbc6b11fdbe0fe6d8821305755
Karthik B <karthik@terraformation.com>
2024-02-02 12:47:28 -0800

    SW-4815 Check that site boundary can support a minimum zone size (#2121)
    - we were previously checking for large boundaries
    - also need to check for small boundaries since zones cannot be smaller
    than 100m x 100m
    - add a check for all disjoint site boundary polygons and prevent any of
    them smaller than 100m x 100m
    - create default zone on each of the disjoint polygons (previously we
    were creating a single zone across polygons that was a bug)
    
    <img width="761" alt="Terraware App 2024-02-01 18-30-10"
    src="https://github.com/terraware/terraware-web/assets/1865174/85daadc8-0200-4793-9d13-2f3c01162ce7">
    
    <img width="763" alt="Terraware App 2024-02-01 18-39-32"
    src="https://github.com/terraware/terraware-web/assets/1865174/0e1e4f7a-6e30-43f9-a763-0ebd912059fa">

--- src/scenes/PlantingSitesRouter/editor/SiteBoundary.tsx
+++ src/scenes/PlantingSitesRouter/editor/SiteBoundary.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useMemo, useState } from 'react';
 import { Box } from '@mui/material';
-import { Feature, FeatureCollection, MultiPolygon } from 'geojson';
+import { Feature, FeatureCollection, MultiPolygon, Polygon, Position } from 'geojson';
 import bbox from '@turf/bbox';
 import bboxPolygon from '@turf/bbox-polygon';
 import centroid from '@turf/centroid';
 import _ from 'lodash';
 import strings from 'src/strings';
-import { PlantingSite } from 'src/types/Tracking';
+import { PlantingSite, PlantingZone } from 'src/types/Tracking';
 import useSnackbar from 'src/utils/useSnackbar';
 import useUndoRedoState from 'src/hooks/useUndoRedoState';
 import { MapEditorMode } from 'src/components/Map/EditableMapDrawV2';
@@ -52,55 +52,71 @@ export default function SiteBoundary({ isSimpleSite, onChange, onValidate, site 
 
   const errorAnnotations = useMemo<Feature[] | undefined>(() => {
     // if bounding area is too large, show error message at center of bounding box, also show the bounding box
-    if (boundary && boundingAreaTooLarge) {
-      const bboxPoly = bboxPolygon(bbox(_.cloneDeep(boundary)));
-      const c = centroid(bboxPoly);
-      const errorText = strings.formatString(strings.SITE_BOUNDING_AREA_TOO_LARGE, boundingArea);
-      return [
-        { ...c, properties: { errorText }, id: 0 },
-        { ...bboxPoly, id: 1 },
-      ];
-    } else {
-      return undefined;
+    if (boundary) {
+      // if bounding area is too large, show that error first and skip other errors to avoid overwhelming user
+      if (boundingAreaTooLarge) {
+        const bboxPoly = bboxPolygon(bbox(_.cloneDeep(boundary)));
+        const c = centroid(bboxPoly);
+        const errorText = strings.formatString(strings.SITE_BOUNDING_AREA_TOO_LARGE, boundingArea);
+        return [
+          { ...c, properties: { errorText }, id: 0 },
+          { ...bboxPoly, id: 1 },
+        ];
+      }
+
+      // check if individual polygons are of a minimum size
+      const individualPolygons: Polygon[] = boundary.coordinates.flatMap((coordinates: Position[][]) => ({
+        type: 'Polygon',
+        coordinates,
+      }));
+      let index = 0;
+      const polygonsTooSmall = individualPolygons
+        .flatMap((poly: Polygon) => {
+          const area = boundingAreaHectares(poly);
+          if (area < 1) {
+            // stopgap to check for 100m x 100m until we have BE API
+            const errorText = strings.formatString(strings.SITE_BOUNDARY_POLYGON_TOO_SMALL, area);
+            return [{ type: 'Feature', geometry: poly, properties: { errorText, fill: true }, id: index++ } as Feature];
+          } else {
+            return [];
+          }
+        })
+        .filter((feature) => !!feature);
+
+      if (polygonsTooSmall.length) {
+        return polygonsTooSmall;
+      }
     }
+    return undefined;
   }, [boundary, boundingArea, boundingAreaTooLarge]);
 
   useEffect(() => {
     if (onValidate) {
-      if (!boundary || boundingAreaTooLarge) {
+      if (!boundary || errorAnnotations?.length) {
         snackbar.toastError(
-          boundingAreaTooLarge
-            ? (strings.formatString(strings.SITE_BOUNDING_AREA_TOO_LARGE, boundingArea) as string)
-            : strings.SITE_BOUNDARY_ABSENT_WARNING
+          errorAnnotations?.length ? strings.SITE_BOUNDARY_ERRORS : strings.SITE_BOUNDARY_ABSENT_WARNING
         );
         onValidate(true);
         return;
       } else {
         onChange('boundary', boundary);
         if (site.id !== -1) {
-          onChange('plantingZones', [
-            defaultZonePayload({
-              boundary,
-              id: 0,
-              name: isSimpleSite ? strings.ZONE : '',
+          // create one zone per disjoint polygon in the site boundary
+          const zones: PlantingZone[] = boundary.coordinates.flatMap((coordinates: Position[][], index: number) => {
+            const zoneBoundary: MultiPolygon = { type: 'MultiPolygon', coordinates: [coordinates] };
+            return defaultZonePayload({
+              boundary: zoneBoundary,
+              id: index,
+              name: isSimpleSite ? `${strings.ZONE}${index || ''}` : '',
               targetPlantingDensity: 1500,
-            }),
-          ]);
+            });
+          });
+          onChange('plantingZones', zones);
         }
         onValidate(false);
       }
     }
-  }, [
-    boundary,
-    boundingArea,
-    boundingAreaTooLarge,
-    isSimpleSite,
-    onChange,
-    onValidate,
-    site.id,
-    siteBoundary,
-    snackbar,
-  ]);
+  }, [boundary, errorAnnotations, isSimpleSite, onChange, onValidate, site.id, siteBoundary, snackbar]);
 
   useEffect(() => {
     const data: Description[] = [
