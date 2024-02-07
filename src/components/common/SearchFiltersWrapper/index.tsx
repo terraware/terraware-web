fc00b795393c8051d4c97d855a4186684c6e86b7
Nick Graziano <nick.graziano@gmail.com>
2024-02-07 10:26:54 -0700

    Add proposed types to comment about refactor in SearchFiltersWrapper

--- src/components/common/SearchFiltersWrapper/index.tsx
+++ src/components/common/SearchFiltersWrapper/index.tsx
@@ -14,15 +14,35 @@ export type SearchInputProps = {
   onSearch: (search: string) => void;
 };
 
-// A "featured filter" is one that resides within its own multi select, between the search bar and the filters hidden
-// behind the filter icon (those are now called "icon filters"). Since the implementer should not care about where the
-// filters are (featured or icon), the `filtersProps.setFilters` and `filterProps.filters` passed in are really used to
-// control "all filters". Ideally the SearchFiltersProps gets turned into IconFiltersProps, and those two aforementioned
-// properties get put at the top level of the SearchProps, to make it more obvious. That is a slightly bigger refactor
-// than I want to embark on right now since there are a good number of places using this component. One side effect of
-// this is that **you can't use featured filters without the icon filters also being set up.** As soon as we decide to
-// implement this wrapper with only the featured filters, it makes sense to do the refactor. Hopefully this note
-// clears up any confusion and serves as a reminder that we should do that in the future.
+/**
+ * A "featured filter" is one that resides within its own multi select, between the search bar and the filters hidden
+ * behind the filter icon (those are now called "icon filters"). Since the implementer should not care about where the
+ * filters are (featured or icon), the `filtersProps.setFilters` and `filterProps.filters` passed in are really used to
+ * control "all filters". Ideally the SearchFiltersProps gets turned into IconFiltersProps, and those two aforementioned
+ * properties get put at the top level of the SearchProps, to make it more obvious. That is a slightly bigger refactor
+ * than I want to embark on right now since there are a good number of places using this component. One side effect of
+ * this is that **you can't use featured filters without the icon filters also being set up.** As soon as we decide to
+ * implement this wrapper with only the featured filters, it makes sense to do the refactor. Hopefully this note
+ * clears up any confusion and serves as a reminder that we should do that in the future.
+ *
+ * The types might look like this:
+ * type FeaturedFilterConfig - unchanged
+ *
+ * type IconFilters = {
+ *   filterOptions: FieldOptionsMap;
+ *   filterColumns: FilterField[];
+ *   optionsRenderer?: (filterName: string, values: FieldValuesPayload) => Option[] | undefined;
+ *   noScroll?: boolean;
+ * }
+ *
+ * type SearchProps = SearchInputProps & {
+ *   iconFilters?: IconFilters[];
+ *   featuredFilters?: FeaturedFilterConfig[];
+ *   currentFilters: Record<string, SearchNodePayload>;
+ *   setCurrentFilters: (filters: Record<string, any>) => void;
+ * };
+ *
+ */
 export type FeaturedFilterConfig = {
   field: string;
   label: string;
