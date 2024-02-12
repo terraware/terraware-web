9dfb6ae42578ffa476f324f98a5cd952633ddc3d
Nicholas Graziano <nick.graziano@gmail.com>
2024-02-12 10:57:47 -0700

    SW-4839 Add the ability to unselect the project in the accession create form (#2149)

--- src/components/ProjectsDropdown/index.tsx
+++ src/components/ProjectsDropdown/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Dropdown } from '@terraware/web-components';
+import React, { useMemo } from 'react';
+import { Dropdown, DropdownItem } from '@terraware/web-components';
 import strings from 'src/strings';
 import { Project } from 'src/types/Project';
+import { useLocalization } from 'src/providers';
 
 type ProjectsDropdownProps<T extends { projectId?: number } | undefined> = {
+  allowUnselect?: boolean;
   availableProjects: Project[] | undefined;
   label?: string | undefined;
   record: T;
@@ -11,26 +13,51 @@ type ProjectsDropdownProps<T extends { projectId?: number } | undefined> = {
 };
 
 function ProjectsDropdown<T extends { projectId?: number } | undefined>({
+  allowUnselect,
   availableProjects,
   label,
   record,
   setRecord,
 }: ProjectsDropdownProps<T>) {
+  const { activeLocale } = useLocalization();
+
+  const projectOptions = useMemo(() => {
+    const options: DropdownItem[] = [];
+
+    if (!activeLocale) {
+      return [];
+    }
+
+    if (allowUnselect) {
+      options.push({
+        label: strings.NO_PROJECT,
+        value: '',
+      });
+    }
+
+    return [
+      ...options,
+      ...(availableProjects || []).map((project) => ({
+        label: project.name,
+        value: project.id,
+      })),
+    ];
+  }, [activeLocale, allowUnselect, availableProjects]);
+
   return (
     <Dropdown
       id='projectId'
       label={label || strings.PROJECT}
       selectedValue={record?.projectId}
-      options={(availableProjects || []).map((project) => ({
-        label: project.name,
-        value: project.id,
-      }))}
-      onChange={(projectId: string) =>
-        setRecord((previousValue) => ({
-          ...previousValue,
-          projectId: Number(projectId),
-        }))
-      }
+      options={projectOptions}
+      onChange={(projectId: string) => {
+        setRecord((previousValue) => {
+          return {
+            ...previousValue,
+            projectId: projectId ? Number(projectId) : null,
+          };
+        });
+      }}
       fullWidth
     />
   );
