b2664857d0db343db828886a6b9d562d547fa7de
Nicholas Graziano <nick.graziano@gmail.com>
2024-02-22 14:55:26 -0700

    SW-4891 Single Deliverable redux and mock data (#2185)
    - Implement redux for single deliverable
    - Add method to DeliverableService that returns mock data for now
    - Only 1 mock document added for now

--- src/redux/features/deliverables/deliverablesThunks.ts
+++ src/redux/features/deliverables/deliverablesThunks.ts
@@ -2,7 +2,7 @@ import { Dispatch } from 'redux';
 import { RootState } from 'src/redux/rootReducer';
 import { SearchCriteria, SearchSortOrder } from 'src/types/Search';
 import DeliverablesService from 'src/services/DeliverablesService';
-import { setDeliverableListAction } from 'src/redux/features/deliverables/deliverablesSlice';
+import { setDeliverableAction, setDeliverableListAction } from 'src/redux/features/deliverables/deliverablesSlice';
 
 export const requestDeliverableList = (
   organizationId: number,
@@ -29,6 +29,31 @@ export const requestDeliverableList = (
         setDeliverableListAction({
           organizationId,
           data: { error: `Error dispatching request deliverables - ${errorMessage}` },
+        })
+      );
+    }
+  };
+};
+
+export const requestDeliverable = (deliverableId: number) => {
+  return async (dispatch: Dispatch, _getState: () => RootState) => {
+    try {
+      const result = await DeliverablesService.getDeliverable(deliverableId);
+
+      if (!result) {
+        dispatch(setDeliverableAction({ deliverableId, data: { error: true } }));
+        return;
+      }
+
+      dispatch(setDeliverableAction({ deliverableId, data: { deliverable: result } }));
+    } catch (e: unknown) {
+      // should not happen, the response above captures any http request errors
+      // tslint:disable-next-line: no-console
+      const errorMessage = (e as Error).message ?? e;
+      dispatch(
+        setDeliverableAction({
+          deliverableId,
+          data: { error: `Error dispatching request to get deliverable - ${errorMessage}` },
         })
       );
     }
