f97458e3e18a4820bb2405e862e61ce36ab1a1e9
Josh Buchea <verctrl@joshbuchea.com>
2024-02-26 13:22:18 -1000

    use useCallback rather than useMemo

--- src/components/DeliverableView/DeliverableStatusBadge.tsx
+++ src/components/DeliverableView/DeliverableStatusBadge.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from '@mui/material';
 import { Badge } from '@terraware/web-components';
 import { BadgeProps } from '@terraware/web-components/components/Badge';
-import { useMemo } from 'react';
+import { useCallback } from 'react';
 import strings from 'src/strings';
 import { DeliverableStatusType } from 'src/types/Deliverables';
 
@@ -9,76 +9,73 @@ type DeliverableStatusBadgeProps = {
   status: DeliverableStatusType;
 };
 
-const DeliverableStatusBadge = (props: DeliverableStatusBadgeProps): JSX.Element => {
+const DeliverableStatusBadge = (props: DeliverableStatusBadgeProps): JSX.Element | undefined => {
   const { status } = props;
   const theme = useTheme();
 
-  const getBadgePropsForStatus = useMemo(
-    () => (): BadgeProps => {
-      let badgeProps: BadgeProps;
+  const getBadgePropsForStatus = useCallback((): BadgeProps => {
+    let badgeProps: BadgeProps;
 
-      switch (status) {
-        case 'Approved':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgSuccessTertiary,
-            borderColor: theme.palette.TwClrBrdrSuccess,
-            labelColor: theme.palette.TwClrTxtSuccess,
-            label: strings.APPROVED,
-          };
-          break;
-        case 'In Review':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgWarningTertiary,
-            borderColor: theme.palette.TwClrBrdrWarning,
-            labelColor: theme.palette.TwClrTxtWarning,
-            label: strings.APPROVED,
-          };
-          break;
-        case 'Needs Translation':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgInfoTertiary,
-            borderColor: theme.palette.TwClrBrdrInfo,
-            labelColor: theme.palette.TwClrTxtInfo,
-            label: strings.APPROVED,
-          };
-          break;
-        case 'Not Needed':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgInfoTertiary,
-            borderColor: theme.palette.TwClrBrdrInfo,
-            labelColor: theme.palette.TwClrTxtInfo,
-            label: strings.NOT_NEEDED,
-          };
-          break;
-        case 'Not Submitted':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgInfoTertiary,
-            borderColor: theme.palette.TwClrBrdrInfo,
-            labelColor: theme.palette.TwClrTxtInfo,
-            label: strings.NOT_SUBMITTED,
-          };
-          break;
-        case 'Rejected':
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgDangerTertiary,
-            borderColor: theme.palette.TwClrBrdrDanger,
-            labelColor: theme.palette.TwClrTxtDanger,
-            label: strings.REJECTED,
-          };
-          break;
-        default:
-          badgeProps = {
-            backgroundColor: theme.palette.TwClrBgInfoTertiary,
-            borderColor: theme.palette.TwClrBrdrInfo,
-            labelColor: theme.palette.TwClrTxtInfo,
-            label: status,
-          };
-      }
+    switch (status) {
+      case 'Approved':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgSuccessTertiary,
+          borderColor: theme.palette.TwClrBrdrSuccess,
+          labelColor: theme.palette.TwClrTxtSuccess,
+          label: strings.APPROVED,
+        };
+        break;
+      case 'In Review':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgWarningTertiary,
+          borderColor: theme.palette.TwClrBrdrWarning,
+          labelColor: theme.palette.TwClrTxtWarning,
+          label: strings.APPROVED,
+        };
+        break;
+      case 'Needs Translation':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgInfoTertiary,
+          borderColor: theme.palette.TwClrBrdrInfo,
+          labelColor: theme.palette.TwClrTxtInfo,
+          label: strings.APPROVED,
+        };
+        break;
+      case 'Not Needed':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgInfoTertiary,
+          borderColor: theme.palette.TwClrBrdrInfo,
+          labelColor: theme.palette.TwClrTxtInfo,
+          label: strings.NOT_NEEDED,
+        };
+        break;
+      case 'Not Submitted':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgInfoTertiary,
+          borderColor: theme.palette.TwClrBrdrInfo,
+          labelColor: theme.palette.TwClrTxtInfo,
+          label: strings.NOT_SUBMITTED,
+        };
+        break;
+      case 'Rejected':
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgDangerTertiary,
+          borderColor: theme.palette.TwClrBrdrDanger,
+          labelColor: theme.palette.TwClrTxtDanger,
+          label: strings.REJECTED,
+        };
+        break;
+      default:
+        badgeProps = {
+          backgroundColor: theme.palette.TwClrBgInfoTertiary,
+          borderColor: theme.palette.TwClrBrdrInfo,
+          labelColor: theme.palette.TwClrTxtInfo,
+          label: status,
+        };
+    }
 
-      return badgeProps;
-    },
-    [status, theme]
-  );
+    return badgeProps;
+  }, [status, theme]);
 
   return (
     <div style={{ float: 'right', marginBottom: '0px', marginLeft: '16px' }}>
