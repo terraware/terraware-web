651265b07e2fe6703fac5ef2bda03f759e017442
Nick Graziano <nick.graziano@gmail.com>
2024-02-27 14:49:11 -0700

    fix merge conflicts

--- src/scenes/AcceleratorRouter/DeliverableView.tsx
+++ src/scenes/AcceleratorRouter/DeliverableView.tsx
@@ -5,34 +5,10 @@ import useDeviceInfo from 'src/utils/useDeviceInfo';
 import Card from 'src/components/common/Card';
 import Page from 'src/components/Page';
 import AcceleratorDocumentsList from 'src/scenes/AcceleratorRouter/AcceleratorDocumentsList';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 65dae5b82a (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components)
-<<<<<<< HEAD:src/components/DeliverableView/index.tsx
-import { EditProps, ViewProps } from './types';
-import TitleBar from './TitleBar';
-import Metadata from './Metadata';
-import DocumentsUploader from './DocumentsUploader';
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 028a74d2f2 (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components)
->>>>>>> 65dae5b82a (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components)
 import { EditProps, ViewProps } from 'src/components/DeliverableView/types';
 import TitleBar from 'src/components/DeliverableView/TitleBar';
-import StatusBar from 'src/components/DeliverableView/StatusBar';
-import Description from 'src/components/DeliverableView/Description';
 import DocumentsUploader from 'src/components/DeliverableView/DocumentsUploader';
-<<<<<<< HEAD
->>>>>>> ea1e7bec59 (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components):src/scenes/AcceleratorRouter/DeliverableView.tsx
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 126ce6a6f7 (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components):src/scenes/AcceleratorRouter/DeliverableView.tsx
->>>>>>> 028a74d2f2 (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components)
->>>>>>> 65dae5b82a (SW-4906 Implement router-specific deliverable views so we can encapsulate the view-agnostic components)
+import Metadata from 'src/components/DeliverableView/Metadata';
 
 export type Props = EditProps & {
   isBusy?: boolean;
