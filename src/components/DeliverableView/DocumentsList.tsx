850fceb796449ee717e6ff7e5f9f07fb503dd8db
Nicholas Graziano <nick.graziano@gmail.com>
2024-02-27 14:27:38 -0700

    SW-4906 Implement documents list in deliverable view (#2212)
    ![2024-02-27 11.07.37.gif](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/wJnPX9wLZAkiGXEBFJxL/a847bc8b-6761-4a42-87d2-a2317276e379.gif)

--- src/components/DeliverableView/DocumentsList.tsx
+++ src/components/DeliverableView/DocumentsList.tsx
@@ -1,10 +1,60 @@
-import { Box } from '@mui/material';
+import { useState } from 'react';
+import { Box, Grid } from '@mui/material';
+import strings from 'src/strings';
+import { SearchSortOrder } from 'src/types/Search';
+import { useLocalization } from 'src/providers';
+import { TableColumnType } from 'src/components/common/table/types';
+import Table from 'src/components/common/table';
+import { SortOrder } from 'src/components/common/table/sort';
 import { ViewProps } from './types';
 
+const columns = (activeLocale: string | null): TableColumnType[] =>
+  activeLocale
+    ? [
+        {
+          key: 'name',
+          name: strings.DOCUMENT_NAME,
+          type: 'string',
+        },
+        {
+          key: 'description',
+          name: strings.DESCRIPTION,
+          type: 'string',
+        },
+        {
+          key: 'dateUploaded',
+          name: strings.DATE_UPLOADED,
+          type: 'string',
+        },
+      ]
+    : [];
+
 const DocumentsList = (props: ViewProps): JSX.Element => {
+  const { activeLocale } = useLocalization();
+
+  const [searchSortOrder, setSearchSortOrder] = useState<SearchSortOrder>({
+    field: 'deliverableName',
+    direction: 'Ascending',
+  } as SearchSortOrder);
+
+  const onSortChange = (order: SortOrder, orderBy: string) =>
+    setSearchSortOrder({
+      field: orderBy,
+      direction: order === 'asc' ? 'Ascending' : 'Descending',
+    });
+
   return (
     <Box display='flex' flexDirection='column'>
-      <div>DocumentsList coming soon {props.isAcceleratorConsole}!</div>
+      <Grid item xs={12}>
+        <Table
+          id={'documents-list'}
+          columns={() => columns(activeLocale)}
+          rows={props.deliverable.documents}
+          orderBy={searchSortOrder.field}
+          order={searchSortOrder.direction === 'Ascending' ? 'asc' : 'desc'}
+          sortHandler={onSortChange}
+        />
+      </Grid>
     </Box>
   );
 };
