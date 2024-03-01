b5176b302ef6ea27eb502c56e0ade91c051f12cb
Josh Buchea <verctrl@joshbuchea.com>
2024-03-01 10:49:44 -1000

    reset submission status when new document is uploaded

--- src/components/DeliverableView/DocumentsUploader.tsx
+++ src/components/DeliverableView/DocumentsUploader.tsx
@@ -1,13 +1,15 @@
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
 import { FileChooser } from '@terraware/web-components';
 import strings from 'src/strings';
 import { useLocalization } from 'src/providers';
+import useUpdateDeliverable from 'src/scenes/AcceleratorRouter/useUpdateDeliverable';
 import { ViewProps } from './types';
 
 const DocumentsUploader = ({ deliverable }: ViewProps): JSX.Element => {
   const theme = useTheme();
   const { activeLocale } = useLocalization();
+  const { update } = useUpdateDeliverable();
 
   const template = useMemo(() => {
     if (activeLocale && deliverable.templateUrl) {
@@ -17,6 +19,14 @@ const DocumentsUploader = ({ deliverable }: ViewProps): JSX.Element => {
     }
   }, [activeLocale, deliverable.templateUrl]);
 
+  const setFiles = useCallback(
+    (files: File[]) => {
+      window.alert(`${files.length} files selected`);
+      update({ ...deliverable, status: 'In Review' });
+    },
+    [deliverable, update]
+  );
+
   return (
     <Box display='flex' flexDirection='column'>
       <Typography marginBottom={theme.spacing(2)} fontSize='20px' lineHeight='28px' fontWeight={600}>
@@ -25,7 +35,7 @@ const DocumentsUploader = ({ deliverable }: ViewProps): JSX.Element => {
       <FileChooser
         acceptFileType='image/*,application/*'
         chooseFileText={strings.CHOOSE_FILE}
-        setFiles={(files: File[]) => window.alert(`${files.length} files selected`)}
+        setFiles={setFiles}
         multipleSelection
         uploadDescription={strings.UPLOAD_FILES_DESCRIPTION}
         uploadText={strings.UPLOAD_FILES_TITLE}
