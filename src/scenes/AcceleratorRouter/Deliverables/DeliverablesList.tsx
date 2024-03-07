9f7bf230fae398f77b311dc915e271c6b0c49ba1
Karthik Balachandran <karthik@terraformation.com>
2024-03-07 09:51:39 -0800

    - undo deliverables list changes

--- src/scenes/AcceleratorRouter/Deliverables/DeliverablesList.tsx
+++ src/scenes/AcceleratorRouter/Deliverables/DeliverablesList.tsx
@@ -1,14 +1,15 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 
 import { Grid, Typography } from '@mui/material';
 import { Separator, TableColumnType } from '@terraware/web-components';
 
 import DeliverablesTable from 'src/components/DeliverablesTable';
-import Page from 'src/components/Page';
 import PageHeader from 'src/components/PageHeader';
 import ParticipantsDropdown from 'src/components/ParticipantsDropdown';
+import PageHeaderWrapper from 'src/components/common/PageHeaderWrapper';
 import { useParticipants } from 'src/hooks/useParticipants';
 import { useLocalization } from 'src/providers';
+import AcceleratorMain from 'src/scenes/AcceleratorRouter/AcceleratorMain';
 import strings from 'src/strings';
 import theme from 'src/theme';
 import { SearchNodePayload } from 'src/types/Search';
@@ -52,6 +53,7 @@ const columns = (activeLocale: string | null): TableColumnType[] =>
 const DeliverablesList = () => {
   const { activeLocale } = useLocalization();
   const { availableParticipants } = useParticipants();
+  const contentRef = useRef(null);
 
   const [participantFilter, setParticipantFilter] = useState<{ participantId?: number }>({ participantId: undefined });
 
@@ -99,10 +101,14 @@ const DeliverablesList = () => {
   );
 
   return (
-    <Page title={<PageHeader title={strings.DELIVERABLES} leftComponent={PageHeaderLeftComponent} />}>
+    <AcceleratorMain>
+      <PageHeaderWrapper nextElement={contentRef.current}>
+        <PageHeader title={strings.DELIVERABLES} leftComponent={PageHeaderLeftComponent} />
+      </PageHeaderWrapper>
+
       {/* -1 for "non-organization scoped search" IE admin search */}
       <DeliverablesTable columns={columns} extraTableFilters={extraTableFilters} organizationId={-1} />
-    </Page>
+    </AcceleratorMain>
   );
 };
 
