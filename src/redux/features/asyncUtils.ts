cb933a523f63213ce9e31f502aaa564f50993d46
Nick Graziano <nick.graziano@gmail.com>
2024-03-12 10:55:30 -0600

    Allow passing in of a composite key creator to the buildReducers function, which allows a slice creator to additionally store a thunk's result in state at a composite key. This allows for the useArgAsKey feature to work with multiple arg properties.

--- src/redux/features/asyncUtils.ts
+++ src/redux/features/asyncUtils.ts
@@ -7,17 +7,32 @@ export type StatusT<T> = { status: Statuses; data?: T };
 export type Status = StatusT<unknown>; // when data is not relevant in the response
 
 export const buildReducers =
-  <T>(asyncThunk: AsyncThunk<any, any, any>, useArgAsKey?: boolean) =>
+  <T>(asyncThunk: AsyncThunk<any, any, any>, useArgAsKey?: boolean, compositeKeyFn?: (args: unknown) => string) =>
   (builder: CaseReducers<any, any> | ActionReducerMapBuilder<any>) =>
     (builder as any)
-      .addCase(asyncThunk.pending, setStatus<T>('pending', useArgAsKey))
-      .addCase(asyncThunk.fulfilled, setStatus<T>('success', useArgAsKey))
-      .addCase(asyncThunk.rejected, setStatus<T>('error', useArgAsKey));
+      .addCase(asyncThunk.pending, setStatus<T>('pending', useArgAsKey, compositeKeyFn))
+      .addCase(asyncThunk.fulfilled, setStatus<T>('success', useArgAsKey, compositeKeyFn))
+      .addCase(asyncThunk.rejected, setStatus<T>('error', useArgAsKey, compositeKeyFn));
 
 export const setStatus =
-  <T>(status: Statuses, useArgAsKey?: boolean) =>
+  <T>(status: Statuses, useArgAsKey?: boolean, compositeKeyFn?: (arg: unknown) => string) =>
   (state: any, action: any) => {
-    const key = useArgAsKey ? action.meta.arg : action.meta.requestId;
     const data: T = action.payload as T;
+
+    state[action.meta.requestId] = { status, data };
+
+    if (!useArgAsKey) {
+      return;
+    }
+
+    let key = action.meta.arg;
+
+    if (compositeKeyFn) {
+      const compositeKey = compositeKeyFn(action.meta.arg);
+      if (compositeKey !== '') {
+        key = compositeKey;
+      }
+    }
+
     state[key] = { status, data };
   };
