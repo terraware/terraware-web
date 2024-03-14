80543c1cdd7de2b8fd4e377711d548fa3bb8ac35
Nicholas Graziano <nick.graziano@gmail.com>
2024-03-14 07:49:41 -0600

    SW-4961 Refactor the deliverables table with search and filters into its own component so we can easily resuse for people table (#2311)

--- src/components/DeliverablesTable/index.tsx
+++ src/components/DeliverablesTable/index.tsx
@@ -1,12 +1,8 @@
 import React, { RefObject, useCallback, useEffect, useMemo, useState } from 'react';
 
-import { Container, Grid } from '@mui/material';
-import { makeStyles } from '@mui/styles';
-import { SortOrder, TableColumnType } from '@terraware/web-components';
+import { TableColumnType } from '@terraware/web-components';
 
-import Card from 'src/components/common/Card';
-import SearchFiltersWrapperV2, { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
-import { BaseTable as Table } from 'src/components/common/table';
+import { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
 import { useLocalization } from 'src/providers';
 import { requestListDeliverables } from 'src/redux/features/deliverables/deliverablesAsyncThunks';
 import { selectDeliverablesSearchRequest } from 'src/redux/features/deliverables/deliverablesSelectors';
@@ -14,10 +10,10 @@ import { useAppDispatch, useAppSelector } from 'src/redux/store';
 import { ListDeliverablesRequestParams } from 'src/services/DeliverablesService';
 import strings from 'src/strings';
 import { DeliverableCategories, DeliverableStatuses, ListDeliverablesElement } from 'src/types/Deliverables';
-import { FieldNodePayload, SearchNodePayload, SearchSortOrder } from 'src/types/Search';
+import { SearchNodePayload, SearchSortOrder } from 'src/types/Search';
 import { SearchAndSortFn } from 'src/utils/searchAndSort';
-import useDebounce from 'src/utils/useDebounce';
 
+import TableWithSearchFilters from '../TableWithSearchFilters';
 import DeliverableCellRenderer from './DeliverableCellRenderer';
 
 interface DeliverablesTableProps {
@@ -27,15 +23,14 @@ interface DeliverablesTableProps {
   pageHeaderRef: RefObject<HTMLDivElement>;
   filterModifiers?: (filters: FilterConfig[]) => FilterConfig[];
   searchAndSort?: SearchAndSortFn<ListDeliverablesElement>;
+  tableId: string;
 }
 
-const useStyles = makeStyles(() => ({
-  mainContainer: {
-    padding: 0,
-  },
-}));
-
-const FUZZY_SEARCH_COLUMNS = ['name', 'project_name'];
+const fuzzySearchColumns = ['name', 'project_name'];
+const defaultSearchOrder: SearchSortOrder = {
+  field: 'name',
+  direction: 'Ascending',
+};
 
 const DeliverablesTable = ({
   columns,
@@ -44,22 +39,14 @@ const DeliverablesTable = ({
   pageHeaderRef,
   organizationId,
   searchAndSort,
+  tableId,
 }: DeliverablesTableProps) => {
   const dispatch = useAppDispatch();
   const { activeLocale } = useLocalization();
-  const classes = useStyles();
 
   const [deliverables, setDeliverables] = useState<ListDeliverablesElement[]>([]);
   const [deliverablesSearchRequestId, setDeliverablesSearchRequestId] = useState('');
   const deliverablesSearchRequest = useAppSelector(selectDeliverablesSearchRequest(deliverablesSearchRequestId));
-
-  const [filters, setFilters] = useState<Record<string, SearchNodePayload>>({});
-  const [searchValue, setSearchValue] = useState('');
-  const debouncedSearchTerm = useDebounce(searchValue, 250);
-  const [searchSortOrder, setSearchSortOrder] = useState<SearchSortOrder>({
-    field: 'name',
-    direction: 'Ascending',
-  } as SearchSortOrder);
 
   const featuredFilters: FilterConfig[] = useMemo(
     () =>
@@ -84,75 +71,26 @@ const DeliverablesTable = ({
     [activeLocale]
   );
 
-  const onSortChange = (order: SortOrder, orderBy: string) =>
-    setSearchSortOrder({
-      field: orderBy,
-      direction: order === 'asc' ? 'Ascending' : 'Descending',
-    });
+  const dispatchSearchRequest = useCallback(
+    (locale: string | null, search: SearchNodePayload, searchSortOrder: SearchSortOrder) => {
+      const listRequest: ListDeliverablesRequestParams = {};
+      if (organizationId !== -1) {
+        listRequest.organizationId = organizationId;
+      }
 
-  const getSearchPayload = useCallback((): SearchNodePayload => {
-    const searchNodeChildren: SearchNodePayload[] = [];
-
-    // Apply search field to search API payload
-    if (debouncedSearchTerm) {
-      const fuzzySearchValueChildren = FUZZY_SEARCH_COLUMNS.map(
-        (field: string): FieldNodePayload => ({
-          operation: 'field',
-          field,
-          type: 'Fuzzy',
-          values: [debouncedSearchTerm],
+      const request = dispatch(
+        requestListDeliverables({
+          locale,
+          listRequest,
+          search,
+          searchSortOrder,
+          searchAndSort,
         })
       );
-
-      searchNodeChildren.push({
-        operation: 'or',
-        children: fuzzySearchValueChildren,
-      });
-    }
-
-    // Apply filters to search API payload
-    if (Object.keys(filters).length > 0) {
-      const filterValueChildren = Object.keys(filters)
-        .filter((field: string) => (filters[field]?.values || []).length > 0)
-        .map((field: string): SearchNodePayload => filters[field]);
-
-      searchNodeChildren.push({
-        operation: 'and',
-        children: filterValueChildren,
-      });
-    }
-
-    // Apply filters that are outside the table but pertinent to the view (like participants or projects)
-    if (extraTableFilters) {
-      extraTableFilters.forEach((searchNodeChild: SearchNodePayload) => {
-        searchNodeChildren.push(searchNodeChild);
-      });
-    }
-
-    return {
-      operation: 'and',
-      children: searchNodeChildren,
-    };
-  }, [debouncedSearchTerm, filters, extraTableFilters]);
-
-  useEffect(() => {
-    const listRequest: ListDeliverablesRequestParams = {};
-    if (organizationId !== -1) {
-      listRequest.organizationId = organizationId;
-    }
-
-    const search: SearchNodePayload = getSearchPayload();
-    const request = dispatch(
-      requestListDeliverables({
-        locale: activeLocale,
-        listRequest,
-        search,
-        searchSortOrder,
-        searchAndSort,
-      })
-    );
-    setDeliverablesSearchRequestId(request.requestId);
-  }, [activeLocale, dispatch, getSearchPayload, organizationId, searchSortOrder, searchAndSort]);
+      setDeliverablesSearchRequestId(request.requestId);
+    },
+    [dispatch, organizationId, searchAndSort]
+  );
 
   useEffect(() => {
     // TODO do something if the request has an error
@@ -162,30 +100,19 @@ const DeliverablesTable = ({
   }, [deliverablesSearchRequest]);
 
   return (
-    <Container ref={pageHeaderRef} maxWidth={false} className={classes.mainContainer}>
-      <Card flushMobile>
-        <Grid item xs={12} sx={{ display: 'flex', marginBottom: '16px', alignItems: 'center' }}>
-          <SearchFiltersWrapperV2
-            search={searchValue}
-            onSearch={setSearchValue}
-            currentFilters={filters}
-            setCurrentFilters={setFilters}
-            featuredFilters={filterModifiers ? filterModifiers(featuredFilters) : featuredFilters}
-          />
-        </Grid>
-
-        <Grid item xs={12}>
-          <Table
-            columns={columns(activeLocale)}
-            rows={deliverables}
-            orderBy={searchSortOrder.field}
-            order={searchSortOrder.direction === 'Ascending' ? 'asc' : 'desc'}
-            Renderer={DeliverableCellRenderer}
-            sortHandler={onSortChange}
-          />
-        </Grid>
-      </Card>
-    </Container>
+    <TableWithSearchFilters
+      cellRenderer={DeliverableCellRenderer}
+      columns={columns}
+      defaultSearchOrder={defaultSearchOrder}
+      dispatchSearchRequest={dispatchSearchRequest}
+      extraTableFilters={extraTableFilters}
+      featuredFilters={featuredFilters}
+      filterModifiers={filterModifiers}
+      fuzzySearchColumns={fuzzySearchColumns}
+      pageHeaderRef={pageHeaderRef}
+      rows={deliverables}
+      tableId={tableId}
+    />
   );
 };
 
---
+++ src/components/TableWithSearchFilters/index.tsx
@@ -0,0 +1,148 @@
+import React, { RefObject, useCallback, useEffect, useMemo, useState } from 'react';
+
+import { Container, Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+import { RendererProps, SortOrder, TableColumnType, TableRowType } from '@terraware/web-components';
+
+import Card from 'src/components/common/Card';
+import SearchFiltersWrapperV2, { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
+import Table from 'src/components/common/table';
+import { useLocalization } from 'src/providers';
+import { FieldNodePayload, SearchNodePayload, SearchSortOrder } from 'src/types/Search';
+import useDebounce from 'src/utils/useDebounce';
+
+interface TableWithSearchFiltersProps {
+  cellRenderer?: (props: RendererProps<TableRowType>) => JSX.Element;
+  columns: (activeLocale: string | null) => TableColumnType[];
+  defaultSearchOrder: SearchSortOrder;
+  dispatchSearchRequest: (locale: string | null, search: SearchNodePayload, searchSortOrder: SearchSortOrder) => void;
+  extraTableFilters?: SearchNodePayload[];
+  pageHeaderRef: RefObject<HTMLDivElement>;
+  featuredFilters?: FilterConfig[];
+  filterModifiers?: (filters: FilterConfig[]) => FilterConfig[];
+  fuzzySearchColumns?: string[];
+  rows: TableRowType[];
+  tableId: string;
+}
+
+const useStyles = makeStyles(() => ({
+  mainContainer: {
+    padding: 0,
+  },
+}));
+
+const TableWithSearchFilters = ({
+  cellRenderer,
+  columns,
+  defaultSearchOrder,
+  dispatchSearchRequest,
+  extraTableFilters,
+  featuredFilters,
+  filterModifiers,
+  fuzzySearchColumns,
+  pageHeaderRef,
+  rows,
+  tableId,
+}: TableWithSearchFiltersProps) => {
+  const { activeLocale } = useLocalization();
+  const classes = useStyles();
+
+  const [filters, setFilters] = useState<Record<string, SearchNodePayload>>({});
+  const [searchValue, setSearchValue] = useState('');
+  const debouncedSearchTerm = useDebounce(searchValue, 250);
+  const [searchSortOrder, setSearchSortOrder] = useState<SearchSortOrder>(defaultSearchOrder);
+
+  const onSortChange = (order: SortOrder, orderBy: string) =>
+    setSearchSortOrder({
+      field: orderBy,
+      direction: order === 'asc' ? 'Ascending' : 'Descending',
+    });
+
+  const getSearchPayload = useCallback((): SearchNodePayload => {
+    const searchNodeChildren: SearchNodePayload[] = [];
+
+    // Apply search field to search API payload
+    if (debouncedSearchTerm && fuzzySearchColumns) {
+      const fuzzySearchValueChildren = fuzzySearchColumns.map(
+        (field: string): FieldNodePayload => ({
+          operation: 'field',
+          field,
+          type: 'Fuzzy',
+          values: [debouncedSearchTerm],
+        })
+      );
+
+      searchNodeChildren.push({
+        operation: 'or',
+        children: fuzzySearchValueChildren,
+      });
+    }
+
+    // Apply filters to search API payload
+    if (Object.keys(filters).length > 0) {
+      const filterValueChildren = Object.keys(filters)
+        .filter((field: string) => (filters[field]?.values || []).length > 0)
+        .map((field: string): SearchNodePayload => filters[field]);
+
+      searchNodeChildren.push({
+        operation: 'and',
+        children: filterValueChildren,
+      });
+    }
+
+    // Apply filters that are outside the table but pertinent to the view (like participants or projects)
+    if (extraTableFilters) {
+      extraTableFilters.forEach((searchNodeChild: SearchNodePayload) => {
+        searchNodeChildren.push(searchNodeChild);
+      });
+    }
+
+    return {
+      operation: 'and',
+      children: searchNodeChildren,
+    };
+  }, [debouncedSearchTerm, filters, fuzzySearchColumns, extraTableFilters]);
+
+  const _featuredFilters = useMemo(() => {
+    if (!featuredFilters) {
+      return undefined;
+    }
+
+    return filterModifiers ? filterModifiers(featuredFilters) : featuredFilters;
+  }, [featuredFilters, filterModifiers]);
+
+  useEffect(() => {
+    const search: SearchNodePayload = getSearchPayload();
+    dispatchSearchRequest(activeLocale, search, searchSortOrder);
+  }, [activeLocale, dispatchSearchRequest, getSearchPayload, searchSortOrder]);
+
+  return (
+    <Container ref={pageHeaderRef} maxWidth={false} className={classes.mainContainer}>
+      <Card flushMobile>
+        <Grid item xs={12} sx={{ display: 'flex', marginBottom: '16px', alignItems: 'center' }}>
+          <SearchFiltersWrapperV2
+            search={searchValue}
+            onSearch={setSearchValue}
+            currentFilters={filters}
+            setCurrentFilters={setFilters}
+            featuredFilters={_featuredFilters}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Table
+            id={tableId}
+            columns={() => columns(activeLocale)}
+            rows={rows}
+            orderBy={searchSortOrder.field}
+            order={searchSortOrder.direction === 'Ascending' ? 'asc' : 'desc'}
+            Renderer={cellRenderer}
+            sortHandler={onSortChange}
+          />
+        </Grid>
+      </Card>
+    </Container>
+  );
+};
+
+export default TableWithSearchFilters;
