4f9182ec059a2a74be932a4a617cb725167a1550
Josh Buchea <verctrl@joshbuchea.com>
2024-03-15 13:02:45 -1000

    conditionally render projects filter in accelerator deliverables table

--- src/components/DeliverablesTable/index.tsx
+++ src/components/DeliverablesTable/index.tsx
@@ -3,6 +3,7 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { TableColumnType } from '@terraware/web-components';
 
 import { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
+import useAcceleratorConsole from 'src/hooks/useAcceleratorConsole';
 import { useProjects } from 'src/hooks/useProjects';
 import { useLocalization } from 'src/providers';
 import { requestListDeliverables } from 'src/redux/features/deliverables/deliverablesAsyncThunks';
@@ -44,6 +45,7 @@ const DeliverablesTable = ({
   const dispatch = useAppDispatch();
   const { activeLocale } = useLocalization();
   const { availableProjects: projects } = useProjects();
+  const { isAcceleratorRoute } = useAcceleratorConsole();
 
   const [deliverables, setDeliverables] = useState<ListDeliverablesElement[]>([]);
   const [deliverablesSearchRequestId, setDeliverablesSearchRequestId] = useState('');
@@ -54,51 +56,52 @@ const DeliverablesTable = ({
     [projects]
   );
 
-  const featuredFilters: FilterConfig[] = useMemo(
-    () =>
-      activeLocale
-        ? [
-            {
-              field: 'project_id',
-              options: (projects || [])?.map((project: Project) => `${project.id}`),
-              searchNodeCreator: (values: (number | string | null)[]) => ({
-                field: 'projectId',
-                operation: 'field',
-                type: 'Exact',
-                values: values.map((value: number | string | null): string | null =>
-                  value === null ? value : `${value}`
-                ),
-              }),
-              label: strings.PROJECTS,
-              renderOption: (id: string | number) => getProjectName(Number(id)),
-              notPresentFilterShown: true,
-              notPresentFilterLabel: strings.NO_PROJECT,
-              pillValuesRenderer: (values: unknown[]): string | undefined => {
-                if (values.length === 1 && values[0] === null) {
-                  return strings.NO_PROJECT;
-                }
+  const featuredFilters: FilterConfig[] = useMemo(() => {
+    const filters: FilterConfig[] = [
+      {
+        field: 'status',
+        // These options are strings for now, but may end up as enums when the BE types come through, if that is
+        // the case we will need to implement the renderOption and pillValueRenderer to render the desired
+        // human readable values
+        options: DeliverableStatuses,
+        label: strings.STATUS,
+      },
+      {
+        field: 'category',
+        // Same note as above
+        options: DeliverableCategories,
+        label: strings.CATEGORY,
+      },
+    ];
 
-                return values.map((value: unknown) => getProjectName(Number(value))).join(', ');
-              },
-            },
-            {
-              field: 'status',
-              // These options are strings for now, but may end up as enums when the BE types come through, if that is
-              // the case we will need to implement the renderOption and pillValueRenderer to render the desired
-              // human readable values
-              options: DeliverableStatuses,
-              label: strings.STATUS,
-            },
-            {
-              field: 'category',
-              // Same note as above
-              options: DeliverableCategories,
-              label: strings.CATEGORY,
-            },
-          ]
-        : [],
-    [activeLocale, getProjectName, projects]
-  );
+    // show the project filter only in the accelerator route
+    // the participant view already has a projects filter above the table
+    if (isAcceleratorRoute) {
+      filters.unshift({
+        field: 'project_id',
+        options: (projects || [])?.map((project: Project) => `${project.id}`),
+        searchNodeCreator: (values: (number | string | null)[]) => ({
+          field: 'projectId',
+          operation: 'field',
+          type: 'Exact',
+          values: values.map((value: number | string | null): string | null => (value === null ? value : `${value}`)),
+        }),
+        label: strings.PROJECTS,
+        renderOption: (id: string | number) => getProjectName(Number(id)),
+        notPresentFilterShown: true,
+        notPresentFilterLabel: strings.NO_PROJECT,
+        pillValueRenderer: (values: unknown[]): string | undefined => {
+          if (values.length === 1 && values[0] === null) {
+            return strings.NO_PROJECT;
+          }
+
+          return values.map((value: unknown) => getProjectName(Number(value))).join(', ');
+        },
+      });
+    }
+
+    return activeLocale ? filters : [];
+  }, [activeLocale, getProjectName, isAcceleratorRoute, projects]);
 
   const dispatchSearchRequest = useCallback(
     (locale: string | null, search: SearchNodePayload, searchSortOrder: SearchSortOrder) => {
