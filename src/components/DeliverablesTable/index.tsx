b2da90c4033750aa564466d44b003c67f4febf11
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-03-19 05:23:48 -1000

    SW-5044: Console: Need a way to filter Deliverables list by Project (#2336)

--- src/components/DeliverablesTable/index.tsx
+++ src/components/DeliverablesTable/index.tsx
@@ -3,6 +3,7 @@ import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { TableColumnType } from '@terraware/web-components';
 
 import { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
+import { useProjects } from 'src/hooks/useProjects';
 import { useLocalization } from 'src/providers';
 import { requestListDeliverables } from 'src/redux/features/deliverables/deliverablesAsyncThunks';
 import { selectDeliverablesSearchRequest } from 'src/redux/features/deliverables/deliverablesSelectors';
@@ -10,6 +11,7 @@ import { useAppDispatch, useAppSelector } from 'src/redux/store';
 import { ListDeliverablesRequestParams } from 'src/services/DeliverablesService';
 import strings from 'src/strings';
 import { DeliverableCategories, DeliverableStatuses, ListDeliverablesElement } from 'src/types/Deliverables';
+import { Project } from 'src/types/Project';
 import { SearchNodePayload, SearchSortOrder } from 'src/types/Search';
 import { SearchAndSortFn } from 'src/utils/searchAndSort';
 
@@ -19,6 +21,7 @@ import DeliverableCellRenderer from './DeliverableCellRenderer';
 interface DeliverablesTableProps {
   columns: (activeLocale: string | null) => TableColumnType[];
   extraTableFilters?: SearchNodePayload[];
+  isAcceleratorRoute?: boolean;
   organizationId: number;
   filterModifiers?: (filters: FilterConfig[]) => FilterConfig[];
   searchAndSort?: SearchAndSortFn<ListDeliverablesElement>;
@@ -35,39 +38,56 @@ const DeliverablesTable = ({
   columns,
   extraTableFilters,
   filterModifiers,
+  isAcceleratorRoute,
   organizationId,
   searchAndSort,
   tableId,
 }: DeliverablesTableProps) => {
   const dispatch = useAppDispatch();
   const { activeLocale } = useLocalization();
+  const { availableProjects: projects, getProjectName } = useProjects();
 
   const [deliverables, setDeliverables] = useState<ListDeliverablesElement[]>([]);
   const [deliverablesSearchRequestId, setDeliverablesSearchRequestId] = useState('');
   const deliverablesSearchRequest = useAppSelector(selectDeliverablesSearchRequest(deliverablesSearchRequestId));
 
-  const featuredFilters: FilterConfig[] = useMemo(
-    () =>
-      activeLocale
-        ? [
-            {
-              field: 'status',
-              // These options are strings for now, but may end up as enums when the BE types come through, if that is
-              // the case we will need to implement the renderOption and pillValueRenderer to render the desired
-              // human readable values
-              options: DeliverableStatuses,
-              label: strings.STATUS,
-            },
-            {
-              field: 'category',
-              // Same note as above
-              options: DeliverableCategories,
-              label: strings.CATEGORY,
-            },
-          ]
-        : [],
-    [activeLocale]
-  );
+  const featuredFilters: FilterConfig[] = useMemo(() => {
+    const filters: FilterConfig[] = [
+      {
+        field: 'status',
+        // These options are strings for now, but may end up as enums when the BE types come through, if that is
+        // the case we will need to implement the renderOption and pillValueRenderer to render the desired
+        // human readable values
+        options: DeliverableStatuses,
+        label: strings.STATUS,
+      },
+      {
+        field: 'category',
+        // Same note as above
+        options: DeliverableCategories,
+        label: strings.CATEGORY,
+      },
+    ];
+
+    // show the project filter only in the accelerator route
+    // the participant view already has a projects filter above the table
+    if (isAcceleratorRoute) {
+      filters.unshift({
+        field: 'project_id',
+        options: (projects || [])?.map((project: Project) => `${project.id}`),
+        searchNodeCreator: (values: (number | string | null)[]) => ({
+          field: 'projectId',
+          operation: 'field',
+          type: 'Exact',
+          values: values.map((value: number | string | null): string | null => (value === null ? value : `${value}`)),
+        }),
+        label: strings.PROJECTS,
+        renderOption: (id: string | number) => getProjectName(Number(id)),
+      });
+    }
+
+    return activeLocale ? filters : [];
+  }, [activeLocale, getProjectName, isAcceleratorRoute, projects]);
 
   const dispatchSearchRequest = useCallback(
     (locale: string | null, search: SearchNodePayload, searchSortOrder: SearchSortOrder) => {
--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { useOrganization } from 'src/providers';
 import { selectProjects } from 'src/redux/features/projects/projectsSelectors';
@@ -6,13 +6,21 @@ import { requestProjects } from 'src/redux/features/projects/projectsThunks';
 import { useAppDispatch, useAppSelector } from 'src/redux/store';
 import { Project } from 'src/types/Project';
 
+import useAcceleratorConsole from './useAcceleratorConsole';
+
 export const useProjects = (record?: { projectId?: number }) => {
   const dispatch = useAppDispatch();
   const { selectedOrganization } = useOrganization();
+  const { isAcceleratorRoute } = useAcceleratorConsole();
 
   const availableProjects = useAppSelector(selectProjects);
 
   const [selectedProject, setSelectedProject] = useState<Project>();
+
+  const getProjectName = useCallback(
+    (projectId: number) => (availableProjects?.find((project: Project) => project.id === projectId) || {}).name || '',
+    [availableProjects]
+  );
 
   useEffect(() => {
     if (availableProjects && record?.projectId) {
@@ -28,5 +36,12 @@ export const useProjects = (record?: { projectId?: number }) => {
     }
   }, [availableProjects, dispatch, selectedOrganization.id]);
 
-  return { availableProjects, selectedProject };
+  // fetch all projects in the accelerator route
+  useEffect(() => {
+    if (isAcceleratorRoute) {
+      void dispatch(requestProjects());
+    }
+  }, [isAcceleratorRoute, dispatch]);
+
+  return { availableProjects, getProjectName, selectedProject };
 };
