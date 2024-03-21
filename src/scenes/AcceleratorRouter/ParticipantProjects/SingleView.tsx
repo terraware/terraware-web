3afc406a25418d0c5cde3d0eb9e906d1fea4c0b3
Nick Graziano <nick.graziano@gmail.com>
2024-03-21 13:28:46 -0600

    Remove file that goes in a subsequent PR

--- src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useMemo } from 'react';
 
 import { Box, useTheme } from '@mui/material';
-import { BusySpinner, Button, DropdownItem } from '@terraware/web-components';
+import { BusySpinner, Button } from '@terraware/web-components';
 
 import Card from 'src/components/common/Card';
-import OptionsMenu from 'src/components/common/OptionsMenu';
 import useNavigateTo from 'src/hooks/useNavigateTo';
-import { useUser } from 'src/providers';
 import strings from 'src/strings';
 
 import PageWithModuleTimeline from '../PageWithModuleTimeline';
@@ -14,44 +12,24 @@ import { useParticipantProjectData } from './ParticipantProjectContext';
 
 const SingleView = () => {
   const theme = useTheme();
-  const { isAllowed } = useUser();
   const { crumbs, projectId, project, status } = useParticipantProjectData();
   const { goToParticipantProjectEdit } = useNavigateTo();
-
-  const onOptionItemClick = useCallback((item: DropdownItem) => {
-    if (item.value === 'export-participant-project') {
-      // TODO
-    }
-  }, []);
 
   const rightComponent = useMemo(
     () => (
       <Box display='flex' flexDirection='row' flexGrow={0} marginRight={theme.spacing(3)} justifyContent='right'>
-        {isAllowed('UPDATE_PARTICIPANT_PROJECT') && (
-          <Button
-            id='editProject'
-            icon='iconEdit'
-            label={strings.EDIT_PROJECT}
-            priority='primary'
-            onClick={() => goToParticipantProjectEdit(projectId)}
-            size='medium'
-            type='productive'
-          />
-        )}
-
-        <OptionsMenu
-          onOptionItemClick={onOptionItemClick}
-          optionItems={[
-            {
-              label: strings.EXPORT,
-              type: 'passive',
-              value: 'export-participant-project',
-            },
-          ]}
+        <Button
+          id='editProject'
+          icon='iconEdit'
+          label={strings.EDIT_PROJECT}
+          priority='primary'
+          onClick={() => goToParticipantProjectEdit(projectId)}
+          size='medium'
+          type='productive'
         />
       </Box>
     ),
-    [goToParticipantProjectEdit, isAllowed, projectId, onOptionItemClick, theme]
+    [goToParticipantProjectEdit, projectId, theme]
   );
 
   return (
