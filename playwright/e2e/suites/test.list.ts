c6d191970b847fe654663f5e678fc55c51408f1a
Alex <alex@terraformation.com>
2024-03-21 14:31:14 -0700

    Format

--- playwright/e2e/suites/test.list.ts
+++ playwright/e2e/suites/test.list.ts
@@ -1,9 +1,9 @@
 import { test } from '@playwright/test';
 
 import AccessionTests from './accession.spec.ts';
+import InventoryTests from './inventory.spec.ts';
 import LocationTests from './locations.spec.ts';
 import SpeciesTests from './species.spec.ts';
-import InventoryTests from './inventory.spec.ts';
 
 test.describe(LocationTests);
 test.describe(SpeciesTests);
