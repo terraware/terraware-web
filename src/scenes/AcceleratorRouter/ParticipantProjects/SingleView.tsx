484509c521b8e83dacdef6a7e83edc5a5b5dda42
Nick Graziano <nick.graziano@gmail.com>
2024-03-21 13:46:55 -0600

    SW-4713 Add participant project option menu that includes export, for now a no-op until BE is done

--- src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { Box, useTheme } from '@mui/material';
-import { BusySpinner, Button } from '@terraware/web-components';
+import { BusySpinner, Button, DropdownItem } from '@terraware/web-components';
 
 import Card from 'src/components/common/Card';
+import OptionsMenu from 'src/components/common/OptionsMenu';
 import useNavigateTo from 'src/hooks/useNavigateTo';
+import { useUser } from 'src/providers';
 import strings from 'src/strings';
 
 import PageWithModuleTimeline from '../PageWithModuleTimeline';
@@ -12,24 +14,44 @@ import { useParticipantProjectData } from './ParticipantProjectContext';
 
 const SingleView = () => {
   const theme = useTheme();
+  const { isAllowed } = useUser();
   const { crumbs, projectId, project, status } = useParticipantProjectData();
   const { goToParticipantProjectEdit } = useNavigateTo();
+
+  const onOptionItemClick = useCallback((item: DropdownItem) => {
+    if (item.value === 'export-participant-project') {
+      // TODO when BE is done
+    }
+  }, []);
 
   const rightComponent = useMemo(
     () => (
       <Box display='flex' flexDirection='row' flexGrow={0} marginRight={theme.spacing(3)} justifyContent='right'>
-        <Button
-          id='editProject'
-          icon='iconEdit'
-          label={strings.EDIT_PROJECT}
-          priority='primary'
-          onClick={() => goToParticipantProjectEdit(projectId)}
-          size='medium'
-          type='productive'
+        {isAllowed('UPDATE_PARTICIPANT_PROJECT') && (
+          <Button
+            id='editProject'
+            icon='iconEdit'
+            label={strings.EDIT_PROJECT}
+            priority='primary'
+            onClick={() => goToParticipantProjectEdit(projectId)}
+            size='medium'
+            type='productive'
+          />
+        )}
+
+        <OptionsMenu
+          onOptionItemClick={onOptionItemClick}
+          optionItems={[
+            {
+              label: strings.EXPORT,
+              type: 'passive',
+              value: 'export-participant-project',
+            },
+          ]}
         />
       </Box>
     ),
-    [goToParticipantProjectEdit, projectId, theme]
+    [goToParticipantProjectEdit, isAllowed, projectId, onOptionItemClick, theme]
   );
 
   return (
