8ba9e6e8b2fa59451ce1da92521a520271b47f70
Josh Buchea <verctrl@joshbuchea.com>
2024-03-25 16:02:09 -1000

    cohort new/edit form updates

--- src/scenes/AcceleratorRouter/Cohorts/CohortForm.tsx
+++ src/scenes/AcceleratorRouter/Cohorts/CohortForm.tsx
@@ -9,6 +9,8 @@ import strings from 'src/strings';
 import { CreateCohortRequestPayload, UpdateCohortRequestPayload } from 'src/types/Cohort';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
 
+// import CohortFieldMeta from './CohortField/Meta';
+
 type CohortFormProps<T extends CreateCohortRequestPayload | UpdateCohortRequestPayload> = {
   busy?: boolean;
   cohort: T;
@@ -92,7 +94,7 @@ export default function CohortForm<T extends CreateCohortRequestPayload | Update
           paddingLeft: theme.spacing(isMobile ? 0 : 4),
           paddingRight: theme.spacing(isMobile ? 0 : 4),
           paddingTop: theme.spacing(5),
-          width: isMobile ? '100%' : '700px',
+          width: '100%',
         }}
       >
         <Grid
@@ -102,29 +104,47 @@ export default function CohortForm<T extends CreateCohortRequestPayload | Update
             borderRadius: theme.spacing(4),
             padding: theme.spacing(3),
           }}
-          width={isMobile ? '100%' : '700px'}
+          width={'100%'}
         >
-          <Grid item xs={12} sx={{ marginTop: theme.spacing(2) }}>
-            <Textfield
-              errorText={validateFields && !localRecord?.name ? strings.REQUIRED_FIELD : ''}
-              id='name'
-              label={strings.NAME}
-              onChange={(value) => updateField('name', value)}
-              required
-              type='text'
-              value={localRecord.name}
-            />
+          <Grid container spacing={theme.spacing(3)} width={'100%'}>
+            <Grid item xs={isMobile ? 12 : 4} sx={{ marginTop: theme.spacing(2) }}>
+              <Textfield
+                errorText={validateFields && !localRecord?.name ? strings.REQUIRED_FIELD : ''}
+                id='name'
+                label={strings.NAME}
+                onChange={(value) => updateField('name', value)}
+                required
+                type='text'
+                value={localRecord.name}
+              />
+            </Grid>
+            <Grid item xs={isMobile ? 12 : 4} sx={{ marginTop: theme.spacing(2) }}>
+              <Dropdown
+                fullWidth={true}
+                label={strings.CURRENT_PHASE}
+                onChange={(value) => updateField('phase', value)}
+                options={currentPhaseDropdownOptions}
+                required
+                selectedValue={localRecord.phase}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={12} sx={{ marginTop: theme.spacing(2) }}>
-            <Dropdown
-              fullWidth={true}
-              label={strings.CURRENT_PHASE}
-              onChange={(value) => updateField('phase', value)}
-              options={currentPhaseDropdownOptions}
-              required
-              selectedValue={localRecord.phase}
+
+          {/* TODO: uncomment this section once createdTime & modifiedTime are available in Cohort records */}
+          {/* <Grid container>
+            <CohortFieldMeta
+              date={cohort.createdTime || 'Mar 2, 2024'}
+              dateLabel={strings.CREATED_ON}
+              user={cohort.createdBy || 'Weese Ritherspoon'}
+              userLabel={strings.CREATED_BY}
             />
-          </Grid>
+            <CohortFieldMeta
+              date={cohort.modifiedTime || 'Mar 2, 2024'}
+              dateLabel={strings.LAST_MODIFIED_ON}
+              user={cohort.modifiedBy || 'Weese Ritherspoon'}
+              userLabel={strings.LAST_MODIFIED_BY}
+            />
+          </Grid> */}
         </Grid>
       </Container>
     </PageForm>
