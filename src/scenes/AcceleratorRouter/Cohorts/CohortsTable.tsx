00df7734cecb8c7ff0dc20747620299f7d8c3729
Josh Buchea <verctrl@joshbuchea.com>
2024-03-26 12:42:38 -1000

    remove columns prop

--- src/scenes/AcceleratorRouter/Cohorts/CohortsTable.tsx
+++ src/scenes/AcceleratorRouter/Cohorts/CohortsTable.tsx
@@ -19,7 +19,6 @@ import { SearchNodePayload, SearchSortOrder } from 'src/types/Search';
 import CohortCellRenderer from './CohortCellRenderer';
 
 interface CohortTableProps {
-  columns: (activeLocale: string | null) => TableColumnType[];
   extraTableFilters?: SearchNodePayload[];
   filterModifiers?: (filters: FilterConfig[]) => FilterConfig[];
 }
