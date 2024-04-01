03dfa5eccb25c26799ccb3c015dc577e87f7edfb
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-04-01 12:29:50 -0700

    Added tests, reordered imports, handled empty strings gracefully

--- src/utils/search.ts
+++ src/utils/search.ts
@@ -6,25 +6,27 @@ function escapeRegExp(input: string) {
 }
 
 /**
- * Check if a string matches a regex pattern
- */
-export const regexMatch = (input: string, stringToMatch: string): boolean => {
-  const regex = new RegExp(escapeRegExp(stringToMatch), 'i');
-  return !!input.match(regex);
-};
-
-/**
  * Checks an exact sequence of words or characters within a given string
  */
-export const phraseMatch = (input: string, word: string): boolean => {
+const phraseMatch = (input: string, word: string): boolean => {
+  if (word.length == 0) return false;
   const regex = new RegExp("\\b" + word + "\\b", "i");
   return !!input.match(regex)
 }
 
 /**
+ * Check if a string matches a regex pattern
+ */
+const regexMatch = (input: string, stringToMatch: string): boolean => {
+  const regex = new RegExp(escapeRegExp(stringToMatch), 'i');
+  return !!input.match(regex);
+};
+
+
+/**
  *   Return inner string if a string is double-quoted, otherwsie null
  */
-export const removeDoubleQuotes = (str: string): string | null => {
+const removeDoubleQuotes = (str: string): string | null => {
   const pattern = /^"([^"]*)"$/;
   const match = pattern.exec(str);
   if (match) {
@@ -33,3 +35,9 @@ export const removeDoubleQuotes = (str: string): string | null => {
     return null; // Return null if the string is not properly double-quoted
   }
 };
+
+export {
+  phraseMatch,
+  regexMatch,
+  removeDoubleQuotes
+}
