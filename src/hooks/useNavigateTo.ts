b213f6e02dcc611d8b875b2efb7c86bd09268e87
Karthik Balachandran <karthik@terraformation.com>
2024-04-03 06:15:14 -0700

    Fix useNavigateTo double memoization
    - memoize at the root payload

--- src/hooks/useNavigateTo.ts
+++ src/hooks/useNavigateTo.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { APP_PATHS } from 'src/constants';
@@ -6,71 +6,45 @@ import { APP_PATHS } from 'src/constants';
 export default function useNavigateTo() {
   const history = useHistory();
 
-  const goToModule = useCallback(
-    (projectId: number, moduleId: number) => {
-      history.push({
-        pathname: APP_PATHS.MODULES_FOR_PROJECT_CONTENT.replace(':projectId', `${projectId}`).replace(
-          ':moduleId',
-          `${moduleId}`
-        ),
-      });
-    },
-    [history]
-  );
-
-  const goToParticipant = useCallback(
-    (participantId: number) => {
-      history.push({
-        pathname: APP_PATHS.ACCELERATOR_PARTICIPANTS_VIEW.replace(':participantId', `${participantId}`),
-      });
-    },
-    [history]
-  );
-
-  const goToParticipantsList = useCallback(() => {
-    history.push({
-      pathname: APP_PATHS.ACCELERATOR_OVERVIEW,
-      search: 'tab=participants',
-    });
-  }, [history]);
-
-  const goToParticipantProject = useCallback(
-    (projectId: number) => {
-      history.push({ pathname: APP_PATHS.ACCELERATOR_PROJECT_VIEW.replace(':projectId', `${projectId}`) });
-    },
-    [history]
-  );
-
-  const goToParticipantProjectEdit = useCallback(
-    (projectId: number) => {
-      history.push({ pathname: APP_PATHS.ACCELERATOR_PROJECT_EDIT.replace(':projectId', `${projectId}`) });
-    },
-    [history]
-  );
-
-  const goToParticipantProjectList = useCallback(() => {
-    history.push({
-      pathname: APP_PATHS.ACCELERATOR_OVERVIEW,
-      search: 'tab=projects',
-    });
-  }, [history]);
-
   return useMemo(
     () => ({
-      goToModule,
-      goToParticipant,
-      goToParticipantsList,
-      goToParticipantProject,
-      goToParticipantProjectEdit,
-      goToParticipantProjectList,
+      goToModule: (projectId: number, moduleId: number) => {
+        history.push({
+          pathname: APP_PATHS.MODULES_FOR_PROJECT_CONTENT.replace(':projectId', `${projectId}`).replace(
+            ':moduleId',
+            `${moduleId}`
+          ),
+        });
+      },
+
+      goToParticipant: (participantId: number) => {
+        history.push({
+          pathname: APP_PATHS.ACCELERATOR_PARTICIPANTS_VIEW.replace(':participantId', `${participantId}`),
+        });
+      },
+
+      goToParticipantsList: () => {
+        history.push({
+          pathname: APP_PATHS.ACCELERATOR_OVERVIEW,
+          search: 'tab=participants',
+        });
+      },
+
+      goToParticipantProject: (projectId: number) => {
+        history.push({ pathname: APP_PATHS.ACCELERATOR_PROJECT_VIEW.replace(':projectId', `${projectId}`) });
+      },
+
+      goToParticipantProjectEdit: (projectId: number) => {
+        history.push({ pathname: APP_PATHS.ACCELERATOR_PROJECT_EDIT.replace(':projectId', `${projectId}`) });
+      },
+
+      goToParticipantProjectList: () => {
+        history.push({
+          pathname: APP_PATHS.ACCELERATOR_OVERVIEW,
+          search: 'tab=projects',
+        });
+      },
     }),
-    [
-      goToModule,
-      goToParticipant,
-      goToParticipantsList,
-      goToParticipantProject,
-      goToParticipantProjectEdit,
-      goToParticipantProjectList,
-    ]
+    [history]
   );
 }
