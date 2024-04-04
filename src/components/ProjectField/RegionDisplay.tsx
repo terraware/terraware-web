623753c9c5c5b31347287b4dda6a742052441155
Karthik Balachandran <karthik@terraformation.com>
2024-04-04 16:03:57 -0700

    - remove region i18n mapping

--- src/components/ProjectField/RegionDisplay.tsx
+++ src/components/ProjectField/RegionDisplay.tsx
@@ -1,11 +1,8 @@
-import { getRegionLabel } from 'src/types/ParticipantProject';
-import { Region } from 'src/types/ParticipantProject';
-
 import { ProjectFieldProps } from './';
 import TextfieldDisplay from './TextfieldDisplay';
 
 const RegionDisplay = ({ label, value }: ProjectFieldProps) => (
-  <TextfieldDisplay id='project-region' label={label} value={getRegionLabel(value as Region)} />
+  <TextfieldDisplay id='project-region' label={label} value={value} />
 );
 
 export default RegionDisplay;
