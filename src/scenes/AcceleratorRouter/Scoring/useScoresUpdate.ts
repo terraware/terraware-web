8b9847cc4c43b74e626abc05efaad483e0047f64
Karthik B <karthik@terraformation.com>
2024-04-04 06:04:22 -0700

    SW-5152 / SW-5169 Redirect to score edit if there is no scores/qualitative data (#2463)
    - added a `hasData` property on the context which checks if there is
    data
    - in read-only view, redirect to edit if there is no data
    - in edit-view, redirect to project details on cancel if there is no
    data (to avoid ending up back in edit view)
    - since we need to deterministically identify if there is no data, added
    blocking checks in the update code to wait until the refresh list
    completes
    - this is a tad ugly but needed, otherwise redirecting back to read-only
    view after save will only end up back in edit view (hasData hasn't had a
    chance to update)
    - also fixed bug 5169 where a new score entry only keeps the latest edit
    (was using originalScore as copy instead of updated score), fixed that

--- src/scenes/AcceleratorRouter/Scoring/useScoresUpdate.ts
+++ src/scenes/AcceleratorRouter/Scoring/useScoresUpdate.ts
@@ -2,7 +2,7 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { Statuses } from 'src/redux/features/asyncUtils';
 import { requestListScores, requestUpdateScores } from 'src/redux/features/scores/scoresAsyncThunks';
-import { selectScoresUpdateRequest } from 'src/redux/features/scores/scoresSelectors';
+import { selectScoreListByRequest, selectScoresUpdateRequest } from 'src/redux/features/scores/scoresSelectors';
 import { useAppDispatch, useAppSelector } from 'src/redux/store';
 import strings from 'src/strings';
 import { Phase, Score } from 'src/types/Score';
@@ -21,7 +21,9 @@ export default function useScoresUpdate(projectId: number): Response {
   const dispatch = useAppDispatch();
 
   const [requestId, setRequestId] = useState('');
+  const [listRequestId, setListRequestId] = useState('');
   const result = useAppSelector(selectScoresUpdateRequest(requestId));
+  const scoreListResult = useAppSelector(selectScoreListByRequest(listRequestId));
 
   const update = useCallback(
     (phase: Phase, scores: Score[]) => {
@@ -37,15 +39,18 @@ export default function useScoresUpdate(projectId: number): Response {
     } else if (result?.status === 'success') {
       snackbar.toastSuccess(strings.CHANGES_SAVED);
       // Refresh scores in store for this project
-      dispatch(requestListScores(projectId));
+      const dispatched = dispatch(requestListScores(projectId));
+      setListRequestId(dispatched.requestId);
     }
   }, [dispatch, projectId, result, snackbar]);
 
   return useMemo<Response>(
     () => ({
-      status: result?.status,
+      // Update is considered successful if both update and refresh list are successful.
+      // This is to help with provider data state.
+      status: result?.status === 'success' ? scoreListResult?.status : result?.status,
       update,
     }),
-    [result?.status, update]
+    [scoreListResult?.status, result?.status, update]
   );
 }
