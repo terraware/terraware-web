d03bbedd416f3ca16130085061d124fd245bfbab
Alex <alex@terraformation.com>
2024-04-05 08:48:45 -0700

    Make chromium default

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,15 +42,15 @@ export default defineConfig({
         },
       ]
     : [
-        // {
-        //   name: 'chromium',
-        //   use: { ...devices['Desktop Chrome'] },
-        // },
+         {
+           name: 'chromium',
+           use: { ...devices['Desktop Chrome'] },
+         },
 
-        {
-          name: 'firefox',
-          use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
 
         // {
         //   name: 'webkit',
