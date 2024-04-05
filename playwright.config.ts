a0711a4bb484ac8fb0b98caf61fffcd11bc11244
Alex Edwards <104874529+alexTerraformation@users.noreply.github.com>
2024-04-05 16:33:07 -0700

    SW-4643 - Add nursery inventory e2e tests (#2487)

--- playwright.config.ts
+++ playwright.config.ts
@@ -37,8 +37,8 @@ export default defineConfig({
   projects: process.env.CI
     ? [
         {
-          name: 'firefox',
-          use: { ...devices['Desktop Firefox'] },
+          name: 'chromium',
+          use: { ...devices['Desktop Chrome'] },
         },
       ]
     : [
@@ -47,15 +47,15 @@ export default defineConfig({
           use: { ...devices['Desktop Chrome'] },
         },
 
-        {
-          name: 'firefox',
-          use: { ...devices['Desktop Firefox'] },
-        },
+        // {
+        //   name: 'firefox',
+        //   use: { ...devices['Desktop Firefox'] },
+        // },
 
-        {
-          name: 'webkit',
-          use: { ...devices['Desktop Safari'] },
-        },
+        // {
+        //   name: 'webkit',
+        //   use: { ...devices['Desktop Safari'] },
+        // },
 
         /* Test against mobile viewports. */
         // {
