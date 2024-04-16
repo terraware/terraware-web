69c1440e2763eb9cf03d4d8f9c4230da0a5d1a61
Nicholas Graziano <nick.graziano@gmail.com>
2024-04-16 17:05:55 -0600

    SW-5122 Stub out welcome banner (#2516)
    - Add `WelcomeBanner` component to the participant home page
    
    
    TODO (later PRs)
    - [x] Store user preference when it is dismissed - #2518
    - [x] Show only if it hasn't been dismissed - #2518
    - [x] Figure out where to put the image asset
    - [x] Determine if this text is stored in strings ~or if we should store somewhere else~ 
    
    
    ![SCR-20240416-ljzi.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/wJnPX9wLZAkiGXEBFJxL/c2714760-de2a-4cb7-8a0e-6d9758b78ff1.png)

---
+++ src/scenes/Home/ParticipantHomeView/WelcomeBanner.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+import { Box, Grid, Typography, useTheme } from '@mui/material';
+
+import DialogCloseButton from 'src/components/common/DialogCloseButton';
+import strings from 'src/strings';
+
+const WelcomeBanner = () => {
+  const theme = useTheme();
+
+  const handleOnClick = () => {
+    // tslint:disable:no-console
+    console.log('close');
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: theme.palette.TwClrBgSecondary,
+        borderRadius: theme.spacing(2),
+        border: `1px solid ${theme.palette.TwClrBrdrSecondary}`,
+        padding: theme.spacing(2),
+        position: 'relative',
+      }}
+    >
+      <Grid container columnSpacing={theme.spacing(2)} paddingRight={0} justifyContent={'flex-start'}>
+        <Grid item xs={2}>
+          <img
+            src='/assets/accelerator-welcome@2x.png'
+            // TODO this might change
+            alt={strings.ACCELERATOR_WELCOME_ALT}
+            width='100%'
+            height='100px'
+            style={{
+              objectFit: 'cover',
+              objectPosition: '0% 50%',
+            }}
+          />
+        </Grid>
+        <Grid item xs={9}>
+          <Typography fontSize={'24px'} fontWeight={600} lineHeight={'32px'}>
+            {strings.ACCELERATOR_WELCOME_HEADER}
+          </Typography>
+          <Typography fontSize={'16px'} fontWeight={400} lineHeight={'24px'}>
+            {strings.ACCELERATOR_WELCOME_CONTENT}
+          </Typography>
+        </Grid>
+
+        <DialogCloseButton
+          onClick={handleOnClick}
+          sx={{
+            color: theme.palette.TwClrIcn,
+            margin: 0,
+            padding: theme.spacing(2),
+            right: 0,
+            top: 0,
+          }}
+        />
+      </Grid>
+    </Box>
+  );
+};
+
+export default WelcomeBanner;
--- src/scenes/Home/ParticipantHomeView/index.tsx
+++ src/scenes/Home/ParticipantHomeView/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 
-import { Grid } from '@mui/material';
+import { Grid, useTheme } from '@mui/material';
+
+import PageWithModuleTimeline from 'src/components/common/PageWithModuleTimeline';
 
 import Header from './Header';
+import WelcomeBanner from './WelcomeBanner';
 
 const ParticipantHomeView = () => {
+  const theme = useTheme();
+
   return (
-    <Grid display={'flex'} flexDirection={'column'}>
-      <Grid item>
-        <Header />
+    <PageWithModuleTimeline>
+      <Grid display={'flex'} flexDirection={'column'}>
+        <Grid item>
+          <Header />
+        </Grid>
+        <Grid item marginTop={theme.spacing(2)}>
+          <WelcomeBanner />
+        </Grid>
       </Grid>
-    </Grid>
+    </PageWithModuleTimeline>
   );
 };
 
