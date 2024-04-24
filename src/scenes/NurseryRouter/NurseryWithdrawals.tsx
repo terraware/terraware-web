ad2e382538be8b2dc036add6dad97fda22c6cd5f
Josh Buchea <verctrl@joshbuchea.com>
2024-04-23 14:01:46 -1000

    replace "Withdrawal Log" --> "Withdrawal History"

--- src/scenes/NurseryRouter/NurseryWithdrawals.tsx
+++ src/scenes/NurseryRouter/NurseryWithdrawals.tsx
@@ -22,7 +22,7 @@ export default function NurseryWithdrawals(): JSX.Element {
             <Grid container spacing={3} sx={{ paddingLeft: theme.spacing(3), paddingBottom: theme.spacing(4) }}>
               <Grid item xs={8}>
                 <Typography sx={{ marginTop: 0, marginBottom: 0, fontSize: '24px', fontWeight: 600 }}>
-                  {strings.WITHDRAWAL_LOG}
+                  {strings.WITHDRAWAL_HISTORY}
                 </Typography>
               </Grid>
             </Grid>
