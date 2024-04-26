7110e4098a1d5d0cbf1103e52aef5505c9c087ec
Josh Buchea <verctrl@joshbuchea.com>
2024-04-26 10:15:44 -1000

    make projectId optional for service list method

--- src/services/ParticipantProjectSpeciesService.ts
+++ src/services/ParticipantProjectSpeciesService.ts
@@ -62,9 +62,17 @@ const get = async (projectId: number, projectSpeciesId: number): Promise<Project
   });
 };
 
-const list = async (projectId: number): Promise<AllSpeciesResponse> => {
+const list = async (projectId?: number): Promise<AllSpeciesResponse> => {
   return new Promise((resolve) => {
-    resolve({ data: { projectSpecies: mockProjectSpecies?.[projectId] || [] }, requestSucceeded: true });
+    if (projectId === undefined) {
+      const allProjectsSpecies: ProjectSpecies[] = [];
+      Object.values(mockProjectSpecies).forEach((speciesList) => {
+        allProjectsSpecies.push(...speciesList);
+      });
+      resolve({ data: { projectSpecies: allProjectsSpecies }, requestSucceeded: true });
+    } else {
+      resolve({ data: { projectSpecies: mockProjectSpecies?.[projectId] || [] }, requestSucceeded: true });
+    }
   });
 };
 
