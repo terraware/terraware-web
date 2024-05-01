035035a5b535506b8134cf645340c78c5d42e528
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-05-01 09:13:56 -1000

    SW-5277: Create SpeciesDeliverableView (#2564)
    SW-5277: Create SpeciesDeliverableView (#2564)

---
+++ src/scenes/DeliverablesRouter/SpeciesDeliverableView.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect, useMemo } from 'react';
+
+import { Box, Typography, useTheme } from '@mui/material';
+import { BusySpinner } from '@terraware/web-components';
+
+import { Crumb } from 'src/components/BreadCrumbs';
+import Metadata from 'src/components/DeliverableView/Metadata';
+import MobileMessage from 'src/components/DeliverableView/MobileMessage';
+import TitleBar from 'src/components/DeliverableView/TitleBar';
+import { EditProps, ViewProps } from 'src/components/DeliverableView/types';
+import Page from 'src/components/Page';
+import Card from 'src/components/common/Card';
+import Button from 'src/components/common/button/Button';
+import { APP_PATHS } from 'src/constants';
+import { useLocalization } from 'src/providers';
+import { useParticipantData } from 'src/providers/Participant/ParticipantContext';
+import { requestListParticipantProjectSpecies } from 'src/redux/features/participantProjectSpecies/participantProjectSpeciesAsyncThunks';
+import { selectParticipantProjectSpeciesListRequest } from 'src/redux/features/participantProjectSpecies/participantProjectSpeciesSelectors';
+import { useAppDispatch, useAppSelector } from 'src/redux/store';
+import RejectedDeliverableMessage from 'src/scenes/DeliverablesRouter/RejectedDeliverableMessage';
+import strings from 'src/strings';
+import useDeviceInfo from 'src/utils/useDeviceInfo';
+
+export type Props = EditProps & {
+  isBusy?: boolean;
+};
+
+const SpeciesDeliverableView = (props: Props): JSX.Element => {
+  const { ...viewProps }: ViewProps = props;
+  const dispatch = useAppDispatch();
+  const { isMobile } = useDeviceInfo();
+  const { activeLocale } = useLocalization();
+  const theme = useTheme();
+  const { currentParticipantProject } = useParticipantData();
+  const participantProjectSpecies = useAppSelector(
+    selectParticipantProjectSpeciesListRequest(currentParticipantProject?.id || -1)
+  );
+
+  const submitButtonIsDisabled = useMemo(() => {
+    return (
+      !participantProjectSpecies?.data?.length ||
+      participantProjectSpecies?.data?.every((species) => species.status === 'Approved')
+    );
+  }, [participantProjectSpecies]);
+
+  useEffect(() => {
+    if (!currentParticipantProject?.id) {
+      return;
+    }
+
+    void dispatch(requestListParticipantProjectSpecies(currentParticipantProject?.id));
+  }, []);
+
+  const crumbs: Crumb[] = useMemo(
+    () => [
+      {
+        name: activeLocale ? strings.DELIVERABLES : '',
+        to: APP_PATHS.DELIVERABLES,
+      },
+    ],
+    [activeLocale]
+  );
+
+  if (isMobile) {
+    return <MobileMessage {...viewProps} />;
+  }
+
+  return (
+    <Page
+      title={<TitleBar {...props} />}
+      crumbs={crumbs}
+      rightComponent={
+        <Button
+          disabled={submitButtonIsDisabled}
+          label={strings.SUBMIT_FOR_APPROVAL}
+          onClick={() => {
+            console.log('submit for approval button pressed');
+          }}
+          size='medium'
+          id='submitDeliverable'
+        />
+      }
+    >
+      {props.isBusy && <BusySpinner />}
+      <Box display='flex' flexDirection='column' flexGrow={1}>
+        <RejectedDeliverableMessage {...viewProps} />
+        <Card style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+          <Metadata {...viewProps} />
+          <Typography marginBottom={theme.spacing(2)} fontSize='20px' lineHeight='28px' fontWeight={600}>
+            {strings.SPECIES}
+          </Typography>
+          {/* TODO: Species table here */}
+        </Card>
+      </Box>
+    </Page>
+  );
+};
+
+export default SpeciesDeliverableView;
--- src/scenes/DeliverablesRouter/index.tsx
+++ src/scenes/DeliverablesRouter/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import ParticipantProvider from 'src/providers/Participant/ParticipantProvider';
+import ProjectProvider from 'src/providers/Project/ProjectProvider';
+
 import DeliverableViewWrapper from './DeliverableViewWrapper';
 import DeliverablesList from './DeliverablesList';
 
 const DeliverablesRouter = (): JSX.Element => {
   return (
     <Routes>
-      <Route path={'/:deliverableId/submissions/:projectId'} element={<DeliverableViewWrapper />} />
+      <Route
+        path={'/:deliverableId/submissions/:projectId'}
+        element={
+          <ProjectProvider>
+            <ParticipantProvider>
+              <DeliverableViewWrapper />
+            </ParticipantProvider>
+          </ProjectProvider>
+        }
+      />
       <Route path={'*'} element={<DeliverablesList />} />
     </Routes>
   );
