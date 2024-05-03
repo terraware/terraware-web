32b6c3bb06acec1bbfa0b4b58fd30046dedf57ce
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-02 21:36:37 -0700

    Move participant context out of page

--- src/components/common/PageWithModuleTimeline/index.tsx
+++ src/components/common/PageWithModuleTimeline/index.tsx
@@ -4,7 +4,6 @@ import { Grid, Theme, useTheme } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 
 import Page, { PageProps } from 'src/components/Page';
-import ParticipantProvider from 'src/providers/Participant/ParticipantProvider';
 import useDeviceInfo from 'src/utils/useDeviceInfo';
 
 import ModuleTimeline from './ModuleTimeline';
@@ -23,26 +22,24 @@ const PageWithModuleTimeline = (props: PageProps) => {
   const { isMobile } = useDeviceInfo();
 
   return (
-    <ParticipantProvider>
-      <Grid container spacing={theme.spacing(0)} paddingRight={'24px'}>
-        <Grid
-          item
-          sx={{
-            maxWidth: { xs: '100%', sm: '100%', md: '100%', lg: 'calc(100% - 206px)', xl: 'calc(100% - 206px)' },
-            width: '100%',
-          }}
-        >
-          <Page {...props} containerClassName={isMobile ? '' : classes.container} />
-        </Grid>
-        <Grid
-          item
-          sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'block', xl: 'block' } }}
-          minWidth={'206px'}
-        >
-          <ModuleTimeline />
-        </Grid>
+    <Grid container spacing={theme.spacing(0)} paddingRight={'24px'}>
+      <Grid
+        item
+        sx={{
+          maxWidth: { xs: '100%', sm: '100%', md: '100%', lg: 'calc(100% - 206px)', xl: 'calc(100% - 206px)' },
+          width: '100%',
+        }}
+      >
+        <Page {...props} containerClassName={isMobile ? '' : classes.container} />
       </Grid>
-    </ParticipantProvider>
+      <Grid
+        item
+        sx={{ display: { xs: 'none', sm: 'none', md: 'none', lg: 'block', xl: 'block' } }}
+        minWidth={'206px'}
+      >
+        <ModuleTimeline />
+      </Grid>
+    </Grid>
   );
 };
 
