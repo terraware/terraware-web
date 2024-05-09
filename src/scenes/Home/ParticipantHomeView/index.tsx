e84d9e35ac58e19498b0b93fd97d21a36937cd0f
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-09 14:15:15 -0700

    Force reload of home screen if swithced to org with no participants

--- src/scenes/Home/ParticipantHomeView/index.tsx
+++ src/scenes/Home/ParticipantHomeView/index.tsx
@@ -4,6 +4,8 @@ import { Grid, useTheme } from '@mui/material';
 
 import DismissibleWrapper from 'src/components/common/DismissibleWrapper';
 import PageWithModuleTimeline from 'src/components/common/PageWithModuleTimeline';
+import useNavigateTo from 'src/hooks/useNavigateTo';
+import { useParticipantData } from 'src/providers/Participant/ParticipantContext';
 
 import CurrentModule from './CurrentModule';
 import Header from './Header';
@@ -13,6 +15,14 @@ import WelcomeBanner from './WelcomeBanner';
 
 const ParticipantHomeView = () => {
   const theme = useTheme();
+
+  const { currentParticipantProject } = useParticipantData();
+  const { goToHome } = useNavigateTo();
+
+  if (!currentParticipantProject) {
+    // Force reload of home screen.
+    goToHome();
+  }
 
   return (
     <PageWithModuleTimeline>
