b43b4e347d420fb42bc3a7df6782b016a58f6252
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-10 12:16:59 -0700

    Refactored module details card out and use also in home screen

--- src/scenes/Home/ParticipantHomeView/CurrentModule.tsx
+++ src/scenes/Home/ParticipantHomeView/CurrentModule.tsx
@@ -1,10 +1,7 @@
-import React, { Box, Grid, Typography } from '@mui/material';
+import React from '@mui/material';
 
-import Card from 'src/components/common/Card';
-import Link from 'src/components/common/Link';
-import { APP_PATHS } from 'src/constants';
+import ModuleDetailsCard from 'src/components/ModuleDetailsCard';
 import { useParticipantData } from 'src/providers/Participant/ParticipantContext';
-import strings from 'src/strings';
 
 const CurrentModule = () => {
   const { activeModules, currentParticipantProject } = useParticipantData();
@@ -16,55 +13,7 @@ const CurrentModule = () => {
   // Only first active modules shown for now. TODO: upgrade to support multiple active modules for overlapping modules
   const currentModule = activeModules[0];
 
-  return (
-    <Card>
-      <Grid container flexDirection={'column'}>
-        <Grid item xs={7}>
-          <Grid container>
-            <Grid item xs={12}>
-              <Grid container justifyContent={'space-between'}>
-                <Grid item>
-                  <Typography fontSize={'16px'} fontWeight={500} lineHeight={'24px'}>
-                    {strings.formatString(strings.TITLE_OVERVIEW, currentModule.title)}
-                  </Typography>
-                </Grid>
-                <Grid item>
-                  <Link
-                    to={APP_PATHS.PROJECT_MODULES.replace(':projectId', `${currentParticipantProject.id}`)}
-                    fontSize={16}
-                    fontWeight={500}
-                  >
-                    {strings.SEE_ALL_MODULES}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Grid>
-
-            <Grid item xs={12}>
-              <Typography fontSize={'24px'} fontWeight={600} lineHeight={'32px'}>
-                {currentModule.name}
-              </Typography>
-            </Grid>
-
-            {currentModule.overview && (
-              <Grid item xs={12}>
-                <Box dangerouslySetInnerHTML={{ __html: currentModule.overview }} />
-              </Grid>
-            )}
-
-            <Grid item xs={12}>
-              <Typography fontSize={'20px'} fontWeight={600} lineHeight={'28px'}>
-                {strings.THIS_MODULE_CONTAINS}
-              </Typography>
-            </Grid>
-
-            {/* TODO module links will come after BE sync */}
-          </Grid>
-        </Grid>
-        <Grid item xs={5} />
-      </Grid>
-    </Card>
-  );
+  return <ModuleDetailsCard projectId={currentParticipantProject.id} module={currentModule} showSeeAllModules={true} />;
 };
 
 export default CurrentModule;
