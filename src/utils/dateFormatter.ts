4736a68b1f5dc2fefbf80a2a30d6cdb81400c83f
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-05-10 14:10:47 -0700

    IsoDate

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -47,7 +47,7 @@ export const getDateTimeDisplayValue = (timestamp: number): string => {
 };
 
 /**
- * Returns <Date>–<Date> (eg. 2023/07/01 - 2023/07/15)
+ * Returns <ISO Date> – <ISO Date> (eg. 2023/07/01 - 2023/07/15)
  */
 export const getDateRangeString = (startDate: string, endDate: string, locale: string | undefined | null) => {
   const formattedStart = new Intl.DateTimeFormat(locale || 'en-US', {
