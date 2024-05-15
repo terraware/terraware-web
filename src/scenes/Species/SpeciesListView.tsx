38ef770b4f2c4d04a272d9a5a9e3a68b777a31f8
Josh Buchea <verctrl@joshbuchea.com>
2024-05-14 14:10:44 -1000

    oops, add line missed in prev commit

--- src/scenes/Species/SpeciesListView.tsx
+++ src/scenes/Species/SpeciesListView.tsx
@@ -259,6 +259,7 @@ export default function SpeciesListView({ reloadData, species }: SpeciesListProp
       {
         field: 'participantProjectSpecies.project.name',
         label: strings.PROJECT,
+        options: (projects || [])?.map((project: Project) => `${project.name}`),
       },
     ];
 
