14f992028273e85374f541f554169a3434850dff
Nick Graziano <nick.graziano@gmail.com>
2024-05-22 13:27:46 -0600

    SW-5410 Make the add species to projects modal a multi select modal

--- src/scenes/Species/AddToProjectModal.tsx
+++ src/scenes/Species/AddToProjectModal.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 
 import { Grid, useTheme } from '@mui/material';
-import { SelectT } from '@terraware/web-components';
+import { MultiSelect } from '@terraware/web-components';
 
 import DialogBox from 'src/components/common/DialogBox/DialogBox';
 import Button from 'src/components/common/button/Button';
@@ -10,21 +10,36 @@ import { Project } from 'src/types/Project';
 
 export interface AddToProjectModalProps {
   onClose: (reload?: boolean) => void;
-  onSubmit: (id: number) => void;
+  onSubmit: (ids: number[]) => void;
   projects?: Project[];
 }
 
 export default function AddToProjectModal(props: AddToProjectModalProps): JSX.Element {
   const { onClose, onSubmit, projects } = props;
   const theme = useTheme();
-  const [selectedProject, setSelectedProject] = useState<Project>();
+
+  const [selectedProjectIds, setSelectedProjectIds] = useState<number[]>([]);
+
+  const handleOnAdd = useCallback((value: number) => {
+    console.log('value', value);
+    setSelectedProjectIds((prev) => [...prev, value]);
+  }, []);
+  console.log('selectedProjectIds', selectedProjectIds);
+  const handleOnRemove = useCallback((value: number) => {
+    setSelectedProjectIds((prev) => prev.filter((prevValue) => prevValue !== value));
+  }, []);
 
   const save = () => {
-    if (selectedProject) {
-      onSubmit(selectedProject.id);
+    if (selectedProjectIds.length > 0) {
+      onSubmit(selectedProjectIds);
       onClose();
     }
   };
+
+  const options: Map<number, string> = useMemo(
+    () => new Map((projects || []).map((project) => [project.id, project.name])),
+    [projects]
+  );
 
   return (
     <DialogBox
@@ -46,19 +61,15 @@ export default function AddToProjectModal(props: AddToProjectModalProps): JSX.El
     >
       <Grid container textAlign={'left'}>
         <Grid item xs={12} sx={{ marginTop: theme.spacing(2) }}>
-          <SelectT<Project>
-            id='project'
+          <MultiSelect<number, string>
+            id={'project'}
             label={strings.PROJECT}
-            placeholder={strings.SELECT}
-            options={projects || []}
-            onChange={(project) => setSelectedProject(project)}
-            selectedValue={selectedProject}
-            fullWidth={true}
-            isEqual={(a: Project, b: Project) => a.id === b.id}
-            renderOption={(project: Project) => project?.name || ''}
-            displayLabel={(project: Project) => project?.name || ''}
-            toT={(name: string) => ({ name }) as Project}
-            required
+            onAdd={handleOnAdd}
+            onRemove={handleOnRemove}
+            options={options}
+            selectedOptions={selectedProjectIds}
+            valueRenderer={(value) => value}
+            fullWidth
           />
         </Grid>
       </Grid>
