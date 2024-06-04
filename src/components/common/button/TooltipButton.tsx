cfac2cdb196e59555cf9f4e6c0e80576435220de
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-06-04 11:38:21 -0700

    SW-5426 Added button with tooltip and used for editing planting sites (#2691)
    ![Screenshot 2024-06-03 at
    9.41.49 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NOxkRPuhAfT4F7nmdXtG/bf2a7d9c-6e93-46e3-b39f-1e769dc20b26.png)

---
+++ src/components/common/button/TooltipButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Props as ButtonProps } from '@terraware/web-components/components/Button/Button';
+import Tooltip from '@terraware/web-components/components/Tooltip/Tooltip';
+
+import Button from './Button';
+
+export type TooltipButtonProps = {
+  tooltip?: string; // if not undefined, hovering over the button will show a tooltip
+};
+
+const TooltipButton = (props: TooltipButtonProps & ButtonProps): JSX.Element => {
+  const { tooltip } = props;
+
+  if (tooltip !== undefined) {
+    return (
+      <Tooltip title={tooltip}>
+        <Button {...props} />
+      </Tooltip>
+    );
+  } else {
+    return <Button {...props} />;
+  }
+};
+
+export default TooltipButton;
