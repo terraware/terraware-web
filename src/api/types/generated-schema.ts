536e8ff224a341273982e4b9fb8c0fdfb636b9c6
Josh Buchea <verctrl@joshbuchea.com>
2024-06-07 10:40:53 -1000

    remove unrelated api schema changes

--- src/api/types/generated-schema.ts
+++ src/api/types/generated-schema.ts
@@ -770,10 +770,6 @@ export interface paths {
     /** Creates a new planting site. */
     post: operations["createPlantingSite"];
   };
-  "/api/v1/tracking/sites/validate": {
-    /** Validates the definition of a new planting site. */
-    post: operations["validatePlantingSite"];
-  };
   "/api/v1/tracking/sites/{id}": {
     /**
      * Gets information about a specific planting site.
@@ -2814,13 +2810,11 @@ export interface components {
     };
     NewPlantingSubzonePayload: {
       boundary: components["schemas"]["MultiPolygon"] | components["schemas"]["Polygon"];
-      /** @description Name of this planting subzone. Two subzones in the same planting zone may not have the same name, but using the same subzone name in different planting zones is valid. */
       name: string;
     };
     /** @description List of planting zones to create. If present and not empty, "boundary" must also be specified. */
     NewPlantingZonePayload: {
       boundary: components["schemas"]["MultiPolygon"] | components["schemas"]["Polygon"];
-      /** @description Name of this planting zone. Two zones in the same planting site may not have the same name. */
       name: string;
       plantingSubzones?: components["schemas"]["NewPlantingSubzonePayload"][];
       targetPlantingDensity?: number;
@@ -3300,16 +3294,6 @@ export interface components {
       progressPercent?: number;
       /** Format: int32 */
       totalPlants: number;
-    };
-    PlantingSiteValidationProblemPayload: {
-      /** @description If the problem is a conflict between two planting zones or two subzones, the list of the conflicting zone or subzone names. */
-      conflictsWith?: string[];
-      /** @description If the problem relates to a particular subzone, its name. If this is present, plantingZone will also be present and will be the name of the zone that contains this subzone. */
-      plantingSubzone?: string;
-      /** @description If the problem relates to a particular planting zone, its name. */
-      plantingZone?: string;
-      /** @enum {string} */
-      problemType: "CannotRemovePlantedSubzone" | "CannotSplitSubzone" | "CannotSplitZone" | "DuplicateSubzoneName" | "DuplicateZoneName" | "ExclusionWithoutBoundary" | "SiteTooLarge" | "SubzoneBoundaryChanged" | "SubzoneBoundaryOverlaps" | "SubzoneInExclusionArea" | "SubzoneNotInZone" | "ZoneBoundaryChanged" | "ZoneBoundaryOverlaps" | "ZoneHasNoSubzones" | "ZoneNotInSite" | "ZoneTooSmall" | "ZonesWithoutSiteBoundary";
     };
     PlantingSubzonePayload: {
       /** @description Area of planting subzone in hectares. */
@@ -4566,13 +4550,6 @@ export interface components {
       /** Format: int64 */
       id: number;
       lastName?: string;
-    };
-    ValidatePlantingSiteResponsePayload: {
-      /** @description True if the request was valid. */
-      isValid: boolean;
-      /** @description List of validation problems found, if any. Empty if the request is valid. */
-      problems: components["schemas"]["PlantingSiteValidationProblemPayload"][];
-      status: components["schemas"]["SuccessOrError"];
     };
     VersionsEntryPayload: {
       appName: string;
@@ -8068,18 +8045,6 @@ export interface operations {
           "application/json": components["schemas"]["UploadAttachmentResponsePayload"];
         };
       };
-      /** @description The request was too large. */
-      413: {
-        content: {
-          "application/json": components["schemas"]["SimpleErrorResponsePayload"];
-        };
-      };
-      /** @description The media type is not supported. */
-      415: {
-        content: {
-          "application/json": components["schemas"]["SimpleErrorResponsePayload"];
-        };
-      };
     };
   };
   /** Lists the timeseries for one or more devices. */
@@ -8648,22 +8613,6 @@ export interface operations {
       200: {
         content: {
           "application/json": components["schemas"]["CreatePlantingSiteResponsePayload"];
-        };
-      };
-    };
-  };
-  /** Validates the definition of a new planting site. */
-  validatePlantingSite: {
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["CreatePlantingSiteRequestPayload"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        content: {
-          "application/json": components["schemas"]["ValidatePlantingSiteResponsePayload"];
         };
       };
     };
