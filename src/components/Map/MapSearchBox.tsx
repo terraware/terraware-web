db18ccf141ab28b758b4d375d3c932816d6bc35b
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-06-18 10:21:05 -0700

    SW-4370 Added address search hooks (#2745)

---
+++ src/components/Map/MapSearchBox.tsx
@@ -0,0 +1,70 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { AddressAutofillFeatureSuggestion, AddressAutofillSuggestion } from '@mapbox/search-js-core';
+import { Autocomplete, TextField } from '@mui/material';
+
+import strings from 'src/strings';
+import useMapboxSearch from 'src/utils/useMapboxSearch';
+
+export type MapSearchBoxProp = {
+  onSelect?: (features: AddressAutofillFeatureSuggestion[] | null) => void;
+};
+
+const MapSearchBox = ({ onSelect }: MapSearchBoxProp) => {
+  const { clear, retrieve, suggest } = useMapboxSearch();
+  const [value, setValue] = useState<AddressAutofillSuggestion>();
+  const [inputValue, setInputValue] = useState<string>('');
+  const [options, setOptions] = useState<AddressAutofillSuggestion[]>([]);
+
+  const fetchSuggestions = useCallback(
+    async (searchText: string) => {
+      const results = await suggest(searchText);
+      setOptions(results);
+    },
+    [suggest]
+  );
+
+  const onSelectSuggestion = useCallback(
+    async (suggestion: AddressAutofillSuggestion | null) => {
+      const results = suggestion ? await retrieve(suggestion) : null;
+      if (onSelect) {
+        onSelect(results);
+      }
+    },
+    [retrieve]
+  );
+
+  useEffect(() => {
+    if (inputValue === '') {
+      clear();
+      setOptions([]);
+      return;
+    }
+
+    fetchSuggestions(inputValue);
+  }, [clear, inputValue, value]);
+
+  return (
+    <Autocomplete
+      id='mapbox-suggestions'
+      sx={{ width: 1000 }}
+      getOptionLabel={(option: AddressAutofillSuggestion) => `${option.feature_name}, ${option.full_address}`}
+      filterOptions={(x) => x}
+      options={options}
+      filterSelectedOptions
+      value={value || null}
+      noOptionsText='No locations'
+      onChange={(event, newValue: AddressAutofillSuggestion | null) => {
+        setOptions(newValue ? [newValue, ...options] : options);
+        setValue(newValue ?? undefined);
+        onSelectSuggestion(newValue);
+      }}
+      onInputChange={(event, newInputValue: string) => {
+        setInputValue(newInputValue);
+      }}
+      renderInput={(params) => <TextField {...params} label={strings.LOCATION} fullWidth />}
+    />
+  );
+};
+
+export default MapSearchBox;
