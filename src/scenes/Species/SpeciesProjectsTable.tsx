23c69ef3979353157acb4c52d6935df20cc06a58
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-06-19 12:31:31 -1000

    SW-5533: Can't add Species to Project (from Species page) even when Deliverable is active (#2757)
    SW-5533: Can't add Species to Project (from Species page) even when Deliverable is active (#2757)

--- src/scenes/Species/SpeciesProjectsTable.tsx
+++ src/scenes/Species/SpeciesProjectsTable.tsx
@@ -99,10 +99,16 @@ export default function SpeciesProjectsTable({
   useEffect(() => {
     const assignedProjectsIds = filteredResults?.map((fr) => Number(fr.projectId));
     const pendingProjects = allProjects?.filter((project) => {
-      return !assignedProjectsIds?.includes(project.id);
+      // only show projects that are not already assigned to the species and have a species deliverable
+      return (
+        !assignedProjectsIds?.includes(project.id) &&
+        currentDeliverables?.find(
+          (deliverable) => deliverable.type === 'Species' && deliverable.projectId === project.id
+        )
+      );
     });
     setSelectableProjects(pendingProjects || []);
-  }, [filteredResults, allProjects]);
+  }, [allProjects, currentDeliverables, filteredResults]);
 
   useEffect(() => {
     let updatedResults = searchResults ?? [];
