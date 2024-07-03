4d3c5da6dc99c63437624480afa87413745e2dbe
Josh Buchea <verctrl@joshbuchea.com>
2024-07-02 19:09:24 -1000

    add DeliverableDisplayVariableValue

---
+++ src/components/DocumentProducer/DeliverableDisplayVariableValue/index.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import { Box, useTheme } from '@mui/material';
+
+import TableDisplay from 'src/components/DocumentProducer/TableDisplay';
+import { TableVariableWithValues, VariableWithValues } from 'src/types/documentProducer/Variable';
+import { VariableValueImageValue, VariableValueSelectValue } from 'src/types/documentProducer/VariableValue';
+import { getImagePath } from 'src/utils/images';
+
+import { displayValue } from '../EditableSection/helpers';
+
+type DeliverableDisplayVariableValueProps = {
+  projectId: number;
+  variable: VariableWithValues;
+};
+
+export default function DeliverableDisplayVariableValue({
+  projectId,
+  variable,
+}: DeliverableDisplayVariableValueProps): React.ReactElement {
+  const theme = useTheme();
+
+  const variableStyles = {
+    color: theme.palette.TwClrTxt,
+    backgroundColor: '#e9e2ba',
+    fontSize: '16px',
+    margin: '0 1px',
+    padding: '0 1px',
+  };
+
+  switch (variable.type) {
+    case 'Text':
+    case 'Number':
+    case 'Date':
+    case 'Link':
+      return <span style={variableStyles}>{variable.values.map((v) => displayValue(v)).join(', ')}</span>;
+    case 'Image':
+      return (
+        <>
+          {variable.values.map((v, index) => {
+            return (
+              <Box
+                key={index}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  width: '100%',
+                }}
+              >
+                <img
+                  src={getImagePath(projectId, v.id, 500, 500)}
+                  alt={(v as VariableValueImageValue).caption ?? `${variable.name}-${index}`}
+                />
+                <p style={{ fontSize: '16px' }}>{(v as VariableValueImageValue).caption}</p>
+              </Box>
+            );
+          })}
+        </>
+      );
+    case 'Select':
+      const selectedValues = (variable.values[0] as VariableValueSelectValue)?.optionValues;
+      return (
+        <span style={variableStyles}>
+          {`${
+            variable.options
+              .filter((o) => selectedValues?.includes(o.id))
+              .map((o) => o.renderedText ?? o.name)
+              .join(', ') || '--'
+          }`}
+        </span>
+      );
+    case 'Table':
+      return (
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+          }}
+        >
+          <TableDisplay variable={variable as TableVariableWithValues} />
+        </Box>
+      );
+    default:
+      return <span />;
+  }
+}
