8c246d470364c626dd0fd3388ebee7dfe44f32bf
Josh Buchea <verctrl@joshbuchea.com>
2024-07-08 13:49:15 -1000

    wrap page form with tfmain to fix edit view button alignment

--- src/components/common/PageForm.tsx
+++ src/components/common/PageForm.tsx
@@ -1,6 +1,7 @@
 import React, { BusySpinner } from '@terraware/web-components';
 import PageForm, { PageFormProps as FormProps } from '@terraware/web-components/components/PageForm';
 
+import TfMain from 'src/components/common/TfMain';
 import strings from 'src/strings';
 
 export type PageFormProps = Omit<FormProps, 'saveButtonText' | 'cancelButtonText'> & {
@@ -13,13 +14,13 @@ export default function WrappedPageForm(props: PageFormProps): JSX.Element {
   const { busy, cancelButtonText, saveButtonText, ...formProps } = props;
 
   return (
-    <>
+    <TfMain>
       {busy && <BusySpinner withSkrim={true} />}
       <PageForm
         {...formProps}
         saveButtonText={saveButtonText || strings.SAVE}
         cancelButtonText={cancelButtonText || strings.CANCEL}
       />
-    </>
+    </TfMain>
   );
 }
