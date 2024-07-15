4c569948ee098896e16ed6b03d0ac19118f6d7c6
constanzauanini <constanza.uanini@admios-sa.com>
2024-07-15 11:53:15 -0300

    Add prescreen

--- src/scenes/ApplicationRouter/portal/Overview/ListApplicationModulesContent.tsx
+++ src/scenes/ApplicationRouter/portal/Overview/ListApplicationModulesContent.tsx
@@ -1,57 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Box, Grid, Typography, useTheme } from '@mui/material';
+import { Box, Typography, useTheme } from '@mui/material';
 import { Button } from '@terraware/web-components';
 import { useDeviceInfo } from '@terraware/web-components/utils';
 
 import strings from 'src/strings';
+import { ApplicationModuleWithDeliverables } from 'src/types/Application';
 
 import { useApplicationData } from '../../provider/Context';
 
 export default function ListModulesContent(): JSX.Element {
   const { applicationSections } = useApplicationData();
   const theme = useTheme();
-  const { isDesktop, isMobile } = useDeviceInfo();
+  const { isMobile } = useDeviceInfo();
+  const [sectionsWithPrescreen, setSectionsWithPrescreen] =
+    useState<ApplicationModuleWithDeliverables[]>(applicationSections);
+
+  useEffect(() => {
+    const newSections = [...applicationSections];
+    const prescreenSection = {
+      id: -1,
+      name: 'Prescreen',
+      overview:
+        'Draw your site map and answer the Prescreen questions to see if you qualify to start the Application for the Accelerator Program. ',
+      deliverables: [],
+    };
+    newSections.push(prescreenSection);
+    setSectionsWithPrescreen(newSections);
+  }, [applicationSections]);
 
   return (
     <Box paddingX={theme.spacing(2)}>
-      {applicationSections.map((section, index) => (
+      {sectionsWithPrescreen.map((section, index) => (
         <Box
           key={`section-${index}`}
           borderBottom={`1px solid ${theme.palette.TwClrBgTertiary}`}
           paddingY={theme.spacing(3)}
           sx={{ '&:last-child': { border: 'none' } }}
         >
-          <Grid container display={'flex'} justifyContent={'space-between'} marginBottom={theme.spacing(1)}>
-            <Grid item xs={12}>
-              <Typography
-                component={'span'}
-                fontSize={'20px'}
-                fontWeight={600}
-                lineHeight={'28px'}
-                marginRight={theme.spacing(3)}
-                whiteSpace={'nowrap'}
-              >
-                {section.name}
-              </Typography>
-
-              {isMobile && <br />}
-            </Grid>
-
-            <Grid item xs={isDesktop ? undefined : 12}>
-              <Button
-                onClick={() => true}
-                label={strings.VIEW}
-                priority={'secondary'}
-                style={isMobile ? { width: '100%' } : {}}
-              />
-            </Grid>
-          </Grid>
-
-          <Box marginBottom={theme.spacing(1)}>
+          <Box marginBottom={theme.spacing(1)} display={'flex'} justifyContent={'space-between'}>
             <Typography fontSize={'24px'} fontWeight={600} lineHeight={'32px'}>
               {section.name}
             </Typography>
+            <Button
+              onClick={() => true}
+              label={strings.VIEW}
+              priority={'secondary'}
+              style={isMobile ? { width: '100%' } : {}}
+            />
           </Box>
 
           <Box>{section.overview}</Box>
