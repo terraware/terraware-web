728da496a6fc4e212fbfab50c903eec8599fe53d
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-07-15 20:24:26 -0700

    SW-5658 Implemented in-progress application review screen (#2856)
    ![Screenshot 2024-07-15 at
    6.29.18 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NOxkRPuhAfT4F7nmdXtG/f4997e3d-21d3-45f5-84b0-a726375c24bf.png)
    
    ![Screenshot 2024-07-15 at
    6.28.05 PM.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/NOxkRPuhAfT4F7nmdXtG/c2fb5f02-0f1e-4bf6-9eb4-685ab799099c.png)

---
+++ src/scenes/ApplicationRouter/portal/Review/ReviewCard.tsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import { Box, Grid, Typography, useTheme } from '@mui/material';
+
+import Card from 'src/components/common/Card';
+import Button from 'src/components/common/button/Button';
+import strings from 'src/strings';
+
+type ReviewCardProps = {
+  sections: {
+    name: string;
+    status: 'Incomplete' | 'Complete';
+  }[];
+};
+
+const ReviewCard = ({ sections }: ReviewCardProps): JSX.Element => {
+  const theme = useTheme();
+
+  const statusText = (status: 'Incomplete' | 'Complete') => (
+    <Typography
+      color={status === 'Incomplete' ? theme.palette.TwClrTxtDanger : theme.palette.TwClrTxtSuccess}
+      display={'inline'}
+      fontWeight={'bold'}
+    >
+      {status}
+    </Typography>
+  );
+
+  const allSectionsCompleted = sections.every(({ status }) => status === 'Complete');
+
+  return (
+    <Card
+      title={strings.REVIEW_YOUR_APPLICATION}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        alignItems: 'center',
+        padding: theme.spacing(3),
+      }}
+    >
+      <Grid
+        container
+        flexDirection={'column'}
+        alignItems={'center'}
+        spacing={theme.spacing(2)}
+        paddingTop={theme.spacing(3)}
+        paddingBottom={theme.spacing(6)}
+      >
+        {sections.map(({ name, status }, index: number) => (
+          <Grid item xs={12} key={index}>
+            <Box
+              borderRadius={theme.spacing(1)}
+              borderColor={theme.palette.TwClrBaseGreen300}
+              border={1}
+              paddingY={theme.spacing(2)}
+              width={'600px'}
+            >
+              <Typography
+                align={'center'}
+                color={theme.palette.TwClrTxt}
+                fontSize='16px'
+                fontWeight={400}
+                lineHeight='24px'
+              >
+                {strings.formatString(strings.REVIEW_APPLICATION_STATUS_TEXT, <b>{name}</b>, statusText(status))}
+              </Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Typography
+        align={'center'}
+        color={theme.palette.TwClrTxt}
+        fontSize='16px'
+        fontWeight={400}
+        lineHeight='24px'
+        marginBottom={theme.spacing(4)}
+      >
+        {allSectionsCompleted ? strings.REVIEW_APPLICATION_COMPLETE : strings.REVIEW_APPLICATION_INCOMPLETE}
+      </Typography>
+
+      <Button disabled={!allSectionsCompleted} label={strings.SUBMIT_APPLICATION} size='medium' onClick={() => {}} />
+    </Card>
+  );
+};
+
+export default ReviewCard;
--- src/scenes/ApplicationRouter/portal/Review/index.tsx
+++ src/scenes/ApplicationRouter/portal/Review/index.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import ApplicationPage from 'src/scenes/ApplicationRouter/portal/ApplicationPage';
+import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
 import strings from 'src/strings';
 
-import ApplicationPage from '../ApplicationPage';
+import ReviewCard from './ReviewCard';
 
 const ReviewView = () => {
-  return <ApplicationPage title={strings.REVIEW}></ApplicationPage>;
+  const { applicationSections, selectedApplication } = useApplicationData();
+
+  if (!selectedApplication) {
+    return null;
+  }
+
+  return (
+    <ApplicationPage title={strings.REVIEW}>
+      {selectedApplication.status !== 'Submitted' && <ReviewCard sections={applicationSections} />}
+    </ApplicationPage>
+  );
 };
 
 export default ReviewView;
