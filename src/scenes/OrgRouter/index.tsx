fbb1d87f6f7e5c09ab4c2addde78e98b9f3d9380
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-07-23 14:06:05 -0700

    Put router behind feature flag

--- src/scenes/OrgRouter/index.tsx
+++ src/scenes/OrgRouter/index.tsx
@@ -47,6 +47,7 @@ import { isPlaceholderOrg, selectedOrgHasFacilityType } from 'src/utils/organiza
 import useDeviceInfo from 'src/utils/useDeviceInfo';
 import useEnvironment from 'src/utils/useEnvironment';
 import useStateLocation from 'src/utils/useStateLocation';
+import isEnabled from 'src/features';
 
 interface OrgRouterProps {
   showNavBar: boolean;
@@ -254,7 +255,7 @@ const OrgRouter = ({ showNavBar, setShowNavBar }: OrgRouterProps) => {
               <Route path={APP_PATHS.OBSERVATIONS + '/*'} element={<ObservationsRouter />} />
               <Route path={APP_PATHS.DELIVERABLES + '/*'} element={<DeliverablesRouter />} />
 
-              <Route path={APP_PATHS.APPLICATIONS + '/*'} element={<ApplicationRouter />} />
+              {isEnabled('Accelerator Application') && <Route path={APP_PATHS.APPLICATIONS + '/*'} element={<ApplicationRouter />} />}
 
               {!isProduction && (
                 <Route path={APP_PATHS.OPT_IN} element={<OptInFeaturesView refresh={reloadPreferences} />} />
