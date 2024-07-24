921ee57dad4c0f9a90fb19bd3009446582854edb
Josh Buchea <verctrl@joshbuchea.com>
2024-07-24 13:18:24 -1000

    add breadcrumbs to prescreen views

--- src/scenes/ApplicationRouter/portal/Prescreen/index.tsx
+++ src/scenes/ApplicationRouter/portal/Prescreen/index.tsx
@@ -1,7 +1,11 @@
 import React, { useMemo } from 'react';
 
+import { Crumb } from 'src/components/BreadCrumbs';
+import { APP_PATHS } from 'src/constants';
+import { useLocalization } from 'src/providers';
 import SectionView from 'src/scenes/ApplicationRouter/portal/Sections/SectionView';
 import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
+import strings from 'src/strings';
 
 import ApplicationPage from '../ApplicationPage';
 import ResultView from './ResultView';
@@ -43,10 +47,28 @@ const PrescreenView = () => {
   }
 };
 
-const PrescreenViewWrapper = () => (
-  <ApplicationPage>
-    <PrescreenView />
-  </ApplicationPage>
-);
+const PrescreenViewWrapper = () => {
+  const { activeLocale } = useLocalization();
+  const { selectedApplication } = useApplicationData();
+
+  const crumbs: Crumb[] = useMemo(
+    () =>
+      activeLocale && selectedApplication?.id
+        ? [
+            {
+              name: strings.ALL_SECTIONS,
+              to: APP_PATHS.APPLICATION_OVERVIEW.replace(':applicationId', `${selectedApplication.id}`),
+            },
+          ]
+        : [],
+    [activeLocale, selectedApplication?.id]
+  );
+
+  return (
+    <ApplicationPage crumbs={crumbs}>
+      <PrescreenView />
+    </ApplicationPage>
+  );
+};
 
 export default PrescreenViewWrapper;
