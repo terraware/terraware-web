4d695b9ca1bfe72aae7fd9e082e90e0aef09a6f3
Josh Buchea <verctrl@joshbuchea.com>
2024-07-24 13:14:15 -1000

    add breadcrumbs to application page

--- src/scenes/ApplicationRouter/portal/ApplicationPage.tsx
+++ src/scenes/ApplicationRouter/portal/ApplicationPage.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Container } from '@mui/material';
+import { Container, Grid, useTheme } from '@mui/material';
 
+import BreadCrumbs, { Crumb } from 'src/components/BreadCrumbs';
 import PageHeader from 'src/components/PageHeader';
 import TfMain from 'src/components/common/TfMain';
 import Button from 'src/components/common/button/Button';
@@ -10,12 +11,15 @@ import useNavigateTo from 'src/hooks/useNavigateTo';
 import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
 import strings from 'src/strings';
 
-type Prop = {
+type Props = {
   children?: ReactNode;
+  crumbs?: Crumb[];
+  hierarchicalCrumbs?: boolean;
 };
 
-const ApplicationPage = ({ children }: Prop) => {
+const ApplicationPage = ({ children, crumbs, hierarchicalCrumbs }: Props) => {
   const { goToHome } = useNavigateTo();
+  const theme = useTheme();
 
   const { allApplications, selectedApplication, setSelectedApplication, reload } = useApplicationData();
 
@@ -36,6 +40,11 @@ const ApplicationPage = ({ children }: Prop) => {
         title={strings.APPLICATION}
         rightComponent={<Button label={strings.EXIT_APPLICATION} onClick={goToHome} priority={'ghost'} />}
       />
+      <Grid container spacing={theme.spacing(1)}>
+        <Grid item xs style={{ flexGrow: 1, padding: `${theme.spacing(2)} ${theme.spacing(4)}` }}>
+          {crumbs && <BreadCrumbs crumbs={crumbs} hierarchical={hierarchicalCrumbs ?? true} />}
+        </Grid>
+      </Grid>
 
       <Container maxWidth={false} sx={{ padding: 0 }}>
         {children}
