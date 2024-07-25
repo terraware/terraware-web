9c2a8a149b4b7cf2eb2134178af81f290f36eb91
Josh Buchea <verctrl@joshbuchea.com>
2024-07-24 19:17:40 -1000

    add application status submit success screen

--- src/scenes/ApplicationRouter/portal/Review/index.tsx
+++ src/scenes/ApplicationRouter/portal/Review/index.tsx
@@ -1,6 +1,10 @@
 import React, { useMemo } from 'react';
 
+import { Box, useTheme } from '@mui/material';
+import { Button } from '@terraware/web-components';
+
 import { Crumb } from 'src/components/BreadCrumbs';
+import Card from 'src/components/common/Card';
 import { APP_PATHS } from 'src/constants';
 import { useLocalization } from 'src/providers';
 import ApplicationPage from 'src/scenes/ApplicationRouter/portal/ApplicationPage';
@@ -8,6 +12,50 @@ import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Contex
 import strings from 'src/strings';
 
 import ReviewCard from './ReviewCard';
+
+type ApplicationStatusProps = {
+  buttonLabel: string;
+  feedback?: string;
+  isFailure: boolean;
+  onClickButton: () => void;
+  title: string;
+};
+
+const ApplicationStatus = ({ buttonLabel, feedback, isFailure, onClickButton, title }: ApplicationStatusProps) => {
+  const { activeLocale } = useLocalization();
+  const theme = useTheme();
+
+  return !activeLocale ? undefined : (
+    <Card
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        alignItems: 'center',
+        padding: theme.spacing(8),
+      }}
+    >
+      <Box alignItems={'center'}>
+        <img src={isFailure ? '/assets/application-failure-splash.svg' : '/assets/application-success-splash.svg'} />
+      </Box>
+      <h3>{title}</h3>
+      {feedback && <Box dangerouslySetInnerHTML={{ __html: feedback }} justifyContent={'center'} />}
+      <Button label={buttonLabel} onClick={onClickButton} priority='secondary' />
+    </Card>
+  );
+};
+
+const ApplicationStatusSubmitted = () => (
+  <ApplicationStatus
+    buttonLabel={strings.EXIT_APPLICATION}
+    feedback='<p>Your Application has been submitted to the Accelerator team. We will review and score your answers.</p><p>Check back to see updates? Look for an email? [need copy]</p>'
+    isFailure={false}
+    onClickButton={() => {
+      // TODO: what does "Exit Application" do?
+    }}
+    title={strings.APPLICATION_SUBMIT_SUCCESS}
+  />
+);
 
 const ReviewView = () => {
   const { applicationSections, selectedApplication } = useApplicationData();
@@ -32,7 +80,7 @@ const ReviewView = () => {
 
   return (
     <ApplicationPage crumbs={crumbs}>
-      {selectedApplication.status !== 'Submitted' && <ReviewCard sections={applicationSections} />}
+      {selectedApplication.status === 'Submitted' && <ApplicationStatusSubmitted />}
     </ApplicationPage>
   );
 };
