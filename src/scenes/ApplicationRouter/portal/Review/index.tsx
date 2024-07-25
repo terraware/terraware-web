8792d96bd53765034026bacd7d230448e1645662
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-07-25 09:47:40 -1000

    SW-5659: FE - Application success screen (#2900)
    SW-5659: FE - Application success screen (#2900)

--- src/scenes/ApplicationRouter/portal/Review/index.tsx
+++ src/scenes/ApplicationRouter/portal/Review/index.tsx
@@ -1,13 +1,66 @@
 import React, { useMemo } from 'react';
 
+import { Box, useTheme } from '@mui/material';
+import { Button } from '@terraware/web-components';
+
 import { Crumb } from 'src/components/BreadCrumbs';
+import Card from 'src/components/common/Card';
 import { APP_PATHS } from 'src/constants';
+import useNavigateTo from 'src/hooks/useNavigateTo';
 import { useLocalization } from 'src/providers';
 import ApplicationPage from 'src/scenes/ApplicationRouter/portal/ApplicationPage';
 import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
 import strings from 'src/strings';
 
 import ReviewCard from './ReviewCard';
+
+type ApplicationStatusProps = {
+  buttonLabel: string;
+  feedback?: string;
+  isFailure: boolean;
+  onClickButton: () => void;
+  title: string;
+};
+
+const ApplicationStatus = ({ buttonLabel, feedback, isFailure, onClickButton, title }: ApplicationStatusProps) => {
+  const { activeLocale } = useLocalization();
+  const theme = useTheme();
+
+  return !activeLocale ? undefined : (
+    <Card
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flexGrow: 1,
+        alignItems: 'center',
+        padding: theme.spacing(8),
+      }}
+    >
+      <Box alignItems={'center'}>
+        <img src={isFailure ? '/assets/application-failure-splash.svg' : '/assets/application-success-splash.svg'} />
+      </Box>
+      <h3>{title}</h3>
+      {feedback && <Box dangerouslySetInnerHTML={{ __html: feedback }} justifyContent={'center'} />}
+      <Button label={buttonLabel} onClick={onClickButton} priority='secondary' />
+    </Card>
+  );
+};
+
+const ApplicationStatusSubmitted = () => {
+  const { goToHome } = useNavigateTo();
+
+  return (
+    <ApplicationStatus
+      buttonLabel={strings.EXIT_APPLICATION}
+      feedback='<p>Your Application has been submitted to the Accelerator team. We will review and score your answers.</p><p>Check back to see updates? Look for an email? [need copy]</p>'
+      isFailure={false}
+      onClickButton={() => {
+        goToHome();
+      }}
+      title={strings.APPLICATION_SUBMIT_SUCCESS}
+    />
+  );
+};
 
 const ReviewView = () => {
   const { applicationSections, selectedApplication } = useApplicationData();
@@ -32,6 +85,7 @@ const ReviewView = () => {
 
   return (
     <ApplicationPage crumbs={crumbs}>
+      {selectedApplication.status === 'In Review' && <ApplicationStatusSubmitted />}
       {selectedApplication.status !== 'Submitted' && <ReviewCard sections={applicationSections} />}
     </ApplicationPage>
   );
