4710caa8c9606bd88d72813585c83b0a110aa793
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-07-24 15:55:27 -1000

    SW-5678: Add breadcrumbs to Application Page, and use Project Name for title (#2896)
    SW-5678: Add breadcrumbs to Application Page, and use Project Name for title (#2896)

--- src/scenes/ApplicationRouter/portal/ApplicationPage.tsx
+++ src/scenes/ApplicationRouter/portal/ApplicationPage.tsx
@@ -1,20 +1,20 @@
 import React, { ReactNode, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Container } from '@mui/material';
-
-import PageHeader from 'src/components/PageHeader';
-import TfMain from 'src/components/common/TfMain';
+import { Crumb } from 'src/components/BreadCrumbs';
+import Page from 'src/components/Page';
 import Button from 'src/components/common/button/Button';
 import useNavigateTo from 'src/hooks/useNavigateTo';
 import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
 import strings from 'src/strings';
 
-type Prop = {
+type Props = {
   children?: ReactNode;
+  crumbs?: Crumb[];
+  hierarchicalCrumbs?: boolean;
 };
 
-const ApplicationPage = ({ children }: Prop) => {
+const ApplicationPage = ({ children, crumbs, hierarchicalCrumbs }: Props) => {
   const { goToHome } = useNavigateTo();
 
   const { allApplications, selectedApplication, setSelectedApplication, reload } = useApplicationData();
@@ -31,16 +31,16 @@ const ApplicationPage = ({ children }: Prop) => {
   }, [allApplications, applicationId, selectedApplication]);
 
   return (
-    <TfMain>
-      <PageHeader
-        title={strings.APPLICATION}
-        rightComponent={<Button label={strings.EXIT_APPLICATION} onClick={goToHome} priority={'ghost'} />}
-      />
-
-      <Container maxWidth={false} sx={{ padding: 0 }}>
-        {children}
-      </Container>
-    </TfMain>
+    <Page
+      crumbs={crumbs}
+      rightComponent={<Button label={strings.EXIT_APPLICATION} onClick={goToHome} priority={'ghost'} />}
+      hierarchicalCrumbs={hierarchicalCrumbs ?? true}
+      // TODO: replace "Project Name" placeholder with actual project name once available in application data
+      title={strings.PROJECT_NAME}
+      titleStyle={{ marginTop: '24px' }}
+    >
+      {children}
+    </Page>
   );
 };
 
--- src/scenes/ApplicationRouter/portal/Review/index.tsx
+++ src/scenes/ApplicationRouter/portal/Review/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
+import { Crumb } from 'src/components/BreadCrumbs';
+import { APP_PATHS } from 'src/constants';
+import { useLocalization } from 'src/providers';
 import ApplicationPage from 'src/scenes/ApplicationRouter/portal/ApplicationPage';
 import { useApplicationData } from 'src/scenes/ApplicationRouter/provider/Context';
+import strings from 'src/strings';
 
 import ReviewCard from './ReviewCard';
 
 const ReviewView = () => {
   const { applicationSections, selectedApplication } = useApplicationData();
+  const { activeLocale } = useLocalization();
+
+  const crumbs: Crumb[] = useMemo(
+    () =>
+      activeLocale && selectedApplication?.id
+        ? [
+            {
+              name: strings.ALL_SECTIONS,
+              to: APP_PATHS.APPLICATION_OVERVIEW.replace(':applicationId', `${selectedApplication.id}`),
+            },
+          ]
+        : [],
+    [activeLocale, selectedApplication?.id]
+  );
 
   if (!selectedApplication) {
     return null;
   }
 
   return (
-    <ApplicationPage>
+    <ApplicationPage crumbs={crumbs}>
       {selectedApplication.status !== 'Submitted' && <ReviewCard sections={applicationSections} />}
     </ApplicationPage>
   );
