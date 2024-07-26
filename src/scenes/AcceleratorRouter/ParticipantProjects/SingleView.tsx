32c4125fea39c334224664c7989e9d667c115b42
Josh Buchea <verctrl@joshbuchea.com>
2024-07-26 10:21:59 -1000

    oops

--- src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/SingleView.tsx
@@ -47,6 +47,7 @@ const SingleView = () => {
   const isAllowedExport = isAllowed('EXPORT_PARTICIPANT_PROJECT');
   const isAllowedViewScoreAndVoting = isAllowed('VIEW_PARTICIPANT_PROJECT_SCORING_VOTING');
 
+  const { allApplications, applicationSections, selectedApplication } = useApplicationData();
   const onOptionItemClick = useCallback((item: DropdownItem) => {
     if (item.value === 'export-participant-project') {
       setExportModalOpen(true);
