03846847fdc502facaa8fe5fdcc20f58347bcab7
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-07-31 16:52:41 -0700

    Added title bar

---
+++ src/components/common/TitleBar.tsx
@@ -0,0 +1,36 @@
+import React, { Box, Typography, useTheme } from '@mui/material';
+
+const TitleBar = (props: {header?: string; title?: string; subtitle?: string}): JSX.Element => {
+  const { header, title, subtitle } = props;
+  const theme = useTheme();
+
+  return (
+    <Box
+      margin={theme.spacing(3)}
+      alignItems='center'
+      justifyContent='space-between'
+      display='flex'
+      flexDirection='row'
+    >
+      <Box display='flex' flexDirection='column'>
+        <Typography fontSize='14px' lineHeight='20px' fontWeight={400} color={theme.palette.TwClrTxt}>
+          {header}
+        </Typography>
+        <Typography
+          fontSize='24px'
+          lineHeight='32px'
+          fontWeight={600}
+          color={theme.palette.TwClrTxt}
+          margin={theme.spacing(1, 0)}
+        >
+          {title}
+        </Typography>
+        <Typography fontSize='14px' lineHeight='20px' fontWeight={400} color={theme.palette.TwClrTxt}>
+          {subtitle}
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default TitleBar;
--- src/scenes/AcceleratorRouter/Applications/ApplicationView.tsx
+++ src/scenes/AcceleratorRouter/Applications/ApplicationView.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+import TitleBar from 'src/components/common/TitleBar';
 
 import Page from 'src/components/Page';
+import { useLocalization } from 'src/providers';
+import { useApplicationData } from 'src/providers/Application/Context';
 import strings from 'src/strings';
 
 const ApplicationView = () => {
-  return <Page title={strings.APPLICATION} contentStyle={{ display: 'block' }}></Page>;
+  const { activeLocale } = useLocalization();
+  const { selectedApplication, setSelectedApplication } = useApplicationData();
+  const pathParams = useParams<{ applicationId: string }>();
+
+  useEffect(() => {
+    setSelectedApplication(Number(pathParams.applicationId ?? -1))
+  }, [setSelectedApplication, pathParams]);
+
+  const titleComponent = useMemo(() => {
+    if (!selectedApplication || !activeLocale) {
+      return undefined
+    }
+
+    return (<TitleBar 
+      header={strings.formatString(strings.DELIVERABLE_PROJECT, selectedApplication.projectName ?? '').toString()}
+      title={selectedApplication.internalName}
+    />);
+  }, [selectedApplication])
+  
+  if (!selectedApplication) {
+    return undefined;
+  }
+
+  return (<Page title={titleComponent} contentStyle={{ display: 'block' }}>
+
+  </Page>);
 };
 
 export default ApplicationView;
