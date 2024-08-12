bc8db5a06593c2e84d2ba4b1aaa17773b4db771e
Nicholas Graziano <nick.graziano@gmail.com>
2024-08-12 10:52:10 -0600

    SW-5767 Implement search and project filter into documents table (#2971)
    - Switch the documents table to use TableWithSearchFilters
    - Add redux for documents search, plumb into documents table through `TableWithSearchFilters`
    
    Depends on https://github.com/terraware/terraware-server/pull/2248

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -26,6 +26,7 @@ export type PageProps = {
   hierarchicalCrumbs?: boolean;
   isLoading?: boolean;
   primaryButton?: PrimaryButtonType;
+  leftComponent?: JSX.Element;
   rightComponent?: React.ReactNode;
   title?: React.ReactNode;
 };
@@ -42,6 +43,7 @@ export default function Page({
   hierarchicalCrumbs,
   isLoading,
   primaryButton,
+  leftComponent,
   rightComponent,
   title,
 }: PageProps): JSX.Element {
@@ -60,17 +62,22 @@ export default function Page({
     <TfMain style={containerStyles}>
       <PageHeaderWrapper nextElement={contentRef.current}>
         <>{crumbs && <BreadCrumbs crumbs={crumbs} hierarchical={hierarchicalCrumbs ?? true} />}</>
-        <Grid container justifyContent='space-between' alignItems='center'>
+        <Grid
+          container
+          justifyContent='space-between'
+          alignItems='center'
+          marginBottom={theme.spacing(2)}
+          paddingX={theme.spacing(2)}
+        >
           {title && typeof title !== 'string' && (
             <Grid item xs={8}>
               {title}
             </Grid>
           )}
           {title && typeof title === 'string' && (
-            <Grid item xs={8}>
+            <Grid item>
               <Typography
                 sx={{
-                  marginBottom: theme.spacing(4),
                   paddingLeft: theme.spacing(3),
                   fontSize: '24px',
                   fontWeight: 600,
@@ -80,6 +87,11 @@ export default function Page({
               >
                 {title}
               </Typography>
+            </Grid>
+          )}
+          {leftComponent && (
+            <Grid item xs={4} style={{ marginRight: 'auto' }}>
+              {leftComponent}
             </Grid>
           )}
           {rightComponent && (
