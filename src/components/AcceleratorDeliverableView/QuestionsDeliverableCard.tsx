72dd93ebe5e0da3781c225a49dede0eed6b73856
Josh Buchea <joshbuchea@users.noreply.github.com>
2024-08-19 11:03:23 -1000

    SW-5830: Question Edit View: Allow user to exit Edit mode of question using either "Save" or "Cancel" button when only Internal Comment is added (#3033)
    SW-5830: Question Edit View: Allow user to exit Edit mode of question using either "Save" or "Cancel" button when only Internal Comment is added (#3033)

--- src/components/AcceleratorDeliverableView/QuestionsDeliverableCard.tsx
+++ src/components/AcceleratorDeliverableView/QuestionsDeliverableCard.tsx
@@ -158,13 +158,13 @@ const QuestionBox = ({
   };
 
   const onSave = () => {
+    setEditingId(undefined);
+
     if (pendingVariableValues.size === 0) {
       return;
     }
 
     update();
-
-    setEditingId(undefined);
   };
 
   const onOptionItemClick = useCallback(
