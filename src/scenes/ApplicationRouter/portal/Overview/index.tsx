b77599a578544fe75d9ca88ea6c660b0fe0a0367
Nicholas Graziano <nick.graziano@gmail.com>
2024-08-19 10:38:42 -0600

    SW-5785 Move strings to localized versions, update 'under review' to 'due diligence' (#3027)

--- src/scenes/ApplicationRouter/portal/Overview/index.tsx
+++ src/scenes/ApplicationRouter/portal/Overview/index.tsx
@@ -4,36 +4,35 @@ import { Box, Card, Typography, useTheme } from '@mui/material';
 import { Button } from '@terraware/web-components';
 
 import useNavigateTo from 'src/hooks/useNavigateTo';
+import { useLocalization } from 'src/providers';
+import { useApplicationData } from 'src/providers/Application/Context';
 import CurrentTimeline from 'src/scenes/ModulesRouter/CurrentTimeline';
 import strings from 'src/strings';
 
-import { useApplicationData } from '../../../../providers/Application/Context';
 import ApplicationPage from '../ApplicationPage';
 import ListApplicationModulesContent from './ListApplicationModulesContent';
 
-const phaseDescription =
-  'Start by completing the Pre-screen. Then answer more in-depth questions about your project and upload any necessary documents in the Application. Each section of the Application will be available to view after your pre-screen has been approved.';
+const applicationSteps = (activeLocale: string | null) =>
+  activeLocale
+    ? [
+        {
+          name: strings.APPLICATION_STEP_PRESCREEN_NAME,
+          description: strings.APPLICATION_STEP_PRESCREEN_DESCRIPTION,
+        },
+        {
+          name: strings.APPLICATION_STEP_APPLICATION_NAME,
+          description: strings.APPLICATION_STEP_APPLICATION_DESCRIPTION,
+        },
+        {
+          name: strings.APPLICATION_STEP_DUEDILIGENCE_NAME,
+          description: strings.APPLICATION_STEP_DUEDILIGENCE_DESCRIPTION,
+        },
+      ]
+    : [];
 
-const applicationSteps = [
-  {
-    name: 'Prescreen',
-    description:
-      'Draw your proposed project site map and complete the Pre-screen questions to determine if you qualify for our Accelerator Program.',
-  },
-  {
-    name: 'Application',
-    description:
-      'For the full Accelerator Program Application, you will answer general questions about your project as well as forest restoration, community impact, financial and legal questions.',
-  },
-  {
-    name: 'Under Review',
-    description:
-      'Once your Application is submitted, the Accelerator team reviews your answers, potentially asks for updates and proceeds to voting and scoring your Application.',
-  },
-];
 const OverviewView = () => {
   const theme = useTheme();
-
+  const { activeLocale } = useLocalization();
   const { selectedApplication } = useApplicationData();
 
   const timelineIndex = useMemo(() => {
@@ -68,10 +67,12 @@ const OverviewView = () => {
 
   return (
     <Card style={{ width: '100%', padding: theme.spacing(3), borderRadius: theme.spacing(3) }}>
-      {selectedApplication?.status && <CurrentTimeline steps={applicationSteps} currentIndex={timelineIndex} />}
+      {selectedApplication?.status && (
+        <CurrentTimeline steps={applicationSteps(activeLocale)} currentIndex={timelineIndex} />
+      )}
 
       <Box paddingY={theme.spacing(3)} borderBottom={`1px solid ${theme.palette.TwClrBgTertiary}`}>
-        <Typography>{phaseDescription}</Typography>
+        <Typography>{strings.APPLICATION_INSTRUCTIONS}</Typography>
       </Box>
       <ListApplicationModulesContent />
     </Card>
