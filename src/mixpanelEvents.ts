74772b85e7d680dacf3518c302a25c0e11b35a0e
constanzauanini <constanza.uanini@admios-sa.com>
2024-09-16 17:23:46 -0300

    SW-6018 Console: Add Mixpanel Event for Project Export (#3190)

--- src/mixpanelEvents.ts
+++ src/mixpanelEvents.ts
@@ -4,6 +4,7 @@ export enum MIXPANEL_EVENTS {
   TOP_BAR_HOME = 'Top Bar Home',
   PART_EX_LEFT_NAV_DELIVERABLES = 'Participant Deliverables Nav Click',
   PART_EX_LEFT_NAV_MODULES = 'Participant Modules Nav Click',
+  CONSOLE_PROJECTS_EXPORT = 'Console Projects Export Click',
   ACCELERATOR_MDDULE_SESSION_EVENT_LINK = 'Accelerator Module Session Event Click',
   ACCELERATOR_MDDULE_ADDITIONAL_LINK = 'Accelerator Modules Additional Link Click',
   HOME_ACCELERATOR_APPLY_BUTTON = 'Apply to Accelerator Click',
