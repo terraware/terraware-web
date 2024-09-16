61f8bec18a2145474ff72c3809f6de63177a2461
constanzauanini <constanza.uanini@admios-sa.com>
2024-09-16 10:06:32 -0300

    SW-6012 Add Mixpanel Events for clicks on session/event "More Info" links

--- src/mixpanelEvents.ts
+++ src/mixpanelEvents.ts
@@ -4,4 +4,5 @@ export enum MIXPANEL_EVENTS {
   TOP_BAR_HOME = 'Top Bar Home',
   PART_EX_LEFT_NAV_DELIVERABLES = 'Participant Deliverables Nav Click',
   PART_EX_LEFT_NAV_MODULES = 'Participant Modules Nav Click',
+  ACCELERATOR_MDDULE_SESSION_EVENT_LINK = 'Accelerator Module Session Event Click',
 }
