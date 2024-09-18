e7c770b790cdbd55ae471c2aa512ac44539a7225
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-18 14:44:41 -0700

    Use BE computed score directly for total average score

--- src/scenes/AcceleratorRouter/ParticipantProjects/Scoring/PhaseScores/ScoreTotal.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Scoring/PhaseScores/ScoreTotal.tsx
@@ -13,42 +13,24 @@ interface ScoreTotalProps {
 const ScoreTotal = ({ isEditing, phaseScores }: ScoreTotalProps) => {
   const theme = useTheme();
 
-  const isPhaseScoreComplete = useMemo(
-    () => phaseScores.scores.every((score: Score) => score.value !== undefined),
-    [phaseScores]
-  );
-
   if (!isEditing) {
     return (
       <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
-        isPhaseScoreComplete ? phaseScores.totalScore : strings.NOT_COMPLETE
+        phaseScores.totalScore ?? strings.NOT_COMPLETE
       }`}</Typography>
     );
   }
 
-  if (isPhaseScoreComplete) {
-    return (
-      <>
-        <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
-          phaseScores.totalScore || 0
-        }`}</Typography>
-        <Typography fontSize='14px' fontWeight='500' lineHeight='20px' color={theme.palette.TwClrTxtSuccess}>
-          {strings.SCORING_COMPLETED}
-        </Typography>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.SCORE}: ${
-          phaseScores.totalScore || 0
-        }`}</Typography>
-        <Typography fontSize='14px' fontWeight='500' lineHeight='20px' color={theme.palette.TwClrTxtDanger}>
-          {strings.SCORING_NOT_COMPLETED}
-        </Typography>
-      </>
-    );
-  }
+  return (
+    <>
+      <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
+        phaseScores.totalScore || ''
+      }`}</Typography>
+      <Typography fontSize='14px' fontWeight='500' lineHeight='20px' color={theme.palette.TwClrTxtSuccess}>
+        {phaseScores.totalScore !== undefined ? strings.SCORING_COMPLETED : strings.SCORING_NOT_COMPLETED}
+      </Typography>
+    </>
+  );
 };
 
 export default ScoreTotal;
