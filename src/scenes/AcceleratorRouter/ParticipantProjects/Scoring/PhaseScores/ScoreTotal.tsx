6671ad8ed515c3a054ac481d6d0ecff3d0de5302
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-09-19 09:25:21 -0700

    SW-6041 Use BE computed score directly for total average score (#3216)

--- src/scenes/AcceleratorRouter/ParticipantProjects/Scoring/PhaseScores/ScoreTotal.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Scoring/PhaseScores/ScoreTotal.tsx
@@ -1,9 +1,9 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { Typography, useTheme } from '@mui/material';
 
 import strings from 'src/strings';
-import { PhaseScores, Score } from 'src/types/Score';
+import { PhaseScores } from 'src/types/Score';
 
 interface ScoreTotalProps {
   isEditing: boolean;
@@ -13,42 +13,29 @@ interface ScoreTotalProps {
 const ScoreTotal = ({ isEditing, phaseScores }: ScoreTotalProps) => {
   const theme = useTheme();
 
-  const isPhaseScoreComplete = useMemo(
-    () => phaseScores.scores.every((score: Score) => score.value !== undefined),
-    [phaseScores]
-  );
-
   if (!isEditing) {
     return (
       <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
-        isPhaseScoreComplete ? phaseScores.totalScore : strings.NOT_COMPLETE
+        phaseScores.totalScore ?? strings.NOT_COMPLETE
       }`}</Typography>
     );
   }
 
-  if (isPhaseScoreComplete) {
-    return (
-      <>
-        <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
-          phaseScores.totalScore || 0
-        }`}</Typography>
-        <Typography fontSize='14px' fontWeight='500' lineHeight='20px' color={theme.palette.TwClrTxtSuccess}>
-          {strings.SCORING_COMPLETED}
-        </Typography>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.SCORE}: ${
-          phaseScores.totalScore || 0
-        }`}</Typography>
-        <Typography fontSize='14px' fontWeight='500' lineHeight='20px' color={theme.palette.TwClrTxtDanger}>
-          {strings.SCORING_NOT_COMPLETED}
-        </Typography>
-      </>
-    );
-  }
+  return (
+    <>
+      <Typography fontSize='20px' fontWeight='600' lineHeight='28px'>{`${strings.TOTAL_AVERAGE_SCORE}: ${
+        phaseScores.totalScore ?? '-'
+      }`}</Typography>
+      <Typography
+        fontSize='14px'
+        fontWeight='500'
+        lineHeight='20px'
+        color={phaseScores.totalScore !== undefined ? theme.palette.TwClrTxtSuccess : theme.palette.TwClrTxtDanger}
+      >
+        {phaseScores.totalScore !== undefined ? strings.SCORING_COMPLETED : strings.SCORING_NOT_COMPLETED}
+      </Typography>
+    </>
+  );
 };
 
 export default ScoreTotal;
