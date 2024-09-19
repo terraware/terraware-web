e32b3c7a6ec8ad0315bde9c1e326aae57730f84e
constanzauanini <constanza.uanini@admios-sa.com>
2024-09-19 09:53:35 -0300

    SW-5938 Replace data check tooltip with a popover

--- src/scenes/Species/TableCellRenderer.tsx
+++ src/scenes/Species/TableCellRenderer.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useState } from 'react';
 
-import { ClickAwayListener, IconButton, Tooltip, useTheme } from '@mui/material';
+import { ClickAwayListener, IconButton, Popover, useTheme } from '@mui/material';
 
 import Link from 'src/components/common/Link';
 import TextTruncated from 'src/components/common/TextTruncated';
@@ -32,9 +32,15 @@ export default function SpeciesCellRenderer(props: RendererProps<TableRowType>):
   };
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setAnchorEl(e.currentTarget);
     e.stopPropagation();
-    setOpenedTooltip(!openedTooltip);
   };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   if (column.key === 'problems') {
     return (
@@ -43,55 +49,41 @@ export default function SpeciesCellRenderer(props: RendererProps<TableRowType>):
         column={column}
         value={
           Array.isArray(value) ? (
-            <ClickAwayListener onClickAway={handleClickAway}>
-              <Tooltip
-                placement='bottom-start'
-                title={
-                  <ProblemTooltip
-                    problems={value as SpeciesProblemElement[]}
-                    openedTooltip={openedTooltip}
-                    setOpenedTooltip={setOpenedTooltip}
-                    reloadData={reloadData}
-                    onRowClick={onRowClick}
-                  />
-                }
-                arrow
-                open={openedTooltip}
+            <>
+              <IconButton
+                onClick={(event) => handleClick(event)}
                 sx={{
-                  '& .MuiTooltip-popper': {
-                    pointerEvents: 'all',
-                  },
-                  '& .MuiTooltip-tooltip': {
-                    padding: 0,
-                    background: theme.palette.TwClrBg,
-                    border: `1px solid ${theme.palette.TwClrBrdrTertiary}`,
-                    boxShadow: `0px 4px 8px ${getRgbaFromHex(theme.palette.TwClrShdw as string, 0.2)}`,
-                    borderRadius: '7px',
-                    color: theme.palette.TwClrTxt,
-                    fontSize: '12px',
-                    maxWidth: '350px',
-
-                    '& .MuiTooltip-arrow': {
-                      color: theme.palette.TwClrTxtInverse,
-                      '&:before': {
-                        border: `1px solid ${theme.palette.TwClrBrdrTertiary}`,
-                      },
-                    },
-                  },
+                  borderRadius: 0,
+                  fontSize: '16px',
+                  height: '48px',
                 }}
               >
-                <IconButton
-                  onClick={(event) => handleClick(event)}
-                  sx={{
-                    borderRadius: 0,
-                    fontSize: '16px',
-                    height: '48px',
+                <Icon name='warning' style={{ fill: theme.palette.TwClrIcnWarning }} />
+              </IconButton>
+              <ClickAwayListener onClickAway={handleClickAway}>
+                <Popover
+                  open={Boolean(anchorEl)}
+                  anchorEl={anchorEl}
+                  onClose={handleClose}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'center',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'center',
                   }}
                 >
-                  <Icon name='warning' style={{ fill: theme.palette.TwClrIcnWarning }} />
-                </IconButton>
-              </Tooltip>
-            </ClickAwayListener>
+                  <ProblemTooltip
+                    problems={value as SpeciesProblemElement[]}
+                    openedTooltip={Boolean(anchorEl)}
+                    reloadData={reloadData}
+                    onRowClick={onRowClick}
+                    onClose={handleClose}
+                  />
+                </Popover>
+              </ClickAwayListener>
+            </>
           ) : null
         }
         row={row}
