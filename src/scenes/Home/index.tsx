7c868ad4d431ece4e16dc6df7f54fb18e694468e
constanzauanini <constanza.uanini@admios-sa.com>
2024-11-13 16:52:54 -0300

    SW-6229 Show non-accelerator home page for contributors (#3330)

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,19 +1,22 @@
 import React, { useMemo } from 'react';
 
 import Page from 'src/components/Page';
+import { useOrganization } from 'src/providers';
 import { useParticipantData } from 'src/providers/Participant/ParticipantContext';
+import { isManagerOrHigher } from 'src/utils/organization';
 
 import ParticipantHomeView from './ParticipantHomeView';
 import TerrawareHomeView from './TerrawareHomeView';
 
 export default function Home(): JSX.Element {
+  const { selectedOrganization } = useOrganization();
   const { orgHasModules } = useParticipantData();
 
   const homeScreen = useMemo((): JSX.Element => {
     if (orgHasModules === undefined) {
       return <Page isLoading={true} />;
     }
-    return orgHasModules ? <ParticipantHomeView /> : <TerrawareHomeView />;
+    return orgHasModules && isManagerOrHigher(selectedOrganization) ? <ParticipantHomeView /> : <TerrawareHomeView />;
   }, [orgHasModules]);
 
   return homeScreen;
