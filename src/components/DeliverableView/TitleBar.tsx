db7613c1bb4b85324a7642537724d9c58dcce940
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-12-06 12:10:22 -0800

    Used project deal name directly

--- src/components/DeliverableView/TitleBar.tsx
+++ src/components/DeliverableView/TitleBar.tsx
@@ -1,7 +1,6 @@
 import React from '@mui/material';
 
 import CommonTitleBar from 'src/components/common/TitleBar';
-import { useParticipantProjectData } from 'src/scenes/AcceleratorRouter/ParticipantProjects/ParticipantProjectContext';
 import strings from 'src/strings';
 import { categoryLabel } from 'src/types/Deliverables';
 
@@ -9,11 +8,9 @@ import { ViewProps } from './types';
 
 const TitleBar = ({ deliverable }: ViewProps): JSX.Element => {
   const { category, name } = deliverable;
-  const { participantProject } = useParticipantProjectData();
-
   return (
     <CommonTitleBar
-      header={strings.formatString(strings.DEAL_NAME, participantProject?.dealName ?? '').toString()}
+      header={strings.formatString(strings.DEAL_NAME, deliverable.projectDealName ?? '').toString()}
       title={name}
       subtitle={strings.formatString(strings.DELIVERABLE_CATEGORY, categoryLabel(category)).toString()}
     />
