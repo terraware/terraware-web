fa3af575f62c61516db3dd6f9395f12d1e91b9d4
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2024-12-06 16:06:11 -0800

    SW-6326 Used deal name for deliverable view (#3400)

--- src/components/DeliverableView/TitleBar.tsx
+++ src/components/DeliverableView/TitleBar.tsx
@@ -1,17 +1,16 @@
 import React from '@mui/material';
 
+import CommonTitleBar from 'src/components/common/TitleBar';
 import strings from 'src/strings';
 import { categoryLabel } from 'src/types/Deliverables';
 
-import CommonTitleBar from '../common/TitleBar';
 import { ViewProps } from './types';
 
 const TitleBar = ({ deliverable }: ViewProps): JSX.Element => {
   const { category, name } = deliverable;
-
   return (
     <CommonTitleBar
-      header={strings.formatString(strings.DELIVERABLE_PROJECT, deliverable.projectName ?? '').toString()}
+      header={strings.formatString(strings.DEAL_NAME, deliverable.projectDealName ?? '').toString()}
       title={name}
       subtitle={strings.formatString(strings.DELIVERABLE_CATEGORY, categoryLabel(category)).toString()}
     />
