a3a373c39ea5cdd28b5ee1bf7a85af329cb1432c
Alex <alex@terraformation.com>
2024-12-30 16:47:04 -0800

    SW-6396 Hide select checkboxes on people page for managers and below
    This prevents users without delete permissions from being able to attempt to delete users

--- src/scenes/PeopleRouter/PeopleListView.tsx
+++ src/scenes/PeopleRouter/PeopleListView.tsx
@@ -399,7 +399,7 @@ export default function PeopleListView(): JSX.Element {
                       rows={results}
                       orderBy='name'
                       Renderer={TableCellRenderer}
-                      showCheckbox={true}
+                      showCheckbox={isAdmin(selectedOrganization)}
                       selectedRows={selectedPeopleRows}
                       setSelectedRows={setSelectedPeopleRows}
                       showTopBar={true}
