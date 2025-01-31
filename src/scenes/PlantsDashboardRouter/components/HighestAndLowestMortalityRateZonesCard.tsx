4dc89385d22534a0bfc0bc28bcaffe4994532bcd
constanzauanini <constanza.uanini@admios-sa.com>
2025-01-31 17:19:46 -0300

    Remove console log

--- src/scenes/PlantsDashboardRouter/components/HighestAndLowestMortalityRateZonesCard.tsx
+++ src/scenes/PlantsDashboardRouter/components/HighestAndLowestMortalityRateZonesCard.tsx
@@ -63,7 +63,6 @@ export default function TotalMortalityRateCard({
 
   if (newPlantsDashboardEnabled) {
     summaries?.[0]?.plantingZones.forEach((zone: PlantingZoneObservationSummary) => {
-      console.log(zone.mortalityRate);
       if (
         zone.mortalityRate !== undefined &&
         zone.mortalityRate !== null &&
