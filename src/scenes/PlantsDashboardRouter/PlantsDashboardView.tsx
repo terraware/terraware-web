c3bc6f34aa26b6ea7bb3373c6fa4ce69b3e1c9fd
constanzauanini <constanza.uanini@admios-sa.com>
2025-01-31 12:05:36 -0300

    SW-6518 Section title copy for "as of [date of Observation]" is not appearing consistently

--- src/scenes/PlantsDashboardRouter/PlantsDashboardView.tsx
+++ src/scenes/PlantsDashboardRouter/PlantsDashboardView.tsx
@@ -143,7 +143,7 @@ export default function PlantsDashboardView(): JSX.Element {
               {strings.MORTALITY_RATE}
             </Typography>
             {hasObservations && (
-              <Typography>{strings.formatString(strings.FROM_X, getLatestObservationLink())}</Typography>
+              <Typography>{strings.formatString(strings.AS_OF_X, getLatestObservationLink())}</Typography>
             )}
           </Box>
         </Grid>
@@ -256,7 +256,7 @@ export default function PlantsDashboardView(): JSX.Element {
               {strings.PLANTING_DENSITY}
             </Typography>
             {hasObservations && (
-              <Typography>{strings.formatString(strings.FROM_X, getLatestObservationLink())}</Typography>
+              <Typography>{strings.formatString(strings.AS_OF_X, getLatestObservationLink())}</Typography>
             )}
           </Box>
         </Grid>
