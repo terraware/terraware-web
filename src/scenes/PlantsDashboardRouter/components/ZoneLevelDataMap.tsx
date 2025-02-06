eb8a329216983a2a393e506a1a13ff0eed5bd031
constanzauanini <constanza.uanini@admios-sa.com>
2025-02-06 15:45:43 -0300

    Get only completed observations for map

--- src/scenes/PlantsDashboardRouter/components/ZoneLevelDataMap.tsx
+++ src/scenes/PlantsDashboardRouter/components/ZoneLevelDataMap.tsx
@@ -48,7 +48,7 @@ export default function ZoneLevelDataMap({ plantingSiteId }: ZoneLevelDataMapPro
   const summaries = useObservationSummaries(plantingSiteId);
   const allObservationsResults = useAppSelector(selectObservationsResults);
   const plantingSiteObservations = allObservationsResults?.filter(
-    (observation) => observation.plantingSiteId === plantingSiteId
+    (observation) => observation.plantingSiteId === plantingSiteId && observation.completedTime
   );
 
   const zoneObservations = useMemo(() => {
