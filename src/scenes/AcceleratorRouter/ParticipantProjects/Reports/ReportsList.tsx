a68ba08198d22dd00184dd081f78f47edbe3b7df
constanzauanini <constanza.uanini@admios-sa.com>
2025-03-06 12:11:23 -0300

    SW-6659 Reports View with Tabs [Console]

---
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Reports/ReportsList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+export default function ReportsList(): JSX.Element {
+  return <Box>Reports</Box>;
+}
---
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Reports/ReportsSettings.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+export default function ReportsSettings(): JSX.Element {
+  return <Box>Settings</Box>;
+}
