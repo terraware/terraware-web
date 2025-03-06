ed7aaf442715bf04a492cf14ff713b5d8174c223
constanzauanini <constanza.uanini@admios-sa.com>
2025-03-06 15:43:23 -0300

    SW-6659 Reports View with Tabs [Console] (#3684)

--- src/scenes/AcceleratorRouter/ParticipantProjects/Reports/ReportsView.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Reports/ReportsView.tsx
@@ -1,9 +1,72 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Box } from '@mui/material';
+import Tabs from '@terraware/web-components/components/Tabs';
+
+import Page from 'src/components/Page';
+import { APP_PATHS } from 'src/constants';
+import { useLocalization } from 'src/providers';
+import strings from 'src/strings';
+import useStickyTabs from 'src/utils/useStickyTabs';
+
+import { useParticipantProjectData } from '../ParticipantProjectContext';
+import ReportsList from './ReportsList';
+import ReportsSettings from './ReportsSettings';
+import ReportsTargets from './ReportsTargets';
 
 const ReportsView = () => {
-  return <Box>reports</Box>;
+  const { crumbs, participantProject } = useParticipantProjectData();
+  const { activeLocale } = useLocalization();
+
+  const tabs = useMemo(() => {
+    if (!activeLocale) {
+      return [];
+    }
+
+    return [
+      {
+        id: 'reports',
+        label: strings.REPORTS,
+        children: <ReportsList />,
+      },
+      {
+        id: 'targets',
+        label: strings.TARGETS,
+        children: <ReportsTargets />,
+      },
+      {
+        id: 'settings',
+        label: strings.SETTINGS,
+        children: <ReportsSettings />,
+      },
+    ];
+  }, [activeLocale]);
+
+  const { activeTab, onTabChange } = useStickyTabs({
+    defaultTab: 'reports',
+    tabs,
+    viewIdentifier: 'project-reports',
+    keepQuery: false,
+  });
+
+  return (
+    <Page
+      hierarchicalCrumbs={false}
+      crumbs={[
+        ...crumbs,
+        {
+          name: participantProject?.dealName || '',
+          to: APP_PATHS.ACCELERATOR_PROJECT_VIEW.replace(':projectId', participantProject?.projectId.toString() || ''),
+        },
+      ]}
+      title={strings.REPORTS}
+      titleStyle={{ paddingTop: '16px' }}
+    >
+      <Box display='flex' flexDirection='column' flexGrow={1}>
+        <Tabs activeTab={activeTab} onTabChange={onTabChange} tabs={tabs} />
+      </Box>
+    </Page>
+  );
 };
 
 export default ReportsView;
