948b0b9369f3449fb1352e5fc64836f2f0edf933
constanzauanini <constanza.uanini@admios-sa.com>
2025-03-10 14:58:43 -0300

    SW-6661 Redux for saving new config

--- src/redux/features/reports/reportsSlice.ts
+++ src/redux/features/reports/reportsSlice.ts
@@ -2,6 +2,9 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 import { ExistingAcceleratorReportConfig } from 'src/types/AcceleratorReport';
 
+import { StatusT, buildReducers } from '../asyncUtils';
+import { requestCreateReportConfig } from './reportsThunks';
+
 type Data = {
   config?: ExistingAcceleratorReportConfig;
 };
@@ -21,8 +24,23 @@ export const projectReportConfigSlice = createSlice({
 
 export const { setProjectReportConfigAction } = projectReportConfigSlice.actions;
 
+/**
+ * Create Report Config
+ */
+const initialCreateReportConfigState: { [key: string]: StatusT<number> } = {};
+
+const createReportConfigSlice = createSlice({
+  name: 'createReportConfigSlice',
+  initialState: initialCreateReportConfigState,
+  reducers: {},
+  extraReducers: (builder) => {
+    buildReducers(requestCreateReportConfig)(builder);
+  },
+});
+
 const reportsReducers = {
   projectReportConfig: projectReportConfigSlice.reducer,
+  projectReportConfigCreate: createReportConfigSlice.reducer,
 };
 
 export default reportsReducers;
--- src/types/AcceleratorReport.ts
+++ src/types/AcceleratorReport.ts
@@ -4,3 +4,8 @@ import strings from 'src/strings';
 export type ExistingAcceleratorReportConfig = components['schemas']['ExistingAcceleratorReportConfigPayload'];
 
 export type NewAcceleratorReportConfig = components['schemas']['NewAcceleratorReportConfigPayload'];
+
+export type CreateAcceleratorReportConfigRequestPayload =
+  components['schemas']['CreateAcceleratorReportConfigRequestPayload'];
+
+export type CreateAcceleratorReportConfigRequest = CreateAcceleratorReportConfigRequestPayload & { projectId: number };
