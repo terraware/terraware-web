18f201c69d37a50c436f9243d8dedc3a24b87f20
Isaac Hudson <isaac.hudson@terraformation.com>
2025-03-18 10:21:22 -0700

    SW-6642 Create base Funding Entity table (#3710)
    Create Funding Entity table
    
    Currently does not include Projects filter due to issues with current `TableWithSearchFilters` component implementation, as well as a lack of current filter capability for handling a column that contains list of objects. Will be handled in a future PR.
    
    Screenshot:
    <img width="1658" alt="image" src="https://github.com/user-attachments/assets/b97cfe08-d7db-46ae-a754-c0b1daad8f17" />

--- src/redux/features/funder/fundingEntitiesAsyncThunks.ts
+++ src/redux/features/funder/fundingEntitiesAsyncThunks.ts
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import FundingEntityService from 'src/services/FundingEntityService';
 import strings from 'src/strings';
+import { SearchNodePayload, SearchSortOrder } from 'src/types/Search';
 
 export const requestFundingEntity = createAsyncThunk(
   'funding-entities/get-one',
@@ -9,6 +10,28 @@ export const requestFundingEntity = createAsyncThunk(
     const response = await FundingEntityService.getUserFundingEntity(userId);
 
     if (response.requestSucceeded) {
+      return response;
+    }
+
+    return rejectWithValue(strings.GENERIC_ERROR);
+  }
+);
+
+export const requestFundingEntities = createAsyncThunk(
+  'funding-entities/list',
+  async (
+    request: {
+      locale: string | null;
+      search?: SearchNodePayload;
+      searchSortOrder?: SearchSortOrder;
+    },
+    { rejectWithValue }
+  ) => {
+    const { locale, search, searchSortOrder } = request;
+
+    const response = await FundingEntityService.listFundingEntities(locale, search, searchSortOrder);
+
+    if (response && response.requestSucceeded) {
       return response;
     }
 
