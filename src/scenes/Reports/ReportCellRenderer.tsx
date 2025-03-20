cd253e94337c63c0a0905772db12524a3047fc4d
Josh Buchea <verctrl@joshbuchea.com>
2025-03-20 13:25:02 -1000

    determine quarter number from date

--- src/scenes/Reports/ReportCellRenderer.tsx
+++ src/scenes/Reports/ReportCellRenderer.tsx
@@ -61,7 +61,7 @@ export default function ReportCellRenderer(props: RendererProps<TableRowType>): 
 
   if (column.key === 'report') {
     const year = row.startDate ? row.startDate?.split('-')?.[0] : '';
-    const quarterNumber = 1;
+    const quarterNumber = row.startDate ? Math.ceil((new Date(row.startDate).getMonth() + 1) / 3) : 0;
     const title = `${year}-Q${quarterNumber}`;
 
     return (
