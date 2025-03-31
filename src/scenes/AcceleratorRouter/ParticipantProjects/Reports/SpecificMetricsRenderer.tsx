30822a5c19c2a7ad35b2276d37f4b476bbf3a28e
constanzauanini <constanza.uanini@admios-sa.com>
2025-03-31 16:59:09 -0300

    SW-6720 Remove link on metric name for TF Expert & Read Only (#3776)

--- src/scenes/AcceleratorRouter/ParticipantProjects/Reports/SpecificMetricsRenderer.tsx
+++ src/scenes/AcceleratorRouter/ParticipantProjects/Reports/SpecificMetricsRenderer.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import Link from 'src/components/common/Link';
 import CellRenderer, { TableRowType } from 'src/components/common/table/TableCellRenderer';
 import { RendererProps } from 'src/components/common/table/types';
+import { useUser } from 'src/providers';
 
 export default function SpecificMetricsRenderer(props: RendererProps<TableRowType>): JSX.Element {
   const { column, row, index, value, onRowClick } = props;
+  const { isAllowed } = useUser();
 
   if (column.key === 'name' && onRowClick) {
     return (
@@ -13,9 +15,13 @@ export default function SpecificMetricsRenderer(props: RendererProps<TableRowTyp
         index={index}
         column={column}
         value={
-          <Link fontSize='16px' onClick={() => onRowClick()}>
-            {value as React.ReactNode}
-          </Link>
+          isAllowed('UPDATE_REPORTS_SETTINGS') ? (
+            <Link fontSize='16px' onClick={() => onRowClick()}>
+              {value as React.ReactNode}
+            </Link>
+          ) : (
+            value
+          )
         }
         row={row}
       />
