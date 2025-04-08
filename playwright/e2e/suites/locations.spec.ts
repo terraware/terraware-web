108e944e8907e9d18e97b262854178373aa72476
Isaac Hudson <isaac.hudson@terraformation.com>
2025-04-08 15:55:16 -0700

    Fix redirect when creating a new org (#3818)
    Fix redirect so that it goes to the new org's home page after user
    creates a new org.
    
    Also add a couple related explicit search fields in to prepare for react
    router 7.

--- playwright/e2e/suites/locations.spec.ts
+++ playwright/e2e/suites/locations.spec.ts
@@ -125,8 +125,6 @@ export default function LocationTests() {
     await page.locator('#create-org-question-website').getByRole('textbox').fill('fakeuniversity.edu');
     await page.getByRole('button', { name: 'Save' }).click();
     await expect(page.getByRole('heading', { name: `You have created ${newOrgName}!` })).toBeVisible();
-    await page.getByRole('button', { name: 'Terraformation (staging)' }).click();
-    await page.getByRole('menuitem', { name: newOrgName }).click();
     await page.getByRole('button', { name: 'Organization' }).click();
     await expect(page.getByText(`Organization Name${newOrgName}`)).toBeVisible();
     await expect(page.getByText('DescriptionThis is my new')).toBeVisible();
