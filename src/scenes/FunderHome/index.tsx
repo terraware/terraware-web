9b001b262bf01f81cda7f1ab2fa1742f5cc8de49
constanzauanini <constanza.uanini@admios-sa.com>
2025-04-10 16:15:55 -0300

    Add tabs to Funder home page

--- src/scenes/FunderHome/index.tsx
+++ src/scenes/FunderHome/index.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { Box } from '@mui/material';
+import { Tabs } from '@terraware/web-components';
 
-import PageHeader from 'src/components/PageHeader';
 import TfMain from 'src/components/common/TfMain';
+import { useLocalization } from 'src/providers';
+import FunderReportView from 'src/scenes/FunderReport/FunderReportView';
+import strings from 'src/strings';
+import useStickyTabs from 'src/utils/useStickyTabs';
 
 export default function FunderHome() {
+  const { activeLocale } = useLocalization();
+
+  const tabs = useMemo(() => {
+    if (!activeLocale) {
+      return [];
+    }
+    return [
+      {
+        id: 'projectProfile',
+        label: strings.PROJECT_PROFILE,
+        children: <Box>Project Profile</Box>,
+      },
+      {
+        id: 'report',
+        label: strings.REPORT,
+        children: <FunderReportView />,
+      },
+    ];
+  }, [activeLocale]);
+
+  const { activeTab, onTabChange } = useStickyTabs({
+    defaultTab: 'projectProfile',
+    tabs,
+    viewIdentifier: 'funder-home',
+    keepQuery: false,
+  });
+
   return (
     <TfMain>
       <Box
@@ -17,7 +48,7 @@ export default function FunderHome() {
         }}
       >
         <Box>
-          <PageHeader title={'Hello Funder'} subtitle={''} />
+          <Tabs activeTab={activeTab} onTabChange={onTabChange} tabs={tabs} />
         </Box>
       </Box>
     </TfMain>
---
+++ src/scenes/FunderReport/FunderReportView.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+const FunderReportView = () => {
+  return <Box>Report View</Box>;
+};
+
+export default FunderReportView;
