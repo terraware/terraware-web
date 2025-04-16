be32e28b4ba9e79204c806fb5d7bbfe2d5d6826a
constanzauanini <constanza.uanini@admios-sa.com>
2025-04-16 12:28:49 -0300

    SW-6400 Do not include planting sites with no maps in new home page site selector (#3847)

--- src/scenes/Home/TerrawareHomeView/PlantingSiteStats.tsx
+++ src/scenes/Home/TerrawareHomeView/PlantingSiteStats.tsx
@@ -187,6 +187,7 @@ export const PlantingSiteStats = () => {
               onChange={(plantingSiteId) => {
                 setSelectedPlantingSiteId(plantingSiteId);
               }}
+              hideNoBoundary
             />
           </Grid>
 
