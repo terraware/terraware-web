4e5367bfef7036e1f523621f9d719180dbfe7616
Isaac Hudson <isaac.hudson@terraformation.com>
2025-04-22 14:34:53 -0700

    SW-6780 Add details to top bar of project profile (#3859)
    Add Application status, Voter decision, Cohort name, Cohort phase, and Score to project profile.
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/Gd93CNnCsYqInZuuYUOX/ef25f370-bb81-418d-b0a9-ddbdfe74b38f.png)

---
+++ src/components/ProjectField/VotingDecisionLink.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { useTheme } from '@mui/material';
+
+import { APP_PATHS } from 'src/constants';
+import VoteBadge from 'src/scenes/AcceleratorRouter/ParticipantProjects/Voting/VoteBadge';
+import strings from 'src/strings';
+import { PhaseVotes } from 'src/types/Votes';
+
+import ProjectBadgeLink from './ProjectBadgeLink';
+
+type VotingDecisionLinkProps = {
+  projectId: string | number | undefined;
+  phaseVotes?: PhaseVotes;
+};
+
+const VotingDecisionLink = ({ projectId, phaseVotes }: VotingDecisionLinkProps) => {
+  const theme = useTheme();
+  const linkTo = APP_PATHS.ACCELERATOR_PROJECT_VOTES.replace(':projectId', `${projectId}`);
+  return (
+    <ProjectBadgeLink
+      label={strings.VOTING_DECISION}
+      linkTo={linkTo}
+      borderLeft={`1px solid ${theme.palette.TwClrBaseGray500}`}
+    >
+      <VoteBadge vote={phaseVotes?.decision} />
+    </ProjectBadgeLink>
+  );
+};
+
+export default VotingDecisionLink;
