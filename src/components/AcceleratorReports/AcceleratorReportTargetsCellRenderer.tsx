cebea803eb876590bb0f5063be8e019d27090f5d
Josh Buchea <joshbuchea@users.noreply.github.com>
2025-04-23 12:47:52 -1000

    SW-6807: Project Org Admin should be able to edit metric targets (#3869)
    SW-6807: Project Org Admin should be able to edit metric targets (#3869)

--- src/components/AcceleratorReports/AcceleratorReportTargetsCellRenderer.tsx
+++ src/components/AcceleratorReports/AcceleratorReportTargetsCellRenderer.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import Link from 'src/components/common/Link';
 import CellRenderer, { TableRowType } from 'src/components/common/table/TableCellRenderer';
 import { RendererProps } from 'src/components/common/table/types';
-import { useUser } from 'src/providers';
+import { useOrganization, useUser } from 'src/providers';
 
 export default function AcceleratorReportTargetsCellRenderer(props: RendererProps<TableRowType>): JSX.Element {
   const { column, row, value, index, onRowClick } = props;
   const { isAllowed } = useUser();
+  const { selectedOrganization } = useOrganization();
+
+  const isAllowedUpdateReportsTargets = useMemo(
+    () => isAllowed('UPDATE_REPORTS_TARGETS', { organization: selectedOrganization }),
+    [isAllowed, selectedOrganization]
+  );
 
   if (column.key === 'name' && onRowClick) {
     return (
       <CellRenderer
         column={column}
         value={
-          isAllowed('UPDATE_REPORTS_SETTINGS') ? (
+          isAllowedUpdateReportsTargets ? (
             <Link fontSize='16px' onClick={() => onRowClick()}>
               {value as React.ReactNode}
             </Link>
