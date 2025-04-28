fee051f2c56de9b23b8e39a6932024fcc41ee2cf
Isaac Hudson <isaac.hudson@terraformation.com>
2025-04-28 15:35:54 -0700

    Add cards and data for submitted reports to Project Profile

--- src/components/ProjectField/InvertedCard.tsx
+++ src/components/ProjectField/InvertedCard.tsx
@@ -5,7 +5,11 @@ import { Box, Grid, Typography, useTheme } from '@mui/material';
 import { ProjectFieldProps, renderFieldValue } from '.';
 import GridEntryWrapper from './GridEntryWrapper';
 
-const InvertedCard = ({ label, md, value, backgroundColor, units }: ProjectFieldProps) => {
+type InvertedCardProps = {
+  rightLabel?: string;
+} & ProjectFieldProps;
+
+const InvertedCard = ({ label, md, value, backgroundColor, units, rightLabel }: InvertedCardProps) => {
   const theme = useTheme();
 
   return (
@@ -18,12 +22,24 @@ const InvertedCard = ({ label, md, value, backgroundColor, units }: ProjectField
         sx={{ backgroundColor }}
       >
         <Grid container alignContent={'left'}>
-          <Grid item xs={12}>
+          <Grid item xs={rightLabel ? 6 : 12}>
             {renderFieldValue(value, units)}
             <Typography fontSize={'16px'} lineHeight={'24px'} fontWeight={600} marginTop={theme.spacing(1)}>
               {label}
             </Typography>
           </Grid>
+          {rightLabel && (
+            <Grid item xs={6} display={'flex'} alignItems={'flex-end'} justifyContent={'flex-end'}>
+              <Typography
+                fontSize={'16px'}
+                lineHeight={'24px'}
+                fontWeight={400}
+                color={theme.palette.TwClrTxtSecondary}
+              >
+                {rightLabel}
+              </Typography>
+            </Grid>
+          )}
         </Grid>
       </Box>
     </GridEntryWrapper>
