ba07354e9c3625d630a7c6c5da5c2fcd098c0190
Isaac Hudson <isaac.hudson@terraformation.com>
2025-04-29 13:54:34 -0700

    Remove unused verticalAlign

--- src/components/ProjectField/Display.tsx
+++ src/components/ProjectField/Display.tsx
@@ -15,7 +15,7 @@ const ProjectFieldDisplay = ({ label, md, rightBorder, value, tooltip, height, u
         <Typography fontSize={'16px'} lineHeight={'24px'} fontWeight={600} marginBottom={theme.spacing(1)}>
           {label}
           {tooltip && (
-            <Tooltip title={tooltip} style={{ verticalAlign: 'center' }}>
+            <Tooltip title={tooltip}>
               <Icon
                 style={{ marginLeft: theme.spacing(1) }}
                 name='info'
