471d4d084603b022e773d1bd57476f9f4cf39f27
Isaac Hudson <isaac.hudson@terraformation.com>
2025-05-08 16:13:13 -0700

    Fix subscript of units causing cards to grow

--- src/components/Units/Co2HectareYear.tsx
+++ src/components/Units/Co2HectareYear.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
-const Co2HectareYear = () => {
-  return (
-    <>
-      tCO<sub>2</sub>/ha/yr
-    </>
-  );
-};
+const Co2HectareYear = () => (
+  <span style={{ display: 'inline-flex', alignItems: 'baseline' }}>
+    tCO
+    <span
+      style={{
+        display: 'inline-block',
+        position: 'relative',
+        fontSize: '65%',
+        bottom: '-0.2em',
+        lineHeight: 0,
+      }}
+    >
+      2
+    </span>
+    /ha/yr
+  </span>
+);
 
 export default Co2HectareYear;
