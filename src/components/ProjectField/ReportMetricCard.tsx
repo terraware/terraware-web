22f6cf8d53c555280bfe25403f710385696dda0a
Isaac Hudson <isaac.hudson@terraformation.com>
2025-05-12 15:09:12 -0700

    Add published reports data to Funder Portal Project Profile

--- src/components/ProjectField/ReportMetricCard.tsx
+++ src/components/ProjectField/ReportMetricCard.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { useTheme } from '@mui/material';
 
 import strings from 'src/strings';
-import { ReportSystemMetric, SystemMetricName } from 'src/types/AcceleratorReport';
+import { PublishedReportMetric, ReportSystemMetric, SystemMetricName } from 'src/types/AcceleratorReport';
 
 import InvertedCard from './InvertedCard';
 
 type ReportMetricCardProps = {
-  metrics: ReportSystemMetric[];
+  metrics?: ReportSystemMetric[];
+  publishedMetrics?: PublishedReportMetric[];
   metricName: SystemMetricName;
   label: string;
   units?: string;
   formatter?: (value: number | undefined) => string;
 };
 
-const ReportMetricCard = ({ label, metrics, metricName, units, formatter }: ReportMetricCardProps) => {
+const ReportMetricCard = ({
+  label,
+  metrics,
+  publishedMetrics,
+  metricName,
+  units,
+  formatter,
+}: ReportMetricCardProps) => {
   const theme = useTheme();
+  if (metrics === undefined && publishedMetrics === undefined) {
+    return <></>;
+  }
 
-  const metric = metrics.find((m) => m.metric === metricName);
-  const value = metric?.overrideValue || metric?.systemValue;
+  const value = useMemo(() => {
+    if (metrics) {
+      const metric = metrics.find((m) => m.metric === metricName);
+      return metric?.overrideValue || metric?.systemValue;
+    } else if (publishedMetrics) {
+      return publishedMetrics.find((m) => m.name === metricName)?.value;
+    }
+  }, [metrics, publishedMetrics]);
 
   return (
     <InvertedCard
