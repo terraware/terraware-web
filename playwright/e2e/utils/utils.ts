f6f6114dc07d50a2ef76166feba02d784e6738c4
Isaac Hudson <isaac.hudson@terraformation.com>
2025-05-14 16:25:31 -0700

    SW-6900 Playwright tests for project profile (#3989)
    Includes updates to the dump file to be able to start running console tests.
    
    Things not included (but can be added if preferred):
    - scores
    - project lead
    - voting
    - funding entities (will likely backfill this one after tests for funding entities have been added)

--- playwright/e2e/utils/utils.ts
+++ playwright/e2e/utils/utils.ts
@@ -1,10 +1,12 @@
-export const waitFor = async (page, selector, timeout = 3000) => {
+import { type BrowserContext, Page } from 'playwright-core';
+
+export const waitFor = async (page: Page, selector: string, timeout = 3000) => {
   // Seems weird to await in here with nothing else going on, but I am doing that explicitly so I can
   // ditch the return and keep the signature Promise<void>
   await page.waitForSelector(selector, { timeout });
 };
 
-export const addCookies = async (context) => {
+export const addCookies = async (context: BrowserContext) => {
   // Make all requests look like they are associated with an existing login session
   // so we don't have to depend on a Keycloak server to run the test suite. The
   // session value here is the base64-encoded session ID from dump/session.sql.
@@ -12,3 +14,6 @@ export const addCookies = async (context) => {
     { name: 'SESSION', value: 'Mjc2NzE0YWQtYWIwYS00OGFhLThlZjgtZGI2NWVjMmU5NTBh', url: 'http://127.0.0.1:3000' },
   ]);
 };
+
+// Just a typing saver for `getByText`
+export const exactOptions = { exact: true };
