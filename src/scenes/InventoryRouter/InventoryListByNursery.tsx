13e38450105bc93d831154be42788d95b5663937
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-05-16 10:00:59 -0700

    Added localization to inventory list tables (#4008)
    Our localization for numbers are working. For some reason both these tables have regressed between staging and prod but this fixes it.

--- src/scenes/InventoryRouter/InventoryListByNursery.tsx
+++ src/scenes/InventoryRouter/InventoryListByNursery.tsx
@@ -5,7 +5,7 @@ import { TableColumnType } from '@terraware/web-components';
 
 import Card from 'src/components/common/Card';
 import EmptyStatePage from 'src/components/emptyStatePages/EmptyStatePage';
-import { useOrganization } from 'src/providers';
+import { useOrganization, useUser } from 'src/providers';
 import { InventoryFiltersType } from 'src/scenes/InventoryRouter/InventoryFilter';
 import InventoryTable from 'src/scenes/InventoryRouter/InventoryTable';
 import { FacilitySpeciesInventoryResult } from 'src/scenes/InventoryRouter/InventoryV2View';
@@ -15,6 +15,7 @@ import { SearchResponseElement, SearchSortOrder } from 'src/types/Search';
 import { getRequestId, setRequestId } from 'src/utils/requestsId';
 import useDebounce from 'src/utils/useDebounce';
 import useForm from 'src/utils/useForm';
+import { useNumberFormatter } from 'src/utils/useNumberFormatter';
 
 const columns = (): TableColumnType[] => [
   { key: 'facility_name', name: strings.NURSERY, type: 'string' },
@@ -67,6 +68,8 @@ export default function InventoryListByNursery({ setReportData }: InventoryListB
   });
   const [filters, setFilters] = useForm<InventoryFiltersType>({});
   const theme = useTheme();
+  const { user } = useUser();
+  const numberFormatter = useNumberFormatter(user?.locale);
 
   const onSearchSortOrder = (order: SearchSortOrder) => {
     const isClientSorted = BE_SORTED_FIELDS.indexOf(order.field) === -1;
@@ -110,11 +113,19 @@ export default function InventoryListByNursery({ setReportData }: InventoryListB
           setShowResults(updatedResult.length > 0);
         }
         if (getRequestId('searchInventory') === requestId) {
-          setSearchResults(updatedResult);
+          setSearchResults(
+            updatedResult.map((result) => ({
+              ...result,
+              germinatingQuantity: numberFormatter.format(Number(result.germinatingQuantity)),
+              notReadyQuantity: numberFormatter.format(Number(result.notReadyQuantity)),
+              readyQuantity: numberFormatter.format(Number(result.readyQuantity)),
+              totalQuantity: numberFormatter.format(Number(result.totalQuantity)),
+            }))
+          );
         }
       }
     }
-  }, [filters, debouncedSearchTerm, selectedOrganization, searchSortOrder, setReportData]);
+  }, [filters, debouncedSearchTerm, numberFormatter, selectedOrganization, searchSortOrder, setReportData]);
 
   useEffect(() => {
     void onApplyFilters();
