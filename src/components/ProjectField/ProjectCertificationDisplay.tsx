52f4907b68c857c71f5fccc1330d8b748f82de79
Isaac Hudson <isaac.hudson@terraformation.com>
2025-05-19 15:44:56 -0700

    View Carbon Certifications

---
+++ src/components/ProjectField/ProjectCertificationDisplay.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Grid, Typography, useTheme } from '@mui/material';
+
+import strings from 'src/strings';
+import { CarbonCertifications } from 'src/types/ParticipantProject';
+
+const ProjectCertificationDisplay = ({ certifications }: { certifications?: CarbonCertifications }) => {
+  const theme = useTheme();
+
+  if (!certifications?.length) {
+    return null;
+  }
+
+  const getImageSource = (certification: string) => {
+    switch (certification) {
+      case 'CCB Standard':
+      default:
+        return '/assets/carbon/CcbStandard.png';
+    }
+  };
+
+  return (
+    <>
+      {certifications.map((certification) => (
+        <Grid item md={4} key={certification} paddingX={theme.spacing(2)}>
+          <Typography fontSize='16px' fontWeight={400} lineHeight='24px' component={'span'}>
+            {strings.CERTIFICATION}
+          </Typography>
+          <div>
+            <img src={getImageSource(certification)} alt={certification} width='200px' />
+          </div>
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default ProjectCertificationDisplay;
