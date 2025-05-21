523ecff578be5baf1144377668aadb6cc8a222c9
Isaac Hudson <isaac.hudson@terraformation.com>
2025-05-21 08:36:18 -0700

    SW-6956 SW-6958 SW-6959 Fix funder portal project profile UI issues (#4044)

--- src/components/ProjectField/Link.tsx
+++ src/components/ProjectField/Link.tsx
@@ -3,15 +3,22 @@ import React from 'react';
 import { useTheme } from '@mui/material';
 
 import Link from 'src/components/common/Link';
+import { useSyncNavigate } from 'src/hooks/useSyncNavigate';
 
 import { ProjectFieldProps } from '.';
 
 const ProjectFieldLink = ({ label, value }: ProjectFieldProps) => {
   const theme = useTheme();
+  const navigate = useSyncNavigate();
+
+  const onClick = () => {
+    navigate(value as string);
+    window.scroll(0, 0);
+  };
 
   return (
     value && (
-      <Link fontSize={'16px'} to={`${value as string}`} style={{ paddingLeft: theme.spacing(2) }}>
+      <Link fontSize={'16px'} onClick={onClick} style={{ paddingLeft: theme.spacing(2) }}>
         {label}
       </Link>
     )
