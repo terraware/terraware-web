819a394bb6674f9c6744e28155ad5db2b7e7ef80
Josh Buchea <verctrl@joshbuchea.com>
2025-05-23 08:27:27 -1000

    modify provider to return a new instance of localized strings each time locale is changed

--- src/strings/index.tsx
+++ src/strings/index.tsx
@@ -37,6 +37,6 @@ export interface LocaleDetails {
 // By default, we have no strings to show, but react-localization requires there to be at least
 // one locale in the LocalizedStrings constructor's argument. We will dynamically update this when
 // we've loaded the strings for the current locale.
-const strings = new LocalizedStrings({ _: {} } as unknown as ILocalizedStringsMap);
+const strings = new LocalizedStrings({ en: english } as unknown as ILocalizedStringsMap);
 
 export default strings;
