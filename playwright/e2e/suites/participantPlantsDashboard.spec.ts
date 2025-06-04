8790ff1303a549bd9e912b4a4f72d4d5960d3856
constanzauanini <constanza.uanini@admios-sa.com>
2025-06-04 11:40:53 -0300

    SW-6889 Add e2e test for new plants dashboards (#4108)

--- playwright/e2e/suites/participantPlantsDashboard.spec.ts
+++ playwright/e2e/suites/participantPlantsDashboard.spec.ts
@@ -7,64 +7,16 @@ test.beforeEach(async ({ context }, testInfo) => {
   await addCookies(context);
 });
 
-export default function PlantsDashboardTests() {
-  test('Console Dashboard Empty State for project', async ({ page }, testInfo) => {
+export default function ParticipantPlantsDashboardTests() {
+  test('Rolled-up dashboard for selected project', async ({ page }, testInfo) => {
     await page.goto('http://127.0.0.1:3000');
     await waitFor(page, '#home');
-    await page.getByRole('link', { name: 'Accelerator Console' }).click();
-    await expect(page.getByRole('main').getByText('Overview')).toBeVisible();
-    await page.getByRole('link', { name: 'Phase 0 Project Deal' }).click();
-    await page.getByRole('tab', { name: 'Plants Dashboard' }).click();
-    await expect(page.getByText('Phase 0 Project Deal', { exact: true })).toBeVisible();
-    await expect(page.getByText('Total Planting Area', { exact: true })).toBeVisible();
-    await expect(page.getByText('0 ha').first()).toBeVisible();
-    await expect(
-      page.getByText('To view the dashboard, you need to have a planting site.', { exact: true })
-    ).toBeVisible();
-    await expect(page.getByText('Planting Site Totals', { exact: true })).toBeVisible();
-    await expect(
-      page
-        .locator('div')
-        .filter({ hasText: /^0 ha$/ })
-        .getByRole('paragraph')
-    ).toBeVisible();
-    await expect(page.getByText('Total Planted Complete', { exact: true })).toBeVisible();
-    await expect(page.getByText('Planting Complete: 0%', { exact: true })).toBeVisible();
-    await expect(page.getByText('Target: 0 ha', { exact: true })).toBeVisible();
-    await expect(page.getByText('0 Plants', { exact: true })).toBeVisible();
-    await expect(page.getByText('Total Planted').nth(1)).toBeVisible();
-    await expect(page.getByText('0 Species', { exact: true })).toBeVisible();
-    await expect(page.getByText('Total Planted').nth(2)).toBeVisible();
-    await expect(page.getByText('Planted Species', { exact: true })).toBeVisible();
-    await expect(page.locator('#plantsBySpecies')).toBeVisible();
-    await expect(page.getByText('Species Categories', { exact: true })).toBeVisible();
-    await expect(page.locator('#speciesByCategory')).toBeVisible();
-    await expect(page.getByText('Project Area Map', { exact: true })).toBeVisible();
-    await expect(page.getByText('0 ha in Total Planting Area', { exact: true })).toBeVisible();
-    await expect(page.getByText('Boundaries')).toBeVisible();
-    await expect(page.locator('div').filter({ hasText: /^Observation Events$/ })).toBeVisible();
-    await expect(
-      page
-        .locator('div')
-        .filter({ hasText: /^Observation Events$/ })
-        .getByRole('checkbox')
-    ).toBeDisabled();
-    await expect(
-      page
-        .locator('div')
-        .filter({ hasText: /^Mortality Rate$/ })
-        .getByRole('checkbox')
-    ).toBeDisabled();
-  });
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Dashboard' }).click();
 
-  test('Console rolled-up dashboard for project', async ({ page }, testInfo) => {
-    await page.goto('http://127.0.0.1:3000');
-    await waitFor(page, '#home');
-    await page.getByRole('link', { name: 'Accelerator Console' }).click();
-    await expect(page.getByRole('main').getByText('Overview')).toBeVisible();
-    await page.getByRole('link', { name: 'Phase 1 Project Deal' }).click();
-    await page.getByRole('tab', { name: 'Plants Dashboard' }).click();
-    await expect(page.getByText('Phase 1 Project Deal', { exact: true })).toBeVisible();
+    await page.getByPlaceholder('No Project Selected').click();
+    await page.getByText('Phase 1 Project', { exact: true }).click();
+
     await expect(page.getByText('Total Planting Area', { exact: true })).toBeVisible();
     await expect(page.getByText('10,879.4 ha').first()).toBeVisible();
     await expect(page.getByText('Project Area Totals', { exact: true })).toBeVisible();
@@ -103,16 +55,15 @@ export default function PlantsDashboardTests() {
     ).toBeDisabled();
   });
 
-  test('Console dashboard for project planting site with no observations', async ({ page }, testInfo) => {
+  test('Planting site dashboard no observations', async ({ page }, testInfo) => {
     await page.goto('http://127.0.0.1:3000');
     await waitFor(page, '#home');
-    await page.getByRole('link', { name: 'Accelerator Console' }).click();
-    await expect(page.getByRole('main').getByText('Overview')).toBeVisible();
-    await page.getByRole('link', { name: 'Phase 1 Project Deal' }).click();
-    await page.getByRole('tab', { name: 'Plants Dashboard' }).click();
-    await expect(page.getByText('Phase 1 Project Deal', { exact: true })).toBeVisible();
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Dashboard' }).click();
+
     await page.getByPlaceholder('Select...').click();
-    await page.getByText('PS1').click();
+    await page.getByText('PS1', { exact: true }).click();
+
     await expect(page.getByText('Total Planting Area', { exact: true })).toBeVisible();
     await expect(page.getByText('6,788.4 ha').first()).toBeVisible();
     await expect(page.getByText('Planting Site Totals', { exact: true })).toBeVisible();
@@ -151,16 +102,14 @@ export default function PlantsDashboardTests() {
     ).toBeDisabled();
   });
 
-  test('Console dashboard for project planting site with observations', async ({ page }, testInfo) => {
+  test('Planting site dashboard observations', async ({ page }, testInfo) => {
     await page.goto('http://127.0.0.1:3000');
     await waitFor(page, '#home');
-    await page.getByRole('link', { name: 'Accelerator Console' }).click();
-    await expect(page.getByRole('main').getByText('Overview')).toBeVisible();
-    await page.getByRole('link', { name: 'Phase 1 Project Deal' }).click();
-    await page.getByRole('tab', { name: 'Plants Dashboard' }).click();
-    await expect(page.getByText('Phase 1 Project Deal', { exact: true })).toBeVisible();
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Dashboard' }).click();
+
     await page.getByPlaceholder('Select...').click();
-    await page.getByText('PS2').click();
+    await page.getByText('PS2', { exact: true }).click();
     await expect(page.getByText('Total Planting Area', { exact: true })).toBeVisible();
     await expect(page.getByText('4,091 ha').first()).toBeVisible();
     await expect(
@@ -244,5 +193,59 @@ export default function PlantsDashboardTests() {
         .filter({ hasText: /^Mortality Rate$/ })
         .getByRole('checkbox')
     ).toBeChecked();
+  });
+
+  test('Plants Dashboard empty state', async ({ page }, testInfo) => {
+    await page.goto('http://127.0.0.1:3000');
+    await waitFor(page, '#home');
+    await page.getByRole('button', { name: 'Terraformation (staging)' }).click();
+    await page.getByRole('menuitem', { name: 'My New Org-' }).click();
+
+    await waitFor(page, '#home');
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Dashboard' }).click();
+
+    await expect(page.getByText('To view data in this dashboard, add a planting site', { exact: true })).toBeVisible();
+    await expect(
+      page.getByText(
+        'This dashboard displays data from your plant tracking and monitoring activities. Add at least one planting site to start tracking where your seedlings are planted and to see the data in this dashboard.',
+        { exact: true }
+      )
+    ).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Add a Planting Site' })).toBeVisible();
+    await expect(page.getByText('Planting Site Totals', { exact: true })).toBeVisible();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /^0 ha$/ })
+        .getByRole('paragraph')
+    ).toBeVisible();
+    await expect(page.getByText('Total Planted Complete', { exact: true })).toBeVisible();
+    await expect(page.getByText('Planting Complete: 0%', { exact: true })).toBeVisible();
+    await expect(page.getByText('Target: 0 ha', { exact: true })).toBeVisible();
+    await expect(page.getByText('0 Plants', { exact: true })).toBeVisible();
+    await expect(page.getByText('Total Planted').nth(1)).toBeVisible();
+    await expect(page.getByText('0 Species', { exact: true })).toBeVisible();
+    await expect(page.getByText('Total Planted').nth(2)).toBeVisible();
+    await expect(page.getByText('Planted Species', { exact: true })).toBeVisible();
+    await expect(page.locator('#plantsBySpecies')).toBeVisible();
+    await expect(page.getByText('Species Categories', { exact: true })).toBeVisible();
+    await expect(page.locator('#speciesByCategory')).toBeVisible();
+    await expect(page.getByText('Project Area Map', { exact: true })).toBeVisible();
+    await expect(page.getByText('0 ha in Total Planting Area', { exact: true })).toBeVisible();
+    await expect(page.getByText('Boundaries')).toBeVisible();
+    await expect(page.locator('div').filter({ hasText: /^Observation Events$/ })).toBeVisible();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /^Observation Events$/ })
+        .getByRole('checkbox')
+    ).toBeDisabled();
+    await expect(
+      page
+        .locator('div')
+        .filter({ hasText: /^Mortality Rate$/ })
+        .getByRole('checkbox')
+    ).toBeDisabled();
   });
 }
