2b92a36a8901a3d589326d60afcddaaabea0ca62
Josh Buchea <verctrl@joshbuchea.com>
2025-06-10 10:40:39 -1000

    disable no-console

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -249,7 +249,7 @@ export default fixupConfigRules([
       'no-class-assign': 'error',
       'no-compare-neg-zero': 'error',
       'no-cond-assign': 'error',
-      'no-console': 'error',
+      'no-console': 'off',
       'no-const-assign': 'error',
       'no-constant-binary-expression': 'error',
       'no-constant-condition': 'error',
