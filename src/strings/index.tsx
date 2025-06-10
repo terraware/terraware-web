2e49dcd4dcb4c29965af23703b0f1da7596960de
Josh Buchea <verctrl@joshbuchea.com>
2025-06-09 14:48:54 -1000

    revert comment removal

--- src/strings/index.tsx
+++ src/strings/index.tsx
@@ -34,6 +34,9 @@ export interface LocaleDetails {
   inDevelopment?: boolean;
 }
 
+// By default, we have no strings to show, but react-localization requires there to be at least
+// one locale in the LocalizedStrings constructor's argument. We will dynamically update this when
+// we've loaded the strings for the current locale.
 const strings = new LocalizedStrings({ _: {} } as unknown as ILocalizedStringsMap);
 
 export default strings;
