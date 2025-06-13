59682e24010dbade4d78ce420d6f0b72960f6543
Steven Grimm <steven@terraformation.com>
2025-06-13 09:44:28 -0700

    Convert strings CSV files for all locales on edit
    When a strings CSV file changes while the dev server is running,
    we automatically regenerate its JavaScript file to avoid having to
    restart the server. Unfortunately, we still have to restart the
    server if strings are being added to the English CSV, because the
    other locales' JavaScript files are missing those strings and
    thus the TypeScript types don't match.
    
    Change the regeneration code to always regenerate all the JS
    files if any of the CSV files change. Then we can freely edit
    the English strings file (or switch between git branches that have
    different edits to the strings file) without restarting the dev
    server.

--- scripts/generate-strings.js
+++ scripts/generate-strings.js
@@ -3,23 +3,9 @@
  *
  * This is run as part of the CI build process; you usually shouldn't need to run it by hand.
  */
-const fs = require('fs/promises');
-const { convertCsvFile } = require('../src/strings/export');
+const { convertAllLocales } = require('../src/strings/export');
 
 const csvDir = 'src/strings/csv';
 const stringsDir = 'src/strings';
 
-async function convertAll() {
-  const files = await fs.readdir(csvDir);
-  const conversions = files.map(async (filename) => {
-    if (filename.endsWith('.csv')) {
-      // eslint-disable no-console
-      console.log(`Converting ${filename} to JavaScript`);
-      await convertCsvFile(`${csvDir}/${filename}`, stringsDir);
-    }
-  });
-
-  await Promise.resolve(Promise.all(conversions));
-}
-
-convertAll();
+convertAllLocales(csvDir, stringsDir);
