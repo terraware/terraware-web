b52daff2e866fc08c107c5f55e1165f252677fd2
Josh Buchea <verctrl@joshbuchea.com>
2025-06-13 12:30:53 -1000

    update comments

--- playwright/e2e/suites/observations.spec.ts
+++ playwright/e2e/suites/observations.spec.ts
@@ -24,10 +24,10 @@ export default function ObservationsTests() {
     await page.locator('li.select-value').first().click();
     await page.getByLabel('Select All').click();
 
-    // enter observation start date
+    // enter observation start date (today)
     await page.getByLabel('Observation Start Date').fill(new Date().toISOString().split('T')[0]);
 
-    // enter observation end date
+    // enter observation end date (today + 30 days)
     const futureDate = new Date();
     futureDate.setDate(futureDate.getDate() + 30);
     const futureDateFormatted = futureDate.toISOString().split('T')[0];
