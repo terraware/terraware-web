1dea4e967273793a58e487c46b4f9c4ff48567ae
Josh Buchea <verctrl@joshbuchea.com>
2025-06-13 10:28:53 -1000

    first pass

---
+++ playwright/e2e/suites/observations.spec.ts
@@ -0,0 +1,109 @@
+import { expect, test } from '@playwright/test';
+
+import { navigateToProjectProfile } from '../utils/navigation';
+import { addCookies, waitFor } from '../utils/utils';
+
+test.setTimeout(20000);
+test.beforeEach(async ({ context }, testInfo) => {
+  await addCookies(context);
+});
+
+export default function ObservationsTests() {
+  test('Schedule Observation', async ({ page }, testInfo) => {
+    await page.goto('http://127.0.0.1:3000');
+    await waitFor(page, '#home');
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Observations' }).click();
+
+    await page.getByRole('button', { name: 'Schedule Observation' }).click();
+    await expect(page.getByText('Schedule Observation')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
+
+    // select planting site & zones
+    await page.locator('#site').click();
+    // await page.locator('li').filter({ hasText: 'PS1' }).click();
+    await page.locator('li.select-value').first().click();
+    await page.getByLabel('Select All').click();
+
+    // enter observation start date
+    await page.getByLabel('Observation Start Date').fill(new Date().toISOString().split('T')[0]);
+
+    // enter observation end date
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 30);
+    const futureDateFormatted = futureDate.toISOString().split('T')[0];
+    await page.getByLabel('Observation End Date').fill(futureDateFormatted);
+
+    // save
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    // wait for success toast message
+    await waitFor(page, '#snackbar div:has-text("Observation scheduled!")');
+  });
+
+  test('Reschedule Observation', async ({ page }, testInfo) => {
+    await page.goto('http://127.0.0.1:3000');
+    await waitFor(page, '#home');
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Observations' }).click();
+
+    // wait for table rows to load
+    await waitFor(page, '#row2');
+
+    // open action menu
+    // await page.locator('tr').filter({ hasText: 'In Progress' }).first().locator('td button').click();
+    await page
+      .locator('tr')
+      .filter({ hasText: 'In Progress' })
+      .first()
+      .getByLabel('More Options')
+      .getByRole('button')
+      .click();
+    // await page.getByRole('row', { name: 'June 2025 In Progress PS2' }).getByLabel('More Options').getByRole('button');
+
+    // click reschedule
+    await page.getByRole('menuitem', { name: 'Reschedule' }).click();
+
+    await expect(page.getByText('Reschedule Observation')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
+
+    // update observation end date
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + 45);
+    const futureDateFormatted = futureDate.toISOString().split('T')[0];
+    await page.getByLabel('Observation End Date').fill(futureDateFormatted);
+
+    // save
+    await page.getByRole('button', { name: 'Save' }).click();
+
+    // wait for success toast message
+    await waitFor(page, '#snackbar div:has-text("Observation rescheduled!")');
+  });
+
+  test('End Observation', async ({ page }, testInfo) => {
+    await page.goto('http://127.0.0.1:3000');
+    await waitFor(page, '#home');
+    await page.getByRole('button', { name: 'Plants' }).click();
+    await page.getByRole('button', { name: 'Observations' }).click();
+
+    // wait for table rows to load
+    await waitFor(page, '#row1');
+
+    // open action menu
+    await page.locator('tr').filter({ hasText: 'In Progress' }).first().locator('td button').click();
+
+    // click reschedule
+    await page.getByRole('menuitem', { name: 'End Observation' }).click();
+
+    // wait for save button to load
+    await waitFor(page, '#save');
+
+    // save
+    await page.locator('#save').click();
+
+    // wait for success toast message
+    await waitFor(page, '#snackbar div:has-text("observation has been ended")');
+  });
+}
