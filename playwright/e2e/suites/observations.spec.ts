598cd73962b636516fb09f903d5168e3488c929f
Josh Buchea <verctrl@joshbuchea.com>
2025-06-16 11:35:31 -1000

    update tests

--- playwright/e2e/suites/observations.spec.ts
+++ playwright/e2e/suites/observations.spec.ts
@@ -58,7 +58,7 @@ export default function ObservationsTests() {
     // open action menu
     await page
       .locator('tr')
-      .filter({ hasText: 'In Progress' })
+      .filter({ hasText: 'Overdue' })
       .first()
       .getByLabel('More Options')
       .getByRole('button')
@@ -71,9 +71,12 @@ export default function ObservationsTests() {
     await expect(page.getByRole('button', { name: 'Cancel' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'Save' })).toBeVisible();
 
-    // update observation end date
+    // update observation start date (today)
+    await page.getByLabel('Observation Start Date').fill(new Date().toISOString().split('T')[0]);
+
+    // enter observation end date (today + 30 days)
     const futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + 45);
+    futureDate.setDate(futureDate.getDate() + 30);
     const futureDateFormatted = futureDate.toISOString().split('T')[0];
     await page.getByLabel('Observation End Date').fill(futureDateFormatted);
 
@@ -95,7 +98,7 @@ export default function ObservationsTests() {
     await waitFor(page, '#row1');
 
     // open action menu
-    await page.locator('tr').filter({ hasText: 'In Progress' }).first().locator('td button').click();
+    await page.locator('tr').filter({ hasText: 'Overdue' }).first().locator('td button').click();
 
     // click reschedule
     await page.getByRole('menuitem', { name: 'End Observation' }).click();
