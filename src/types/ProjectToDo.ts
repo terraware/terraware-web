5e584afa97a1cb0e227e8d8d3b5b86fceeb9a875
Isaac Hudson <isaac.hudson@terraformation.com>
2025-07-01 10:57:31 -0700

    Fix max-classes-per-file lint error (#4224)

--- src/types/ProjectToDo.ts
+++ src/types/ProjectToDo.ts
@@ -1,15 +1,6 @@
-import { isInTheFuture } from '@terraware/web-components/utils/date';
 import { DateTime } from 'luxon';
 
-import {
-  DeliverableCategoryType,
-  DeliverableStatusTypeWithOverdue,
-  DeliverableTypeType,
-  ListDeliverablesElementWithOverdueAndDueDate,
-} from './Deliverables';
 import { ModuleEventStatus, ModuleEventType, ModuleEventWithStartTime } from './Module';
-
-const ONE_DAY_IN_MILLIS = 1000 * 3600 * 24;
 
 export type ToDoBadge = 'Not Submitted' | 'Event' | 'In Review' | 'Not Accepted' | 'Overdue' | 'Completed';
 export type ToDoType = 'Deliverable' | ModuleEventType;
@@ -50,78 +41,6 @@ export interface ToDoItem {
    * returns the section of the to do list it should be in
    */
   getSection(): ToDoSection;
-}
-
-export class DeliverableToDoItem implements ToDoItem {
-  id: number;
-  moduleId: number;
-  moduleName: string;
-  projectId: number;
-  category: DeliverableCategoryType;
-  dueDate: DateTime;
-  name: string;
-  status: DeliverableStatusTypeWithOverdue;
-  type: DeliverableTypeType;
-
-  constructor(item: ListDeliverablesElementWithOverdueAndDueDate) {
-    this.id = item.id;
-    this.category = item.category;
-    this.dueDate = DateTime.fromISO(item.dueDate);
-    this.name = item.name;
-    this.moduleId = item.moduleId;
-    this.moduleName = item.moduleName;
-    this.projectId = item.projectId;
-    this.status = item.status ?? 'Not Submitted';
-    this.type = item.type;
-  }
-
-  isCompleted = (): boolean => this.status === 'Approved' || this.status === 'Not Needed';
-
-  getDate = (): DateTime => this.dueDate;
-
-  getDisplayDateTimeString = (): string => this.dueDate.toFormat('EEEE, LLLL dd yyyy');
-
-  getBadge = (): ToDoBadge => {
-    switch (this.status) {
-      case 'Approved':
-      case 'Completed':
-      case 'Not Needed':
-        return 'Completed';
-      case 'In Review':
-      case 'Needs Translation':
-        return 'In Review';
-      case 'Not Submitted':
-        if (Date.now() > this.dueDate.valueOf()) {
-          return 'Overdue';
-        } else {
-          return 'Not Submitted';
-        }
-      case 'Rejected':
-        if (Date.now() > this.dueDate.valueOf()) {
-          return 'Overdue';
-        } else {
-          return 'Not Accepted';
-        }
-      case 'Overdue':
-        return 'Overdue';
-    }
-  };
-
-  getTitle = (): string => this.name;
-
-  getType = (): ToDoType => 'Deliverable';
-
-  getSection = (): ToDoSection => {
-    if (this.isCompleted()) {
-      return 'Completed';
-    }
-
-    if (isInTheFuture(this.dueDate) && this.dueDate.diffNow().toMillis() > ONE_DAY_IN_MILLIS * 7) {
-      return 'Upcoming';
-    } else {
-      return 'To Do';
-    }
-  };
 }
 
 export class EventToDoItem implements ToDoItem {
