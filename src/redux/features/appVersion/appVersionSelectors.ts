d9dea692a54cc1d1dd8206fe2dc32a9ad72ce933
Josh Buchea <verctrl@joshbuchea.com>
2025-07-10 11:06:09 -1000

    revert add logs

--- src/redux/features/appVersion/appVersionSelectors.ts
+++ src/redux/features/appVersion/appVersionSelectors.ts
@@ -5,11 +5,8 @@ import { RootState } from 'src/redux/rootReducer';
 export const selectAppVersion = (state: RootState) => state.appVersion.version;
 const currentAppVersion = process.env.REACT_APP_TERRAWARE_FE_BUILD_VERSION;
 
-export const selectIsAppVersionStale = (state: RootState) => {
-  console.log('currentAppVersion:', currentAppVersion);
-  console.log('state.appVersion.version:', state.appVersion.version);
-  return !!state.appVersion.version && state.appVersion.version.toString().trim() !== currentAppVersion;
-};
+export const selectIsAppVersionStale = (state: RootState) =>
+  !!state.appVersion.version && state.appVersion.version.toString().trim() !== currentAppVersion;
 
 // simple example of composable selectors
 export const selectIsAppVersionStaleExample = createSelector(
