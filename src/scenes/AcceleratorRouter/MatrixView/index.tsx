4df9705a44bb23f6b516a2791485d000011a2dcf
constanzauanini <constanza.uanini@admios-sa.com>
2025-07-17 17:31:38 -0300

    SW-7170 Add new page and nav item (#4281)

---
+++ src/scenes/AcceleratorRouter/MatrixView/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Page from 'src/components/Page';
+import strings from 'src/strings';
+
+const MatrixView = () => {
+  return <Page title={strings.MATRIX_VIEW} />;
+};
+
+export default MatrixView;
