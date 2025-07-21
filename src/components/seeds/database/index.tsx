7b33eb96914c9db460037cffe5f06c6a7edfe542
constanzauanini <constanza.uanini@admios-sa.com>
2025-07-21 12:04:35 -0300

    Export

--- src/components/seeds/database/index.tsx
+++ src/components/seeds/database/index.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Link } from 'react-router';
 
-import { Box, CircularProgress, Container, Grid, useTheme } from '@mui/material';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import { Box, CircularProgress, Container, Grid, Button as MUIButton, useTheme } from '@mui/material';
 import { DropdownItem, Message } from '@terraware/web-components';
 import { DatabaseColumn } from '@terraware/web-components/components/table/types';
+import { download, generateCsv, mkConfig } from 'export-to-csv';
 import _ from 'lodash';
-import { MRT_ColumnDef, MaterialReactTable, useMaterialReactTable } from 'material-react-table';
+import { MRT_ColumnDef, MRT_Row, MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 
 import PageHeader from 'src/components/PageHeader';
 import ProjectAssignTopBarButton from 'src/components/ProjectAssignTopBarButton';
@@ -605,6 +607,23 @@ export default function Database(props: DatabaseProps): JSX.Element {
     void initAccessions();
   }, [initAccessions]);
 
+  const csvConfig = mkConfig({
+    fieldSeparator: ',',
+    decimalSeparator: '.',
+    useKeysAsHeaders: true,
+  });
+
+  const handleExportRows = (rows: MRT_Row<SearchResponseElementWithId>[]) => {
+    const rowData = rows.map((row) => row.original);
+    const csv = generateCsv(csvConfig)(rowData as any);
+    download(csvConfig)(csv);
+  };
+
+  const handleExportData = () => {
+    const csv = generateCsv(csvConfig)((searchResults as any) || []);
+    download(csvConfig)(csv);
+  };
+
   const columnsMRT = useMemo<MRT_ColumnDef<SearchResponseElementWithId>[]>(
     () => [
       {
@@ -689,6 +708,42 @@ export default function Database(props: DatabaseProps): JSX.Element {
     enableColumnPinning: true,
     enableEditing: true,
     editDisplayMode: 'cell',
+    renderTopToolbarCustomActions: ({ table }) => {
+      return (
+        <Box
+          sx={{
+            display: 'flex',
+            gap: '16px',
+            padding: '8px',
+            flexWrap: 'wrap',
+          }}
+        >
+          <MUIButton
+            //export all data that is currently in the table (ignore pagination, sorting, filtering, etc.)
+            onClick={handleExportData}
+            startIcon={<FileDownloadIcon />}
+          >
+            Export All Data
+          </MUIButton>
+          <MUIButton
+            disabled={table.getPrePaginationRowModel().rows.length === 0}
+            //export all rows, including from the next page, (still respects filtering and sorting)
+            onClick={() => handleExportRows(table.getPrePaginationRowModel().rows)}
+            startIcon={<FileDownloadIcon />}
+          >
+            Export All Rows
+          </MUIButton>
+          <MUIButton
+            disabled={table.getRowModel().rows.length === 0}
+            //export all rows as seen on the screen (respects pagination, sorting, filtering, etc.)
+            onClick={() => handleExportRows(table.getRowModel().rows)}
+            startIcon={<FileDownloadIcon />}
+          >
+            Export Page Rows
+          </MUIButton>
+        </Box>
+      );
+    },
   });
 
   return (
