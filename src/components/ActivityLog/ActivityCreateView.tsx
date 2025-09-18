a4a85df99530094c1dedb1f8aa8899dd960a588c
Josh Buchea <joshbuchea@users.noreply.github.com>
2025-09-18 09:30:27 -1000

    SW-7317: FE: Add Activity View (Part 1) (#4462)
    SW-7317: FE: Add Activity View (Part 1) (#4462)

--- src/components/ActivityLog/ActivityCreateView.tsx
+++ src/components/ActivityLog/ActivityCreateView.tsx
@@ -11,16 +11,15 @@ import { useSyncNavigate } from 'src/hooks/useSyncNavigate';
 import { useLocalization } from 'src/providers';
 import { useParticipantData } from 'src/providers/Participant/ParticipantContext';
 
+import ActivityDetailsForm from './ActivityDetailsForm';
 import MapSplitView from './MapSplitView';
 
 export default function ActivityCreateView(): JSX.Element {
   const { strings } = useLocalization();
   const theme = useTheme();
   const navigate = useSyncNavigate();
-
   const { currentParticipantProject } = useParticipantData();
   const pathParams = useParams<{ projectId: string }>();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const projectId = Number(pathParams.projectId);
 
   const activityLogLocation = useMemo(
@@ -43,7 +42,7 @@ export default function ActivityCreateView(): JSX.Element {
         saveButtonText={strings.SAVE}
         saveID='saveAddActivity'
       >
-        <Box marginBottom='32px' paddingLeft={theme.spacing(3)}>
+        <Box marginBottom='32px' marginTop='2px' paddingLeft={theme.spacing(4)}>
           <Typography fontSize='24px' fontWeight={600} lineHeight='32px' variant='h1'>
             {strings.formatString(strings.ADD_ACTIVITY_FOR_PROJECT, currentParticipantProject?.name || '...')}
           </Typography>
@@ -58,9 +57,11 @@ export default function ActivityCreateView(): JSX.Element {
           }}
         >
           <MapSplitView>
-            <Typography fontSize='20px' fontWeight='bold' variant='h2'>
+            <Typography fontSize='20px' fontWeight='bold' marginBottom='24px' variant='h2'>
               {strings.ADD_ACTIVITY}
             </Typography>
+
+            <ActivityDetailsForm projectId={projectId} />
           </MapSplitView>
         </Card>
       </PageForm>
--- src/types/Activity.ts
+++ src/types/Activity.ts
@@ -1,4 +1,5 @@
 import { components } from 'src/api/types/generated-schema';
+import defaultStrings from 'src/strings';
 
 export type Activity = Omit<
   components['schemas']['AdminActivityPayload'],
@@ -19,6 +20,40 @@ export type ActivityPayload = components['schemas']['ActivityPayload'];
 export type ActivityMediaFile = components['schemas']['ActivityMediaFilePayload'];
 export type AdminActivityPayload = components['schemas']['AdminActivityPayload'];
 export type AdminActivityMediaFile = components['schemas']['AdminActivityMediaFilePayload'];
+export type CreateActivityRequestPayload = components['schemas']['CreateActivityRequestPayload'];
+export type UpdateActivityRequestPayload = components['schemas']['UpdateActivityRequestPayload'];
+
+export type ActivityType = Activity['type'];
+export const ACTIVITY_TYPES: ActivityType[] = [
+  'Drone Flight',
+  'Monitoring',
+  'Nursery',
+  'Planting',
+  'Seed Collection',
+  'Site Visit',
+  'Stakeholder Engagement',
+];
+
+export const activityTypeLabel = (activityType: ActivityType, strings: typeof defaultStrings) => {
+  switch (activityType) {
+    case 'Drone Flight':
+      return strings.DRONE_FLIGHT;
+    case 'Monitoring':
+      return strings.MONITORING;
+    case 'Nursery':
+      return strings.NURSERY;
+    case 'Planting':
+      return strings.PLANTING;
+    case 'Seed Collection':
+      return strings.SEED_COLLECTION;
+    case 'Site Visit':
+      return strings.SITE_VISIT;
+    case 'Stakeholder Engagement':
+      return strings.STAKEHOLDER_ENGAGEMENT;
+    default:
+      return '';
+  }
+};
 
 export type MockActivityStatus = 'Changed' | 'Do Not Use' | 'Not Verified' | 'Published' | 'Verified';
 
