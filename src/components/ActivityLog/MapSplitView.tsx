f4c9d1cbd934e3fecba96393dfcfcc5645d8b70d
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-09-25 12:11:50 -0700

    Added markers on the map list view

--- src/components/ActivityLog/MapSplitView.tsx
+++ src/components/ActivityLog/MapSplitView.tsx
@@ -1,17 +1,75 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { Box } from '@mui/material';
 
-import MapComponent from 'src/components/NewMap';
+import MapComponent, { MapFeatureSection } from 'src/components/NewMap';
+import { MapMarker, MapMarkerGroup } from 'src/components/NewMap/types';
+import { Activity, activityTypeColor } from 'src/types/Activity';
 import useMapboxToken from 'src/utils/useMapboxToken';
 
 type MapSplitViewProps = {
+  activities?: Activity[];
   children: React.ReactNode;
+  focusedActivityId?: number;
+  setFocusedActivityId?: (id: number) => void;
   topComponent?: React.ReactNode;
 };
 
-export default function MapSplitView({ children, topComponent }: MapSplitViewProps): JSX.Element {
+export default function MapSplitView({
+  activities,
+  children,
+  focusedActivityId,
+  setFocusedActivityId,
+  topComponent,
+}: MapSplitViewProps): JSX.Element {
   const { token, mapId } = useMapboxToken();
+
+  const onActivityMarkerClick = useCallback((id: number) => () => setFocusedActivityId?.(id), [setFocusedActivityId]);
+
+  const markerGroups = useMemo((): MapMarkerGroup[] => {
+    if (!activities) {
+      return [];
+    }
+
+    return activities.map((activity): MapMarkerGroup => {
+      const markers = activity.media
+        .map((media): MapMarker | undefined => {
+          if (media.geolocation) {
+            return {
+              id: `activity-${activity.id}-media-${media.fileId}`,
+              longitude: media.geolocation.coordinates[1],
+              latitude: media.geolocation.coordinates[0],
+              onClick: onActivityMarkerClick(activity.id),
+              selected: focusedActivityId === activity.id,
+            };
+          } else {
+            return undefined;
+          }
+        }, [])
+        .filter((marker): marker is MapMarker => marker !== undefined);
+
+      return {
+        label: `Activity ${activity.id}`,
+        markers,
+        markerGroupId: `activity-${activity.id}`,
+        style: {
+          iconColor: activityTypeColor(activity.type),
+          iconName: 'iconPhoto',
+          type: 'icon',
+        },
+      };
+    });
+  }, [activities, focusedActivityId, onActivityMarkerClick]);
+
+  const mapFeatures = useMemo((): MapFeatureSection[] => {
+    return [
+      {
+        sectionTitle: '',
+        groups: markerGroups,
+        type: 'marker',
+      },
+    ];
+  }, [markerGroups]);
 
   return (
     <Box display='flex' flexDirection='column' flexGrow={1}>
@@ -22,6 +80,7 @@ export default function MapSplitView({ children, topComponent }: MapSplitViewPro
         drawerHideHeader
         drawerOpen
         drawerSize='large'
+        features={mapFeatures}
         mapId={mapId}
         token={token ?? ''}
       />
