cee59b23a4746619ecf7bb4d1ab8f6163e6bcc7c
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-09-29 15:26:51 -0700

    Updated props for map split view and make markers visible (#4514)
    New props enable more flexible uses for edit view and details view.

--- src/components/ActivityLog/ActivitiesListView.tsx
+++ src/components/ActivityLog/ActivitiesListView.tsx
@@ -212,12 +212,25 @@ const ActivitiesListView = ({ projectId }: ActivitiesListViewProps): JSX.Element
     }));
   }, [activities, strings]);
 
+  const activityMarkerHighlighted = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (activityId: number, fileId: number) => {
+      return focusedActivityId === activityId;
+    },
+    [focusedActivityId]
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const onActivityMarkerClick = useCallback((activityId: number, fileId: number) => {
+    setFocusedActivityId((prevValue) => (prevValue === activityId ? undefined : activityId));
+  }, []);
+
   return (
     <MapSplitView
-      activities={activities}
-      focusedActivityId={focusedActivityId}
+      activities={activities} // TODO: Use visible activites after pagination/filtering
+      activityMarkerHighlighted={activityMarkerHighlighted}
+      onActivityMarkerClick={onActivityMarkerClick}
       projectId={projectId}
-      setFocusedActivityId={setFocusedActivityId}
     >
       <DateRange
         field='dateRange'
