77423edf1c61056533ece5622fd59f34c9159241
Josh Buchea <verctrl@joshbuchea.com>
2025-10-03 17:58:38 -1000

    revert import update

--- src/components/ActivityLog/ActivitiesListView.tsx
+++ src/components/ActivityLog/ActivitiesListView.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router';
 
 import { Box, Grid, Typography, useTheme } from '@mui/material';
 
