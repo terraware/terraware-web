0bdfa58b2342052aed0146c9420c5951b9d5c9b6
Josh Buchea <verctrl@joshbuchea.com>
2025-10-07 13:01:51 -1000

    refactor activity badges, show activity badges in edit view (console)

---
+++ src/components/ActivityLog/ActivityStatusBadges.tsx
@@ -0,0 +1,32 @@
+import React, { useMemo } from 'react';
+
+import { Box, useTheme } from '@mui/material';
+
+import { Activity } from 'src/types/Activity';
+
+import ActivityStatusBadge from './ActivityStatusBadge';
+
+type ActivityStatusBadgesProps = {
+  activity: Activity;
+};
+
+const ActivityStatusBadges = ({ activity }: ActivityStatusBadgesProps): JSX.Element => {
+  const theme = useTheme();
+
+  const isChanged = useMemo(() => {
+    return (
+      activity.modifiedTime && activity.createdTime && new Date(activity.modifiedTime) > new Date(activity.createdTime)
+    );
+  }, [activity.modifiedTime, activity.createdTime]);
+
+  return (
+    <Box alignItems='center' display='flex' flexDirection='row' flexWrap='wrap' gap={1} marginY={theme.spacing(1)}>
+      {isChanged && <ActivityStatusBadge status='Changed' />}
+      <ActivityStatusBadge status={activity.isVerified ? 'Verified' : 'Not Verified'} />
+      {/* TODO: render badge for 'Do Not Use' when applicable */}
+      {/* TODO: render badge for 'Published' when applicable */}
+    </Box>
+  );
+};
+
+export default ActivityStatusBadges;
