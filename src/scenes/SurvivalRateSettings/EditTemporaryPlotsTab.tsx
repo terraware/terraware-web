60fbcf3bb5a9cdbf0f7e73c1b0966a5fc2377356
constanzauanini <constanza.uanini@admios-sa.com>
2025-10-14 10:15:46 -0300

    Add tabs for edit view

---
+++ src/scenes/SurvivalRateSettings/EditTemporaryPlotsTab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+
+import strings from 'src/strings';
+
+const EditPermanentPlotsTab = () => {
+  return <Box>{strings.TEMPORARY_PLOTS}</Box>;
+};
+
+export default EditPermanentPlotsTab;
