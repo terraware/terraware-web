05551f1de22203864c63b324bced0c742369e276
Isaac Hudson <isaac.hudson@terraformation.com>
2025-10-20 13:29:02 -0700

    Fix prettier

--- src/scenes/NurseryRouter/NurseryWithdrawalsTabContent.tsx
+++ src/scenes/NurseryRouter/NurseryWithdrawalsTabContent.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { Typography, useTheme } from '@mui/material';
 import { IconTooltip } from '@terraware/web-components';
 
-
 import Card from 'src/components/common/Card';
 import strings from 'src/strings';
 
