048a14de1fee8da77f44a60c21216e71e9b058a2
Josh Buchea <verctrl@joshbuchea.com>
2025-10-22 07:59:56 -1000

    auto-play video

--- src/components/ActivityLog/ActivityDetailView.tsx
+++ src/components/ActivityLog/ActivityDetailView.tsx
@@ -442,6 +442,7 @@ const ActivityDetailView = ({
           mediaStream ? (
             <MuxPlayer
               accentColor={theme.palette.TwClrBgBrand}
+              autoPlay
               playbackId={mediaStream.playbackId}
               playbackToken={mediaStream.playbackToken}
               style={{
