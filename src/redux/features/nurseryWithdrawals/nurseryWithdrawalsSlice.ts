f3ec4c57c20bd1f55221a4755998e0ee84acff8c
Isaac Hudson <isaac.hudson@terraformation.com>
2025-10-29 13:51:15 -0700

    Update Withdrawal History table to use redux

--- src/redux/features/nurseryWithdrawals/nurseryWithdrawalsSlice.ts
+++ src/redux/features/nurseryWithdrawals/nurseryWithdrawalsSlice.ts
@@ -1,7 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { StatusT, buildReducers } from '../asyncUtils';
-import { SpeciesPlot, requestPlantingSiteWithdrawnSpecies } from './nurseryWithdrawalsThunks';
+import { StatusT, buildReducers } from 'src/redux/features/asyncUtils';
+import { NurseryWithdrawalsSearchResponseElement } from 'src/services/NurseryWithdrawalService';
+import { FieldOptionsMap } from 'src/types/Search';
+
+import {
+  SpeciesPlot,
+  requestCountNurseryWithdrawals,
+  requestListNurseryWithdrawals,
+  requestNurseryWithdrawalsFilterOptions,
+  requestPlantingSiteWithdrawnSpecies,
+} from './nurseryWithdrawalsThunks';
 
 const initialStatePlantingSiteWithdrawnSpecies: { [key: string]: StatusT<SpeciesPlot[]> } = {};
 
@@ -14,8 +23,44 @@ export const plantingSiteWithdrawnSpeciesSlice = createSlice({
   },
 });
 
+const initialStateNurseryWithdrawalsList: { [key: string]: StatusT<NurseryWithdrawalsSearchResponseElement[]> } = {};
+
+export const nurseryWithdrawalsListSlice = createSlice({
+  name: 'nurseryWithdrawalsListSlice',
+  initialState: initialStateNurseryWithdrawalsList,
+  reducers: {},
+  extraReducers: (builder) => {
+    buildReducers(requestListNurseryWithdrawals)(builder);
+  },
+});
+
+const initialStateNurseryWithdrawalsFilterOptions: { [key: string]: StatusT<FieldOptionsMap> } = {};
+
+export const nurseryWithdrawalsFilterOptionsSlice = createSlice({
+  name: 'nurseryWithdrawalsFilterOptionsSlice',
+  initialState: initialStateNurseryWithdrawalsFilterOptions,
+  reducers: {},
+  extraReducers: (builder) => {
+    buildReducers(requestNurseryWithdrawalsFilterOptions)(builder);
+  },
+});
+
+const initialStateNurseryWithdrawalsCount: { [key: string]: StatusT<number> } = {};
+
+export const nurseryWithdrawalsCountSlice = createSlice({
+  name: 'nurseryWithdrawalsCountSlice',
+  initialState: initialStateNurseryWithdrawalsCount,
+  reducers: {},
+  extraReducers: (builder) => {
+    buildReducers(requestCountNurseryWithdrawals)(builder);
+  },
+});
+
 const nurseryWithdrawalsReducers = {
   plantingSiteWithdrawnSpecies: plantingSiteWithdrawnSpeciesSlice.reducer,
+  nurseryWithdrawalsList: nurseryWithdrawalsListSlice.reducer,
+  nurseryWithdrawalsFilterOptions: nurseryWithdrawalsFilterOptionsSlice.reducer,
+  nurseryWithdrawalsCount: nurseryWithdrawalsCountSlice.reducer,
 };
 
 export default nurseryWithdrawalsReducers;
--- src/redux/features/nurseryWithdrawals/nurseryWithdrawalsThunks.ts
+++ src/redux/features/nurseryWithdrawals/nurseryWithdrawalsThunks.ts
@@ -1,7 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { NurseryWithdrawalService } from 'src/services';
+import { NurseryWithdrawalsCountParams, NurseryWithdrawalsSearchParams } from 'src/services/NurseryWithdrawalService';
 import strings from 'src/strings';
+import { FieldOptionsMap, SearchResponseElement } from 'src/types/Search';
 
 export type SpeciesPlot = {
   monitoringPlotId: number;
@@ -18,6 +20,60 @@ export const requestPlantingSiteWithdrawnSpecies = createAsyncThunk(
 
     if (response) {
       return response.data?.plots;
+    }
+
+    return rejectWithValue(strings.GENERIC_ERROR);
+  }
+);
+
+export const requestListNurseryWithdrawals = createAsyncThunk(
+  'nurseryWithdrawals/list',
+  async (request: NurseryWithdrawalsSearchParams, { rejectWithValue }) => {
+    const { organizationId, searchCriteria, sortOrder, limit, offset } = request;
+
+    const response: SearchResponseElement[] | null = await NurseryWithdrawalService.listNurseryWithdrawals(
+      organizationId,
+      searchCriteria,
+      sortOrder,
+      limit,
+      offset
+    );
+
+    if (response) {
+      return response;
+    }
+
+    return rejectWithValue(strings.GENERIC_ERROR);
+  }
+);
+
+export const requestNurseryWithdrawalsFilterOptions = createAsyncThunk(
+  'nurseryWithdrawals/filterOptions',
+  async (request: { organizationId: number }, { rejectWithValue }) => {
+    const { organizationId } = request;
+
+    const response: FieldOptionsMap | null = await NurseryWithdrawalService.getFilterOptions(organizationId);
+
+    if (response) {
+      return response;
+    }
+
+    return rejectWithValue(strings.GENERIC_ERROR);
+  }
+);
+
+export const requestCountNurseryWithdrawals = createAsyncThunk(
+  'nurseryWithdrawals/count',
+  async (request: NurseryWithdrawalsCountParams, { rejectWithValue }) => {
+    const { organizationId, searchCriteria } = request;
+
+    const response: number | null = await NurseryWithdrawalService.countNurseryWithdrawals(
+      organizationId,
+      searchCriteria
+    );
+
+    if (response !== null) {
+      return response;
     }
 
     return rejectWithValue(strings.GENERIC_ERROR);
