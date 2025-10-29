7558e1291e02e1c1a02ef1d16fbf7df55da00d18
Josh Buchea <verctrl@joshbuchea.com>
2025-10-28 14:03:29 -1000

    group highlighted activities, render dropdown to select quarter

--- src/components/ActivityLog/ActivityHighlightsModal.tsx
+++ src/components/ActivityLog/ActivityHighlightsModal.tsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 
 import { Box, IconButton, Typography } from '@mui/material';
-import { Icon } from '@terraware/web-components';
+import { Dropdown, Icon } from '@terraware/web-components';
 import DialogBox, { DialogBoxSize } from '@terraware/web-components/components/DialogBox/DialogBox';
 import { useDeviceInfo } from '@terraware/web-components/utils';
 
+import { useLocalization } from 'src/providers';
 import { Activity } from 'src/types/Activity';
+import { groupActivitiesByQuarter } from 'src/utils/activityUtils';
 
 import ActivityHighlightsView from './ActivityHighlightsView';
 
@@ -36,7 +38,31 @@ const ActivityHighlightsModal = ({
   projectId,
   title = '',
 }: ActivityHighlightsModalProps) => {
+  const { activeLocale, strings } = useLocalization();
   const { isMobile, isTablet } = useDeviceInfo();
+
+  const [selectedQuarter, setSelectedQuarter] = useState<string | number | undefined>(undefined);
+
+  const highlightedActivities = useMemo(() => activities.filter((activity) => activity.isHighlight), [activities]);
+
+  const groupedActivities = useMemo(
+    () => groupActivitiesByQuarter(highlightedActivities, strings),
+    [highlightedActivities, strings]
+  );
+  console.log('groupedActivities:', groupedActivities);
+
+  const selectedGroup = useMemo(() => {
+    return groupedActivities.find((group) => group.quarter === selectedQuarter);
+  }, [groupedActivities, selectedQuarter]);
+
+  const dropdownOptions = useMemo(() => {
+    return groupedActivities
+      .map((group) => ({
+        label: group.quarter,
+        value: group.quarter,
+      }))
+      .sort((a, b) => b.label.localeCompare(a.label, activeLocale || undefined));
+  }, [activeLocale, groupedActivities]);
 
   const onClose = useCallback(() => {
     onCancel?.();
@@ -53,23 +79,38 @@ const ActivityHighlightsModal = ({
     }
   }, [isMobile, isTablet]);
 
+  const onChangeActivityQuarter = useCallback((value: string | number | undefined) => {
+    setSelectedQuarter(value);
+  }, []);
+
+  useEffect(() => {
+    if (open && dropdownOptions.length > 0 && !selectedQuarter) {
+      setSelectedQuarter(dropdownOptions[0].value);
+    }
+  }, [open, dropdownOptions, selectedQuarter]);
+
   return (
     <Box sx={containerStyles}>
       <DialogBox onClose={onClose} open={open} size={dialogSize} skrim title={title}>
         <Box
-          alignItems='center'
+          alignItems='flex-start'
           display='flex'
           flexDirection='row'
           justifyContent='space-between'
           marginBottom='24px'
           width='100%'
         >
-          <Box alignItems='center' display='flex' flexDirection='row' justifyContent='flex-start'>
-            <Typography fontSize='24px' fontWeight={600} paddingRight='24px'>
+          <Box display='flex' sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+            <Typography fontSize='24px' fontWeight={600} paddingRight='24px' sx={{ whiteSpace: 'nowrap' }}>
               {title}
             </Typography>
 
-            <Typography>TODO: render quarter dropdown</Typography>
+            <Dropdown
+              label=''
+              onChange={onChangeActivityQuarter}
+              options={dropdownOptions}
+              selectedValue={selectedQuarter}
+            />
           </Box>
 
           <IconButton onClick={onClose}>
@@ -77,7 +118,7 @@ const ActivityHighlightsModal = ({
           </IconButton>
         </Box>
 
-        <ActivityHighlightsView activities={activities} projectId={projectId} />
+        <ActivityHighlightsView activities={selectedGroup?.activities || []} projectId={projectId} />
       </DialogBox>
     </Box>
   );
