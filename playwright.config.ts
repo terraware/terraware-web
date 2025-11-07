27daef40836a3b0270bd23b49c272c59f75dded5
Isaac Hudson <isaac.hudson@terraformation.com>
2025-11-07 13:13:33 -0800

    Test stability with 1 worker

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,9 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests. */
-  workers: process.env.CI ? 2 : 4,
+  // workers: process.env.CI ? 2 : undefined,
+  // workers: process.env.CI ? 2 : 1,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -32,7 +34,7 @@ export default defineConfig({
       slowMo: parseInt(process.env.SLOW_MO || '0'),
     },
   },
-  timeout: 60000,
+  timeout: process.env.CI ? 60000 : parseInt(process.env.TIMEOUT || '15000'),
 
   /* Configure projects for major browsers */
   projects: process.env.CI
