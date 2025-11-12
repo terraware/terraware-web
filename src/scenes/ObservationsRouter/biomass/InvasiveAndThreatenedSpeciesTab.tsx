53ca9c16ddf1df7ed91a54c5360ea7add4bfd189
constanzauanini <constanza.uanini@admios-sa.com>
2025-11-12 09:32:51 -0300

    SW-7702 Biomass Invasive & Threatened Species Tab

--- src/scenes/ObservationsRouter/biomass/InvasiveAndThreatenedSpeciesTab.tsx
+++ src/scenes/ObservationsRouter/biomass/InvasiveAndThreatenedSpeciesTab.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import { Typography, useTheme } from '@mui/material';
+
 import Card from 'src/components/common/Card';
+import strings from 'src/strings';
 
 const InvasiveAndThreatenedSpeciesTab = () => {
-  return <Card radius='24px' />;
+  const theme = useTheme();
+  return (
+    <Card radius='24px'>
+      <Typography fontSize='20px' lineHeight='28px' fontWeight={600} color={theme.palette.TwClrTxt}>
+        {strings.PHOTO_NORTHEAST_QUADRAT}
+      </Typography>
+    </Card>
+  );
 };
 
 export default InvasiveAndThreatenedSpeciesTab;
