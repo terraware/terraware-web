0ec04bd351c333de6ef21e2c8c35e7d93c9bfcbc
constanzauanini <constanza.uanini@admios-sa.com>
2025-11-19 13:47:09 -0300

    SW-7702 Biomass Invasive & Threatened Species Tab (#4806)

--- src/scenes/ObservationsRouter/biomass/InvasiveAndThreatenedSpeciesTab.tsx
+++ src/scenes/ObservationsRouter/biomass/InvasiveAndThreatenedSpeciesTab.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 
 import Card from 'src/components/common/Card';
+import QuadratComponent from 'src/scenes/ObservationsRouter/biomass/QuadratComponent';
+import { ObservationMonitoringPlotResultsPayload } from 'src/types/Observations';
 
-const InvasiveAndThreatenedSpeciesTab = () => {
-  return <Card radius='24px' />;
+type InvasiveAndThreatenedSpeciesTabProps = {
+  monitoringPlot: ObservationMonitoringPlotResultsPayload | undefined;
+};
+
+const InvasiveAndThreatenedSpeciesTab = ({ monitoringPlot }: InvasiveAndThreatenedSpeciesTabProps) => {
+  return (
+    <Card radius='24px'>
+      <QuadratComponent quadrat='Northwest' monitoringPlot={monitoringPlot} />
+      <QuadratComponent quadrat='Northeast' monitoringPlot={monitoringPlot} />
+      <QuadratComponent quadrat='Southwest' monitoringPlot={monitoringPlot} />
+      <QuadratComponent quadrat='Southeast' monitoringPlot={monitoringPlot} />
+    </Card>
+  );
 };
 
 export default InvasiveAndThreatenedSpeciesTab;
