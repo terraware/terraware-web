f2680fe69b6c13e6ff612e86774e49db96fd4fb4
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-11-18 16:14:57 -0800

    Show project updated status when it is not verified but previously verified

--- src/components/ActivityLog/ActivityStatusBadges.tsx
+++ src/components/ActivityLog/ActivityStatusBadges.tsx
@@ -2,6 +2,8 @@ import React, { useMemo } from 'react';
 
 import { Box, useTheme } from '@mui/material';
 
+import { ActivityStatusTag } from 'src/types/Activity';
+
 import ActivityStatusBadge from './ActivityStatusBadge';
 import { TypedActivity } from './types';
 
@@ -11,17 +13,6 @@ type ActivityStatusBadgesProps = {
 
 const ActivityStatusBadges = ({ activity }: ActivityStatusBadgesProps): JSX.Element => {
   const theme = useTheme();
-
-  const isChangedAfterVerified = useMemo(() => {
-    if (activity.type === 'funder' || activity.type === 'base') {
-      return false;
-    }
-    return (
-      activity.payload.modifiedTime &&
-      activity.payload.verifiedTime &&
-      new Date(activity.payload.modifiedTime) > new Date(activity.payload.verifiedTime)
-    );
-  }, [activity]);
 
   const isChangedAfterPublished = useMemo(() => {
     if (activity.type === 'funder' || activity.type === 'base') {
@@ -42,11 +33,19 @@ const ActivityStatusBadges = ({ activity }: ActivityStatusBadgesProps): JSX.Elem
     return activity.payload.publishedTime;
   }, [activity]);
 
+  const status = useMemo((): ActivityStatusTag | undefined => {
+    if (activity.type === 'admin') {
+      if (activity.payload.status === 'Not Verified' && activity.payload.verifiedTime) {
+        return 'Project Updated';
+      }
+      return activity.payload.status;
+    }
+  }, [activity]);
+
   return (
     <Box alignItems='center' display='flex' flexDirection='row' flexWrap='wrap' gap={1} marginY={theme.spacing(1)}>
       {isChangedAfterPublished && <ActivityStatusBadge status='Unpublished Changes' />}
-      {activity.type === 'admin' && <ActivityStatusBadge status={activity.payload.status} />}
-      {isChangedAfterVerified && <ActivityStatusBadge status='Project Updated' />}
+      {status && <ActivityStatusBadge status={status} />}
       {/* TODO: render badge for 'Do Not Use' when applicable */}
       {isPublished && <ActivityStatusBadge status='Published' />}
     </Box>
