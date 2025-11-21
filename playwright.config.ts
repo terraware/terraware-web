5cbab331869400648e9ac81b487f71e37ffff093
Isaac Hudson <isaac.hudson@terraformation.com>
2025-11-21 14:02:29 -0800

    Update workers for local only

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,9 +19,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests. */
-  // workers: process.env.CI ? 2 : undefined,
-  // workers: process.env.CI ? 2 : 1,
-  workers: 1,
+  workers: process.env.CI ? 1 : 4,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
--- playwright/e2e/utils/utils.ts
+++ playwright/e2e/utils/utils.ts
@@ -3,7 +3,7 @@ import { Page } from 'playwright-core';
 export const waitFor = async (page: Page, selector: string, timeout = 3000) => {
   // Seems weird to await in here with nothing else going on, but I am doing that explicitly so I can
   // ditch the return and keep the signature Promise<void>
-  await page.waitForSelector(selector, { timeout });
+  await page.locator(selector).waitFor({ state: 'visible', timeout });
 };
 
 // Just a typing saver for various usages
