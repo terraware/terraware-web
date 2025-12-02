f93adf0b6561ad6598254ecd778c6a028b831702
constanzauanini <constanza.uanini@admios-sa.com>
2025-12-02 13:34:51 -0300

    Fix typo

--- src/scenes/ObservationsRouter/biomass/EditNotesModal.tsx
+++ src/scenes/ObservationsRouter/biomass/EditNotesModal.tsx
@@ -33,52 +33,55 @@ const EditNotesModal = ({ onClose, observationId, monitoringPlotId }: EditNotesM
   const biomassMeasurements = observationResults?.biomassMeasurements;
   const [record, setRecord] = useForm(biomassMeasurements);
 
-  const onSubmit = useCallback(async () => {
-    const notheastQuadratPayload: QuadratUpdateOperationPayload = {
-      type: 'Quadrat',
-      position: 'NortheastCorner',
-      description: record?.quadrats.find((q) => q.position === 'NortheastCorner')?.description,
-    };
+  const onSubmit = useCallback(() => {
+    void (async () => {
+      const northeastQuadratPayload: QuadratUpdateOperationPayload = {
+        type: 'Quadrat',
+        position: 'NortheastCorner',
+        description: record?.quadrats.find((q) => q.position === 'NortheastCorner')?.description,
+      };
 
-    const northwestQuadratPayload: QuadratUpdateOperationPayload = {
-      type: 'Quadrat',
-      position: 'NorthwestCorner',
-      description: record?.quadrats.find((q) => q.position === 'NorthwestCorner')?.description,
-    };
+      const northwestQuadratPayload: QuadratUpdateOperationPayload = {
+        type: 'Quadrat',
+        position: 'NorthwestCorner',
+        description: record?.quadrats.find((q) => q.position === 'NorthwestCorner')?.description,
+      };
 
-    const southeastQuadratPayload: QuadratUpdateOperationPayload = {
-      type: 'Quadrat',
-      position: 'SoutheastCorner',
-      description: record?.quadrats.find((q) => q.position === 'SoutheastCorner')?.description,
-    };
+      const southeastQuadratPayload: QuadratUpdateOperationPayload = {
+        type: 'Quadrat',
+        position: 'SoutheastCorner',
+        description: record?.quadrats.find((q) => q.position === 'SoutheastCorner')?.description,
+      };
 
-    const southwestQuadratPayload: QuadratUpdateOperationPayload = {
-      type: 'Quadrat',
-      position: 'SouthwestCorner',
-      description: record?.quadrats.find((q) => q.position === 'SouthwestCorner')?.description,
-    };
+      const southwestQuadratPayload: QuadratUpdateOperationPayload = {
+        type: 'Quadrat',
+        position: 'SouthwestCorner',
+        description: record?.quadrats.find((q) => q.position === 'SouthwestCorner')?.description,
+      };
 
-    if (monitoringPlotId) {
-      const payloads: QuadratUpdateOperationPayload[] = [
-        notheastQuadratPayload,
-        northwestQuadratPayload,
-        southeastQuadratPayload,
-        southwestQuadratPayload,
-      ];
+      if (monitoringPlotId) {
+        const payloads: QuadratUpdateOperationPayload[] = [
+          northeastQuadratPayload,
+          northwestQuadratPayload,
+          southeastQuadratPayload,
+          southwestQuadratPayload,
+        ];
 
-      const result = await update({
-        observationId,
-        plotId: monitoringPlotId,
-        updateObservationRequestPayload: {
-          updates: payloads,
-        },
-      });
+        const result = await update({
+          observationId,
+          plotId: monitoringPlotId,
+          updateObservationRequestPayload: {
+            updates: payloads,
+          },
+        });
 
-      if ('error' in result) {
-        snackbar.toastError();
+        if ('error' in result) {
+          snackbar.toastError();
+          return;
+        }
       }
-    }
-    onClose();
+      onClose();
+    })();
   }, [monitoringPlotId, observationId, onClose, record?.quadrats, update, snackbar]);
 
   return (
