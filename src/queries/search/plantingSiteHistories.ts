3b394c422dd1178bb690fb469d11cbbc518936ba
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2025-12-03 00:25:19 -0800

    Added search planting site history IDs

---
+++ src/queries/search/plantingSiteHistories.ts
@@ -0,0 +1,54 @@
+import { baseApi as api } from '../baseApi';
+import { QueryTagTypes } from '../tags';
+
+type PlantingSiteHistoryIdResult = {
+  createdTime: string;
+  id: string;
+};
+
+type ListPlantingSiteHistoryIdsApiResponse = {
+  results: PlantingSiteHistoryIdResult[];
+};
+
+const injectedRtkApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    listPlantingSiteHistoryIds: build.query<PlantingSiteHistoryIds[], number>({
+      query: (plantingSiteId) => ({
+        url: '/api/v1/search',
+        method: 'POST',
+        body: {
+          prefix: 'plantingSiteHistories',
+          fields: ['createdTime', 'id'],
+          search: {
+            operation: 'field',
+            field: 'plantingSite.id',
+            values: [`${plantingSiteId}`],
+          },
+          sortOrder: [
+            {
+              direction: 'Descending',
+              field: 'createdTime',
+            },
+          ],
+        },
+      }),
+      providesTags: (_result, _error, plantingSiteId) => [{ type: QueryTagTypes.PlantingSites, id: plantingSiteId }],
+      transformResponse: (results: ListPlantingSiteHistoryIdsApiResponse, _meta, plantingSiteId) =>
+        results.results.map((result) => ({
+          createdTime: result.createdTime,
+          plantingSiteHistoryId: Number(result.id),
+          plantingSiteId,
+        })),
+    }),
+  }),
+});
+
+export type PlantingSiteHistoryIds = {
+  createdTime: string;
+  plantingSiteHistoryId: number;
+  plantingSiteId: number;
+};
+
+export { injectedRtkApi as api };
+
+export const { useListPlantingSiteHistoryIdsQuery, useLazyListPlantingSiteHistoryIdsQuery } = injectedRtkApi;
