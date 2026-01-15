9a230b0fbcc21bae8b03f3bd9f6404ec1e220f02
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2026-01-14 21:51:34 -0800

    count tree species instead of monitoring plot species for biomass

--- src/utils/observation.ts
+++ src/utils/observation.ts
@@ -1,3 +1,4 @@
+import { ExistingBiomassMeasurementPayload } from 'src/queries/generated/observations';
 import { ObservationSpeciesResults, ObservationSpeciesResultsPayload } from 'src/types/Observations';
 
 export const getObservationSpeciesLivePlantsCount = (
@@ -10,4 +11,22 @@ export const getObservationSpeciesDeadPlantsCount = (
   species: ObservationSpeciesResults[] | ObservationSpeciesResultsPayload[] | undefined
 ): number | undefined => {
   return species?.reduce((sum, s) => sum + (s.totalDead || 0), 0);
+};
+
+export const getBiomassObservationLiveTreeCount = (
+  biomassMeasurement: ExistingBiomassMeasurementPayload | undefined
+) => {
+  if (!biomassMeasurement) {
+    return 0;
+  }
+  return biomassMeasurement.trees.filter((tree) => !tree.isDead).length;
+};
+
+export const getBiomassObservationDeadTreeCount = (
+  biomassMeasurement: ExistingBiomassMeasurementPayload | undefined
+) => {
+  if (!biomassMeasurement) {
+    return 0;
+  }
+  return biomassMeasurement.trees.filter((tree) => tree.isDead).length;
 };
