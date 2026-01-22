7bebcb219c83e221ea61f625b5c791d7a3e59da5
Isaac Hudson <isaac.hudson@terraformation.com>
2026-01-22 11:17:21 -0800

    Change to hook instead of component

---
+++ src/hooks/useCameraPosition.ts
@@ -0,0 +1,22 @@
+import { useCallback, useMemo } from 'react';
+
+import { useApp } from '@playcanvas/react/hooks';
+import { Vec3 } from 'playcanvas';
+
+export const useCameraPosition = () => {
+  const app = useApp();
+
+  const setCamera = useCallback(
+    (focus: [number, number, number], position?: [number, number, number]) => {
+      const camera = app.root.findByName('camera');
+      // @ts-expect-error - cameraControls and its script are added dynamically to the camera entity
+      const controls = camera?.script?.cameraControls;
+      if (controls && camera) {
+        controls.reset(new Vec3(focus), position ? new Vec3(position) : camera.getPosition());
+      }
+    },
+    [app]
+  );
+
+  return useMemo(() => ({ setCamera }), [setCamera]);
+};
