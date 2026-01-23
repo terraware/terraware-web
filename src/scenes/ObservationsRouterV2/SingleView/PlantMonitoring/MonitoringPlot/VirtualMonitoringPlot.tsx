e6cf6c9ebc87474239c7a2452261cf3d4b1252a7
Isaac Hudson <isaac.hudson@terraformation.com>
2026-01-23 14:39:59 -0800

    SW-7959 Initial Viewer for Virtual Monitoring Plots (#5084)
    Implement start of viewer for virtual monitoring plots. For now, hardcode the ply model to one that's local in the `public/assets` folder.

---
+++ src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/MonitoringPlot/VirtualMonitoringPlot.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { Box } from '@mui/material';
+import { Application, Entity } from '@playcanvas/react';
+import { Camera, Script } from '@playcanvas/react/components';
+import { CameraControls } from 'playcanvas/scripts/esm/camera-controls.mjs';
+
+import SplatAutoRotate from 'src/components/GaussianSplat/SplatAutoRotate';
+import SplatModel from 'src/components/GaussianSplat/SplatModel';
+
+const VirtualMonitoringPlot = () => {
+  // http://localhost:3000/observations/358/stratum/2/plot/40321018/virtual?organizationId=243
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        height: 'calc(100vh - 96px)',
+        position: 'relative',
+      }}
+    >
+      <Application
+        style={{
+          width: '80%',
+          height: '80%',
+          display: 'block',
+          margin: '0 auto',
+        }}
+        graphicsDeviceOptions={{ antialias: false }}
+      >
+        <Entity name='camera'>
+          <Camera clearColor='#1a1a1a' fov={60} />
+          <Script script={CameraControls} />
+        </Entity>
+
+        <SplatModel
+          // splatSrc={'/assets/models/test/PlyExamples/trees3.sog'}
+          splatSrc={'/assets/models/test/PlyExamples/outside.ply'}
+          position={[0, -0.1, 0]}
+          rotation={[-90, 0, 0]}
+        />
+        <SplatAutoRotate />
+      </Application>
+    </Box>
+  );
+};
+
+export default VirtualMonitoringPlot;
