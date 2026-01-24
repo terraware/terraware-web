eca58c69078a29f78eb16c609d747434d31c1385
Isaac Hudson <isaac.hudson@terraformation.com>
2026-01-23 16:39:33 -0800

    SW-7975 Add cameraPosition to annotations to move camera

--- src/components/GaussianSplat/Annotation.tsx
+++ src/components/GaussianSplat/Annotation.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { Entity } from '@playcanvas/react';
 import { Script } from '@playcanvas/react/components';
 import { Annotation as PcAnnotation } from 'playcanvas/scripts/esm/annotations.mjs';
+
+import { useCameraPosition } from 'src/hooks/useCameraPosition';
 
 import './annotation-styles.css';
 
@@ -11,17 +13,30 @@ interface AnnotationProps {
   title: string;
   text?: string;
   label?: string | number;
+  cameraPosition?: [number, number, number];
 }
 
+/**
+ * A 3D annotation component that displays a hotspot and tooltip in the scene.
+ * When clicked, the camera will move to look at the annotation.
+ *
+ * @param props.position - The world position of the annotation
+ * @param props.title - The title displayed in the tooltip
+ * @param props.text - Optional description text displayed in the tooltip
+ * @param props.label - Optional label displayed on the hotspot
+ * @param props.cameraPosition - Optional camera position to move to when clicked. If undefined, camera stays at current position
+ */
 const Annotation = (props: AnnotationProps) => {
-  const { position, ...annotationProps } = props;
-  // todo add camera position which happens on click
+  const { position, cameraPosition, ...annotationProps } = props;
+  const { setCamera } = useCameraPosition();
 
-  // todo fix the offset from the top left of the screen so that the hotspots are placed correctly
+  const handleClick = useCallback(() => {
+    setCamera(position, cameraPosition);
+  }, [position, cameraPosition, setCamera]);
 
   return (
     <Entity name={`annotation${props.label}`} position={position}>
-      <Script script={PcAnnotation} {...annotationProps} />
+      <Script script={PcAnnotation} {...annotationProps} onClickCallback={handleClick} />
     </Entity>
   );
 };
