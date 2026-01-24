9a73380b5006ab06b124bdcc7530a25be253bd35
Isaac Hudson <isaac.hudson@terraformation.com>
2026-01-23 16:09:24 -0800

    SW-7959 Add ability to specify initial camera position via a hook (#5111)
    Create `useCameraPosition` hook for `setCamera`. 
    
    Moves the `Application`component to the Modal component so that `useApp` can be used in the `VirtualMonitoringPlot`(along with `useCameraPosition` which relies on `useApp`).

--- src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/MonitoringPlot/VirtualMonitoringPlot.tsx
+++ src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/MonitoringPlot/VirtualMonitoringPlot.tsx
@@ -1,63 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Box } from '@mui/material';
-import { Application, Entity } from '@playcanvas/react';
+import { Entity } from '@playcanvas/react';
 import { Camera, Script } from '@playcanvas/react/components';
 import { Color } from 'playcanvas';
 import { AnnotationManager } from 'playcanvas/scripts/esm/annotations.mjs';
 import { CameraControls } from 'playcanvas/scripts/esm/camera-controls.mjs';
+import { Grid } from 'playcanvas/scripts/esm/grid.mjs';
 
 import Annotation from 'src/components/GaussianSplat/Annotation';
-import SplatAutoRotate from 'src/components/GaussianSplat/SplatAutoRotate';
 import SplatControls from 'src/components/GaussianSplat/SplatControls';
 import SplatModel from 'src/components/GaussianSplat/SplatModel';
+import { useCameraPosition } from 'src/hooks/useCameraPosition';
 
 const VirtualMonitoringPlot = () => {
+  const { setCamera } = useCameraPosition();
+
+  useEffect(() => {
+    setCamera([0, 0.1, 0], [0, 0.1, 0]);
+  }, [setCamera]);
   return (
-    <Box
-      sx={{
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-      }}
-    >
-      <Application
-        style={{
-          width: '100%',
-          height: '100%',
-          display: 'block',
-          margin: '0 auto',
-        }}
-        graphicsDeviceOptions={{ antialias: false }}
-      >
-        <Entity name='camera'>
-          <Camera clearColor='#1a1a1a' fov={60} />
-          <Script script={CameraControls} />
-        </Entity>
+    <>
+      <Entity name='camera'>
+        <Camera clearColor='#000000' fov={60} />
+        <Script script={CameraControls} moveSpeed={0.3} moveFastSpeed={0.5} moveSlowSpeed={0.15} rotateSpeed={0.1} />
+      </Entity>
 
-        <SplatModel
-          splatSrc={'/assets/models/test/PlyExamples/outside.ply'}
-          position={[0, -0.1, 0]}
-          rotation={[-90, 0, 0]}
-        />
-        <SplatAutoRotate />
+      <Entity name={'grid'} scale={[100, 100, 100]}>
+        <Script script={Grid} />
+      </Entity>
 
-        <Script
-          script={AnnotationManager}
-          hotspotSize={200}
-          opacity={1}
-          hotspotColor={new Color(1, 0, 0, 1)}
-          hoverColor={new Color(0, 1, 0, 1)}
-        />
-        <Annotation
-          label={2}
-          position={[0, 0.1, 0.4]}
-          title={'An annotation'}
-          text={'This annotation is for testing stuff.'}
-        />
-        <SplatControls />
-      </Application>
-    </Box>
+      <SplatModel splatSrc={'/assets/models/test/PlyExamples/outside.ply'} rotation={[-90, 0, 0]} />
+
+      <Script
+        script={AnnotationManager}
+        hotspotSize={200}
+        opacity={1}
+        hotspotColor={new Color(1, 0, 0, 1)}
+        hoverColor={new Color(0, 1, 0, 1)}
+      />
+      <Annotation
+        label={2}
+        position={[0, 0.1, 0.4]}
+        title={'An annotation'}
+        text={'This annotation is for testing stuff.'}
+      />
+      <SplatControls />
+    </>
   );
 };
 
