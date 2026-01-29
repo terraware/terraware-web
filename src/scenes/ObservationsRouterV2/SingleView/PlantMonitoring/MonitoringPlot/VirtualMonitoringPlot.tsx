17504d0a48334c0209e43f87f22b36c9b80b510a
Isaac Hudson <isaac.hudson@terraformation.com>
2026-01-29 10:02:07 -0800

    SW-7996 Disable teleport in XR

--- src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/MonitoringPlot/VirtualMonitoringPlot.tsx
+++ src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/MonitoringPlot/VirtualMonitoringPlot.tsx
@@ -43,7 +43,7 @@ const VirtualMonitoringPlot = ({ observationId, fileId }: VirtualMonitoringPlotP
           <Script script={CameraControls} moveSpeed={0.3} moveFastSpeed={0.5} moveSlowSpeed={0.15} rotateSpeed={0.1} />
         </Entity>
         <Script script={XrControllers} />
-        <Script script={XrNavigation} />
+        <Script script={XrNavigation} enableTeleport={false} />
       </Entity>
 
       {/* When a rerender occurs (such as changing showAnnotations), the splat model disappears (https://github.com/playcanvas/react/pull/298 and https://github.com/playcanvas/react/issues/302) */}
