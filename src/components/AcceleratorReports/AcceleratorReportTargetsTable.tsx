5754e68a3a17c12ce24a39eb2f4f29fc624e0664
constanzauanini <constanza.uanini@admios-sa.com>
2026-02-04 17:44:56 -0300

    fix

--- src/components/AcceleratorReports/AcceleratorReportTargetsTable.tsx
+++ src/components/AcceleratorReports/AcceleratorReportTargetsTable.tsx
@@ -1,7 +1,6 @@
 import React, { type JSX, useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 
-import { Box } from '@mui/material';
 import { EditableTable, EditableTableColumn } from '@terraware/web-components';
 import { DateTime } from 'luxon';
 import { MRT_TableInstance, MRT_ToggleDensePaddingButton } from 'material-react-table';
