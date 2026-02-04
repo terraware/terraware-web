da63cee8d3952b9d3de82bb7e3d5fbfafb1f6ec0
Isaac Hudson <isaac.hudson@terraformation.com>
2026-02-04 09:14:57 -0800

    Fix rtk tags

--- src/queries/extensions/observationSplats.ts
+++ src/queries/extensions/observationSplats.ts
@@ -5,18 +5,18 @@ api.enhanceEndpoints({
   endpoints: {
     listObservationSplats: {
       providesTags: (_results, _error, payload) => [
-        { type: QueryTagTypes.ObservationSplats, observationId: payload.observationId },
+        { type: QueryTagTypes.ObservationSplats, id: payload.observationId },
       ],
     },
     generateObservationSplatFile: {
       invalidatesTags: (_results, _error, payload) => [
-        { type: QueryTagTypes.ObservationSplats, observationId: payload.observationId },
+        { type: QueryTagTypes.ObservationSplats, id: payload.observationId },
       ],
     },
     getObservationSplatFile: {
       providesTags: (_results, _error, payload) => [
-        { type: QueryTagTypes.ObservationMedia, fileId: payload.fileId },
-        { type: QueryTagTypes.ObservationSplats, fileId: payload.fileId },
+        { type: QueryTagTypes.ObservationMedia, id: payload.fileId },
+        { type: QueryTagTypes.ObservationSplats, id: payload.fileId },
       ],
     },
     listObservationSplatAnnotations: {
@@ -24,7 +24,7 @@ api.enhanceEndpoints({
         ...(results
           ? results.annotations.map((annotation) => ({
               type: QueryTagTypes.ObservationSplats,
-              fileId: annotation.fileId,
+              id: annotation.fileId,
             }))
           : []),
         { type: QueryTagTypes.ObservationSplats, id: 'LIST' },
