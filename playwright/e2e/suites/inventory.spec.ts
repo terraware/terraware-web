81e975e0e639bb8801e8492dd12c0bf8e9566964
constanzauanini <constanza.uanini@admios-sa.com>
2026-02-10 14:30:31 -0300

    Fix

--- playwright/e2e/suites/inventory.spec.ts
+++ playwright/e2e/suites/inventory.spec.ts
@@ -317,7 +317,7 @@ const getBatchNumberBySpeciesAndNursery = async (page: Page, species: string, nu
       .locator('..')
       .evaluate((el) => el.id)
   )
-    .replace('-scientificName', '')
+    .replace('-species_scientificName', '')
     .replace('_noLink', '');
   return (await page.locator(`#${rowNumber}-batchNumber`).textContent()) as string;
 };
