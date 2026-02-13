0b322d87b82fa5ec075b6bc44f931d73316ef936
Steven Grimm <steven@terraformation.com>
2026-02-12 16:28:14 -0800

    Remove additional unused functions

--- src/redux/features/organizations/organizationsSlice.ts
+++ src/redux/features/organizations/organizationsSlice.ts
@@ -1,14 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { OrganizationFeaturesData } from 'src/services/OrganizationService';
-import { InternalTag } from 'src/types/InternalTag';
 
 import { StatusT, buildReducers } from '../asyncUtils';
-import {
-  requestOrganizationFeatures,
-  requestOrganizationInternalTags,
-  requestUpdateOrganizationInternalTags,
-} from './organizationsAsyncThunks';
+import { requestOrganizationFeatures } from './organizationsAsyncThunks';
 
 // Define the initial state
 const initialStateFeatures: { [key: string]: StatusT<OrganizationFeaturesData> } = {};
@@ -22,33 +17,8 @@ export const featuresSlice = createSlice({
   },
 });
 
-// Define the initial state
-const initialStateInternalTags: { [key: string]: StatusT<InternalTag[]> } = {};
-
-export const internalTagsSlice = createSlice({
-  name: 'internalTagsSlice',
-  initialState: initialStateInternalTags,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestOrganizationInternalTags)(builder);
-  },
-});
-
-const initialStateInternalTagsUpdateSlice: { [key: string]: StatusT<number> } = {};
-
-export const internalTagsUpdateSlice = createSlice({
-  name: 'internalTagsUpdateSlice',
-  initialState: initialStateInternalTagsUpdateSlice,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestUpdateOrganizationInternalTags, true)(builder);
-  },
-});
-
 const organizationsReducers = {
   features: featuresSlice.reducer,
-  internalTags: internalTagsSlice.reducer,
-  internalTagsUpdate: internalTagsUpdateSlice.reducer,
 };
 
 export default organizationsReducers;
