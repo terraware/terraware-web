32f8f90aa3864dfbc77c5ccbdbb708ab75663e38
Steven Grimm <steven@terraformation.com>
2026-02-12 16:29:43 -0800

    More unused functions

--- src/redux/features/organizations/organizationsAsyncThunks.ts
+++ src/redux/features/organizations/organizationsAsyncThunks.ts
@@ -1,9 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { OrganizationService } from 'src/services';
-import { Response2 } from 'src/services/HttpService';
 import strings from 'src/strings';
-import { UpdateOrganizationInternalTagsRequestPayload } from 'src/types/Organization';
 
 export const requestOrganizationFeatures = createAsyncThunk(
   'organizations/features',
@@ -13,38 +11,6 @@ export const requestOrganizationFeatures = createAsyncThunk(
     if (response !== null && response.requestSucceeded) {
       const { applications, deliverables, modules, reports, seedFundReports } = response;
       return { applications, deliverables, modules, reports, seedFundReports };
-    }
-
-    return rejectWithValue(strings.GENERIC_ERROR);
-  }
-);
-
-export const requestOrganizationInternalTags = createAsyncThunk(
-  'organizations/internalTags',
-  async ({ organizationId }: { organizationId: number }, { rejectWithValue }) => {
-    const response = await OrganizationService.getOrganizationInternalTags(organizationId);
-
-    if (response !== null && response.requestSucceeded && response?.data?.internalTags !== undefined) {
-      return response.data.internalTags;
-    }
-
-    return rejectWithValue(strings.GENERIC_ERROR);
-  }
-);
-
-export const requestUpdateOrganizationInternalTags = createAsyncThunk(
-  'internalTags/update',
-  async (
-    request: { organizationId: number; payload: UpdateOrganizationInternalTagsRequestPayload },
-    { rejectWithValue }
-  ) => {
-    const response: Response2<number> = await OrganizationService.updateOrganizationInternalTags(
-      request.organizationId,
-      request.payload
-    );
-
-    if (response && response.requestSucceeded && response.data) {
-      return response.data;
     }
 
     return rejectWithValue(strings.GENERIC_ERROR);
