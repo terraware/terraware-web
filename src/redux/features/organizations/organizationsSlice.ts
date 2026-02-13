9d26849ded0b69ea1b1eb8e8bda846655772a40a
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2026-02-12 16:38:59 -0800

    SW-8054 Remove Organizations console page (#5219)
    We're removing support for the Accelerator internal tag, so get rid of the console
    page that manages which orgs have the tag. Also remove API client code that was
    only used by that page.

--- src/redux/features/organizations/organizationsSlice.ts
+++ src/redux/features/organizations/organizationsSlice.ts
@@ -1,18 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { OrganizationFeaturesData } from 'src/services/OrganizationService';
-import { InternalTag } from 'src/types/InternalTag';
-import { OrganizationWithInternalTags } from 'src/types/Organization';
 
 import { StatusT, buildReducers } from '../asyncUtils';
-import {
-  requestAddAcceleratorOrganization,
-  requestListAllOrganizationsInternalTags,
-  requestOrganizationFeatures,
-  requestOrganizationInternalTags,
-  requestRemoveAcceleratorOrganizations,
-  requestUpdateOrganizationInternalTags,
-} from './organizationsAsyncThunks';
+import { requestOrganizationFeatures } from './organizationsAsyncThunks';
 
 // Define the initial state
 const initialStateFeatures: { [key: string]: StatusT<OrganizationFeaturesData> } = {};
@@ -26,70 +17,8 @@ export const featuresSlice = createSlice({
   },
 });
 
-// Define the initial state
-const initialStateInternalTags: { [key: string]: StatusT<InternalTag[]> } = {};
-
-export const internalTagsSlice = createSlice({
-  name: 'internalTagsSlice',
-  initialState: initialStateInternalTags,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestOrganizationInternalTags)(builder);
-  },
-});
-
-const initialStateInternalTagsUpdateSlice: { [key: string]: StatusT<number> } = {};
-
-export const internalTagsUpdateSlice = createSlice({
-  name: 'internalTagsUpdateSlice',
-  initialState: initialStateInternalTagsUpdateSlice,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestUpdateOrganizationInternalTags, true)(builder);
-  },
-});
-
-const initialStateAddAcceleratorOrganizationSlice: { [key: string]: StatusT<number> } = {};
-
-export const addAcceleratorOrganizationSlice = createSlice({
-  name: 'addAcceleratorOrganizationSlice',
-  initialState: initialStateAddAcceleratorOrganizationSlice,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestAddAcceleratorOrganization, true)(builder);
-  },
-});
-
-const initialStateRemoveAcceleratorOrganizationsSlice: { [key: string]: StatusT<number> } = {};
-
-export const removeAcceleratorOrganizationsSlice = createSlice({
-  name: 'removeAcceleratorOrganizationsSlice',
-  initialState: initialStateRemoveAcceleratorOrganizationsSlice,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestRemoveAcceleratorOrganizations, true)(builder);
-  },
-});
-
-const initialStateListAllOrganizationsInternalTagsSlice: { [key: string]: StatusT<OrganizationWithInternalTags[]> } =
-  {};
-
-export const listAllOrganizationsInternalTagsSlice = createSlice({
-  name: 'listAllOrganizationsInternalTagsSlice',
-  initialState: initialStateListAllOrganizationsInternalTagsSlice,
-  reducers: {},
-  extraReducers: (builder) => {
-    buildReducers(requestListAllOrganizationsInternalTags, true)(builder);
-  },
-});
-
 const organizationsReducers = {
   features: featuresSlice.reducer,
-  internalTags: internalTagsSlice.reducer,
-  internalTagsUpdate: internalTagsUpdateSlice.reducer,
-  addAcceleratorOrganization: addAcceleratorOrganizationSlice.reducer,
-  removeAcceleratorOrganizations: removeAcceleratorOrganizationsSlice.reducer,
-  listAllOrganizationsInternalTags: listAllOrganizationsInternalTagsSlice.reducer,
 };
 
 export default organizationsReducers;
