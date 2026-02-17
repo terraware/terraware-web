97834e101a490242e9b94e5258826cee467560b6
Isaac Hudson <isaac.hudson@terraformation.com>
2026-02-17 16:19:51 -0500

    Remove Projects Dropdown

--- src/scenes/AcceleratorRouter/Deliverables/DeliverablesList.tsx
+++ src/scenes/AcceleratorRouter/Deliverables/DeliverablesList.tsx
@@ -1,75 +1,18 @@
-import React, { useMemo, useState } from 'react';
-
-import { Grid, Typography } from '@mui/material';
-import { Separator } from '@terraware/web-components';
+import React, { useMemo } from 'react';
 
 import DeliverablesTable from 'src/components/DeliverablesTable';
 import PageHeader from 'src/components/PageHeader';
-import ProjectsDropdown from 'src/components/ProjectsDropdown';
 import { FilterField } from 'src/components/common/FilterGroup';
 import { FilterConfig } from 'src/components/common/SearchFiltersWrapperV2';
-import { useProjects } from 'src/hooks/useProjects';
 import { useLocalization } from 'src/providers';
 import AcceleratorMain from 'src/scenes/AcceleratorRouter/AcceleratorMain';
-import strings from 'src/strings';
-import theme from 'src/theme';
 import { DeliverableTypes } from 'src/types/Deliverables';
-import { SearchNodePayload } from 'src/types/Search';
 
 const DeliverablesList = () => {
-  const { activeLocale } = useLocalization();
-  const { availableProjects: availableProjectOptions } = useProjects();
-
-  const [projectFilter, setProjectFilter] = useState<{ projectId?: number }>({ projectId: undefined });
-
-  const availableProjects = useMemo(() => availableProjectOptions || [], [availableProjectOptions]);
-
-  const extraTableFilters: SearchNodePayload[] = useMemo(
-    () =>
-      projectFilter.projectId
-        ? [
-            {
-              operation: 'field',
-              field: 'projectId',
-              type: 'Exact',
-              values: [`${projectFilter.projectId}`],
-            },
-          ]
-        : [],
-    [projectFilter]
-  );
-
-  const PageHeaderLeftComponent = useMemo(
-    () =>
-      activeLocale ? (
-        <>
-          <Grid container sx={{ marginTop: theme.spacing(0.5) }}>
-            <Grid item>
-              <Separator height={'40px'} />
-            </Grid>
-            <Grid item>
-              <Typography sx={{ lineHeight: '40px' }} component={'span'}>
-                {strings.PROJECT}
-              </Typography>
-            </Grid>
-            <Grid item sx={{ marginLeft: theme.spacing(1.5) }}>
-              <ProjectsDropdown
-                allowUnselect
-                availableProjects={availableProjects}
-                label={''}
-                record={projectFilter}
-                setRecord={setProjectFilter}
-                unselectLabel={strings.ALL}
-              />
-            </Grid>
-          </Grid>
-        </>
-      ) : null,
-    [activeLocale, availableProjects, projectFilter]
-  );
+  const { strings } = useLocalization();
 
   const iconFilters: FilterConfig[] = useMemo(() => {
-    const _filters = [
+    return [
       {
         field: 'type',
         label: strings.TYPE,
@@ -81,17 +24,14 @@ const DeliverablesList = () => {
           value.toString() === 'Questions' ? 'Questionnaire' : value.toString(),
       },
     ];
-
-    return activeLocale ? _filters : [];
-  }, [activeLocale]);
+  }, [strings]);
 
   return (
     <AcceleratorMain>
-      <PageHeader title={strings.DELIVERABLES} leftComponent={PageHeaderLeftComponent} />
+      <PageHeader title={strings.DELIVERABLES} />
 
       {/* -1 for "non-organization scoped search" IE admin search */}
       <DeliverablesTable
-        extraTableFilters={extraTableFilters}
         isAcceleratorRoute={true}
         organizationId={-1}
         tableId={'acceleratorDeliverablesTable'}
