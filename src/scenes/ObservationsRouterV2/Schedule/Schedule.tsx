055ef77abf20a81d2926aeaea5a31e933fb0eea2
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2026-02-20 12:42:21 -0800

    SW-8018 Implemented schedule/reschedule observations using RTK Query (#5274)

---
+++ src/scenes/ObservationsRouterV2/Schedule/Schedule.tsx
@@ -0,0 +1,44 @@
+import React, { type JSX, useCallback, useEffect } from 'react';
+
+import { BusySpinner } from '@terraware/web-components';
+
+import { APP_PATHS } from 'src/constants';
+import { useSyncNavigate } from 'src/hooks/useSyncNavigate';
+import { useLocalization } from 'src/providers';
+import { ScheduleObservationRequestPayload, useScheduleObservationMutation } from 'src/queries/generated/observations';
+import useSnackbar from 'src/utils/useSnackbar';
+
+import ScheduleObservationForm from './ScheduleObservationForm';
+
+export default function ScheduleObservation(): JSX.Element {
+  const navigate = useSyncNavigate();
+  const snackbar = useSnackbar();
+  const { strings } = useLocalization();
+
+  const goToObservations = useCallback(() => navigate(APP_PATHS.OBSERVATIONS), [navigate]);
+
+  const [schedule, scheduleResponse] = useScheduleObservationMutation();
+
+  useEffect(() => {
+    if (scheduleResponse.isError) {
+      snackbar.toastError();
+    } else if (scheduleResponse.isSuccess) {
+      snackbar.toastSuccess(strings.OBSERVATION_SCHEDULED);
+      goToObservations();
+    }
+  }, [goToObservations, snackbar, scheduleResponse.isError, scheduleResponse.isSuccess, strings.OBSERVATION_SCHEDULED]);
+
+  const onSave = useCallback(
+    (data: ScheduleObservationRequestPayload) => {
+      void schedule(data);
+    },
+    [schedule]
+  );
+
+  return (
+    <>
+      {scheduleResponse.isLoading && <BusySpinner withSkrim={true} />}
+      <ScheduleObservationForm title={strings.SCHEDULE_OBSERVATION} onCancel={goToObservations} onSchedule={onSave} />
+    </>
+  );
+}
