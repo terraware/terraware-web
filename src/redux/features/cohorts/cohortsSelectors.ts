c2ba88991d23be1a7331c21021b3cbbffaca1d5e
Isaac Hudson <isaac.hudson@terraformation.com>
2026-02-19 17:24:27 -0800

    Remove cohorts from redux

--- src/redux/features/cohorts/cohortsSelectors.ts
+++
@@ -1,9 +0,0 @@
-import { RootState } from 'src/redux/rootReducer';
-import { Cohort } from 'src/types/Cohort';
-
-export const selectCohort =
-  (cohortId: number) =>
-  (state: RootState): Cohort | undefined =>
-    state.cohorts?.cohorts?.find((cohort) => cohort.id === cohortId);
-
-export const selectCohortRequest = (state: RootState, requestId: string) => state.cohortsRequests[requestId];
