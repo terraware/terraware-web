106a4f7603499fa0e8094e670e5acfac7ba97062
Tommy Lau <8563294+tommylau@users.noreply.github.com>
2026-02-20 18:40:00 -0800

    Implemented abandon observation modal

--- src/scenes/ObservationsRouterV2/index.tsx
+++ src/scenes/ObservationsRouterV2/index.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 
 import SurvivalRateSettings from '../SurvivalRateSettings';
 import EditSurvivalRateSettings from '../SurvivalRateSettings/EditSurvivalRateSettings';
+import AbandonObservationModalProvider from './Abandon';
 import ObservationListView from './ListView';
 import ReassignPlotModalProvider from './Reassign';
 import ObservationScheduleRouter from './Schedule';
@@ -10,15 +11,17 @@ import ObservationSingleView from './SingleView';
 
 const ObservationsRouterV2 = (): JSX.Element => {
   return (
-    <ReassignPlotModalProvider>
-      <Routes>
-        <Route path={'/:observationId/*'} element={<ObservationSingleView />} />
-        <Route path={'/schedule/*'} element={<ObservationScheduleRouter />} />
-        <Route path={'/survival-rate-settings/:plantingSiteId'} element={<SurvivalRateSettings />} />
-        <Route path={'/survival-rate-settings/:plantingSiteId/edit'} element={<EditSurvivalRateSettings />} />
-        <Route path={'/*'} element={<ObservationListView />} />
-      </Routes>
-    </ReassignPlotModalProvider>
+    <AbandonObservationModalProvider>
+      <ReassignPlotModalProvider>
+        <Routes>
+          <Route path={'/:observationId/*'} element={<ObservationSingleView />} />
+          <Route path={'/schedule/*'} element={<ObservationScheduleRouter />} />
+          <Route path={'/survival-rate-settings/:plantingSiteId'} element={<SurvivalRateSettings />} />
+          <Route path={'/survival-rate-settings/:plantingSiteId/edit'} element={<EditSurvivalRateSettings />} />
+          <Route path={'/*'} element={<ObservationListView />} />
+        </Routes>
+      </ReassignPlotModalProvider>
+    </AbandonObservationModalProvider>
   );
 };
 
