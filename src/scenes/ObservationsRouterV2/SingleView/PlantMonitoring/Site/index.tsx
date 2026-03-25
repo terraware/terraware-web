5b061a28a6b500f54d7c40a024b571f6af2d4099
constanzauanini <constanza.uanini@admios-sa.com>
2026-03-25 17:53:17 -0300

    SW-8224 Update Observation and Monitoring Plot cards and charts to latest UI/UX (#5499)
    Before:
    <img width="1721" height="898" alt="Screenshot 2026-03-25 at 5 27 09 PM"
    src="https://github.com/user-attachments/assets/2136dcf6-61b9-41bf-954d-4adfacc58531"
    />
    
    Now (new UI)
    <img width="1724" height="893" alt="Screenshot 2026-03-25 at 5 26 04 PM"
    src="https://github.com/user-attachments/assets/60b5bba3-b2e5-47d6-8d30-10e958c38233"
    />

--- src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/Site/index.tsx
+++ src/scenes/ObservationsRouterV2/SingleView/PlantMonitoring/Site/index.tsx
@@ -1,13 +1,15 @@
 import React, { type JSX, useCallback, useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router';
 
-import { Typography, useTheme } from '@mui/material';
-import { getDateDisplayValue } from '@terraware/web-components/utils';
+import { Box, Typography, useTheme } from '@mui/material';
+import { IconTooltip } from '@terraware/web-components';
+import { getDateDisplayValue, useDeviceInfo } from '@terraware/web-components/utils';
 
 import { Crumb } from 'src/components/BreadCrumbs';
 import ListMapView from 'src/components/ListMapView';
 import Page from 'src/components/Page';
 import SurvivalRateMessageV2 from 'src/components/SurvivalRate/SurvivalRateMessageV2';
+import Card from 'src/components/common/Card';
 import OptionsMenu from 'src/components/common/OptionsMenu';
 import { APP_PATHS } from 'src/constants';
 import { useLocalization } from 'src/providers';
@@ -18,9 +20,12 @@ import MatchSpeciesModal from 'src/scenes/ObservationsRouterV2/SingleView/MatchS
 import UnrecognizedSpeciesPageMessage from 'src/scenes/ObservationsRouterV2/SingleView/UnrecognizedSpeciesPageMessage';
 import { useOnSaveMergedSpeciesRtk } from 'src/scenes/ObservationsRouterV2/useOnSaveMergedSpeciesRtk';
 import { getShortDate } from 'src/utils/dateFormatter';
+import { getObservationSpeciesDeadPlantsCount, getObservationSpeciesLivePlantsCount } from 'src/utils/observation';
 import { useDefaultTimeZone } from 'src/utils/useTimeZoneUtils';
 
-import AggregatedPlantsStats from '../AggregatedPlantsStats';
+import ObservationDataNumbers from '../../BiomassMeasurements/ObservationDataNumbers';
+import SpeciesSurvivalRateChart from '../SpeciesSurvivalRateChart';
+import SpeciesTotalPlantsChart from '../SpeciesTotalPlantsChart';
 import useObservationSpecies from '../useObservationSpecies';
 import StratumList from './StratumList';
 
@@ -79,6 +84,9 @@ const SiteDetails = (): JSX.Element => {
   }, [activeLocale, defaultTimezone, plantingSite?.name, plantingSite?.timeZone, results, theme.palette.TwClrTxt]);
 
   const species = useObservationSpecies(results?.species ?? []);
+  const { isDesktop } = useDeviceInfo();
+  const livePlants = useMemo(() => getObservationSpeciesLivePlantsCount(species), [species]);
+  const deadPlants = useMemo(() => getObservationSpeciesDeadPlantsCount(species), [species]);
 
   const resultsSpecies = results?.species;
 
@@ -128,6 +136,19 @@ const SiteDetails = (): JSX.Element => {
     return permanentPlots.some((plot) => plot.status === 'Completed');
   }, [results?.strata]);
 
+  const items = [
+    { label: strings.TOTAL_PLANTS, tooltip: strings.TOOLTIP_TOTAL_PLANTS, value: results?.totalPlants },
+    { label: strings.LIVE_PLANTS, tooltip: strings.TOOLTIP_LIVE_PLANTS, value: livePlants },
+    { label: strings.DEAD_PLANTS, tooltip: strings.PLOT_DEAD_PLANTS_TOOLTIP, value: deadPlants },
+    { label: strings.SPECIES, tooltip: strings.PLOT_SPECIES_TOOLTIP, value: results?.totalSpecies },
+    { label: strings.PLANT_DENSITY, tooltip: strings.PLANT_DENSITY_MISSING_TOOLTIP, value: results?.plantingDensity },
+    {
+      label: strings.SURVIVAL_RATE,
+      tooltip: strings.SURVIVAL_RATE_COLUMN_TOOLTIP,
+      value: hasObservedPermanentPlots ? `${results?.survivalRate ?? '-'}%` : '',
+    },
+  ];
+
   useEffect(() => {
     if (unrecognizedSpecies.length) {
       setShowPageMessage(true);
@@ -151,28 +172,57 @@ const SiteDetails = (): JSX.Element => {
         />
       )}
       <SurvivalRateMessageV2 selectedPlantingSiteId={results?.plantingSiteId} />
-      <AggregatedPlantsStats
-        completedTime={results?.completedTime}
-        hasObservedPermanentPlots={hasObservedPermanentPlots}
-        totalSpecies={results?.totalSpecies}
-        plantingDensity={results?.plantingDensity}
-        survivalRate={results?.survivalRate}
-        species={species}
-      />
-      <ListMapView
-        search={null}
-        initialView='list'
-        onView={onView}
-        style={{ width: '100%' }}
-        list={<StratumList />}
-        map={
-          <ObservationMapWrapper
-            observationId={observationId}
-            plantingSiteId={results?.plantingSiteId}
-            isMapVisible={isMapVisible}
-          />
-        }
-      />
+      <Card radius='24px' style={{ width: '100%' }}>
+        <ObservationDataNumbers items={items} isCompleted={!!results?.completedTime} />
+        <Box display='flex' gap={3} flexDirection={isDesktop ? 'row' : 'column'} flexWrap='wrap' marginBottom={3}>
+          <Box flex={1} minWidth='500px'>
+            <Box display='flex' alignItems='center'>
+              <Typography fontSize='20px' fontWeight={600}>
+                {strings.NUMBER_OF_LIVE_PLANTS_PER_SPECIES}
+              </Typography>
+              <IconTooltip title={strings.NUMBER_OF_LIVE_PLANTS_PER_SPECIES_TOOLTIP} />
+            </Box>
+            <Box height='245px'>
+              <SpeciesTotalPlantsChart
+                chartId='observationSpeciesTotalChart'
+                minHeight='245px'
+                species={species}
+                isNotCompleted={!results?.completedTime}
+              />
+            </Box>
+          </Box>
+          <Box flex={1} minWidth='500px'>
+            <Box display='flex' alignItems='center'>
+              <Typography fontSize='20px' fontWeight={600}>
+                {strings.SURVIVAL_RATE_PER_SPECIES_AS_OF_THIS_OBSERVATION}
+              </Typography>
+              <IconTooltip title={strings.SURVIVAL_RATE_PER_SPECIES_AS_OF_THIS_OBSERVATION_TOOLTIP} />
+            </Box>
+            <Box height='245px'>
+              <SpeciesSurvivalRateChart
+                chartId='observationSurvivalRateChart'
+                species={hasObservedPermanentPlots ? species : []}
+                minHeight='245px'
+                isNotCompleted={!results?.completedTime}
+              />
+            </Box>
+          </Box>
+        </Box>
+        <ListMapView
+          search={null}
+          initialView='list'
+          onView={onView}
+          style={{ width: '100%' }}
+          list={<StratumList />}
+          map={
+            <ObservationMapWrapper
+              observationId={observationId}
+              plantingSiteId={results?.plantingSiteId}
+              isMapVisible={isMapVisible}
+            />
+          }
+        />
+      </Card>
     </Page>
   );
 };
