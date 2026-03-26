4e56f5a7e442c5e650c0e62fe01216a38b337758
constanzauanini <constanza.uanini@admios-sa.com>
2026-03-26 09:57:53 -0300

    SW-8232 Remove red color

--- src/scenes/SurvivalRateSettings/SurvivalRateInstructions.tsx
+++ src/scenes/SurvivalRateSettings/SurvivalRateInstructions.tsx
@@ -13,13 +13,7 @@ const SurvivalRateInstructions = () => {
         <Icon name='info' fillColor={theme.palette.TwClrIcnSecondary} size='medium' />
       </Box>
       <Typography fontSize={'14px'}>
-        {strings.formatString(
-          strings.SURVIVAL_RATE_SETTINGS_INFO,
-          <b>{strings.INSTRUCTIONS}</b>,
-          <Typography color={theme.palette.TwClrTxtDanger} fontSize={'14px'} display={'inline'}>
-            {strings.SURVIVAL_RATE_SETTINGS_MESSAGING}
-          </Typography>
-        )}
+        {strings.formatString(strings.SURVIVAL_RATE_SETTINGS_INFO, <b>{strings.INSTRUCTIONS}</b>)}
       </Typography>
     </Box>
   );
