8d48253afcc9b1082a35c9a96f9a703aa1b7140f
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2026-04-08 08:24:41 -0700

    Assume deployment role to deploy to ECS (#5565)
    For deploying to ECS, we need to switch to the deployment
    identity for the destination tier. Update the deploy script
    to do that.

--- .buildkite/scripts/deploy-ecs.sh
+++ .buildkite/scripts/deploy-ecs.sh
@@ -6,16 +6,30 @@ case "$TIER" in
     PROD)
         ECS_CLUSTER=prod-terraware-cluster
         ECS_SERVICE=prod-terraware-frontend
+        ASSUME_ROLE="${PROD_AWS_ROLE}"
         ;;
     STAGING)
         ECS_CLUSTER=staging-terraware-cluster
         ECS_SERVICE=staging-terraware-frontend
+        ASSUME_ROLE="${STAGING_AWS_ROLE}"
         ;;
     *)
         echo "--- :amazon-ecs: No ECS deploy needed for ${TIER} builds."
         exit 0
         ;;
 esac
+
+.buildkite/scripts/install-deps.sh --tools
+
+echo "--- :aws: Assuming role in target tier"
+
+credentials=$(aws sts assume-role --role-arn "$ASSUME_ROLE" --role-session-name buildkite-deploy)
+AWS_ACCESS_KEY_ID=$(echo "$credentials" | jq -r '.Credentials.AccessKeyId');\
+AWS_SECRET_ACCESS_KEY=$(echo "$credentials" | jq -r '.Credentials.SecretAccessKey');\
+AWS_SESSION_TOKEN=$(echo "$credentials" | jq -r '.Credentials.SessionToken');
+export AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY
+export AWS_SESSION_TOKEN
 
 echo "--- :amazon-ecs: Deploying to ECS cluster=${ECS_CLUSTER} service=${ECS_SERVICE}"
 
