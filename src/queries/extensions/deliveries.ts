39516b68a7cdfdbbaaeb0a729186d97fc020e213
Tommy Lau <8563294+tommylau@users.noreply.github.com>
2026-04-14 16:19:56 -0700

    Fixed formatting

--- src/queries/extensions/deliveries.ts
+++ src/queries/extensions/deliveries.ts
@@ -7,9 +7,7 @@ api.enhanceEndpoints({
       providesTags: (_result, _error, deliveryId) => [{ type: QueryTagTypes.AcceleratorProjects, id: deliveryId }],
     },
     reassignDelivery: {
-      invalidatesTags: (_result, _error, payload) => [
-        { type: QueryTagTypes.AcceleratorProjects, id: payload.id },
-      ],
+      invalidatesTags: (_result, _error, payload) => [{ type: QueryTagTypes.AcceleratorProjects, id: payload.id }],
     },
   },
 });
