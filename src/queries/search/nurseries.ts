52da33e46b08b964f4ee0abdf3126985e80db4bc
Josh Buchea <joshbuchea@users.noreply.github.com>
2026-04-28 14:21:01 -1000

    Fix Planting Site column filter in Nursery Withdrawals Table not working (#5643)

--- src/queries/search/nurseries.ts
+++ src/queries/search/nurseries.ts
@@ -111,7 +111,7 @@ const parseSearchNurseryWithdrawalsArgs = (
   if (args.destinationNames?.length) {
     searchChildren.push({
       operation: 'field',
-      field: 'destinationNames',
+      field: 'destinationName',
       type: 'Exact',
       values: args.destinationNames,
     });
