240c2ec3459e77f8de7faef1e7a7e11beaeec1e4
Josh Buchea <verctrl@joshbuchea.com>
2026-05-08 15:07:34 -1000

    change "Out-planting" --> "Planting"

--- src/utils/withdrawalPurposes.tsx
+++ src/utils/withdrawalPurposes.tsx
@@ -3,7 +3,7 @@ import strings from 'src/strings';
 export function withdrawalPurposes() {
   return [
     { label: strings.NURSERY, value: 'Nursery' },
-    { label: strings.OUT_PLANTING, value: 'Out-planting' },
+    { label: strings.PLANTING, value: 'Out-planting' },
     { label: strings.VIABILITY_TESTING, value: 'Viability Testing' },
     { label: strings.OTHER, value: 'Other' },
   ];
