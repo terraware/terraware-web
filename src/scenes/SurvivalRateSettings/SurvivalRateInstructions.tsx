3b6c6dbdda29dcbcf0152f6878affa21b4527f3d
constanzauanini <constanza.uanini@admios-sa.com>
2026-05-15 15:40:33 -0300

    SW-8357 Update survival rate settings description (#5707)

--- src/scenes/SurvivalRateSettings/SurvivalRateInstructions.tsx
+++ src/scenes/SurvivalRateSettings/SurvivalRateInstructions.tsx
@@ -3,9 +3,11 @@ import React from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
 import { Icon } from '@terraware/web-components';
 
+import isEnabled from 'src/features';
 import strings from 'src/strings';
 
 const SurvivalRateInstructions = () => {
+  const isWeightedSurvivalRatesEnabled = isEnabled('Weighted Survival Rates');
   const theme = useTheme();
   return (
     <Box display='flex'>
@@ -13,7 +15,12 @@ const SurvivalRateInstructions = () => {
         <Icon name='info' fillColor={theme.palette.TwClrIcnSecondary} size='medium' />
       </Box>
       <Typography fontSize={'14px'}>
-        {strings.formatString(strings.SURVIVAL_RATE_SETTINGS_INFO, <b>{strings.INSTRUCTIONS}</b>)}
+        {strings.formatString(
+          isWeightedSurvivalRatesEnabled
+            ? strings.WEIGHTED_SURVIVAL_RATE_SETTINGS_INFO
+            : strings.SURVIVAL_RATE_SETTINGS_INFO,
+          <b>{strings.INSTRUCTIONS}</b>
+        )}
       </Typography>
     </Box>
   );
