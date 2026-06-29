230192f380be04e884167d89ccb0b9acab7834c8
constanzauanini <constanza.uanini@admios-sa.com>
2026-06-29 09:53:32 -0300

    SW-8591 Add change history in Inventory Planning View (#5958)

--- src/queries/plantingSeasons/events.ts
+++ src/queries/plantingSeasons/events.ts
@@ -1,10 +1,42 @@
 import { baseApi as api } from '../baseApi';
-import { EventLogEntryPayload, ListEventLogEntriesApiResponse } from '../generated/events';
+import {
+  type EventLogEntryPayload,
+  type ListEventLogEntriesApiResponse,
+  type ListEventLogEntriesRequestPayload,
+} from '../generated/events';
 import { QueryTagTypes } from '../tags';
+
+export type EventSubject = NonNullable<ListEventLogEntriesRequestPayload['subjects']>[number];
 
 export type ListPlantingSeasonEventsArgs = {
   organizationId: number;
   plantingSeasonId: number;
+};
+
+export type ListInventoryPlanningEventsArgs = {
+  organizationId: number;
+  plantingSeasonId?: number;
+  plantingSiteId?: number;
+  speciesId?: number;
+};
+
+const orderEvents = (events: EventLogEntryPayload[]): EventLogEntryPayload[] => {
+  return events.map((event) => ({ ...event })).reverse();
+};
+
+const transformInventoryPlanningEvents = (
+  results: ListEventLogEntriesApiResponse,
+  speciesId?: number
+): EventLogEntryPayload[] => {
+  return orderEvents(
+    results.events
+      .filter((event) => event.action.type !== 'Created')
+      .filter(
+        (event) =>
+          speciesId === undefined ||
+          (event.subject.type === 'PlantingSeasonAllocatedSpecies' && event.subject.speciesId === speciesId)
+      )
+  );
 };
 
 const injectedRtkApi = api.injectEndpoints({
@@ -16,18 +48,46 @@ const injectedRtkApi = api.injectEndpoints({
         body: {
           organizationId,
           plantingSeasonId,
+          subjects: [
+            'PlantingDateRequest',
+            'PlantingDateRequestSpecies',
+            'PlantingSeason',
+            'PlantingSeasonScheduledDate',
+            'PlantingSeasonScheduledDateSpecies',
+            'PlantingSeasonSpeciesTarget',
+            'PlantingSeasonWithdrawal',
+          ] satisfies EventSubject[],
         },
       }),
       providesTags: (_results, _error, { plantingSeasonId }) => [
         { type: QueryTagTypes.PlantingSeasons, id: plantingSeasonId },
         { type: QueryTagTypes.PlantingSeasonDates, id: plantingSeasonId },
       ],
-      transformResponse: (results: ListEventLogEntriesApiResponse) =>
-        results.events.map((event) => ({ ...event })).reverse(),
+      transformResponse: (results: ListEventLogEntriesApiResponse) => orderEvents(results.events),
+    }),
+    listInventoryPlanningEvents: build.query<EventLogEntryPayload[], ListInventoryPlanningEventsArgs>({
+      query: ({ organizationId, plantingSeasonId, plantingSiteId }) => ({
+        url: '/api/v1/events/list',
+        method: 'POST',
+        body: {
+          organizationId,
+          plantingSeasonId,
+          plantingSiteId,
+          subjects: ['PlantingSeasonAllocatedSpecies'],
+        },
+      }),
+      providesTags: [{ type: QueryTagTypes.InventoryPlanning, id: 'LIST' }],
+      transformResponse: (results: ListEventLogEntriesApiResponse, _meta, { speciesId }) =>
+        transformInventoryPlanningEvents(results, speciesId),
     }),
   }),
 });
 
 export { injectedRtkApi as api };
 
-export const { useListPlantingSeasonEventsQuery, useLazyListPlantingSeasonEventsQuery } = injectedRtkApi;
+export const {
+  useListPlantingSeasonEventsQuery,
+  useLazyListPlantingSeasonEventsQuery,
+  useListInventoryPlanningEventsQuery,
+  useLazyListInventoryPlanningEventsQuery,
+} = injectedRtkApi;
