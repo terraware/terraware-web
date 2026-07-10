b918e43a1881a2039d5657483cd3dc291c31afc4
Steven Grimm <steven@terraformation.com>
2026-07-10 15:34:02 -0700

    Explain why we are setting the option to true

--- .buildkite/scripts/upload-playwright-results.sh
+++ .buildkite/scripts/upload-playwright-results.sh
@@ -9,5 +9,7 @@ sudo mv playwright-report playwright/public/report
 
 # We need to disable KMS encryption on public artifacts so they will be
 # downloadable without AWS authentication. There's no good way to do this
-# selectively using the built-in artifacts support, so do it explicitly.
+# selectively using the built-in artifacts support, so do it explicitly
+# by telling the agent to use the default server-side encryption mode,
+# which uses an AWS-managed key that works for public files.
 BUILDKITE_S3_SSE_ENABLED=true buildkite-agent artifact upload 'playwright/public/**/*'
