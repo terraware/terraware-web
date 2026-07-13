650001a059bea300f7e67e045eaf0411055e1962
constanzauanini <constanza.uanini@admios-sa.com>
2026-07-13 16:39:30 -0300

    SW-8633 Fix condition to show tooltip (#6055)

--- src/components/BatchWithdrawFlow/flow/SelectPurposeForm.tsx
+++ src/components/BatchWithdrawFlow/flow/SelectPurposeForm.tsx
@@ -529,11 +529,11 @@ export default function SelectPurposeForm(props: SelectPurposeFormProps): JSX.El
     return (
       <>
         {strings.PLANTING}
-        {noReadySeedlings ? (
-          <IconTooltip placement='top' title={strings.PLANTINGS_REQUIRE_READY_TO_PLANT_SEEDLINGS} />
-        ) : outplantDisabled ? (
+        {outplantDisabled ? (
           <IconTooltip placement='top' title={strings.PLANTINGS_REQUIRE_PLANTING_SITES} />
-        ) : null}
+        ) : (
+          <IconTooltip placement='top' title={strings.PLANTINGS_REQUIRE_READY_TO_PLANT_SEEDLINGS} />
+        )}
       </>
     );
   };
