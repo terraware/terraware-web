d38f3280b46b3ecc196d268065be82f24f685f2d
Isaac Hudson <isaac.hudson@terraformation.com>
2026-07-14 15:16:02 -0700

    update tag invalidation

--- src/queries/extensions/userInternalInterests.ts
+++ src/queries/extensions/userInternalInterests.ts
@@ -7,7 +7,10 @@ api.enhanceEndpoints({
       providesTags: (_result, _error, userId) => [{ type: QueryTagTypes.InternalInterests, id: userId }],
     },
     updateUserInternalInterests: {
-      invalidatesTags: (_result, _error, payload) => [{ type: QueryTagTypes.InternalInterests, id: payload.userId }],
+      invalidatesTags: (_result, _error, payload) => [
+        { type: QueryTagTypes.InternalInterests, id: payload.userId },
+        { type: QueryTagTypes.Users, id: payload.userId },
+      ],
     },
   },
 });
