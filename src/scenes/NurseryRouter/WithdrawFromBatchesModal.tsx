a80744fd004e465a28a2de332e4d6e382dc02046
constanzauanini <constanza.uanini@admios-sa.com>
2026-07-16 15:06:32 -0300

    SW-8634 Fix withdrawableSubstrata condition

--- src/scenes/NurseryRouter/WithdrawFromBatchesModal.tsx
+++ src/scenes/NurseryRouter/WithdrawFromBatchesModal.tsx
@@ -615,7 +615,9 @@ const Step2Content = ({
         .map((substratum) => ({
           ...substratum,
           species: substratum.species.filter(
-            (species) => species.quantity > 0 && (batchesBySpecies.get(species.speciesId)?.length ?? 0) > 0
+            (species) =>
+              species.quantity - species.withdrawnQuantity > 0 &&
+              (batchesBySpecies.get(species.speciesId)?.length ?? 0) > 0
           ),
         }))
         .filter((substratum) => substratum.species.length > 0),
