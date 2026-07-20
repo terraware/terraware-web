ef24d5c6e3bf9af33092c044c35e6d383d5f04f0
Tommy Lau <8563294+tommylau@users.noreply.github.com>
2026-07-20 13:27:45 -0700

    Updated inventory batch to use table row menu

--- src/scenes/InventoryRouter/view/QuantitiesMenu.tsx
+++ src/scenes/InventoryRouter/view/QuantitiesMenu.tsx
@@ -1,8 +1,6 @@
-import React, { type JSX, useState } from 'react';
+import React, { type JSX } from 'react';
 
-import { MenuItem, MenuList, Popover, Typography, useTheme } from '@mui/material';
-import { Button, Tooltip } from '@terraware/web-components';
-
+import TableRowPopupMenu from 'src/components/common/table/TableRowPopupMenu';
 import strings from 'src/strings';
 
 import { ModalValuesType } from './BatchesCellRenderer';
@@ -14,21 +12,8 @@ export type QuantitiesMenuProps = {
 
 export default function QuantitiesMenu(props: QuantitiesMenuProps): JSX.Element {
   const { setModalValues, batch } = props;
-  const theme = useTheme();
 
-  const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLButtonElement | null>(null);
-  const openMenu = Boolean(menuAnchorEl);
-
-  const openMenuHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setMenuAnchorEl(event.currentTarget);
-  };
-
-  const closeMenuHandler = () => {
-    setMenuAnchorEl(null);
-  };
-
-  const openChangeQuantityHandler = (event: any, type: string) => {
-    closeMenuHandler();
+  const openChangeQuantityHandler = (type: string) => {
     setModalValues({
       openChangeQuantityModal: true,
       type,
@@ -36,61 +21,23 @@ export default function QuantitiesMenu(props: QuantitiesMenuProps): JSX.Element 
     });
   };
 
-  return (
-    <>
-      <Popover
-        open={openMenu}
-        anchorEl={menuAnchorEl}
-        onClose={closeMenuHandler}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-      >
-        <MenuList sx={{ padding: theme.spacing(2, 0) }}>
-          <MenuItem
-            id='change-germinating'
-            onClick={(event) => openChangeQuantityHandler(event, 'germinating')}
-            sx={{ padding: theme.spacing(1, 2) }}
-            disabled={Number(batch.germinatingQuantity) === 0}
-          >
-            <Typography color={theme.palette.TwClrBaseGray800} paddingLeft={1}>
-              {strings.CHANGE_GERMINATION_ESTABLISHMENT_STATUS}
-            </Typography>
-          </MenuItem>
+  const menuItems = [
+    {
+      label: strings.CHANGE_GERMINATION_ESTABLISHMENT_STATUS,
+      disabled: Number(batch.germinatingQuantity) === 0,
+      onClick: () => openChangeQuantityHandler('germinating'),
+    },
+    {
+      label: strings.CHANGE_ACTIVE_GROWTH_STATUS,
+      disabled: Number(batch.activeGrowthQuantity) === 0,
+      onClick: () => openChangeQuantityHandler('active-growth'),
+    },
+    {
+      label: strings.CHANGE_HARDENING_OFF_STATUS,
+      disabled: Number(batch.hardeningOffQuantity) === 0,
+      onClick: () => openChangeQuantityHandler('hardening-off'),
+    },
+  ];
 
-          <MenuItem
-            id='change-active-growth'
-            onClick={(event) => openChangeQuantityHandler(event, 'active-growth')}
-            sx={{ padding: theme.spacing(1, 2) }}
-            disabled={Number(batch.activeGrowthQuantity) === 0}
-          >
-            <Typography color={theme.palette.TwClrBaseGray800} paddingLeft={1}>
-              {strings.CHANGE_ACTIVE_GROWTH_STATUS}
-            </Typography>
-          </MenuItem>
-
-          <MenuItem
-            id='change-hardening-off'
-            onClick={(event) => openChangeQuantityHandler(event, 'hardening-off')}
-            sx={{ padding: theme.spacing(1, 2) }}
-            disabled={Number(batch.hardeningOffQuantity) === 0}
-          >
-            <Typography color={theme.palette.TwClrBaseGray800} paddingLeft={1}>
-              {strings.CHANGE_HARDENING_OFF_STATUS}
-            </Typography>
-          </MenuItem>
-        </MenuList>
-      </Popover>
-      <Tooltip title={strings.MORE_OPTIONS}>
-        <Button
-          onClick={(event) => event && openMenuHandler(event)}
-          icon='menuVertical'
-          type='passive'
-          priority='ghost'
-          size='medium'
-        />
-      </Tooltip>
-    </>
-  );
+  return <TableRowPopupMenu menuItems={menuItems} />;
 }
