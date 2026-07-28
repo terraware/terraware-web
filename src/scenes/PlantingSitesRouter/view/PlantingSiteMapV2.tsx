4ba1f9d55ee88f26107b533eaf039d74e1f1f93e
Tommy Lau <8563294+tommylau523@users.noreply.github.com>
2026-07-28 05:49:20 -0700

    SW-8451 Added name tag to planting site map (#6152)

--- src/scenes/PlantingSitesRouter/view/PlantingSiteMapV2.tsx
+++ src/scenes/PlantingSitesRouter/view/PlantingSiteMapV2.tsx
@@ -1,15 +1,22 @@
-import React, { type JSX, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import React, { type JSX, useCallback, useMemo, useRef, useState } from 'react';
 import { MapRef } from 'react-map-gl/mapbox';
 
 import { Box } from '@mui/material';
 
 import MapComponent from 'src/components/NewMap';
 import { MapLegendGroup } from 'src/components/NewMap/MapLegend';
-import { MapLayer, MapLayerFeature, MapLayerFeatureId } from 'src/components/NewMap/types';
+import {
+  MapLayer,
+  MapLayerFeature,
+  MapLayerFeatureId,
+  MapNameTag,
+  MapPoint,
+  MapViewState,
+} from 'src/components/NewMap/types';
 import useMapFeatureStyles from 'src/components/NewMap/useMapFeatureStyles';
 import useMapUtils from 'src/components/NewMap/useMapUtils';
 import usePlantingSiteMapLegend from 'src/components/NewMap/usePlantingSiteMapLegend';
-import { getBoundingBoxFromMultiPolygons } from 'src/components/NewMap/utils';
+import { getBoundingBoxFromPoints, getBoundsZoomLevel } from 'src/components/NewMap/utils';
 import usePlantingSite from 'src/hooks/usePlantingSite';
 import useMapboxToken from 'src/utils/useMapboxToken';
 
@@ -108,13 +115,64 @@ const PlantingSiteMapV2 = ({ plantingSiteId }: PlantingSiteMapV2Props): JSX.Elem
     }
   }, []);
 
-  useEffect(() => {
-    if (plantingSite?.boundary) {
-      fitBounds(getBoundingBoxFromMultiPolygons([plantingSite.boundary]));
+  const nameTags = useMemo((): MapNameTag[] | undefined => {
+    if (!plantingSite || plantingSite.boundary === undefined) {
+      return undefined;
+    } else {
+      const points = plantingSite.boundary.coordinates
+        .flat()
+        .flat()
+        .map(
+          ([lng, lat]): MapPoint => ({
+            lat,
+            lng,
+          })
+        );
+
+      const bbox = getBoundingBoxFromPoints(points);
+      const latitude = (bbox.maxLat + bbox.minLat) / 2;
+      const longitude = (bbox.maxLng + bbox.minLng) / 2;
+
+      return [
+        {
+          label: plantingSite.name,
+          longitude,
+          latitude,
+          onClick: () => {
+            fitBounds(bbox);
+          },
+        },
+      ];
     }
   }, [fitBounds, plantingSite]);
 
-  if (!token) {
+  const initialViewState = useMemo((): MapViewState | undefined => {
+    if (!plantingSite || plantingSite.boundary === undefined) {
+      return undefined;
+    } else {
+      const points = plantingSite.boundary.coordinates
+        .flat()
+        .flat()
+        .map(
+          ([lng, lat]): MapPoint => ({
+            lat,
+            lng,
+          })
+        );
+
+      const bbox = getBoundingBoxFromPoints(points);
+      const latitude = (bbox.maxLat + bbox.minLat) / 2;
+      const longitude = (bbox.maxLng + bbox.minLng) / 2;
+      const zoom = getBoundsZoomLevel(bbox, 400, 400);
+      return {
+        latitude,
+        longitude,
+        zoom,
+      };
+    }
+  }, [plantingSite]);
+
+  if (!token || !plantingSite?.boundary) {
     return <Box />;
   }
 
@@ -124,9 +182,11 @@ const PlantingSiteMapV2 = ({ plantingSiteId }: PlantingSiteMapV2Props): JSX.Elem
       drawerOpen={drawerOpen}
       drawerSize={'small'}
       legends={legends}
+      initialViewState={initialViewState}
       mapId={mapId}
       mapLayers={layers}
       mapRef={mapRef}
+      nameTags={nameTags}
       onTokenExpired={refreshToken}
       setDrawerOpen={setDrawerOpenCallback}
       token={token}
