a194d084af012a1f769072a979e0e92a17fd0204
Tommy Lau <8563294+tommylau@users.noreply.github.com>
2026-08-13 12:46:36 -0700

    Narrow report invalidations

--- src/queries/extensions/acceleratorReportIndicators.ts
+++ src/queries/extensions/acceleratorReportIndicators.ts
@@ -14,14 +14,12 @@ api.enhanceEndpoints({
       invalidatesTags: (_results, _error, args) => [
         { type: QueryTagTypes.ProjectIndicators, id: 'LIST' },
         projectAcceleratorReportTag(args.projectId),
-        { type: QueryTagTypes.PublishedAcceleratorReport },
       ],
     },
     updateProjectIndicator: {
       invalidatesTags: (_results, _error, payload) => [
         { type: QueryTagTypes.ProjectIndicators, id: payload.indicatorId },
         projectAcceleratorReportTag(payload.projectId),
-        { type: QueryTagTypes.PublishedAcceleratorReport },
       ],
     },
     listCommonIndicators: {
@@ -34,16 +32,12 @@ api.enhanceEndpoints({
       invalidatesTags: () => [
         { type: QueryTagTypes.CommonIndicators, id: 'LIST' },
         { type: QueryTagTypes.AcceleratorReport },
-        { type: QueryTagTypes.ProjectAcceleratorReport },
-        { type: QueryTagTypes.PublishedAcceleratorReport },
       ],
     },
     updateCommonIndicator: {
       invalidatesTags: (_results, _error, payload) => [
         { type: QueryTagTypes.CommonIndicators, id: payload.indicatorId },
         { type: QueryTagTypes.AcceleratorReport },
-        { type: QueryTagTypes.ProjectAcceleratorReport },
-        { type: QueryTagTypes.PublishedAcceleratorReport },
       ],
     },
   },
