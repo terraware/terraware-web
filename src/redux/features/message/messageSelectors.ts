a8d69ed2c48903d71db3f250c32ef0685312340a
Steven Grimm <1248649+sgrimm@users.noreply.github.com>
2026-08-18 11:03:13 -0700

    Remove more unused source files (#6271)
    Get rid of the remaining source files flagged as unused by Knip.

--- src/redux/features/message/messageSelectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from '@reduxjs/toolkit';
-
-import { RootState } from 'src/redux/rootReducer';
-
-export const selectMessages = (state: RootState) => state.message.messages;
-
-export const selectMessage = (key: string) => createSelector(selectMessages, (messages) => messages[key]);
