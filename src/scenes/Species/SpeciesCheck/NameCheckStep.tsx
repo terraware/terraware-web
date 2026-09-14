a7d4e052eb5357117990eb8a7fc0b284b079bfae
constanzauanini <constanza.uanini@admios-sa.com>
2026-09-14 16:27:13 -0300

    SW-8878 Show sueggestions count in NameCheck step

--- src/scenes/Species/SpeciesCheck/NameCheckStep.tsx
+++ src/scenes/Species/SpeciesCheck/NameCheckStep.tsx
@@ -5,7 +5,7 @@ import { Box, Checkbox, Typography, useTheme } from '@mui/material';
 import strings from 'src/strings';
 import { Species, SpeciesProblemElement } from 'src/types/Species';
 
-import ProjectCheckSummary, { ProjectCheckSummaryProps } from './ProjectCheckSummary';
+import ProjectCheckSummary, { ProjectCheckSummaryProps, suggestionsCountLabel } from './ProjectCheckSummary';
 
 const issueLabel = (problem: SpeciesProblemElement): string => {
   switch (problem.type) {
@@ -60,7 +60,10 @@ const NameCheckStep = ({
               borderTop: index > 0 ? `1px solid ${theme.palette.TwClrBrdrTertiary}` : undefined,
             }}
           >
-            <ProjectCheckSummary {...summary} />
+            <ProjectCheckSummary
+              {...summary}
+              updatesLabel={summary.updates && summary.updates > 0 ? suggestionsCountLabel(summary.updates) : undefined}
+            />
           </Box>
         ))}
       </Box>
